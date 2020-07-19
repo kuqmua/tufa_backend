7266a027c95b3f322ad07985ae9f593531aeb20c
sergey shmakov <tyfaproject@gmail.com>
2020-07-19 10:32:14 +0300

    html tags in variables from now in xml parser

--- src/main.rs
+++ src/main.rs
@@ -60,10 +60,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut vec_of_arxiv_posts: Vec<ArxivPost> = Vec::new();
     let mut write_count = 0;
+    let end_index_title_string = ". (arXiv";
+    let start_index_description_string = "<p>";
+    let end_index_description_string = "\n</p>";
+    let start_index_creator_link_string = "<a href=\"";
+    let end_index_creator_link_string = "\">";//there is no start_index_creator_name_string because its end_index_creator_link_string + its len()
+    let end_index_creator_name_string = "</a>";
+    
     while write_count < txt.len() {
         let mut arxiv_post: ArxivPost = ArxivPost::new();
         let start_title = txt[write_count].clone();
-        if let Some(index) = start_title.find(". (arXiv") {
+        if let Some(index) = start_title.find(end_index_title_string) {
             arxiv_post.title = start_title[..index].to_string();
         } else {
             arxiv_post.title = start_title;
@@ -72,20 +79,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         arxiv_post.link = txt[write_count + 1].clone();
         let description_start = txt[write_count + 2].clone();
-        if let Some(index_from_start) = description_start.find("<p>") {
-            if let Some(index_from_end) = description_start.find("\n</p>") {
+        if let Some(index_from_start) = description_start.find(start_index_description_string) {
+            if let Some(index_from_end) = description_start.find(end_index_description_string) {
                 arxiv_post.description =
-                    description_start[index_from_start + "<p>".len()..index_from_end].to_string();
+                    description_start[index_from_start + start_index_description_string.len()..index_from_end].to_string();
             } else {
                 arxiv_post.description =
-                    description_start[index_from_start + "<p>".len()..].to_string();
+                    description_start[index_from_start + start_index_description_string.len()..].to_string();
                 println!(
                     "error to find index_from_end, but index_from_start was found, write_count={}",
                     write_count
                 );
             }
         } else {
-            if let Some(index_from_end) = description_start.find(" </p>") {
+            if let Some(index_from_end) = description_start.find(end_index_description_string) {
                 arxiv_post.description = description_start[..index_from_end].to_string();
                 println!(
                     "error to find index_from_start, but index_from_end was found, write_count={}",
@@ -99,15 +106,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let creators_string = txt[write_count + 3].clone();
         let mut string_part_for_loop = creators_string;
         let mut creators_count = 0;
-        while let Some(link_index_from_start) = string_part_for_loop.find("<a href=\"") {
+        while let Some(link_index_from_start) = string_part_for_loop.find(start_index_creator_link_string) {
             if creators_count > 256 {
                 panic!("why creators_count > 256?"); //переписать когда примешь стандарт по которому ошибки обрабатывать будешь
             } else {
-                if let Some(link_index_from_end) = string_part_for_loop.find("\">") {
-                    if let Some(name_index_from_end) = string_part_for_loop.find("</a>") {
+                if let Some(link_index_from_end) = string_part_for_loop.find(end_index_creator_link_string) {
+                    if let Some(name_index_from_end) = string_part_for_loop.find(end_index_creator_name_string) {
                         let mut creator = Creator::new();
-                        creator.link = string_part_for_loop[link_index_from_start + "<a href=\"".len()..link_index_from_end].to_string();
-                        let name_index_from_start = link_index_from_end + "\">".len();//это же за два символа считается да в длине ?
+                        creator.link = string_part_for_loop[link_index_from_start + start_index_creator_link_string.len()..link_index_from_end].to_string();
+                        let name_index_from_start = link_index_from_end + end_index_creator_link_string.len();//это же за два символа считается да в длине ?
                         println!("creator.link = {}", creator.link);
                         println!("string_part_for_loop.len() = {}", string_part_for_loop.len());
                         println!("name_index_from_start = {}", name_index_from_start);
@@ -116,11 +123,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         creator.name = string_part_for_loop[name_index_from_start..name_index_from_end].to_string();
                         println!("creator.name = {}", creator.name);
                         arxiv_post.creators.push(creator);
-                        string_part_for_loop = string_part_for_loop[name_index_from_end + "</a>".len()..].to_string();
+                        string_part_for_loop = string_part_for_loop[name_index_from_end + end_index_creator_name_string.len()..].to_string();
                         creators_count += 1;
                     } else {
                         let mut creator = Creator::new();
-                        creator.link = string_part_for_loop[link_index_from_start + "<a href=\"".len()..link_index_from_end].to_string();
+                        creator.link = string_part_for_loop[link_index_from_start + start_index_creator_link_string.len()..link_index_from_end].to_string();
                         creators_count += 1;
                         println!(
                     "error to find name_index_from_end, but name_index_from_start(what equals to link_index_from_end + its len) was found, Creator pushed with default creator.name, creators_count={}",
@@ -143,6 +150,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     println!("{}", write_count);
     println!("{:#?}", vec_of_arxiv_posts[0].creators[0].name);
-
     Ok(())
 }
