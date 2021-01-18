26f79b02350fd7ebb049871dac399ec53d8cee05
tyfa <tyfaproject@gmail.com>
2021-01-19 01:33:08 +0300

    test commit 2

---
+++ src/config.rs
@@ -0,0 +1,10 @@
+// const ENABLE_REDDIT: bool = true;
+// const ENABLE_ARXIV: bool = true;
+// const ENABLE_BIORXIV: bool = true;
+// const ENABLE_MEDRXIV: bool = true;
+// const ENABLE_TWITTER: &str = "";
+
+pub const REDDIT_URL: &str = "https://www.reddit.com/";
+pub const ARXIV_URL: &str = "http://export.arxiv.org/rss/";
+pub const BIORXIV_URL: &str = "http://connect.biorxiv.org/";
+pub const MEDRXIV_URL: &str = "http://connect.medrxiv.org/";
