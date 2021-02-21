6c068a92cf5c29d8de874db82f81c4f10f6ae287
tyfa <tyfaproject@gmail.com>
2021-02-21 17:50:07 +0300

    start filtering by enums value after fetch

--- src/fetch/metainfo_fetch_structures.rs
+++ src/fetch/metainfo_fetch_structures.rs
@@ -1,14 +1,17 @@
+#[derive(Debug)] //only for prints
 pub enum HandledFetchStatusInfo {
     Initialized,
     ResToTextError(String),
     ResStatusError(reqwest::StatusCode),
     Success,
 }
+#[derive(Debug)] //only for prints
 pub enum UnhandledFetchStatusInfo {
     Failure(String),
     Initialized,
     Success,
 }
+#[derive(Debug)] //only for prints
 pub enum AreThereItems {
     Yep,
     Initialized,
--- src/fetch/rxiv_part.rs
+++ src/fetch/rxiv_part.rs
@@ -3,8 +3,12 @@ extern crate serde;
 extern crate serde_xml_rs;
 
 use crate::check_net::check_link::check_link;
+use crate::fetch::metainfo_fetch_structures::AreThereItems;
+use crate::fetch::metainfo_fetch_structures::HandledFetchStatusInfo;
+use crate::fetch::metainfo_fetch_structures::UnhandledFetchStatusInfo;
 use crate::fetch::rxiv_fetch_and_parse_xml::rxiv_fetch_and_parse_xml;
 use crate::fetch::rxiv_kind_enum::RxivKind;
+use crate::fetch::rxiv_structures::RxivPostStruct;
 use std::collections::HashMap;
 
 pub fn rxiv_part(
@@ -27,6 +31,30 @@ pub fn rxiv_part(
                 rxiv_kind_clone
             );
         };
+        let succesfully_fetched_and_parsed_posts: HashMap<String, (RxivPostStruct, RxivKind)> =
+            HashMap::new();
+        for (_, value) in fff {
+            // println!(
+            //     "unhandled {:#?} - handled {:#?} - arethereitems {:#?}",
+            //     value.2, value.3, value.4
+            // );
+            match value.2 {
+                UnhandledFetchStatusInfo::Success => match value.3 {
+                    HandledFetchStatusInfo::Success => match value.4 {
+                        AreThereItems::Yep => {}
+                        AreThereItems::Initialized => {}
+                        AreThereItems::NopeButThereIsTag(String) => {}
+                        AreThereItems::ConversionFromStrError(something1, something2) => {}
+                        AreThereItems::NopeNoTag(String) => {}
+                    },
+                    HandledFetchStatusInfo::Initialized => {}
+                    HandledFetchStatusInfo::ResToTextError(String) => {}
+                    HandledFetchStatusInfo::ResStatusError(status_code) => {}
+                },
+                UnhandledFetchStatusInfo::Initialized => {}
+                UnhandledFetchStatusInfo::Failure(string_failure) => {}
+            }
+        }
         true
     } else {
         if enable_prints {
