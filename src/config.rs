bea7904626b1fb92aee195481aa63cc6225a276e
tyfa <tyfaproject@gmail.com>
2021-02-21 09:43:26 +0300

    common const to enable parts of prints

--- src/config.rs
+++ src/config.rs
@@ -1,26 +1,28 @@
-// pub const ENABLE_REDDIT: bool = false;
-pub const ENABLE_ARXIV: bool = true;
-pub const ENABLE_BIORXIV: bool = true;
-pub const ENABLE_MEDRXIV: bool = true;
-//pub const ENABLE_TWITTER: bool = true;
-
+pub const ENABLE_ALL_PROVIDERS: bool = true;
+// pub const ENABLE_REDDIT: bool = if ENABLE_ALL_PROVIDERS { false } else { false };
+pub const ENABLE_ARXIV: bool = ENABLE_ALL_PROVIDERS & true | false; //ENABLE_ALL_PROVIDERS ? true :
+pub const ENABLE_BIORXIV: bool = ENABLE_ALL_PROVIDERS & true | false;
+pub const ENABLE_MEDRXIV: bool = ENABLE_ALL_PROVIDERS & true | false;
+//pub const ENABLE_TWITTER: bool = if ENABLE_ALL_PROVIDERS { false } else { false };
+//-------------------------------------------------------
 // pub const REDDIT_URL: &str = "https://www.reddit.com/";
 pub const ARXIV_URL: &str = "https://www.google.com/"; //https://arxiv.org/ //"http://export.arxiv.org/rss/"
 pub const BIORXIV_URL: &str = "https://www.google.com/"; //http://connect.biorxiv.org/
 pub const MEDRXIV_URL: &str = "https://www.google.com/"; //http://connect.medrxiv.org/
-                                                         //enable prints for arxiv
-
-// pub const ENABLE_PRINTS_REDDIT: bool = true;
-pub const ENABLE_PRINTS_ARXIV: bool = true;
-pub const ENABLE_PRINTS_BIORXIV: bool = true;
-pub const ENABLE_PRINTS_MEDRXIV: bool = true;
-//pub const ENABLE_PRINTS_TWITTER: bool = true;
-
-// pub const ENABLE_ERROR_PRINTS_REDDIT: bool = true;
-pub const ENABLE_ERROR_PRINTS_ARXIV: bool = true;
-pub const ENABLE_ERROR_PRINTS_BIORXIV: bool = true;
-pub const ENABLE_ERROR_PRINTS_MEDRXIV: bool = true;
-//pub const ENABLE_PRINTS_TWITTER: bool = true;
-
+                                                         //-------------------------------------------------------
+pub const ENABLE_ALL_PROVIDERS_PRINTS: bool = false;
+// pub const ENABLE_PRINTS_REDDIT: bool = ENABLE_ALL_PROVIDERS_PRINTS & true | false ;
+pub const ENABLE_PRINTS_ARXIV: bool = ENABLE_ALL_PROVIDERS_PRINTS & true | false;
+pub const ENABLE_PRINTS_BIORXIV: bool = ENABLE_ALL_PROVIDERS_PRINTS & true | false;
+pub const ENABLE_PRINTS_MEDRXIV: bool = ENABLE_ALL_PROVIDERS_PRINTS & true | false;
+//pub const ENABLE_PRINTS_TWITTER: bool = ENABLE_ALL_PROVIDERS_PRINTS & true | false;
+//-------------------------------------------------------
+pub const ENABLE_ALL_PROVIDERS_ERROR_PRINTS: bool = true;
+// pub const ENABLE_ERROR_PRINTS_REDDIT: bool = ENABLE_ALL_PROVIDERS_ERROR_PRINTS & true | false;
+pub const ENABLE_ERROR_PRINTS_ARXIV: bool = ENABLE_ALL_PROVIDERS_ERROR_PRINTS & true | false;
+pub const ENABLE_ERROR_PRINTS_BIORXIV: bool = ENABLE_ALL_PROVIDERS_ERROR_PRINTS & true | false;
+pub const ENABLE_ERROR_PRINTS_MEDRXIV: bool = ENABLE_ALL_PROVIDERS_ERROR_PRINTS & true | false;
+//pub const ENABLE_PRINTS_TWITTER: bool = ENABLE_ALL_PROVIDERS_ERROR_PRINTS & true | false;
+//-------------------------------------------------------
 pub const ENABLE_PRINTS_HANDLE: bool = true;
 pub const ENABLE_ERROR_PRINTS_HANDLE: bool = true;
