42951aa770ed2373a8749c5849b7e4564ae6c2f5
tyfa <tyfaproject@gmail.com>
2021-03-06 20:16:34 +0300

    немного поработал над записью в формате json логов

--- src/fetch/rxiv_handle_errors_arrays.rs
+++ src/fetch/rxiv_handle_errors_arrays.rs
@@ -1,7 +1,8 @@
 use crate::fetch::rxiv_kind_enum::RxivKind;
+use crate::overriding::prints::print_error_red;
 use crate::overriding::prints::print_warning_yellow;
+use chrono::Local;
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashMap;
 use std::{fs::File, io::Write};
@@ -73,24 +74,32 @@ pub fn rxiv_handle_errors_arrays(
                     .len()
                     .to_string();
         print_warning_yellow(file!().to_string(), line!().to_string(), warning_message);
-
         for (key, value) in unhandled_success_handled_success_are_there_items_nope_no_tag_posts {
-            let mut fileonos =
-                File::create("logs/warning_logs/errorlogs.json").expect("could not create file");
-            let john = json!({
-                "name": value.1,
-                "age": 43,
-                // "phones": [
-                //     "+44 1234567",
-                //     "+44 2345678"
-                // ]
+            let file_name = format!(
+                "logs/warning_logs/{:?}_{}.json",
+                value.2,
+                key.replace("/", "_")
+            ); //подобрать функцию которая из стринги делает сейвовый путь
+            let mut fileonos = File::create(file_name).expect("could not create file");
+            let json_object = json!({
+                "topic": key,
+                "url": value.0,
+                "page_content": value.1,
+                "part_of": format!("{:?}", value.2),
+                "date": Local::now().to_string()
             });
-            // writeln!(&mut fileonos, "{}", warning_message).unwrap();
-            let j = serde_json::to_string_pretty(&john).unwrap();
-            let result_of_writing = fileonos.write(j.as_bytes()); //warning_message
-            match result_of_writing {
-                Ok(_) => println!("записано"),
-                Err(e) => println!("error {}", e),
+            let result_of_stringify_json_object_pretty = serde_json::to_string_pretty(&json_object);
+            match result_of_stringify_json_object_pretty {
+                Ok(string_json_prettyfied) => {
+                    let result_of_writing = fileonos.write(string_json_prettyfied.as_bytes()); //warning_message
+                    match result_of_writing {
+                        Ok(_) => println!("записано"),
+                        Err(e) => {
+                            print_error_red(file!().to_string(), line!().to_string(), e.to_string())
+                        }
+                    }
+                }
+                Err(e) => print_error_red(file!().to_string(), line!().to_string(), e.to_string()),
             }
         }
     }
