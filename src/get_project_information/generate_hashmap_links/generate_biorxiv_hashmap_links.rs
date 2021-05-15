7069ffa89fea8c26ceafd05c6a119914febb080a
kuqmua <serezhashmakov@outlook.com>
2021-05-15 17:51:18 +0300

    for biorxiv remove keys

--- src/get_project_information/generate_hashmap_links/generate_biorxiv_hashmap_links.rs
+++ src/get_project_information/generate_hashmap_links/generate_biorxiv_hashmap_links.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
-pub fn generate_biorxiv_hashmap_links(biorxiv_names: HashMap<&'static str, &str>) -> Vec<String> {
+pub fn generate_biorxiv_hashmap_links(biorxiv_names: Vec<&'static str>) -> Vec<String> {
     //example http://connect.biorxiv.org/biorxiv_xml.php?subject=animal_behavior_and_cognition
     let first_part_of_link: &str = "http://connect.biorxiv.org/biorxiv_xml.php?subject=";
     let mut biorxiv_links: Vec<String> = Vec::with_capacity(biorxiv_names.len());
-    for (key, value) in biorxiv_names {
+    for value in biorxiv_names {
         biorxiv_links.push(format!("{}{}", first_part_of_link, value));
     }
     biorxiv_links
