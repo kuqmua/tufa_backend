d3ed41db64ebca3d2e7d46a1b81a75386372ce39
kuqmua <serezhashmakov@outlook.com>
2021-05-22 15:22:00 +0300

    first test!

--- src/main.rs
+++ src/main.rs
@@ -88,6 +88,21 @@ fn main() {
     entry();
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::get_project_information::get_config::get_config_information::CONFIG;
+    #[test]
+    fn it_works() {
+        //     pub reddit_user_agent: String,
+        // pub reddit_client_id: String,
+        // pub reddit_client_secret: String,
+        // pub reddit_username: String,
+        // pub reddit_password: String,
+        let f = &CONFIG.reddit_authorization.reddit_user_agent;
+        assert_eq!(f, "example");
+    }
+}
+
 // use std::sync::mpsc::channel;
 // use std::time::Instant;
 // use threadpool::ThreadPool;
