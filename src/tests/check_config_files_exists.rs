a4b1f802dcc83e0978fe8456971f2026e5fcabcd
kuqmua <serezhashmakov@outlook.com>
2021-05-27 20:06:49 +0300

    another constants

--- src/tests/check_config_files_exists.rs
+++ src/tests/check_config_files_exists.rs
@@ -1,11 +1,14 @@
+use crate::project_constants::CONFIG_FILE_EXTENSION;
+use crate::project_constants::PATH_TO_CONFIG;
 use crate::project_constants::VECTOR_OF_MODES;
 use std::fs::File;
 #[test]
 fn check_config_files_exists() {
-    let start: &str = "./config/"; //move this into project constants
-    let end: &str = ".toml";
     for mode in VECTOR_OF_MODES {
-        let file = File::open(format!("{}{}{}", start, mode, end));
+        let file = File::open(format!(
+            "{}{}{}",
+            PATH_TO_CONFIG, mode, CONFIG_FILE_EXTENSION
+        ));
         match file {
             Ok(_) => {}
             Err(e) => panic!("file: {} error: {}", mode, e),
