cee7a4dea0b5e47cb0ef20186ba199c15bff6153
kuqmua <serezhashmakov@outlook.com>
2021-05-30 15:08:01 +0300

    project mode later as env variable only

--- src/get_project_information/project_constants.rs
+++ src/get_project_information/project_constants.rs
@@ -1,5 +1,5 @@
 pub const LOAD_USER_CREDENTIALS_FILE_ERROR_MESSAGE: &str = "сan not load user_credentials file";
 pub const LOAD_CONFIG_FILE_ERROR_MESSAGE: &str = "сan not load config file";
 pub const PATH_TO_CONFIG: &str = "./config/";
-pub const PROJECT_MODE: &str = "Development";
+pub const PROJECT_MODE: &str = "Development";//later as ENV variable only
 pub const USER_CREDENTIALS_FILE_NAME: &str = "User_credentials";
