0d41286aa3b3a6c3eeb8004593829fa843359050
kuqmua <serezhashmakov@outlook.com>
2021-05-30 15:48:28 +0300

    remove strum crate

--- src/fetch/rss_provider_kind_enum.rs
+++ src/fetch/rss_provider_kind_enum.rs
@@ -1,18 +1,10 @@
-use strum::EnumMessage;
-#[derive(Clone, Debug, EnumMessage, serde_derive::Serialize, serde_derive::Deserialize)]
+#[derive(Clone, Debug,  serde_derive::Serialize, serde_derive::Deserialize)]
 pub enum ProviderKind {
-    #[strum(message = "arxiv")]
     Arxiv,
-    #[strum(message = "biorxiv")]
     Biorxiv,
-    #[strum(message = "github")]
     Github,
-    #[strum(message = "habr")]
     Habr,
-    #[strum(message = "medrxiv")]
     Medrxiv,
-    #[strum(message = "reddit")]
     Reddit,
-    #[strum(message = "twitter")]
     Twitter,
 }
