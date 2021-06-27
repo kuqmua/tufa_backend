8b3442c196c5d03ab80981be113ce2f6ce49209c
kuqmua <serezhashmakov@outlook.com>
2021-06-27 11:31:56 +0300

    success connect to cloud mongodb

--- libs/mongo_demo/src/lib.rs
+++ libs/mongo_demo/src/lib.rs
@@ -3,8 +3,9 @@ use mongodb::{bson::doc, options::ClientOptions, Client};
 pub async fn mongo_main() -> mongodb::error::Result<()> {
     //Parse your connection string into an options struct
     //mongodb+srv:<username>:<password>@<cluster-url>/test?w=majority
+    //mongodb://root:rootpassword@localhost:27017
     let mut client_options =
-        ClientOptions::parse("mongodb://root:rootpassword@localhost:27017").await?;
+        ClientOptions::parse("mongodb+srv://mongodbcloudlogin:mongodbcloudpassword@tufa-mongo.y2xob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").await?;
     //Manually set an option
     client_options.app_name = Some("Rust Demo".to_string());
     //Get a handle to the cluster
