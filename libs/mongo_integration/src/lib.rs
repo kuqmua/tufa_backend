74675ca87c217ac84664e8288b86aa737822b3d1
kuqmua <serezhashmakov@outlook.com>
2021-06-28 22:17:55 +0300

    starting with mongo create collections

--- libs/mongo_integration/src/lib.rs
+++ libs/mongo_integration/src/lib.rs
@@ -1,20 +1,44 @@
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use mongodb::{
+    bson::{doc, Document},
+    options::ClientOptions,
+    options::CreateCollectionOptions,
+    Client,
+};
 #[tokio::main]
-pub async fn mongo_main() -> mongodb::error::Result<()> {
+pub async fn mongo_integration() -> mongodb::error::Result<()> {
     //Parse your connection string into an options struct
-    //mongodb+srv:<username>:<password>@<cluster-url>/test?w=majority
     //mongodb://root:rootpassword@localhost:27017
+    //mongodb+srv://mongodbcloudlogin:mongodbcloudpassword@tufa-mongo.y2xob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
     let mut client_options =
-        ClientOptions::parse("mongodb+srv://mongodbcloudlogin:mongodbcloudpassword@tufa-mongo.y2xob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").await?;
+        ClientOptions::parse("mongodb://root:rootpassword@localhost:27017").await?;
     //Manually set an option
     client_options.app_name = Some("Rust Demo".to_string());
     //Get a handle to the cluster
     let client = Client::with_options(client_options)?;
     //Ping the server to see if you can connect to the cluster
-    client
-        .database("admin")
-        .run_command(doc! {"ping": 1}, None)
-        .await?;
+    let db = client.database("admin");
+    println!("1");
+    let dd = db.create_collection("test collection", None).await;
+    match dd {
+        Ok(()) => {
+            println!("@@@@");
+        }
+        Err(e) => {
+            println!("####, {:#?}", e);
+        }
+    }
+    let collection = db.collection("test collection");
+    println!("2");
+    let docs = vec![
+        doc! { "title": "1984", "author": "George Orwell" },
+        doc! { "title": "Animal Farm", "author": "George Orwell" },
+        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
+    ];
+    println!("3");
+    // Insert some documents into the "mydb.books" collection.
+    collection.insert_many(docs, None).await?;
+    println!("4");
+    db.run_command(doc! {"ping": 1}, None).await?;
     println!("Connected successfully.");
     //List the names of the databases in that cluster
     for db_name in client.list_database_names(None, None).await? {
--- libs/providers_info_lib/src/get_project_information/generate_hashmap_links/generate_arxiv_hashmap_links.rs
+++ libs/providers_info_lib/src/get_project_information/generate_hashmap_links/generate_arxiv_hashmap_links.rs
@@ -1,4 +1,21 @@
+// extern crate mongo_demo;
+
+// use futures::executor::block_on;
 pub fn generate_arxiv_hashmap_links(arxiv_names: Vec<&'static str>) -> Vec<String> {
+    // #[tokio::main]
+    // async fn main() {
+
+    let result_of_mongo_integration = mongo_integration::mongo_integration();
+    match result_of_mongo_integration {
+        Ok(()) => {
+            println!("nice!");
+        }
+        Err(e) => {
+            println!("F");
+        }
+    }
+
+    // }
     //example http://export.arxiv.org/rss/astro-ph.CO
     let first_part_of_link: &str = "http://export.arxiv.org/rss/";
     let mut arxiv_links: Vec<String> = Vec::with_capacity(arxiv_names.len());
