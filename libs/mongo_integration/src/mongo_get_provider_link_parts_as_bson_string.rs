b8faa2f04feaa462e189ca68c1a632286825049f
kuqmua <serezhashmakov@outlook.com>
2021-07-29 20:41:33 +0300

    less code for mongo_get_provider_link_parts_as_bson_string

--- libs/mongo_integration/src/mongo_get_provider_link_parts_as_bson_string.rs
+++ libs/mongo_integration/src/mongo_get_provider_link_parts_as_bson_string.rs
@@ -1,8 +1,8 @@
 use futures::stream::TryStreamExt;
 use mongodb::{
     bson::{self, doc, Document},
-    options::{ClientOptions, FindOptions},
-    Client,
+    options::ClientOptions,
+    Client, Collection,
 };
 
 use config_lib::get_project_information::provider_kind_enum::ProviderKind;
@@ -134,102 +134,20 @@ pub async fn mongo_get_provider_link_parts_as_bson_string(
                                 } else {
                                     option_aggregation_stage_1_get_docs_in_random_order_with_limit = None;
                                 }
-                                match option_aggregation_stage_1_get_docs_in_random_order_with_limit
-                                {
-                                    Some(
-                                        aggregation_stage_1_get_docs_in_random_order_with_limit,
-                                    ) => {
-                                        // let aggregation_stage_1_get_docs_in_random_order_with_limit =
-                                        //     doc! { "$sample" : {"size": 5 }};
-                                        // let aggregation_stage_2_get_docs_with_limit = doc! { "$limit": 5 };
-                                        let pipeline = vec![
-                                            aggregation_stage_1_get_docs_in_random_order_with_limit,
-                                        ];
-                                        let cursor_result =
-                                            collection.aggregate(pipeline, None).await;
-                                        match cursor_result {
-                                            Ok(mut cursor) => {
-                                                let mut vec_of_strings: Vec<String> = Vec::new();
-                                                while let Some(document) = cursor.try_next().await?
-                                                {
-                                                    let bson_option = document.get(
-                                                        db_collection_document_field_name_handle,
-                                                    );
-                                                    match bson_option {
-                                                        Some(bson_handle) => match bson_handle {
-                                                            bson::Bson::String(
-                                                                stringified_bson,
-                                                            ) => vec_of_strings
-                                                                .push(stringified_bson.to_string()),
-                                                            _ => {
-                                                                println!("(todo change this print) different mongo type")
-                                                            }
-                                                        },
-                                                        None => {
-                                                            println!(
-                                                        "no db_collection_document_field_name_handle: {}",
-                                                        db_collection_document_field_name_handle
-                                                    );
-                                                        }
-                                                    }
-                                                }
-                                                if !vec_of_strings.is_empty() {
-                                                    vec_of_strings_to_return = vec_of_strings
-                                                } else {
-                                                    vec_of_strings_to_return = Vec::new()
-                                                }
-                                            }
-                                            Err(e) => {
-                                                vec_of_strings_to_return = Vec::new();
-                                                println!(
-                                            "(todo change this print)  collection.find, {:#?}",
-                                            e
-                                        )
-                                            }
-                                        }
-                                    }
-                                    None => {
-                                        let cursor_result = collection.aggregate(None, None).await;
-                                        match cursor_result {
-                                            Ok(mut cursor) => {
-                                                let mut vec_of_strings: Vec<String> = Vec::new();
-                                                while let Some(document) = cursor.try_next().await?
-                                                {
-                                                    let bson_option = document.get(
-                                                        db_collection_document_field_name_handle,
-                                                    );
-                                                    match bson_option {
-                                                        Some(bson_handle) => match bson_handle {
-                                                            bson::Bson::String(
-                                                                stringified_bson,
-                                                            ) => vec_of_strings
-                                                                .push(stringified_bson.to_string()),
-                                                            _ => {
-                                                                println!("(todo change this print) different mongo type")
-                                                            }
-                                                        },
-                                                        None => {
-                                                            println!(
-                                                        "no db_collection_document_field_name_handle: {}",
-                                                        db_collection_document_field_name_handle
-                                                    );
-                                                        }
-                                                    }
-                                                }
-                                                if !vec_of_strings.is_empty() {
-                                                    vec_of_strings_to_return = vec_of_strings
-                                                } else {
-                                                    vec_of_strings_to_return = Vec::new()
-                                                }
-                                            }
-                                            Err(e) => {
-                                                vec_of_strings_to_return = Vec::new();
-                                                println!(
-                                            "(todo change this print)  collection.find, {:#?}",
-                                            e
-                                        )
-                                            }
-                                        }
+                                // let aggregation_stage_1_get_docs_in_random_order_with_limit =
+                                //     doc! { "$sample" : {"size": 5 }};
+                                // let aggregation_stage_2_get_docs_with_limit = doc! { "$limit": 5 };
+                                let result = do_something(
+                                    collection,
+                                    db_collection_document_field_name_handle,
+                                    option_aggregation_stage_1_get_docs_in_random_order_with_limit,
+                                )
+                                .await;
+                                match result {
+                                    Ok(vec_of_strings) => vec_of_strings_to_return = vec_of_strings,
+                                    Err(e) => {
+                                        vec_of_strings_to_return = Vec::new();
+                                        println!("let result = do_something( error {:#?}", e)
                                     }
                                 }
                             } else {
@@ -262,4 +180,50 @@ pub async fn mongo_get_provider_link_parts_as_bson_string(
         vec_of_strings_to_return.len()
     );
     Ok(vec_of_strings_to_return)
+}
+
+async fn do_something(
+    collection: Collection<Document>,
+    db_collection_document_field_name_handle: &str,
+    option_aggregation_stage_1_get_docs_in_random_order_with_limit: Option<Document>,
+) -> Result<Vec<String>, mongodb::error::Error> {
+    let cursor_result = collection
+        .aggregate(
+            option_aggregation_stage_1_get_docs_in_random_order_with_limit,
+            None,
+        )
+        .await;
+    match cursor_result {
+        Ok(mut cursor) => {
+            let mut vec_of_strings: Vec<String> = Vec::new();
+            while let Some(document) = cursor.try_next().await? {
+                let bson_option = document.get(db_collection_document_field_name_handle);
+                match bson_option {
+                    Some(bson_handle) => match bson_handle {
+                        bson::Bson::String(stringified_bson) => {
+                            vec_of_strings.push(stringified_bson.to_string())
+                        }
+                        _ => {
+                            println!("(todo change this print) different mongo type")
+                        }
+                    },
+                    None => {
+                        println!(
+                            "no db_collection_document_field_name_handle: {}",
+                            db_collection_document_field_name_handle
+                        );
+                    }
+                }
+            }
+            if !vec_of_strings.is_empty() {
+                return Ok(vec_of_strings);
+            } else {
+                return Ok(Vec::new());
+            }
+        }
+        Err(e) => {
+            println!("(todo change this print)  collection.find, {:#?}", e);
+            return Ok(Vec::new());
+        }
+    }
 }
