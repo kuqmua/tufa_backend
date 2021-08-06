1ed2af2472237790d4155518a0ec49fcd1bf8481
kuqmua <serezhashmakov@outlook.com>
2021-08-06 20:44:16 +0300

    check user credentails struct fileds if empty

--- libs/config_lib/src/get_project_information/get_user_credentials/user_credentials_structures.rs
+++ libs/config_lib/src/get_project_information/get_user_credentials/user_credentials_structures.rs
@@ -10,13 +10,184 @@ pub struct UserCredentialsStruct {
 
 impl UserCredentialsStruct {
     pub fn new(path_to_config: &str) -> Result<Self, ConfigError> {
+        //todo: check path_to_config is empty
         // maybe add different user logic later ?
         let mut config = Config::new();
-        config.merge(File::with_name(&format!(
+        let config_merge_result = config.merge(File::with_name(&format!(
             "{}{}",
             path_to_config, USER_CREDENTIALS_FILE_NAME
-        )))?;
-        config.try_into()
+        )));
+        match config_merge_result {
+            Ok(_) => {
+                // config.try_into();
+                let config_result: Result<Self, ConfigError> = config.try_into();
+                match config_result {
+                    Ok(user_credentials_handle) => {
+                        UserCredentialsStruct::wrap_custom_config_checks(user_credentials_handle)
+                    }
+                    Err(e) => Err(e),
+                }
+            }
+            Err(e) => Err(e),
+        }
+    }
+    #[allow(clippy::unnecessary_wraps)]
+    fn wrap_custom_config_checks(
+        user_credentials_handle: UserCredentialsStruct,
+    ) -> Result<Self, ConfigError> {
+        if !user_credentials_handle
+            .github_authorization
+            .github_name
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> =
+                Err(ConfigError::Message("github_name is not valid".to_string()));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .github_authorization
+            .github_token
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "github_token is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        //
+        if !user_credentials_handle
+            .reddit_authorization
+            .reddit_user_agent
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "reddit_user_agent is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .reddit_authorization
+            .reddit_client_id
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "reddit_client_id is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .reddit_authorization
+            .reddit_client_secret
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "reddit_client_secret is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .reddit_authorization
+            .reddit_username
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "reddit_username is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .reddit_authorization
+            .reddit_password
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "reddit_password is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        /////////
+        if !user_credentials_handle
+            .mongo_own_authorization
+            .mongo_own_login
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_own_login is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_own_authorization
+            .mongo_own_password
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_own_password is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_own_authorization
+            .mongo_own_ip
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_own_ip is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_own_authorization
+            .mongo_own_port
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_own_port is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        /////////
+        if !user_credentials_handle
+            .mongo_cloud_authorization
+            .mongo_cloud_login
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_cloud_login is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_cloud_authorization
+            .mongo_cloud_password
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_cloud_password is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_cloud_authorization
+            .mongo_cloud_cluster_name
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_cloud_cluster_name is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        if !user_credentials_handle
+            .mongo_cloud_authorization
+            .mongo_cloud_cluster_params
+            .is_empty()
+        {
+            let error: Result<UserCredentialsStruct, ConfigError> = Err(ConfigError::Message(
+                "mongo_cloud_cluster_params is not valid".to_string(),
+            ));
+            drop(error);
+        }
+        Ok(user_credentials_handle)
     }
 }
 
