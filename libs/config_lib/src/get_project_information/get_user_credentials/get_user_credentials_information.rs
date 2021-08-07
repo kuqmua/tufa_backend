2230a4f571a6f17d81993913a2d600cb900e3335
kuqmua <serezhashmakov@outlook.com>
2021-08-07 17:35:26 +0300

    remove old user credentials struct

--- libs/config_lib/src/get_project_information/get_user_credentials/get_user_credentials_information.rs
+++ libs/config_lib/src/get_project_information/get_user_credentials/get_user_credentials_information.rs
@@ -1,4 +1,4 @@
-use crate::get_project_information::get_user_credentials::user_credentials_structures::UserCredentialsStruct;
+use crate::get_project_information::get_user_credentials::structures_definitions::user_credentials_struct_def::UserCredentialsStruct;
 use crate::get_project_information::project_constants::LOAD_USER_CREDENTIALS_FILE_ERROR_MESSAGE;
 use crate::get_project_information::project_constants::PATH_TO_CONFIG;
 
--- libs/config_lib/src/lib.rs
+++ libs/config_lib/src/lib.rs
@@ -38,7 +38,6 @@ pub mod get_project_information {
             pub mod user_credentials_struct_impl;
         }
         pub mod get_user_credentials_information;
-        pub mod user_credentials_structures;
     }
     pub mod project_constants;
     pub mod provider_kind_enum;
