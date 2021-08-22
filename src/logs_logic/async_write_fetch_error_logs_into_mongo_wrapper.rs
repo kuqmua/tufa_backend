8b1ba35f04938a7d09c31ed91b68dc4d1d4ca057
kuqmua <serezhashmakov@outlook.com>
2021-08-22 15:50:46 +0300

    drop_mongo_logs_collection_wrapper_for_providers into different file

--- src/logs_logic/async_write_fetch_error_logs_into_mongo_wrapper.rs
+++ src/logs_logic/async_write_fetch_error_logs_into_mongo_wrapper.rs
@@ -4,7 +4,7 @@ use crate::fetch::provider_log_into_json::provider_log_into_json;
 use crate::fetch::rss_metainfo_fetch_structures::AreThereItems;
 use crate::fetch::rss_metainfo_fetch_structures::HandledFetchStatusInfo;
 use crate::fetch::rss_metainfo_fetch_structures::UnhandledFetchStatusInfo;
-use crate::logs_logic::drop_mongo_provider_logs_collection_if_need::drop_mongo_provider_logs_collection_if_need;
+use crate::logs_logic::drop_mongo_logs_collection_wrapper_for_providers::drop_mongo_logs_collection_wrapper_for_providers;
 
 use config_lib::get_project_information::get_config::get_lazy_config_information::CONFIG;
 use config_lib::get_project_information::project_constants::get_mongo_url;
@@ -304,100 +304,4 @@ pub async fn mongo_insert_docs_in_empty_collection_wrapper_under_old_tokio_versi
             return (provider_kind, false);
         }
     }
-}
-
-pub async fn drop_mongo_logs_collection_wrapper_for_providers(
-    provider_kind_handle: ProviderKind,
-    mongo_url: String,
-    db_collection_handle_second_part_clone: &str,
-    db_name_handle: &str,
-) -> (ProviderKind, bool) {
-    let result_of_dropping_collection: (ProviderKind, bool);
-    match provider_kind_handle {
-        ProviderKind::Arxiv => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_arxiv,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Biorxiv => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_biorxiv,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Github => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_github,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Habr => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_habr,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Medrxiv => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_medrxiv,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Reddit => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_reddit,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-        ProviderKind::Twitter => {
-            result_of_dropping_collection = drop_mongo_provider_logs_collection_if_need(
-                CONFIG
-                    .enable_providers_cleaning_warning_logs_db_collections_in_mongo
-                    .enable_cleaning_warning_logs_db_collections_in_mongo_for_twitter,
-                provider_kind_handle,
-                db_collection_handle_second_part_clone.to_string(),
-                mongo_url,
-                db_name_handle.to_string(),
-            )
-            .await;
-        }
-    };
-    result_of_dropping_collection
 }
