b0afa95e4b2c2d73810f00390bb8a1632ee32e47
kuqmua <serezhashmakov@outlook.com>
2021-09-05 13:28:34 +0300

    add new ConfigStruct function (as comment now) but config dont work. why?

--- libs/config_lib/src/get_project_information/get_config/structures_implementations/config_struct_impl/new_impl.rs
+++ libs/config_lib/src/get_project_information/get_config/structures_implementations/config_struct_impl/new_impl.rs
@@ -1,3 +1,74 @@
+// use crate::get_project_information::get_config::structures_definitions::config_struct_def::ConfigStruct;
+// use crate::get_project_information::project_constants::PROJECT_MODE;
+
+// use config::{Config, ConfigError, File};
+
+// impl ConfigStruct {
+//     pub fn new(
+//         &self,
+//         mode_handler: Option<&str>,
+//         path_to_config: &str,
+//     ) -> Result<Self, ConfigError> {
+//         println!("test");
+//         let mode_string: String;
+//         if let Some(mode) = mode_handler {
+//             mode_string = mode.to_string();
+//         } else {
+//             mode_string = std::env::var("RUN_ENV").unwrap_or_else(|_| PROJECT_MODE.into());
+//         }
+//         let mut config = Config::new();
+//         match config.set("env", mode_string.clone()) {
+//             Ok(config_set_env_ok) => {
+//                 match config_set_env_ok.merge(File::with_name(&format!(
+//                     "{}{}",
+//                     path_to_config, mode_string
+//                 ))) {
+//                     Ok(_) => {
+//                         match config.try_into() {
+//                             Ok(config_handle) => {
+//                                 ConfigStruct::wrap_config_checks(&self, config_handle)
+//                             }
+//                             Err(e) => {
+//                                 //cannot use print_colorful_message coz circular dependency
+//                                 println!(
+//                                     "{}{}\nconfig.try_into error: {:#?}",
+//                                     file!().to_string(),
+//                                     line!().to_string(),
+//                                     e
+//                                 );
+//                                 Err(e)
+//                             }
+//                         }
+//                     }
+//                     Err(e) => {
+//                         //cannot use print_colorful_message coz circular dependency
+//                         println!(
+//                             "{}{}\nconfig.merge(File::with_name({}{})) error: {:#?}",
+//                             file!().to_string(),
+//                             line!().to_string(),
+//                             path_to_config,
+//                             mode_string,
+//                             e
+//                         );
+//                         Err(e)
+//                     }
+//                 }
+//             }
+//             Err(e) => {
+//                 //cannot use print_colorful_message coz circular dependency
+//                 println!(
+//                     "{}{}\nconfig.set(\"env\", {}) error: {:#?}",
+//                     file!().to_string(),
+//                     line!().to_string(),
+//                     mode_string,
+//                     e
+//                 );
+//                 Err(e)
+//             }
+//         }
+//     }
+// }
+
 use crate::get_project_information::get_config::structures_definitions::config_struct_def::ConfigStruct;
 use crate::get_project_information::project_constants::PROJECT_MODE;
 
