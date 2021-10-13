e9f8cbb37c99a1d392df94554044577bb94ab3de
kuqmua <serezhashmakov@outlook.com>
2021-10-13 21:24:38 +0300

    remove File::open from get_providers_json_local_data

--- libs/providers_info_lib/src/get_project_information/get_providers_json_local_data.rs
+++ libs/providers_info_lib/src/get_project_information/get_providers_json_local_data.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn get_providers_json_local_data(
@@ -12,43 +11,31 @@ pub fn get_providers_json_local_data(
     let mut vec_of_link_parts_hashmap: HashMap<String, Vec<String>> = HashMap::new();
     //todo: do it async in parallel
     for provider_name in vec_of_provider_names {
-        let result_of_opening_file = File::open(format!(
+        let result_of_reading_to_string = fs::read_to_string(&format!(
             "{}{}{}{}",
             path, provider_name, second_part_of_file_name, file_extension
         ));
-        match result_of_opening_file {
-            Ok(mut file) => {
-                let mut contents = String::new();
-                let result_of_reading_to_string = file.read_to_string(&mut contents);
-                match result_of_reading_to_string {
-                    Ok(_) => {
-                        let file_content_from_str_result: Result<
-                            ProviderLinkPartStruct,
-                            serde_json::Error,
-                        > = serde_json::from_str(&contents);
-                        match file_content_from_str_result {
-                            Ok(file_content_as_struct) => {
-                                let mut vec_of_link_parts: Vec<String> =
-                                    Vec::with_capacity(file_content_as_struct.data.len());
-                                for link_part in file_content_as_struct.data {
-                                    vec_of_link_parts.push(link_part)
-                                }
-                                vec_of_link_parts_hashmap.insert(provider_name, vec_of_link_parts);
-                            }
-                            Err(e) => println!("file_content_from_str_result error {:#?}", e),
+        match result_of_reading_to_string {
+            Ok(file_content) => {
+                let file_content_from_str_result: Result<
+                    ProviderLinkPartStruct,
+                    serde_json::Error,
+                > = serde_json::from_str(&file_content);
+                match file_content_from_str_result {
+                    Ok(file_content_as_struct) => {
+                        let mut vec_of_link_parts: Vec<String> =
+                            Vec::with_capacity(file_content_as_struct.data.len());
+                        for link_part in file_content_as_struct.data {
+                            vec_of_link_parts.push(link_part)
                         }
+                        vec_of_link_parts_hashmap.insert(provider_name, vec_of_link_parts);
                     }
-                    Err(e) => {
-                        println!(
-                            "cannot read_to_string from file {}{}{}{}, reason: {}",
-                            path, provider_name, second_part_of_file_name, file_extension, e
-                        )
-                    }
+                    Err(e) => println!("file_content_from_str_result error {:#?}", e),
                 }
             }
             Err(e) => {
                 println!(
-                    "cannot open {}{}{}{}, reason: {}",
+                    "cannot read_to_string from file {}{}{}{}, reason: {}",
                     path, provider_name, second_part_of_file_name, file_extension, e
                 )
             }
