5176e4a059e9e51161dbd00e1524753616c6f059
kuqmua <serezhashmakov@outlook.com>
2021-10-13 21:34:41 +0300

    remove file::open from ci_check_new_env_vars

--- libs/tests_lib/src/tests/continuous_integration/ci_check_new_env_vars.rs
+++ libs/tests_lib/src/tests/continuous_integration/ci_check_new_env_vars.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 
 use config_lib::get_project_information::project_constants::ENV_FILE_NAME;
 
@@ -9,29 +8,17 @@ use crate::tests::tests_constants::PATH_TO_ENV_FILE;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 pub fn ci_check_new_env_vars() {
-    let result_of_opening_file = File::open(format!(
+    let result_of_reading_to_string = fs::read_to_string(&format!(
         "{}{}",
         PATH_TO_ENV_FILE, ENV_FILE_NAME
     ));
-    match result_of_opening_file {
-        Ok(mut file) => {
-            let mut contents = String::new();
-            let result_of_reading_to_string = file.read_to_string(&mut contents);
-            match result_of_reading_to_string {
-                Ok(_) => {
-                    assert_eq!(contents, ENV_FILE_CONTENT);
-                }
-                Err(e) => {
-                    panic!(
-                        "cannot read_to_string from file {}{}, reason: {}",
-                        PATH_TO_ENV_FILE, ENV_FILE_NAME, e
-                    )
-                }
-            }
+    match result_of_reading_to_string {
+        Ok(file_content) => {
+            assert_eq!(file_content, ENV_FILE_CONTENT);
         }
         Err(e) => {
             panic!(
-                "cannot open {}{}, reason: {}",
+                "cannot read_to_string from file {}{}, reason: {}",
                 PATH_TO_ENV_FILE, ENV_FILE_NAME, e
             )
         }
