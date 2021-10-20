4c08478768c6697a6451f060b99e8ca95079ad9d
kuqmua <serezhashmakov@outlook.com>
2021-10-20 21:42:51 +0300

    EnvStringVar env_var_name_kind from EnvVar::get_env_name

--- libs/config_lib/src/get_project_information/env_var_string_enum.rs
+++ libs/config_lib/src/get_project_information/env_var_string_enum.rs
@@ -8,71 +8,14 @@ use strum_macros::EnumIter;
 
 use dotenv::dotenv;
 
-use crate::get_project_information::env_var_string_names_constants::STARTING_CHECK_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::UNHANDLED_SUCCESS_HANDLED_SUCCESS_ARE_THERE_ITEMS_INITIALIZED_POSTS_DIR_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::USER_CREDENTIALS_DUMMY_HANDLE_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::WARNING_LOGS_DIRECTORY_NAME_ENV_NAME;
-
-// [mongo_params]
-use crate::get_project_information::env_var_string_names_constants::DB_PROVIDERS_LOGS_COLLECTION_DOCUMENT_FIELD_NAME_HANDLE_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::DB_PROVIDERS_LOGS_COLLECTION_HANDLE_SECOND_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::DB_PROVIDERS_LOGS_NAME_HANDLE_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::LOG_FILE_EXTENSION_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::PATH_TO_PROVIDER_LINK_PARTS_FOLDER_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::PROVIDERS_DB_COLLECTION_DOCUMENT_FIELD_NAME_HANDLE_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::PROVIDERS_DB_COLLECTION_HANDLE_SECOND_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::PROVIDERS_DB_NAME_HANDLE_ENV_NAME;
-
-// [mongo_params.mongo_url_parts]
-use crate::get_project_information::env_var_string_names_constants::MONGO_FIRST_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_FOURTH_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_SECOND_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_THIRD_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_FIFTH_HANDLE_URL_PART_ENV_NAME;
-
-// [postgres_params]
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_FIRST_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_FOURTH_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_SECOND_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_THIRD_HANDLE_URL_PART_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_FIFTH_HANDLE_URL_PART_ENV_NAME;
-
-// [enable_providers]
-use crate::get_project_information::env_var_string_names_constants::ARXIV_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::BIORXIV_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::GITHUB_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::HABR_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MEDRXIV_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::REDDIT_LINK_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::TWITTER_LINK_ENV_NAME;
-
-use crate::get_project_information::env_var_string_names_constants::GITHUB_NAME_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::GITHUB_TOKEN_ENV_NAME;
-
-use crate::get_project_information::env_var_string_names_constants::MONGO_IP_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_LOGIN_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_PASSWORD_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_PORT_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::MONGO_PARAMS_ENV_NAME;
-
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_LOGIN_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_PASSWORD_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_IP_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_PORT_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::POSTGRES_DB_ENV_NAME;
-
-use crate::get_project_information::env_var_string_names_constants::REDDIT_CLIENT_ID_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::REDDIT_CLIENT_SECRET_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::REDDIT_PASSWORD_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::REDDIT_USERNAME_ENV_NAME;
-use crate::get_project_information::env_var_string_names_constants::REDDIT_USER_AGENT_ENV_NAME;
-
 use crate::get_project_information::config_error_inner_type_enum::ConfigErrorInnerType;
 use crate::get_project_information::config_error::ConfigError;
 
 use crate::get_project_information::env_var_types_enum::EnvVarTypes;
 
 use crate::get_project_information::project_constants::ENV_FILE_NAME;
+
+use crate::get_project_information::env_var_enum::EnvVar;
 
 #[derive(
     EnumVariantCount,
@@ -138,60 +81,60 @@ pub enum EnvStringVar {
 impl EnvStringVar {
     pub fn get_env_name(env_var_name_kind: EnvStringVar) -> &'static str {
         match env_var_name_kind {
-            EnvStringVar::GithubName => GITHUB_NAME_ENV_NAME,
-            EnvStringVar::GithubToken => GITHUB_TOKEN_ENV_NAME,
+            EnvStringVar::GithubName => EnvVar::get_env_name(EnvVar::GithubName),
+            EnvStringVar::GithubToken => EnvVar::get_env_name(EnvVar::GithubToken),
 
-            EnvStringVar::RedditUserAgent => REDDIT_USER_AGENT_ENV_NAME,
-            EnvStringVar::RedditClientId => REDDIT_CLIENT_ID_ENV_NAME,
-            EnvStringVar::RedditClientSecret => REDDIT_CLIENT_SECRET_ENV_NAME,
-            EnvStringVar::RedditUsername => REDDIT_USERNAME_ENV_NAME,
-            EnvStringVar::RedditPassword => REDDIT_PASSWORD_ENV_NAME,
+            EnvStringVar::RedditUserAgent => EnvVar::get_env_name(EnvVar::RedditUserAgent),
+            EnvStringVar::RedditClientId => EnvVar::get_env_name(EnvVar::RedditClientId),
+            EnvStringVar::RedditClientSecret => EnvVar::get_env_name(EnvVar::RedditClientSecret),
+            EnvStringVar::RedditUsername => EnvVar::get_env_name(EnvVar::RedditUsername),
+            EnvStringVar::RedditPassword => EnvVar::get_env_name(EnvVar::RedditPassword),
 
-            EnvStringVar::StartingCheckLink => STARTING_CHECK_LINK_ENV_NAME,
-            EnvStringVar::UserCredentialsDummyHandle => USER_CREDENTIALS_DUMMY_HANDLE_ENV_NAME,
-            EnvStringVar::WarningLogsDirectoryName => WARNING_LOGS_DIRECTORY_NAME_ENV_NAME,
-            EnvStringVar::UnhandledSuccessHandledSuccessAreThereItemsInitializedPostsDir => UNHANDLED_SUCCESS_HANDLED_SUCCESS_ARE_THERE_ITEMS_INITIALIZED_POSTS_DIR_ENV_NAME,
+            EnvStringVar::StartingCheckLink => EnvVar::get_env_name(EnvVar::StartingCheckLink),
+            EnvStringVar::UserCredentialsDummyHandle => EnvVar::get_env_name(EnvVar::UserCredentialsDummyHandle),
+            EnvStringVar::WarningLogsDirectoryName => EnvVar::get_env_name(EnvVar::WarningLogsDirectoryName),
+            EnvStringVar::UnhandledSuccessHandledSuccessAreThereItemsInitializedPostsDir => EnvVar::get_env_name(EnvVar::UnhandledSuccessHandledSuccessAreThereItemsInitializedPostsDir),
            
-            EnvStringVar::ProvidersDbNameHandle => PROVIDERS_DB_NAME_HANDLE_ENV_NAME,
-            EnvStringVar::ProvidersDbCollectionHandleSecondPart => PROVIDERS_DB_COLLECTION_HANDLE_SECOND_PART_ENV_NAME,
-            EnvStringVar::ProvidersDbCollectionDocumentFieldNameHandle => PROVIDERS_DB_COLLECTION_DOCUMENT_FIELD_NAME_HANDLE_ENV_NAME,
-            EnvStringVar::PathToProviderLinkPartsFolder => PATH_TO_PROVIDER_LINK_PARTS_FOLDER_ENV_NAME,
-            EnvStringVar::LogFileExtension => LOG_FILE_EXTENSION_ENV_NAME,
-            EnvStringVar::DbProvidersLogsNameHandle => DB_PROVIDERS_LOGS_NAME_HANDLE_ENV_NAME,
-            EnvStringVar::DbProvidersLogsCollectionHandleSecondPart => DB_PROVIDERS_LOGS_COLLECTION_HANDLE_SECOND_PART_ENV_NAME,
-            EnvStringVar::DbProvidersLogsCollectionDocumentFieldNameHandle => DB_PROVIDERS_LOGS_COLLECTION_DOCUMENT_FIELD_NAME_HANDLE_ENV_NAME,
+            EnvStringVar::ProvidersDbNameHandle => EnvVar::get_env_name(EnvVar::ProvidersDbNameHandle),
+            EnvStringVar::ProvidersDbCollectionHandleSecondPart => EnvVar::get_env_name(EnvVar::ProvidersDbCollectionHandleSecondPart),
+            EnvStringVar::ProvidersDbCollectionDocumentFieldNameHandle => EnvVar::get_env_name(EnvVar::ProvidersDbCollectionDocumentFieldNameHandle),
+            EnvStringVar::PathToProviderLinkPartsFolder => EnvVar::get_env_name(EnvVar::PathToProviderLinkPartsFolder),
+            EnvStringVar::LogFileExtension => EnvVar::get_env_name(EnvVar::LogFileExtension),
+            EnvStringVar::DbProvidersLogsNameHandle => EnvVar::get_env_name(EnvVar::DbProvidersLogsNameHandle),
+            EnvStringVar::DbProvidersLogsCollectionHandleSecondPart => EnvVar::get_env_name(EnvVar::DbProvidersLogsCollectionHandleSecondPart),
+            EnvStringVar::DbProvidersLogsCollectionDocumentFieldNameHandle => EnvVar::get_env_name(EnvVar::DbProvidersLogsCollectionDocumentFieldNameHandle),
 
-            EnvStringVar::MongoFirstHandleUrlPart => MONGO_FIRST_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::MongoSecondHandleUrlPart => MONGO_SECOND_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::MongoThirdHandleUrlPart => MONGO_THIRD_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::MongoFourthHandleUrlPart => MONGO_FOURTH_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::MongoFifthHandleUrlPart => MONGO_FIFTH_HANDLE_URL_PART_ENV_NAME,
+            EnvStringVar::MongoFirstHandleUrlPart => EnvVar::get_env_name(EnvVar::MongoFirstHandleUrlPart),
+            EnvStringVar::MongoSecondHandleUrlPart => EnvVar::get_env_name(EnvVar::MongoSecondHandleUrlPart),
+            EnvStringVar::MongoThirdHandleUrlPart => EnvVar::get_env_name(EnvVar::MongoThirdHandleUrlPart),
+            EnvStringVar::MongoFourthHandleUrlPart => EnvVar::get_env_name(EnvVar::MongoFourthHandleUrlPart),
+            EnvStringVar::MongoFifthHandleUrlPart => EnvVar::get_env_name(EnvVar::MongoFifthHandleUrlPart),
 
-            EnvStringVar::MongoLogin => MONGO_LOGIN_ENV_NAME,
-            EnvStringVar::MongoPassword => MONGO_PASSWORD_ENV_NAME,
-            EnvStringVar::MongoIp => MONGO_IP_ENV_NAME,
-            EnvStringVar::MongoPort => MONGO_PORT_ENV_NAME,
-            EnvStringVar::MongoParams => MONGO_PARAMS_ENV_NAME,
+            EnvStringVar::MongoLogin => EnvVar::get_env_name(EnvVar::MongoLogin),
+            EnvStringVar::MongoPassword => EnvVar::get_env_name(EnvVar::MongoPassword),
+            EnvStringVar::MongoIp => EnvVar::get_env_name(EnvVar::MongoIp),
+            EnvStringVar::MongoPort => EnvVar::get_env_name(EnvVar::MongoPort),
+            EnvStringVar::MongoParams => EnvVar::get_env_name(EnvVar::MongoParams),
 
-            EnvStringVar::PostgresFirstHandleUrlPart => POSTGRES_FIRST_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::PostgresSecondHandleUrlPart => POSTGRES_SECOND_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::PostgresThirdHandleUrlPart => POSTGRES_THIRD_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::PostgresFourthHandleUrlPart => POSTGRES_FOURTH_HANDLE_URL_PART_ENV_NAME,
-            EnvStringVar::PostgresFifthHandleUrlPart => POSTGRES_FIFTH_HANDLE_URL_PART_ENV_NAME,
+            EnvStringVar::PostgresFirstHandleUrlPart => EnvVar::get_env_name(EnvVar::PostgresFirstHandleUrlPart),
+            EnvStringVar::PostgresSecondHandleUrlPart => EnvVar::get_env_name(EnvVar::PostgresSecondHandleUrlPart),
+            EnvStringVar::PostgresThirdHandleUrlPart => EnvVar::get_env_name(EnvVar::PostgresThirdHandleUrlPart),
+            EnvStringVar::PostgresFourthHandleUrlPart => EnvVar::get_env_name(EnvVar::PostgresFourthHandleUrlPart),
+            EnvStringVar::PostgresFifthHandleUrlPart => EnvVar::get_env_name(EnvVar::PostgresFifthHandleUrlPart),
 
-            EnvStringVar::PostgresLogin => POSTGRES_LOGIN_ENV_NAME,
-            EnvStringVar::PostgresPassword => POSTGRES_PASSWORD_ENV_NAME,
-            EnvStringVar::PostgresIp => POSTGRES_IP_ENV_NAME,
-            EnvStringVar::PostgresPort => POSTGRES_PORT_ENV_NAME,
-            EnvStringVar::PostgresDb => POSTGRES_DB_ENV_NAME,
+            EnvStringVar::PostgresLogin => EnvVar::get_env_name(EnvVar::PostgresLogin),
+            EnvStringVar::PostgresPassword => EnvVar::get_env_name(EnvVar::PostgresPassword),
+            EnvStringVar::PostgresIp => EnvVar::get_env_name(EnvVar::PostgresIp),
+            EnvStringVar::PostgresPort => EnvVar::get_env_name(EnvVar::PostgresPort),
+            EnvStringVar::PostgresDb => EnvVar::get_env_name(EnvVar::PostgresDb),
 
-            EnvStringVar::ArxivLink => ARXIV_LINK_ENV_NAME,
-            EnvStringVar::BiorxivLink => BIORXIV_LINK_ENV_NAME,
-            EnvStringVar::GithubLink => GITHUB_LINK_ENV_NAME,
-            EnvStringVar::HabrLink => HABR_LINK_ENV_NAME,
-            EnvStringVar::MedrxivLink => MEDRXIV_LINK_ENV_NAME,
-            EnvStringVar::RedditLink => REDDIT_LINK_ENV_NAME,
-            EnvStringVar::TwitterLink => TWITTER_LINK_ENV_NAME,
+            EnvStringVar::ArxivLink => EnvVar::get_env_name(EnvVar::ArxivLink),
+            EnvStringVar::BiorxivLink => EnvVar::get_env_name(EnvVar::BiorxivLink),
+            EnvStringVar::GithubLink => EnvVar::get_env_name(EnvVar::GithubLink),
+            EnvStringVar::HabrLink => EnvVar::get_env_name(EnvVar::HabrLink),
+            EnvStringVar::MedrxivLink => EnvVar::get_env_name(EnvVar::MedrxivLink),
+            EnvStringVar::RedditLink => EnvVar::get_env_name(EnvVar::RedditLink),
+            EnvStringVar::TwitterLink => EnvVar::get_env_name(EnvVar::TwitterLink),
         }
     }
     pub fn get_length() -> usize {
