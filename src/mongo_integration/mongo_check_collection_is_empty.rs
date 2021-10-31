2083224fa712a2977a68f93ba05953bae0c7dd47
kuqmua <serezhashmakov@outlook.com>
2021-10-31 11:54:21 +0300

    mongo_check_collection_is_empty rewritten

--- src/mongo_integration/mongo_check_collection_is_empty.rs
+++ src/mongo_integration/mongo_check_collection_is_empty.rs
@@ -1,5 +1,3 @@
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
 use mongodb::bson::Document;
 use mongodb::{options::ClientOptions, Client};
 
@@ -8,66 +6,17 @@ pub async fn mongo_check_collection_is_empty(
     mongo_url: &str,
     db_name_handle: &str,
     db_collection_handle: &str,
-) -> Result<Option<bool>, mongodb::error::Error> {
+) -> Result<bool, mongodb::error::Error> {
     let client_options = ClientOptions::parse(mongo_url).await?;
-    let client_result = Client::with_options(client_options);
-    let result_flag: Option<bool>;
-    match client_result {
-        Ok(client) => {
-            //declare db name. there is no create db method in mongo
-            let db = client.database(db_name_handle);
-            let mut needed_db_collection: Option<String> = None;
-            for collection_name in db.list_collection_names(None).await? {
-                if collection_name == *db_collection_handle {
-                    needed_db_collection = Some(collection_name);
-                }
-            }
-            match needed_db_collection {
-                Some(collection_name) => {
-                    let collection = db.collection::<Document>(&collection_name);
-                    let documents_number_result = collection.count_documents(None, None).await;
-                    match documents_number_result {
-                        Ok(documents_number) => {
-                            if documents_number == 0 {
-                                result_flag = Some(true)
-                            } else {
-                                result_flag = Some(false)
-                            }
-                        }
-                        Err(e) => {
-                            result_flag = None;
-                            print_colorful_message(
-                                None,
-                                PrintType::WarningHigh,
-                                file!().to_string(),
-                                line!().to_string(),
-                                format!("documents_number_result error, {:#?}", e),
-                            );
-                        }
-                    }
-                }
-                None => {
-                    result_flag = None;
-                    print_colorful_message(
-                        None,
-                        PrintType::WarningLow,
-                        file!().to_string(),
-                        line!().to_string(),
-                        "needed_db_collection is None".to_string(),
-                    );
-                }
-            }
-        }
-        Err(e) => {
-            result_flag = None;
-            print_colorful_message(
-                None,
-                PrintType::WarningLow,
-                file!().to_string(),
-                line!().to_string(),
-                format!("client_result error, {:#?}", e),
-            );
-        }
+    let client = Client::with_options(client_options)?;
+    let db = client.database(db_name_handle); //declare db name. there is no create db method in mongo
+    let documents_number = db
+        .collection::<Document>(db_collection_handle)
+        .count_documents(None, None)
+        .await?;
+    if documents_number == 0 {
+        Ok(true)
+    } else {
+        Ok(false)
     }
-    Ok(result_flag)
 }
