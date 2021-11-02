1f8e3a1bbd5d7a6a0c3a6036cb60eff53045eaab
kuqmua <serezhashmakov@outlook.com>
2021-11-02 19:44:38 +0300

    update mongo_drop_collection_wrapper - ? instead of match

--- src/mongo_integration/mongo_drop_collection_wrapper.rs
+++ src/mongo_integration/mongo_drop_collection_wrapper.rs
@@ -4,7 +4,6 @@ use mongodb::{options::ClientOptions, Client};
 use crate::prints::print_colorful_message::print_colorful_message;
 use crate::prints::print_type_enum::PrintType;
 
-//it will fail on runtime if remove #[tokio::main]
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_drop_collection_wrapper(
     mongo_url: &str,
@@ -12,95 +11,26 @@ pub async fn mongo_drop_collection_wrapper(
     db_collection_name: &str,
     check_if_collection_empty: bool,
 ) -> Result<bool, mongodb::error::Error> {
-    let result_flag: bool;
-    let client_options_result = ClientOptions::parse(mongo_url).await;
-    match client_options_result {
-        Ok(client_options) => {
-            let client_result = Client::with_options(client_options);
-            match client_result {
-                Ok(client) => {
-                    let db = client.database(db_name);
-                    let collection = db.collection::<Document>(db_collection_name);
-                    if check_if_collection_empty {
-                        let documents_number_result = collection.count_documents(None, None).await;
-                        match documents_number_result {
-                            Ok(documents_number) => {
-                                if documents_number > 0 {
-                                    result_flag = false;
-                                    print_colorful_message(
-                                        None,
-                                        PrintType::WarningLow,
-                                        file!().to_string(),
-                                        line!().to_string(),
-                                        "collection is not empty".to_string(),
-                                    );
-                                } else {
-                                    let collection_drop_result = collection.drop(None).await;
-                                    match collection_drop_result {
-                                        Ok(()) => result_flag = true,
-                                        Err(e) => {
-                                            result_flag = false;
-                                            print_colorful_message(
-                                                None,
-                                                PrintType::Error,
-                                                file!().to_string(),
-                                                line!().to_string(),
-                                                format!("collection_drop_result error {:#?}", e),
-                                            );
-                                        }
-                                    }
-                                }
-                            }
-                            Err(e) => {
-                                result_flag = false;
-                                print_colorful_message(
-                                    None,
-                                    PrintType::Error,
-                                    file!().to_string(),
-                                    line!().to_string(),
-                                    format!("documents_number_result error {:#?}", e),
-                                );
-                            }
-                        }
-                    } else {
-                        let collection_drop_result = collection.drop(None).await;
-                        match collection_drop_result {
-                            Ok(()) => result_flag = true,
-                            Err(e) => {
-                                result_flag = false;
-                                print_colorful_message(
-                                    None,
-                                    PrintType::Error,
-                                    file!().to_string(),
-                                    line!().to_string(),
-                                    format!("collection_drop_result error {:#?}", e),
-                                );
-                            }
-                        }
-                    }
-                }
-                Err(e) => {
-                    result_flag = false;
-                    print_colorful_message(
-                        None,
-                        PrintType::Error,
-                        file!().to_string(),
-                        line!().to_string(),
-                        format!("client_result error {:#?}", e),
-                    );
-                }
-            }
-        }
-        Err(e) => {
-            result_flag = false;
-            print_colorful_message(
-                None,
-                PrintType::Error,
-                file!().to_string(),
-                line!().to_string(),
-                format!("client_options_result error {:#?}", e),
-            );
-        }
+    let client_options = ClientOptions::parse(mongo_url).await?;
+    let client = Client::with_options(client_options)?;
+    let db = client.database(db_name);
+    let collection = db.collection::<Document>(db_collection_name);
+    if !check_if_collection_empty {
+        collection.drop(None).await?;
+        return Ok(true)
     }
-    Ok(result_flag)
+    let documents_number = collection.count_documents(None, None).await?;
+    if documents_number == 0 {
+        collection.drop(None).await?;
+        Ok(true)
+    } else {
+        print_colorful_message(
+            None,
+            PrintType::WarningLow,
+            file!().to_string(),
+            line!().to_string(),
+            "collection is not empty".to_string(),
+        );
+        Ok(false)
+    }
 }
