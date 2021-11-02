066fc4c4d2142956e5065630d759781e5c27c61e
kuqmua <serezhashmakov@outlook.com>
2021-11-02 19:34:47 +0300

    mongo_drop_empty_collection

---
+++ src/mongo_integration/mongo_drop_empty_collection.rs
@@ -0,0 +1,30 @@
+use mongodb::bson::Document;
+use mongodb::{options::ClientOptions, Client};
+
+use crate::prints::print_colorful_message::print_colorful_message;
+use crate::prints::print_type_enum::PrintType;
+
+#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+pub async fn mongo_drop_empty_collection(
+    mongo_url: &str,
+    db_name: &str,
+    db_collection_name: &str,
+) -> Result<bool, mongodb::error::Error> {
+    let client_options = ClientOptions::parse(mongo_url).await?;
+    let client = Client::with_options(client_options)?;
+    let collection = client.database(db_name).collection::<Document>(db_collection_name);
+    let documents_number = collection.count_documents(None, None).await?;
+    if documents_number > 0 {
+        print_colorful_message(
+            None,
+            PrintType::WarningLow,
+            file!().to_string(),
+            line!().to_string(),
+            "collection is not empty".to_string(),
+        );
+        Ok(false)
+    } else {
+        collection.drop(None).await?;
+        Ok(true)   
+    }
+}
