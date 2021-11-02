400a6392dc00db7e70def3d6575af9715351c1cc
kuqmua <serezhashmakov@outlook.com>
2021-11-02 20:19:09 +0300

    mongo_drop_db_checked_on_empty

---
+++ src/mongo_integration/mongo_drop_db_checked_on_empty.rs
@@ -0,0 +1,14 @@
+use mongodb::{options::ClientOptions, Client};
+
+#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+pub async fn mongo_drop_db_checked_on_empty(mongo_url: &str, db_name: &str) -> Result<bool, mongodb::error::Error> {
+    let client_options = ClientOptions::parse(mongo_url).await?;
+    let client = Client::with_options(client_options)?;
+    let db = client.database(db_name);
+    let collections_names_list = db.list_collection_names(None).await?;
+    if !collections_names_list.is_empty() {
+        return Ok(false)
+    }
+    db.drop(None).await?;
+    Ok(true)
+}
