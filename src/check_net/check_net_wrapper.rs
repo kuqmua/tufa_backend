cecad7439623d123ed916405ae5b6a815b605703
kuqmua <serezhashmakov@outlook.com>
2021-11-07 16:12:02 +0300

    postgres_check_availability in check_net_wrapper

--- src/check_net/check_net_wrapper.rs
+++ src/check_net/check_net_wrapper.rs
@@ -2,13 +2,11 @@ use crate::check_net::fetch_link::fetch_link;
 
 use crate::config_mods::config::CONFIG;
 
-use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
-use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
-
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-
 use crate::mongo_integration::mongo_check_availability::mongo_check_availability;
+use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
+
+use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
+use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
 
 use crate::check_net::check_net_error_enum::CheckNetError;
 
@@ -20,7 +18,7 @@ pub fn check_net_wrapper() -> Result<(), CheckNetError> {
     if !StatusCode::is_success(&status_code) {
         return Err(CheckNetError::StartingLinkCode { status_code });
     }
-    PgConnection::establish(&postgres_get_db_url())?;
+    postgres_check_availability(&postgres_get_db_url())?;
     mongo_check_availability(&mongo_get_db_url())?;
     Ok(())
 }
--- src/postgres_integration/postgres_check_availability.rs
+++ src/postgres_integration/postgres_check_availability.rs
@@ -3,10 +3,8 @@ use diesel::prelude::*;
 
 use diesel::prelude::ConnectionError;
 
-use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
-
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn postgres_check_availability() -> Result<(), ConnectionError> {
-    PgConnection::establish(&postgres_get_db_url())?;
+pub fn postgres_check_availability(postgres_url: &str) -> Result<(), ConnectionError> {
+    PgConnection::establish(postgres_url)?;
     Ok(())
 }
