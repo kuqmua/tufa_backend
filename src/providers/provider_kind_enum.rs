428618270a73961290e93c83d441915c459b020f
kuqmua <serezhashmakov@outlook.com>
2021-11-07 17:20:46 +0300

    ProviderKind::get_enabled_providers_vec

--- src/providers/provider_kind_enum.rs
+++ src/providers/provider_kind_enum.rs
@@ -34,6 +34,12 @@ use crate::prints::print_type_enum::PrintType;
 use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
 use crate::mongo_integration::mongo_possibly_get_documents_as_string_vector::mongo_possibly_get_documents_as_string_vector;
 
+///////////////
+
+use std::fs;
+
+use crate::providers::providers_info::providers_init_json_schema::ProvidersInitJsonSchema;
+
 #[derive(
     EnumVariantCount,
     EnumIter,
@@ -157,6 +163,15 @@ impl ProviderKind {
             string_name_vec.push(ProviderKind::get_string_name(provider_kind));
         }
         string_name_vec
+    }
+    pub fn get_enabled_providers_vec() -> Vec<ProviderKind> {
+        let mut providers_vec: Vec<ProviderKind> = Vec::with_capacity(ProviderKind::get_length());
+        for provider_kind in
+            ProviderKind::iter().filter(|element| ProviderKind::is_enabled(*element))
+        {
+            providers_vec.push(provider_kind);
+        }
+        providers_vec
     }
     pub fn get_mongo_initialization_vec() -> Vec<&'static str> {
         let mut vec_of_filtered_provider_names: Vec<&'static str> =
@@ -467,4 +482,55 @@ impl ProviderKind {
             ProviderKind::Habr => &CONFIG.providers_check_links.habr_link,
         }
     }
+    //////////////////////
+
+    // #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+    // pub fn get_providers_json_local_data() -> HashMap<ProviderKind, Vec<String>> {
+    //     let mut vec_of_link_parts_hashmap: HashMap<ProviderKind, Vec<String>> = HashMap::new();
+    //     //todo: do it async in parallel
+    //     for provider_name in ProviderKind::get_enabled_string_name_vec() {
+    //         let result_of_reading_to_string = fs::read_to_string(&format!(
+    //             "{}{}{}{}",
+    //             CONFIG.mongo_params.path_to_provider_link_parts_folder,
+    //             provider_name,
+    //             CONFIG
+    //                 .mongo_params
+    //                 .providers_db_collection_handle_second_part,
+    //             CONFIG.mongo_params.log_file_extension
+    //         ));
+    //         match result_of_reading_to_string {
+    //             Ok(file_content) => {
+    //                 let file_content_from_str_result: Result<
+    //                     ProvidersInitJsonSchema,
+    //                     serde_json::Error,
+    //                 > = serde_json::from_str(&file_content);
+    //                 match file_content_from_str_result {
+    //                     Ok(file_content_as_struct) => {
+    //                         let mut vec_of_link_parts: Vec<String> =
+    //                             Vec::with_capacity(file_content_as_struct.data.len());
+    //                         for link_part in file_content_as_struct.data {
+    //                             vec_of_link_parts.push(link_part)
+    //                         }
+    //                         vec_of_link_parts_hashmap.insert(provider_name, vec_of_link_parts);
+    //                     }
+    //                     Err(e) => println!("file_content_from_str_result error {:#?}", e),
+    //                 }
+    //             }
+    //             Err(e) => {
+    //                 println!(
+    //                     "cannot read_to_string from file {}{}{}{}, reason: {}",
+    //                     CONFIG.mongo_params.path_to_provider_link_parts_folder,
+    //                     provider_name,
+    //                     CONFIG
+    //                         .mongo_params
+    //                         .providers_db_collection_handle_second_part,
+    //                     CONFIG.mongo_params.log_file_extension,
+    //                     e
+    //                 )
+    //             }
+    //         }
+    //     }
+    //     println!("vec_of_link_parts_hashmap {:#?}", vec_of_link_parts_hashmap);
+    //     vec_of_link_parts_hashmap
+    // }
 }
