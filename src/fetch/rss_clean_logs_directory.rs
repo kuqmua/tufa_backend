c018b9c6c4ffa1ba1c377d7f709d71036086d139
kuqmua <serezhashmakov@outlook.com>
2021-11-09 21:50:54 +0300

    CleanLogsDirError

--- src/fetch/rss_clean_logs_directory.rs
+++ src/fetch/rss_clean_logs_directory.rs
@@ -1,35 +1,30 @@
 use crate::providers::provider_kind_enum::ProviderKind;
 
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
-
 use std::fs;
 use std::path::Path;
 
-#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn rss_clean_logs_directory(provider_kind: ProviderKind) {
-    let path = ProviderKind::get_path_to_logs_directory(provider_kind);
-    if Path::new(&path).is_dir() {
-        let result_of_recursively_removing_warning_logs_directory = fs::remove_dir_all(&path);
-        match result_of_recursively_removing_warning_logs_directory {
-            Ok(_) => {
-                print_colorful_message(
-                    Some(&provider_kind),
-                    PrintType::Success,
-                    file!().to_string(),
-                    line!().to_string(),
-                    format!("folder {} has been deleted", &path),
-                );
-            }
-            Err(e) => {
-                print_colorful_message(
-                    Some(&provider_kind),
-                    PrintType::Error,
-                    file!().to_string(),
-                    line!().to_string(),
-                    format!("delete folder problem{} {}", &path, e.to_string()),
-                );
-            }
-        }
+#[derive(Debug)]
+pub enum CleanLogsDirError {
+    PathIsNotDir { path: String },
+    RemoveDirAll { error: std::io::Error},
+}
+impl From<String> for CleanLogsDirError {
+    fn from(e: String) -> Self {
+        CleanLogsDirError::PathIsNotDir { path: e }
     }
+}
+impl From<std::io::Error> for CleanLogsDirError {
+    fn from(e: std::io::Error) -> Self {
+        CleanLogsDirError::RemoveDirAll { error: e }
+    }
+}
+
+#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+pub fn rss_clean_logs_directory(provider_kind: ProviderKind) -> Result<(), CleanLogsDirError> {
+    let path = ProviderKind::get_path_to_logs_directory(provider_kind);
+    if !Path::new(&path).is_dir() {
+        return Err(CleanLogsDirError::PathIsNotDir { path });
+    }
+    fs::remove_dir_all(&path)?;
+    Ok(())
 }
