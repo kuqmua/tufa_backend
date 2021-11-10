b03c4ae5b5b6e540d8472add8c744f251ca248b5
kuqmua <serezhashmakov@outlook.com>
2021-11-10 20:39:33 +0300

    write_string_into_file and write_json_into_file result types with WriteJsonIntoFileError

--- src/helpers/write_json_into_file.rs
+++ src/helpers/write_json_into_file.rs
@@ -2,15 +2,24 @@ use serde_json::Value;
 
 use crate::helpers::write_string_into_file::write_string_into_file;
 
-#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn write_json_into_file(path_to_file: String, json_object: Value) -> bool {
-    let result_stringified_json = serde_json::to_string_pretty(&json_object);
-    match result_stringified_json {
-        Ok(stringified_json) => {
-            write_string_into_file(path_to_file, stringified_json)
-        },
-        Err(e) => {
-            false
-        }
+#[derive(Debug)]
+pub enum WriteJsonIntoFileError {
+    SerdeJsonError(serde_json::Error),
+    StdIoError(std::io::Error),
+}
+impl From<serde_json::Error> for WriteJsonIntoFileError {
+    fn from(e:serde_json::Error) -> Self {
+        WriteJsonIntoFileError::SerdeJsonError(e)
     }
+}
+impl From<std::io::Error> for WriteJsonIntoFileError {
+    fn from(e: std::io::Error) -> Self {
+        WriteJsonIntoFileError::StdIoError(e)
+    }
+}
+
+#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+pub fn write_json_into_file(path_to_file: String, json_object: Value) -> Result<(), WriteJsonIntoFileError> {
+    let stringified_json = serde_json::to_string_pretty(&json_object)?;
+    Ok(write_string_into_file(path_to_file, stringified_json)?)
 }
--- src/helpers/write_string_into_file.rs
+++ src/helpers/write_string_into_file.rs
@@ -1,46 +1,9 @@
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
-
 use std::{fs::File, io::Write};
+use std::io::Error;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn write_string_into_file(file_name: String, stringified_json: String) -> bool {
-    let result_of_creating_file = File::create(&file_name);
-    match result_of_creating_file {
-        Ok(mut created_for_logs_file) => {
-            let result_of_writing = created_for_logs_file.write(stringified_json.as_bytes()); //warning_message
-            match result_of_writing {
-                Ok(_) => {
-                    print_colorful_message(
-                        None,
-                        PrintType::Success,
-                        file!().to_string(),
-                        line!().to_string(),
-                        format!("logs were written in file {}", &file_name),
-                    );
-                    true
-                }
-                Err(e) => {
-                    print_colorful_message(
-                        None,
-                        PrintType::Error,
-                        file!().to_string(),
-                        line!().to_string(),
-                        format!("error writing in file {} {}", file_name, e.to_string()),
-                    );
-                    false
-                }
-            }
-        }
-        Err(e) => {
-            print_colorful_message(
-                None,
-                PrintType::Error,
-                file!().to_string(),
-                line!().to_string(),
-                format!("error creating file {} {}", &file_name, e.to_string()),
-            );
-            false
-        }
-    }
+pub fn write_string_into_file(file_name: String, stringified_json: String) -> Result<(), Error> {
+    let mut log_file = File::create(&file_name)?;
+    log_file.write(stringified_json.as_bytes())?;
+    Ok(())
 }
