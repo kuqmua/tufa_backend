b592d080b0e3622c0f7768bbd18fffe890df2bc9
kuqmua <serezhashmakov@outlook.com>
2021-11-13 14:15:41 +0300

    write_string_into_file write -> write_all (rust analyzer info)

--- src/helpers/write_string_into_file.rs
+++ src/helpers/write_string_into_file.rs
@@ -4,6 +4,6 @@ use std::{fs::File, io::Write};
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn write_string_into_file(file_name: String, stringified_json: String) -> Result<(), Error> {
     let mut log_file = File::create(&file_name)?;
-    log_file.write(stringified_json.as_bytes())?;
+    log_file.write_all(stringified_json.as_bytes())?;
     Ok(())
 }
