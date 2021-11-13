440969d82376ee0c9e490d451fbb2fe6a9603d89
kuqmua <serezhashmakov@outlook.com>
2021-11-13 19:22:00 +0300

    generate_hashmap_with_empty_string_vecs_for_enabled_providers

--- src/providers/provider_kind_enum.rs
+++ src/providers/provider_kind_enum.rs
@@ -421,6 +421,16 @@ impl ProviderKind {
                 .clone();
         Ok(vec_provider_kind_with_collection_names)
     }
+    pub fn generate_hashmap_with_empty_string_vecs_for_enabled_providers(
+    ) -> HashMap<ProviderKind, Vec<String>> {
+        let mut hashmap_with_empty_vecs = HashMap::<ProviderKind, Vec<String>>::with_capacity(
+            ProviderKind::get_enabled_providers_vec().len(),
+        );
+        for provider_kind in ProviderKind::get_enabled_providers_vec().iter() {
+            hashmap_with_empty_vecs.insert(*provider_kind, Vec::<String>::new());
+        }
+        hashmap_with_empty_vecs
+    }
     pub fn enable_links_limit_for(provider_kind: ProviderKind) -> bool {
         match provider_kind {
             ProviderKind::Arxiv => {
