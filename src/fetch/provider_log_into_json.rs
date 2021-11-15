f0b678a5dd6a7aad5c4aba6d0d7bcd52fb9db513
kuqmua <serezhashmakov@outlook.com>
2021-11-15 20:35:06 +0300

    AreThereItems -> NoItemsError for provider_log_into_json. build will fail

--- src/fetch/provider_log_into_json.rs
+++ src/fetch/provider_log_into_json.rs
@@ -1,9 +1,6 @@
-use crate::fetch::rss_metainfo_fetch_structures::AreThereItems;
+use crate::fetch::rss_metainfo_fetch_structures::NoItemsError;
 use crate::providers::provider_kind_enum::ProviderKind;
 use chrono::Local;
-
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
 
 use serde_json::json;
 use serde_json::Value;
@@ -11,41 +8,31 @@ use serde_json::Value;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn provider_log_into_json(
     link: &str,
-    are_there_items: &AreThereItems,
+    no_items_error: &NoItemsError,
     provider_kind: &ProviderKind,
-) -> Option<Value> {
-    match are_there_items {
-        AreThereItems::Yep => {
-            print_colorful_message(
-                    Some(provider_kind),
-                    PrintType::WarningHigh,
-                    file!().to_string(),
-                    line!().to_string(),
-                    "UnhandledFetchStatusInfo::Success, HandledFetchStatusInfo::Success, AreThereItems::Yep --- its not suppose to happend".to_string(),
-                );
-            None
-        }
-        AreThereItems::NopeButThereIsTag(fetch_result_string) => {
+) -> Value {
+    match no_items_error {
+        NoItemsError::ThereIsTag(fetch_result_string) => {
             //"</item>" tag
-            Some(json!({
+            json!({
                 "link": link,
                 "fetch_result_string": fetch_result_string,
-                "part_of": format!("{:?}", provider_kind),
+                "part_of": ProviderKind::get_string_name(*provider_kind),
                 "date": Local::now().to_string()
-            }))
+            })
         }
-        AreThereItems::ConversionFromStrError(fetch_result_string, error) => Some(json!({
+        NoItemsError::ConversionFromStrError(fetch_result_string, error) => json!({
             "link": link,
             "fetch_result_string": fetch_result_string,
             "error": error,
-            "part_of": format!("{:?}", provider_kind),
+            "part_of": ProviderKind::get_string_name(*provider_kind),
             "date": Local::now().to_string()
-        })),
-        AreThereItems::NopeNoTag(tag) => Some(json!({
+        }),
+        NoItemsError::NoTag(tag) => json!({
             "link": link,
             "tag": tag,
-            "part_of": format!("{:?}", provider_kind),
+            "part_of": ProviderKind::get_string_name(*provider_kind),
             "date": Local::now().to_string()
-        })),
+        }),
     }
 }
