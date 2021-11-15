70df583c44f2228fe8f42f6a86f2893fdbd26479
kuqmua <serezhashmakov@outlook.com>
2021-11-15 20:39:19 +0300

    AreThereItems -> NoItemsError for rss_async_write_fetch_error_logs_into_files_wrapper. build will fail

--- src/fetch/rss_async_write_fetch_error_logs_into_files_wrapper.rs
+++ src/fetch/rss_async_write_fetch_error_logs_into_files_wrapper.rs
@@ -1,5 +1,5 @@
 use crate::fetch::provider_log_into_json::provider_log_into_json;
-use crate::fetch::rss_metainfo_fetch_structures::AreThereItems;
+use crate::fetch::rss_metainfo_fetch_structures::NoItemsError;
 use crate::fetch::rss_write_error_logs_into_file_for_provider_wrapper_checker::rss_write_error_logs_into_file_for_provider_wrapper_checker;
 use crate::providers::provider_kind_enum::ProviderKind;
 use futures::future::join_all;
@@ -7,25 +7,20 @@ use std::time::Instant;
 
 use crate::config_mods::config::CONFIG;
 
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
-
 #[deny(clippy::indexing_slicing)] //, clippy::unwrap_used
 #[tokio::main]
 pub async fn rss_async_write_fetch_error_logs_into_files_wrapper(
-    error_posts: Vec<(String, AreThereItems, ProviderKind)>,
+    error_posts: Vec<(String, NoItemsError, ProviderKind)>,
 ) {
     let time = Instant::now();
     let mut vec_of_write_into_files_futures = Vec::with_capacity(error_posts.len());
     for (link, are_there_items, provider_kind) in error_posts {
-        let option_json = provider_log_into_json(
+        let json_object = provider_log_into_json(
             &link.clone(), //todo understand lifetimes to remove it
             &are_there_items,
             &provider_kind,
         );
-        match option_json {
-            Some(json_object) => {
-                vec_of_write_into_files_futures
+        vec_of_write_into_files_futures
                     .push(rss_write_error_logs_into_file_for_provider_wrapper_checker(
                     json_object,
                     provider_kind,
@@ -35,17 +30,6 @@ pub async fn rss_async_write_fetch_error_logs_into_files_wrapper(
                     &CONFIG.params.warning_logs_directory_name,
                     link,
                 ));
-            }
-            None => {
-                print_colorful_message(
-                    Some(&provider_kind),
-                    PrintType::WarningHigh,
-                    file!().to_string(),
-                    line!().to_string(),
-                    "UnhandledFetchStatusInfo::Success, HandledFetchStatusInfo::Success, AreThereItems::Yep --- its not suppose to happend".to_string(),
-                );
-            }
-        }
     }
     let _ = join_all(vec_of_write_into_files_futures).await; //todo: add state of success/unsuccess
     if CONFIG.params.enable_time_measurement_prints {
