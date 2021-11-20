e6ca3ff9bbc8f89a03a4cbed02ac1d2b6a515293
kuqmua <serezhashmakov@outlook.com>
2021-11-20 17:01:12 +0300

    rewrite write_string_into_file. build will fail

--- src/helpers/write_string_into_file.rs
+++ src/helpers/write_string_into_file.rs
@@ -2,8 +2,13 @@ use std::io::Error;
 use std::{fs::File, io::Write};
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn write_string_into_file(file_name: String, stringified_json: String) -> Result<(), Error> {
-    let mut log_file = File::create(&file_name)?;
+pub fn write_string_into_file(path_to_file: String, stringified_json: String) -> Result<(), Error> {
+    let path = std::path::Path::new(&path_to_file);
+    if let Some(prefix) = path.parent() {
+        std::fs::create_dir_all(prefix)?;
+    }
+    let mut log_file = File::create(path)?;
     log_file.write_all(stringified_json.as_bytes())?;
+    log_file.sync_all()?;
     Ok(())
 }
