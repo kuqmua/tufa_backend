6b3b887a3727c3262a9ecc00d912f49c0366a10c
kuqmua <serezhashmakov@outlook.com>
2021-11-20 15:33:15 +0300

    mongo_get_documents_as_string_vector correct conversion to string

--- src/mongo_integration/mongo_get_documents_as_string_vector.rs
+++ src/mongo_integration/mongo_get_documents_as_string_vector.rs
@@ -16,7 +16,29 @@ pub async fn mongo_get_documents_as_string_vector(
     while let Some(document) = cursor.try_next().await? {
         let bson_option = document.get(db_collection_document_field_name_handle);
         if let Some(bson_handle) = bson_option {
-            vec_of_strings.push(bson_handle.to_string());
+            match bson_handle {
+                mongodb::bson::Bson::Double(_) => todo!(),
+                mongodb::bson::Bson::String(value) => vec_of_strings.push(value.to_string()),
+                mongodb::bson::Bson::Array(_) => todo!(),
+                mongodb::bson::Bson::Document(_) => todo!(),
+                mongodb::bson::Bson::Boolean(_) => todo!(),
+                mongodb::bson::Bson::Null => todo!(),
+                mongodb::bson::Bson::RegularExpression(_) => todo!(),
+                mongodb::bson::Bson::JavaScriptCode(_) => todo!(),
+                mongodb::bson::Bson::JavaScriptCodeWithScope(_) => todo!(),
+                mongodb::bson::Bson::Int32(_) => todo!(),
+                mongodb::bson::Bson::Int64(_) => todo!(),
+                mongodb::bson::Bson::Timestamp(_) => todo!(),
+                mongodb::bson::Bson::Binary(_) => todo!(),
+                mongodb::bson::Bson::ObjectId(_) => todo!(),
+                mongodb::bson::Bson::DateTime(_) => todo!(),
+                mongodb::bson::Bson::Symbol(_) => todo!(),
+                mongodb::bson::Bson::Decimal128(_) => todo!(),
+                mongodb::bson::Bson::Undefined => todo!(),
+                mongodb::bson::Bson::MaxKey => todo!(),
+                mongodb::bson::Bson::MinKey => todo!(),
+                mongodb::bson::Bson::DbPointer(_) => todo!(),
+            }
         }
     }
     Ok(vec_of_strings)
