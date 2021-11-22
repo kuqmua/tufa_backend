3ddd35b99ed9cba7b9ff6f45b925b78c240aded1
kuqmua <serezhashmakov@outlook.com>
2021-11-22 20:30:18 +0300

    fmt

--- src/providers/get_providers_link_parts_wrapper.rs
+++ src/providers/get_providers_link_parts_wrapper.rs
@@ -7,11 +7,13 @@ use crate::providers::providers_info::get_providers_link_parts::get_providers_li
 use crate::helpers::resource::Resource;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub async fn get_providers_link_parts_wrapper(resource: &Resource ) -> HashMap<ProviderKind, Vec<String>> {
+pub async fn get_providers_link_parts_wrapper(
+    resource: &Resource,
+) -> HashMap<ProviderKind, Vec<String>> {
     //todo: write Resource logic
     match resource {
         Resource::Local => get_providers_link_parts_as_hashmap(resource).await,
         Resource::Mongodb => get_providers_link_parts_as_hashmap(resource).await,
-        Resource::PostgreSql => todo!(),//get_providers_link_parts_wrapper still exists coz there is no postgres implementation
+        Resource::PostgreSql => todo!(), //get_providers_link_parts_wrapper still exists coz there is no postgres implementation
     }
 }
