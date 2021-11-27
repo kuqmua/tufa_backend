936dca780bb12419f81b9586a5c40f8f1e31ffd2
kuqmua <serezhashmakov@outlook.com>
2021-11-27 14:46:39 +0300

    EnvI64Var get_env_values_hashmap in own module

--- src/config_mods/config_values_types_enums/env_var_i64_enum.rs
+++ src/config_mods/config_values_types_enums/env_var_i64_enum.rs
@@ -6,15 +6,7 @@ use strum::IntoEnumIterator;
 
 use strum_macros::EnumIter;
 
-use dotenv::dotenv;
-
-use crate::config_mods::config_error_mods::config_env_var_error_type_enum::ConfigEnvVarErrorType;
-use crate::config_mods::config_error_mods::config_error::ConfigError;
-use crate::config_mods::config_error_mods::config_error_inner_type_enum::ConfigErrorInnerType;
-use crate::config_mods::config_error_mods::var_or_int_parse_error_enum::VarOrIntParseError;
 use crate::config_mods::env_var_enum::EnvVar;
-
-use crate::constants::project_constants::ENV_FILE_NAME;
 
 #[derive(
     EnumVariantCount,
@@ -87,50 +79,5 @@ impl EnvI64Var {
             );
         }
         config_env_var_name_kind_string_to_enum_struct_hasmap
-    }
-    pub fn get_env_values_hashmap() -> Result<HashMap<EnvI64Var, i64>, ConfigError<'static>> {
-        let was_dotenv_enable: bool;
-        match dotenv() {
-            Ok(_) => {
-                was_dotenv_enable = true;
-            }
-            Err(e) => {
-                was_dotenv_enable = false;
-                println!(
-                    "dotenv() failed, trying without {} error: {:?}",
-                    ENV_FILE_NAME, e
-                );
-            }
-        }
-        let mut hmap: HashMap<EnvI64Var, i64> = HashMap::new();
-        let mut error_option: Option<ConfigError> = None;
-        for env_var_name_kind in EnvI64Var::iter() {
-            match EnvI64Var::get_string_from_env_var(env_var_name_kind, was_dotenv_enable) {
-                Ok(env_var_string) => match env_var_string.parse::<i64>() {
-                    Ok(handle) => {
-                        hmap.insert(env_var_name_kind, handle);
-                    }
-                    Err(e) => {
-                        error_option = Some(ConfigError {
-                            env_var_name_kind: ConfigEnvVarErrorType::I64(env_var_name_kind),
-                            was_dotenv_enable,
-                            env_name: EnvI64Var::get_env_name(env_var_name_kind),
-                            env_error: ConfigErrorInnerType::VarOrIntParseErrorErrorHandle(
-                                VarOrIntParseError::Int(e),
-                            ),
-                        });
-                        break;
-                    }
-                },
-                Err(e) => {
-                    error_option = Some(e);
-                    break;
-                }
-            }
-        }
-        if let Some(error) = error_option {
-            return Err(error);
-        }
-        Ok(hmap)
     }
 }
