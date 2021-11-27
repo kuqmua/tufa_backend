c98e81db2e49a54f7b2e8075be36795a7c4a8db0
kuqmua <serezhashmakov@outlook.com>
2021-11-27 14:25:46 +0300

    EnvStringVar into_string_name_and_kind_tuple_vec in own module

--- src/config_mods/config_values_types_enums/env_var_string_enum.rs
+++ src/config_mods/config_values_types_enums/env_var_string_enum.rs
@@ -174,14 +174,4 @@ impl EnvStringVar {
         }
         env_var_name_kind_vec
     }
-    pub fn into_string_name_and_kind_tuple_vec() -> Vec<(&'static str, EnvStringVar)> {
-        let mut env_var_name_kind_vec = Vec::with_capacity(EnvStringVar::get_length());
-        for env_var_name_kind in EnvStringVar::iter() {
-            env_var_name_kind_vec.push((
-                EnvStringVar::get_env_name(env_var_name_kind),
-                env_var_name_kind,
-            ));
-        }
-        env_var_name_kind_vec
-    }
 }
