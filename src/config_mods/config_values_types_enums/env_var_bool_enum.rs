5ca68016a7233661c8b7f416fdade9abcafdd02b
kuqmua <serezhashmakov@outlook.com>
2021-11-27 15:23:55 +0300

    EnvBoolVar into_string_name_and_kind_tuple_vec in own module

--- src/config_mods/config_values_types_enums/env_var_bool_enum.rs
+++ src/config_mods/config_values_types_enums/env_var_bool_enum.rs
@@ -543,14 +543,4 @@ impl EnvBoolVar {
         }
         env_var_name_kind_vec
     }
-    pub fn into_string_name_and_kind_tuple_vec() -> Vec<(&'static str, EnvBoolVar)> {
-        let mut env_var_name_kind_vec = Vec::with_capacity(EnvBoolVar::get_length());
-        for env_var_name_kind in EnvBoolVar::iter() {
-            env_var_name_kind_vec.push((
-                EnvBoolVar::get_env_name(env_var_name_kind),
-                env_var_name_kind,
-            ));
-        }
-        env_var_name_kind_vec
-    }
 }
