3a98a4f6cafc26f5551846d2196afa0e912feff9
kuqmua <serezhashmakov@outlook.com>
2021-11-27 15:30:14 +0300

    EnvBoolVar get_env_name in own module

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,7 @@ pub mod config_mods {
     }
     pub mod config_values_types_enums {
         pub mod env_var_bool_enum_functions {
+            pub mod get_env_name;
             pub mod get_env_values_hashmap;
             pub mod get_string_from_env_var;
             pub mod into_string_name_and_kind_hashmap;
