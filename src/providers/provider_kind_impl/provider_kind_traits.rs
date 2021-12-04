37518ae14a6d95e226e07242996335dd3eb2d153
kuqmua <serezhashmakov@outlook.com>
2021-12-04 12:13:13 +0300

    implemented fn is_success_prints_enabled(&self) -> bool

--- src/providers/provider_kind_impl/provider_kind_traits.rs
+++ src/providers/provider_kind_impl/provider_kind_traits.rs
@@ -213,6 +213,45 @@ impl ProviderKindTrait for ProviderKind {
             }
         }
     }
+    fn is_success_prints_enabled(&self) -> bool {
+        match self {
+            ProviderKind::Arxiv => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_arxiv
+            }
+            ProviderKind::Biorxiv => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_biorxiv
+            }
+            ProviderKind::Github => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_github
+            }
+            ProviderKind::Habr => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_habr
+            }
+            ProviderKind::Medrxiv => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_medrxiv
+            }
+            ProviderKind::Reddit => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_reddit
+            }
+            ProviderKind::Twitter => {
+                CONFIG
+                    .enable_success_providers_prints
+                    .enable_success_prints_for_twitter
+            }
+        }
+    }
     #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
     fn is_link_limits_enabled(&self) -> bool {
         match self {
