bcdaa3f34e9615a9d9b56b329e7656ce3abcf876
kuqmua <serezhashmakov@outlook.com>
2021-12-05 10:32:25 +0300

    EnableProviders is_enabled

--- src/config_mods/config_structs/enable_providers_struct.rs
+++ src/config_mods/config_structs/enable_providers_struct.rs
@@ -1,3 +1,5 @@
+use crate::providers::provider_kind_enum::ProviderKind;
+
 #[derive(Default, Debug, Clone, PartialEq)]
 pub struct EnableProviders {
     pub enable_arxiv: bool,
@@ -7,4 +9,19 @@ pub struct EnableProviders {
     pub enable_medrxiv: bool,
     pub enable_reddit: bool,
     pub enable_twitter: bool,
+}
+
+impl EnableProviders {
+    #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+    fn is_enabled(&self, provider_kind: ProviderKind) -> bool {
+        match provider_kind {
+            ProviderKind::Arxiv => self.enable_arxiv,
+            ProviderKind::Biorxiv => self.enable_biorxiv,
+            ProviderKind::Github => self.enable_github,
+            ProviderKind::Habr => self.enable_habr,
+            ProviderKind::Medrxiv => self.enable_medrxiv,
+            ProviderKind::Reddit => self.enable_reddit,
+            ProviderKind::Twitter => self.enable_twitter,
+        }
+    }
 }
