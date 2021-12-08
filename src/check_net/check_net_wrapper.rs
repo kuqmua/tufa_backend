d15971e59ba1fbbab24deb6ba896f9470980945d
kuqmua <serezhashmakov@outlook.com>
2021-12-08 20:00:02 +0300

    use check_net_availability in check_net_wrapper

--- src/check_net/check_net_wrapper.rs
+++ src/check_net/check_net_wrapper.rs
@@ -1,6 +1,7 @@
-use crate::check_net::check_link_status_code::check_link_status_code;
-
 use crate::config_mods::lazy_static_config::CONFIG;
+
+use crate::check_net::check_net_availability::check_net_availability;
+use crate::check_net::check_net_error_enum::CheckNetError;
 
 use crate::mongo_integration::mongo_check_availability::mongo_check_availability;
 use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
@@ -8,16 +9,9 @@ use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
 use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
 use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
 
-use crate::check_net::check_net_error_enum::CheckNetError;
-
-use reqwest::StatusCode;
-
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_net_wrapper() -> Result<(), CheckNetError> {
-    let status_code = check_link_status_code(&CONFIG.starting_check_link)?;
-    if !StatusCode::is_success(&status_code) {
-        return Err(CheckNetError::StartingLinkCode { status_code });
-    }
+    check_net_availability(&CONFIG.starting_check_link)?;
     postgres_check_availability(&postgres_get_db_url())?;
     mongo_check_availability(&mongo_get_db_url())?;
     Ok(())
