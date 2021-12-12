3232d261f57cf6c5dc048215b36fad9b2aa39e4a
kuqmua <serezhashmakov@outlook.com>
2021-12-12 14:50:18 +0300

    add enable functions in ProviderKindTrait (build will fail)

--- src/traits/provider_kind_trait.rs
+++ src/traits/provider_kind_trait.rs
@@ -7,6 +7,29 @@ use crate::providers::provider_kind_enum::{CleanLogsDirError, RemoveDirError};
 use crate::providers::get_providers_json_local_data_processed_error::GetProvidersJsonLocalDataProcessedError;
 
 pub trait ProviderKindTrait {
+    fn is_mongo_initialization_enabled(&self) -> bool;
+    fn is_mongo_write_error_logs_enabled(&self) -> bool;
+    fn is_mongo_cleaning_warning_logs_db_enabled(&self) -> bool;
+    fn is_mongo_cleaning_warning_logs_db_collections_enabled(&self) -> bool;
+    fn is_mongo_link_parts_randomize_order_enabled(&self) -> bool;
+
+    fn is_postgres_initialization_enabled(&self) -> bool;
+
+    fn is_write_error_logs_in_local_folder_enabled(&self) -> bool;
+    fn is_cleaning_warning_logs_directory_enabled(&self) -> bool;
+
+    fn is_enabled(&self) -> bool;
+    fn is_prints_enabled(&self) -> bool;
+    fn is_warning_high_prints_enabled(&self) -> bool;
+    fn is_warning_low_prints_enabled(&self) -> bool;
+    fn is_success_prints_enabled(&self) -> bool;
+    fn is_partial_success_prints_enabled(&self) -> bool;
+    fn is_error_prints_enabled(&self) -> bool;
+    fn is_time_measurement_prints_enabled(&self) -> bool;
+    fn is_info_prints_enabled(&self) -> bool;
+
+    fn is_links_limit_enabled(&self) -> bool;
+    ///
     fn get_check_link(&self) -> &'static str;
     fn get_item_handle(&self) -> Option<&'static str>;
     fn get_links_limit_for_provider(&self) -> i64;
