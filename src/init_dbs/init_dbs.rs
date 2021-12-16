3dc68e2b0d98d47cd87319c9e8edfcfb408cd738
kuqmua <serezhashmakov@outlook.com>
2021-12-16 20:54:48 +0300

    init_dbs refactoring

--- src/init_dbs/init_dbs.rs
+++ src/init_dbs/init_dbs.rs
@@ -37,44 +37,49 @@ pub async fn init_dbs() -> Result<(), InitDbsError> {
     if !errors_hashmap.is_empty() {
         return Err(InitDbsError::GetProvidersJsonLocalData(errors_hashmap));
     }
-    // let mut f =  Vec::with_capacity(2);
-    // if !CONFIG.mongo_enable_initialization {
-    //     f.push(asyncinit_mongo(providers_json_local_data_hashmap));
-    // }
-    // if !CONFIG.postgres_enable_initialization {
-    //     f.push(init_postgres(providers_json_local_data_hashmap_clone));
-    // }
     providers_json_local_data_hashmap = success_hashmap.clone();
     providers_json_local_data_hashmap_clone = success_hashmap;
-    
-
     let (mongo_insert_data_option_result, postgres_insert_data_option_result) = tokio::join!(
         //todo: remove option coz its just an vec of functions. enable logic can be checked before
-        init_mongo(providers_json_local_data_hashmap),
-        init_postgres(providers_json_local_data_hashmap_clone)
+        async {
+            if CONFIG.mongo_enable_initialization {
+                return Some(init_mongo(providers_json_local_data_hashmap).await);
+            }
+            None
+        },
+        async {
+            if !CONFIG.postgres_enable_initialization {
+                return Some(init_postgres(providers_json_local_data_hashmap_clone).await);
+            }
+            None
+        }
     );
-    if let Err(err) = mongo_insert_data_option_result {
-        match err {
-            MongoInitDbError::ClientOptionsParse(e) => return Err(InitDbsError::MongoClientOptionsParse(e)),
-            MongoInitDbError::ClientWithOptions(e) => return Err(InitDbsError::MongoClientWithOptions(e)),
-            MongoInitDbError::CollectionCountDocuments((pk, e)) => return Err(InitDbsError::MongoCollectionCountDocuments((pk, e))),
-            MongoInitDbError::CollectionIsNotEmpty((pk, documents_number)) => return Err(InitDbsError::MongoCollectionIsNotEmpty((pk, documents_number))),
-            MongoInitDbError::CollectionInsertMany((pk, e)) => return Err(InitDbsError::MongoCollectionInsertMany((pk, e)))
+    if let Some(result) = mongo_insert_data_option_result {
+        if let Err(err) = result {
+            match err {
+                MongoInitDbError::ClientOptionsParse(e) => return Err(InitDbsError::MongoClientOptionsParse(e)),
+                MongoInitDbError::ClientWithOptions(e) => return Err(InitDbsError::MongoClientWithOptions(e)),
+                MongoInitDbError::CollectionCountDocuments((pk, e)) => return Err(InitDbsError::MongoCollectionCountDocuments((pk, e))),
+                MongoInitDbError::CollectionIsNotEmpty((pk, documents_number)) => return Err(InitDbsError::MongoCollectionIsNotEmpty((pk, documents_number))),
+                MongoInitDbError::CollectionInsertMany((pk, e)) => return Err(InitDbsError::MongoCollectionInsertMany((pk, e)))
+            }
         }
     }
-    if let Err(err) = postgres_insert_data_option_result {
-        match err {
-            PostgresInitDbError::LoadingProvidersLinkParts(e) => {
-                return Err(InitDbsError::PostgresLoadingProvidersLinkParts(e));
-            }
-            PostgresInitDbError::ProvidersLinkPartsIsNotEmpty(e_vec) => {
-                return Err(InitDbsError::PostgresProvidersLinkPartsIsNotEmpty(e_vec));
-            }
-            PostgresInitDbError::InsertPosts(e) => {
-                return Err(InitDbsError::PostgresInsertPosts(e));
-            }
-            PostgresInitDbError::EstablishConnection(e) => {
-                return Err(InitDbsError::PostgresEstablishConnection(e));
+    if let Some(result) = postgres_insert_data_option_result {
+        if let Err(err) = result {
+            match err {
+                PostgresInitDbError::LoadingProvidersLinkParts(e) => {
+                    return Err(InitDbsError::PostgresLoadingProvidersLinkParts(e));
+                }
+                PostgresInitDbError::ProvidersLinkPartsIsNotEmpty(e_vec) => {
+                    return Err(InitDbsError::PostgresProvidersLinkPartsIsNotEmpty(e_vec));
+                }
+                PostgresInitDbError::InsertPosts(e) => {
+                    return Err(InitDbsError::PostgresInsertPosts(e));
+                }
+                PostgresInitDbError::EstablishConnection(e) => {
+                    return Err(InitDbsError::PostgresEstablishConnection(e));
+                }
             }
         }
     }
