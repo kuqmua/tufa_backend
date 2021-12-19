7d7881a08c2dade934e7a6e9b78ba5516026281c
kuqmua <serezhashmakov@outlook.com>
2021-12-19 13:49:09 +0300

    fixed some tests logic

--- src/tests/continuous_integration/ci_check_compromised_github_auth_info_from_env.rs
+++ src/tests/continuous_integration/ci_check_compromised_github_auth_info_from_env.rs
@@ -3,20 +3,19 @@ use dotenv::dotenv;
 use crate::constants::tests_constants::_USER_CREDENTIALS_DUMMY_HANDLE;
 
 use crate::config_mods::env_var_enum::EnvVar;
-
-use crate::traits::env_var_trait::EnvVarTrait;
+use crate::traits::enum_extention::EnumExtenstion;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 fn ci_check_compromised_github_auth_info() {
     match dotenv() {
         Ok(_) => {
-            match std::env::var(EnvVar::GithubName.get_env_name()) {
+            match std::env::var(EnvVar::GithubName.to_upper_snake_case()) {
                 Ok(github_name) => {
                     assert!(
                         !(github_name != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::GithubName.get_env_name(),
+                        EnvVar::GithubName.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         github_name
                     );
@@ -24,17 +23,17 @@ fn ci_check_compromised_github_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::GithubName.get_env_name(),
+                        EnvVar::GithubName.to_upper_snake_case(),
                         e
                     );
                 }
             }
-            match std::env::var(EnvVar::GithubToken.get_env_name()) {
+            match std::env::var(EnvVar::GithubToken.to_upper_snake_case()) {
                 Ok(github_token) => {
                     assert!(
                         !(github_token != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::GithubToken.get_env_name(),
+                        EnvVar::GithubToken.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         github_token
                     );
@@ -42,7 +41,7 @@ fn ci_check_compromised_github_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::GithubToken.get_env_name(),
+                        EnvVar::GithubToken.to_upper_snake_case(),
                         e
                     );
                 }
--- src/tests/continuous_integration/ci_check_compromised_reddit_auth_info_from_env.rs
+++ src/tests/continuous_integration/ci_check_compromised_reddit_auth_info_from_env.rs
@@ -3,20 +3,19 @@ use dotenv::dotenv;
 use crate::constants::tests_constants::_USER_CREDENTIALS_DUMMY_HANDLE;
 
 use crate::config_mods::env_var_enum::EnvVar;
-
-use crate::traits::env_var_trait::EnvVarTrait;
+use crate::traits::enum_extention::EnumExtenstion;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 fn ci_check_compromised_reddit_auth_info() {
     match dotenv() {
         Ok(_) => {
-            match std::env::var(EnvVar::RedditUserAgent.get_env_name()) {
+            match std::env::var(EnvVar::RedditUserAgent.to_upper_snake_case()) {
                 Ok(reddit_user_agent) => {
                     assert!(
                         !(reddit_user_agent != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::RedditUserAgent.get_env_name(),
+                        EnvVar::RedditUserAgent.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         reddit_user_agent
                     );
@@ -24,17 +23,17 @@ fn ci_check_compromised_reddit_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::RedditUserAgent.get_env_name(),
+                        EnvVar::RedditUserAgent.to_upper_snake_case(),
                         e
                     );
                 }
             }
-            match std::env::var(EnvVar::RedditClientId.get_env_name()) {
+            match std::env::var(EnvVar::RedditClientId.to_upper_snake_case()) {
                 Ok(reddit_client_id) => {
                     assert!(
                         !(reddit_client_id != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::RedditClientId.get_env_name(),
+                        EnvVar::RedditClientId.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         reddit_client_id
                     );
@@ -42,17 +41,17 @@ fn ci_check_compromised_reddit_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::RedditClientId.get_env_name(),
+                        EnvVar::RedditClientId.to_upper_snake_case(),
                         e
                     );
                 }
             }
-            match std::env::var(EnvVar::RedditClientSecret.get_env_name()) {
+            match std::env::var(EnvVar::RedditClientSecret.to_upper_snake_case()) {
                 Ok(rediit_client_secret) => {
                     assert!(
                         !(rediit_client_secret != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::RedditClientSecret.get_env_name(),
+                        EnvVar::RedditClientSecret.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         rediit_client_secret
                     );
@@ -60,17 +59,17 @@ fn ci_check_compromised_reddit_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::RedditClientSecret.get_env_name(),
+                        EnvVar::RedditClientSecret.to_upper_snake_case(),
                         e
                     );
                 }
             }
-            match std::env::var(EnvVar::RedditUsername.get_env_name()) {
+            match std::env::var(EnvVar::RedditUsername.to_upper_snake_case()) {
                 Ok(reddit_username) => {
                     assert!(
                         !(reddit_username != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::RedditUsername.get_env_name(),
+                        EnvVar::RedditUsername.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         reddit_username
                     );
@@ -78,17 +77,17 @@ fn ci_check_compromised_reddit_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::RedditUsername.get_env_name(),
+                        EnvVar::RedditUsername.to_upper_snake_case(),
                         e
                     );
                 }
             }
-            match std::env::var(EnvVar::RedditPassword.get_env_name()) {
+            match std::env::var(EnvVar::RedditPassword.to_upper_snake_case()) {
                 Ok(reddit_password) => {
                     assert!(
                         !(reddit_password != _USER_CREDENTIALS_DUMMY_HANDLE),
                         "{} != {}, found {}",
-                        EnvVar::RedditPassword.get_env_name(),
+                        EnvVar::RedditPassword.to_upper_snake_case(),
                         _USER_CREDENTIALS_DUMMY_HANDLE,
                         reddit_password
                     );
@@ -96,7 +95,7 @@ fn ci_check_compromised_reddit_auth_info() {
                 Err(e) => {
                     panic!(
                         "{} not found in env vars, error: {:#?}",
-                        EnvVar::RedditPassword.get_env_name(),
+                        EnvVar::RedditPassword.to_upper_snake_case(),
                         e
                     );
                 }
