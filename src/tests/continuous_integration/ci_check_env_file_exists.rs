7bbcc3050853291293c298a52eda545e9688677c
kuqmua <serezhashmakov@outlook.com>
2021-12-25 12:43:45 +0300

    little change in test

--- src/tests/continuous_integration/ci_check_env_file_exists.rs
+++ src/tests/continuous_integration/ci_check_env_file_exists.rs
@@ -7,9 +7,7 @@ use crate::constants::tests_constants::_PATH_TO_ENV_FILE;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 fn ci_check_env_file_exists() {
-    let file = File::open(format!("{}{}", _PATH_TO_ENV_FILE, ENV_FILE_NAME));
-    match file {
-        Ok(_) => {}
-        Err(e) => panic!("file: {}{} error: {}", _PATH_TO_ENV_FILE, ENV_FILE_NAME, e),
+    if let Err(e) = File::open(format!("{}{}", _PATH_TO_ENV_FILE, ENV_FILE_NAME)) {
+        panic!("file: {}{} error: {}", _PATH_TO_ENV_FILE, ENV_FILE_NAME, e);
     }
 }
