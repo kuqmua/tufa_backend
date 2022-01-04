aa71c8e8bfd57a6dd52b298ab2b76bd712879e0f
kuqmua <serezhashmakov@outlook.com>
2022-01-04 22:08:45 +0300

    derive_box_err_from_err not usefull version

--- proc_macro_libs/box_err_from_err/src/lib.rs
+++ proc_macro_libs/box_err_from_err/src/lib.rs
@@ -1,8 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(BoxErrFromErrDerive)]
+pub fn derive_box_err_from_err(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("derive_enum_extension syn::parse(input) failed");
+    let ident = &ast.ident;
+    // let string_ident = ident.to_string();
+    // let write_stroke = syn::Ident::new(&string_ident, ident.span());
+    // let error_name_ident = syn::Ident::new(error_name, ident.span());s
+    let gen = quote! {
+        impl fmt::Display for #ident {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "#write_stroke")
+            }
+        }
+
+        impl From<ConnectionError> for #ident {
+            fn from(error: ConnectionError) -> Self {
+                #ident(Box::new(error))
+            }
+        }
+    };
+    gen.into()
 }
