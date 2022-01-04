5a542212875c9c3bb277cdccec5493b537b5f437
kuqmua <serezhashmakov@outlook.com>
2022-01-04 21:15:06 +0300

    postgres_check_availability best practice

--- src/check_net/check_net_wrapper.rs
+++ src/check_net/check_net_wrapper.rs
@@ -6,39 +6,32 @@ use crate::mongo_integration::mongo_check_availability::mongo_check_availability
 use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
 
 use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
+use crate::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityError;
 use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
-
-use diesel::prelude::ConnectionError;
-
-use thiserror::Error;
 
 use crate::check_net::check_net_availability::CheckNetAvailabilityError;
 
-#[derive(Error, displaydoc::Display, Debug)]
+#[derive(thiserror::Error, displaydoc::Display, Debug)]
 pub enum CheckNetError {
-    ///CheckNetError:  CheckNetAvailabilityError: {source:?}
-    CheckNetAvailabilityError {
-        source: Box<CheckNetAvailabilityError>,
-    },
-    ///CheckNetError: Postgres: {source:?}
-    Postgres { source: Box<ConnectionError> },
-    ///CheckNetError: Mongo: {source:?}
-    Mongo { source: Box<mongodb::error::Error> },
+    ///CheckNetError:  CheckNetAvailabilityError: {0:?}
+    CheckNetAvailabilityError(#[source] Box<CheckNetAvailabilityError>),
+    ///CheckNetError: Postgres: {0:?}
+    Postgres(#[source] PostgresCheckAvailabilityError),
+    ///CheckNetError: Mongo: {0:?}
+    Mongo(#[source] Box<mongodb::error::Error>),
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_net_wrapper() -> Result<(), Box<CheckNetError>> {
     //todo to it in parallel?
     if let Err(e) = check_net_availability(&CONFIG.starting_check_link) {
-        return Err(Box::new(CheckNetError::CheckNetAvailabilityError {
-            source: e,
-        }));
+        return Err(Box::new(CheckNetError::CheckNetAvailabilityError(e)));
     }
     if let Err(e) = postgres_check_availability(&postgres_get_db_url()) {
-        return Err(Box::new(CheckNetError::Postgres { source: e }));
+        return Err(Box::new(CheckNetError::Postgres(e)));
     }
     if let Err(e) = mongo_check_availability(&mongo_get_db_url()) {
-        return Err(Box::new(CheckNetError::Mongo { source: e }));
+        return Err(Box::new(CheckNetError::Mongo(e)));
     }
     Ok(())
 }
--- src/postgres_integration/postgres_check_availability.rs
+++ src/postgres_integration/postgres_check_availability.rs
@@ -1,10 +1,31 @@
+use std::fmt;
+
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
-#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn postgres_check_availability(postgres_url: &str) -> Result<(), Box<ConnectionError>> {
-    if let Err(e) = PgConnection::establish(postgres_url) {
-        return Err(Box::new(e));
+#[derive(thiserror::Error, displaydoc::Display, Debug)]
+pub struct PostgresCheckAvailabilityError(
+    /// postgres connection error `{0}`
+    #[source]
+    Box<ConnectionError>,
+);
+
+impl fmt::Display for PostgresCheckAvailabilityError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PostgresCheckAvailabilityError")
     }
+}
+
+impl From<ConnectionError> for PostgresCheckAvailabilityError {
+    fn from(error: ConnectionError) -> Self {
+        PostgresCheckAvailabilityError(Box::new(error))
+    }
+}
+
+#[deny(clippy::indexing_slicing, clippy::unwrap_used)]
+pub fn postgres_check_availability(
+    postgres_url: &str,
+) -> Result<(), PostgresCheckAvailabilityError> {
+    PgConnection::establish(postgres_url)?;
     Ok(())
 }
