f060f8fbd37befc7f59abfde10381bc6961b3a69
kuqmua <serezhashmakov@outlook.com>
2022-01-04 18:49:49 +0300

    cleaning

--- src/check_net/check_net_availability.rs
+++ src/check_net/check_net_availability.rs
@@ -12,57 +12,18 @@ pub enum CheckNetAvailabilityError {
     StatusCode { source: StatusCodeError },
 }
 
-#[derive(Debug)] //Default,//serde_derive::Serialize, serde_derive::Deserialize
+#[derive(Debug)]
 pub struct StatusCodeError {
     pub status_code: StatusCode,
 }
-// use std::error::Error;
+
 impl std::error::Error for StatusCodeError {}
 
 impl fmt::Display for StatusCodeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Oh no, something bad went down")
+        write!(f, "StatusCodeError error")
     }
 }
-// pub trait Error: Debug + Display {
-//     fn description(&self) -> &str { /* ... */ }
-//     fn cause(&self) -> Option<&Error> { /* ... */ }
-//     fn source(&self) -> Option<&(Error + 'static)> { /* ... */ }
-// }
-
-// impl Error for StatusCode {
-//     fn source(&self) -> Option<Self> {
-//         Some(self)
-//     }
-
-//     // fn type_id(&self, _: private::Internal) -> std::any::TypeId
-//     // where
-//     //     Self: 'static,
-//     // {
-//     //     std::any::TypeId::of::<Self>()
-//     // }
-
-//     // fn backtrace(&self) -> Option<&std::backtrace::Backtrace> {
-//     //     None
-//     // }
-
-//     fn description(&self) -> &str {
-//         "description() is deprecated; use Display"
-//     }
-
-//     fn cause(&self) -> Option<&dyn std::error::Error> {
-//         self.source()
-//     }
-//     // fn backtrace(&self) -> Option<&Backtrace> {
-//     //     None
-//     // }
-//     // fn description(&self) -> &str {
-//     //     None
-//     // }
-//     // fn cause(&self) -> Option<&dyn Error> {
-//     //     None
-//     // }
-// }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_net_availability(link: &str) -> Result<(), Box<CheckNetAvailabilityError>> {
