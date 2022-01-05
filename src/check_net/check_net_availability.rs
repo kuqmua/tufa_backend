fd29e47b83e4f0bf9be3d295fabbf7771682b39e
kuqmua <serezhashmakov@outlook.com>
2022-01-05 21:38:31 +0300

    NetCheckAvailabilityError impl From

--- src/check_net/check_net_availability.rs
+++ src/check_net/check_net_availability.rs
@@ -9,26 +9,42 @@ pub struct NetCheckAvailabilityError {
     pub source: Box<CheckNetAvailabilityError>,
 }
 
+impl From<CheckLinkStatusCodeError> for NetCheckAvailabilityError {
+    fn from(error: CheckLinkStatusCodeError) -> Self {
+        NetCheckAvailabilityError {
+            source: Box::new(CheckNetAvailabilityError::CheckLinkStatusCodeError(error)),
+        }
+    }
+}
+
+impl From<StatusCodeError> for NetCheckAvailabilityError {
+    fn from(error: StatusCodeError) -> Self {
+        NetCheckAvailabilityError {
+            source: Box::new(CheckNetAvailabilityError::StatusCodeError(error)),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum CheckNetAvailabilityError {
-    ReqwestError(CheckLinkStatusCodeError),
-    StatusCode(StatusCodeError),
+    CheckLinkStatusCodeError(CheckLinkStatusCodeError),
+    StatusCodeError(StatusCodeError),
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_net_availability(link: &str) -> Result<(), NetCheckAvailabilityError> {
-    // let status_code = check_link_status_code(link)?;
-    // check_is_status_code_successfull(status_code)?;
-    // Ok(())
-    match check_link_status_code(link) {
-        Err(e) => Err(NetCheckAvailabilityError {
-            source: Box::new(CheckNetAvailabilityError::ReqwestError(e)),
-        }),
-        Ok(status_code) => match check_is_status_code_successfull(status_code) {
-            Err(e) => Err(NetCheckAvailabilityError {
-                source: Box::new(CheckNetAvailabilityError::StatusCode(e)),
-            }),
-            Ok(_) => Ok(()),
-        },
-    }
+    let status_code = check_link_status_code(link)?;
+    check_is_status_code_successfull(status_code)?;
+    Ok(())
+    // match check_link_status_code(link) {
+    //     Err(e) => Err(NetCheckAvailabilityError {
+    //         source: Box::new(CheckNetAvailabilityError::CheckLinkStatusCodeError(e)),
+    //     }),
+    //     Ok(status_code) => match check_is_status_code_successfull(status_code) {
+    //         Err(e) => Err(NetCheckAvailabilityError {
+    //             source: Box::new(CheckNetAvailabilityError::StatusCodeError(e)),
+    //         }),
+    //         Ok(_) => Ok(()),
+    //     },
+    // }
 }
