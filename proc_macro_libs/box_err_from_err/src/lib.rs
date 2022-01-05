b6ff7ca5563551dbb1d881c0a9d7ab8bf38ddf45
kuqmua <serezhashmakov@outlook.com>
2022-01-05 12:27:46 +0300

    derive_box_err_from_err done

--- proc_macro_libs/box_err_from_err/src/lib.rs
+++ proc_macro_libs/box_err_from_err/src/lib.rs
@@ -1,15 +1,70 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn;
+use syn::{self, Ident};
 
 #[proc_macro_derive(BoxErrFromErrDerive)]
 pub fn derive_box_err_from_err(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput =
         syn::parse(input).expect("derive_enum_extension syn::parse(input) failed");
     let ident = &ast.ident;
-    // let string_ident = ident.to_string();
-    // let write_stroke = syn::Ident::new(&string_ident, ident.span());
-    // let error_name_ident = syn::Ident::new(error_name, ident.span());s
+    let data = &ast.data;
+    let error_type_ident: Ident;
+    match data {
+        syn::Data::Struct(data_struct) => match &data_struct.fields {
+            syn::Fields::Unnamed(unnamed_field) => {
+                if unnamed_field.unnamed.len() != 1 {
+                    panic!(
+                        "unnamed_field.unnamed != 1, length is {}",
+                        unnamed_field.unnamed.len()
+                    );
+                }
+                let field = &unnamed_field.unnamed[0];
+                match &field.ty {
+                    syn::Type::Path(type_path) => {
+                        if type_path.path.segments.len() != 1 {
+                            panic!(
+                                "type_path.path.segments != 1, length is {}",
+                                type_path.path.segments.len()
+                            );
+                        }
+                        let path_segment = &type_path.path.segments[0];
+                        match &path_segment.arguments {
+                            syn::PathArguments::AngleBracketed(angle_bracketed_generic_arguments) => {
+                                if angle_bracketed_generic_arguments.args.len() != 1 {
+                                    panic!(
+                                        "angle_bracketed_generic_arguments.args != 1, length is {}",
+                                        angle_bracketed_generic_arguments.args.len()
+                                    );
+                                }
+                                let generic_argument = &angle_bracketed_generic_arguments.args[0];
+                                match generic_argument {
+                                    syn::GenericArgument::Type(type_handle) => {
+                                        match type_handle {
+                                            syn::Type::Path(type_path) => {
+                                                if type_path.path.segments.len() != 1 {
+                                                    panic!(
+                                                        "type_path.path.segments != 1, length is {}",
+                                                        type_path.path.segments.len()
+                                                    );
+                                                }
+                                                error_type_ident = type_path.path.segments[0].ident.clone();
+                                            },
+                                            _ => panic!("type_handle is not a syn::Type::Path!"),
+                                        }
+                                    },
+                                    _ => panic!("generic_argument is not a syn::GenericArgument::Type!"),
+                                }
+                            },
+                            _ => panic!("path_segment.arguments is not a syn::PathArguments::AngleBracketed!"),
+                        }
+                    }
+                    _ => panic!("field.ty is not a syn::Type::Path!"),
+                }
+            }
+            _ => panic!("data_struct.fields is not a syn::Fields::Unnamed!"),
+        },
+        _ => panic!("data is not a Struct!"),
+    }
     let gen = quote! {
         impl fmt::Display for #ident {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -17,8 +72,8 @@ pub fn derive_box_err_from_err(input: TokenStream) -> TokenStream {
             }
         }
 
-        impl From<ConnectionError> for #ident {
-            fn from(error: ConnectionError) -> Self {
+        impl From<#error_type_ident> for #ident {
+            fn from(error: #error_type_ident) -> Self {
                 #ident(Box::new(error))
             }
         }
