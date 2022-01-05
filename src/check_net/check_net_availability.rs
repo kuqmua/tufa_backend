f21d134d5a896b1acfd2925824a62055024237e9
kuqmua <serezhashmakov@outlook.com>
2022-01-05 21:26:27 +0300

    BoxErrFromErrDerive for check_net_availability

--- src/check_net/check_net_availability.rs
+++ src/check_net/check_net_availability.rs
@@ -1,28 +1,12 @@
-use std::fmt;
-
 use crate::check_net::check_link_status_code::check_link_status_code;
 use crate::check_net::check_link_status_code::CheckLinkStatusCodeError;
 
 use crate::check_net::check_is_status_code_successfull::check_is_status_code_successfull;
 use crate::check_net::check_is_status_code_successfull::StatusCodeError;
 
-#[derive(displaydoc::Display, Debug)]
+#[derive(displaydoc::Display, Debug, BoxErrFromErrDerive)]
 pub struct NetCheckAvailabilityError {
     pub source: Box<CheckNetAvailabilityError>,
-}
-
-impl fmt::Display for NetCheckAvailabilityError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "dont know what to write inside impl fmt::Display")
-    }
-}
-
-impl From<CheckNetAvailabilityError> for NetCheckAvailabilityError {
-    fn from(error: CheckNetAvailabilityError) -> Self {
-        NetCheckAvailabilityError {
-            source: Box::new(error),
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -33,6 +17,9 @@ pub enum CheckNetAvailabilityError {
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_net_availability(link: &str) -> Result<(), NetCheckAvailabilityError> {
+    // let status_code = check_link_status_code(link)?;
+    // check_is_status_code_successfull(status_code)?;
+    // Ok(())
     match check_link_status_code(link) {
         Err(e) => Err(NetCheckAvailabilityError {
             source: Box::new(CheckNetAvailabilityError::ReqwestError(e)),
