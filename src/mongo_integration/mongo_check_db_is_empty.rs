06bc2a1a4d2e66ae98ba1c78e8bb4fb507625e98
kuqmua <serezhashmakov@outlook.com>
2022-01-06 20:01:16 +0300

    cleaning mongo_check_db_is_empty

--- src/mongo_integration/mongo_check_db_is_empty.rs
+++ src/mongo_integration/mongo_check_db_is_empty.rs
@@ -5,9 +5,11 @@ pub async fn mongo_check_db_is_empty(
     mongo_url: &str,
     db_name: &str,
 ) -> Result<bool, mongodb::error::Error> {
-    let client_options = ClientOptions::parse(mongo_url).await?;
-    let client = Client::with_options(client_options)?;
-    let db = client.database(db_name);
-    let collections_names_list = db.list_collection_names(None).await?;
-    Ok(collections_names_list.is_empty())
+    Ok(
+        Client::with_options(ClientOptions::parse(mongo_url).await?)?
+            .database(db_name)
+            .list_collection_names(None)
+            .await?
+            .is_empty(),
+    )
 }
