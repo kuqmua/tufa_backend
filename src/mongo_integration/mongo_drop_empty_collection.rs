764bc87b730f6ea9a968f14f3ab95c8aee04e2fd
kuqmua <serezhashmakov@outlook.com>
2022-01-06 20:17:58 +0300

    cleaning mongo_drop_empty_collection

--- src/mongo_integration/mongo_drop_empty_collection.rs
+++ src/mongo_integration/mongo_drop_empty_collection.rs
@@ -1,9 +1,6 @@
 use mongodb::bson::Document;
 use mongodb::{options::ClientOptions, Client};
 
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
-
 #[derive(Debug)]
 pub enum MongoDropEmptyCollectionError {
     MongoError(mongodb::error::Error),
@@ -21,20 +18,11 @@ pub async fn mongo_drop_empty_collection(
     db_name: &str,
     db_collection_name: &str,
 ) -> Result<(), MongoDropEmptyCollectionError> {
-    let client_options = ClientOptions::parse(mongo_url).await?;
-    let client = Client::with_options(client_options)?;
-    let collection = client
+    let collection = Client::with_options(ClientOptions::parse(mongo_url).await?)?
         .database(db_name)
         .collection::<Document>(db_collection_name);
     let documents_number = collection.count_documents(None, None).await?;
     if documents_number > 0 {
-        print_colorful_message(
-            None,
-            PrintType::WarningLow,
-            file!().to_string(),
-            line!().to_string(),
-            "collection is not empty".to_string(),
-        );
         Err(MongoDropEmptyCollectionError::NotEmpty(documents_number))
     } else {
         collection.drop(None).await?;
