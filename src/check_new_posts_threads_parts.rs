c00ee92125ab5d817c7ac34e8adf801ac8948840
kuqmua <serezhashmakov@outlook.com>
2022-01-06 17:25:10 +0300

    use tokio task instead of thread inside check_new_posts_threads_parts

--- src/check_new_posts_threads_parts.rs
+++ src/check_new_posts_threads_parts.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
-use std::thread;
 
 use std::sync::{Arc, Mutex};
 
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::prints::print_type_enum::PrintType;
+use futures::future::join_all;
 
 use crate::fetch::info_structures::common_rss_structures::CommonRssPostStruct;
 use crate::fetch::rss_filter_fetched_and_parsed_posts::PostErrorVariant;
@@ -33,15 +31,15 @@ pub async fn check_new_posts_threads_parts(
     )>,
     ResourceError,
 > {
-    let mut threads_vec = Vec::with_capacity(providers_link_parts.len());
-    let posts_and_errors = Vec::with_capacity(providers_link_parts.len()); //todo: with_capacity
+    let mut tasks_vec = Vec::with_capacity(providers_link_parts.len());
+    let posts_and_errors = Vec::with_capacity(providers_link_parts.len());
     let posts_and_errors_arc_mutex = Arc::new(Mutex::new(posts_and_errors));
     //check if provider_names are unique
     for (provider_kind, link_parts) in providers_link_parts {
         if !link_parts.is_empty() {
             let posts_and_errors_handle_arc = Arc::clone(&posts_and_errors_arc_mutex);
             let vec_of_provider_links = provider_kind.generate_provider_links(link_parts);
-            threads_vec.push(thread::spawn(move || {
+            tasks_vec.push(tokio::task::spawn(async move {
                 providers_new_posts_check(
                     provider_kind,
                     vec_of_provider_links,
@@ -50,9 +48,7 @@ pub async fn check_new_posts_threads_parts(
             }));
         }
     }
-    for thread_vec in threads_vec {
-        thread_vec.join().unwrap();
-    }
+    let _ = join_all(tasks_vec).await;
     let posts_and_errors_to_return = posts_and_errors_arc_mutex
         .lock()
         .unwrap()
