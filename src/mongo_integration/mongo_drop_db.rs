9b3f890f7420ce4e46731bc9fd47300dbc1ba0db
kuqmua <serezhashmakov@outlook.com>
2022-01-06 20:18:16 +0300

    cleaning mongo_drop_db

--- src/mongo_integration/mongo_drop_db.rs
+++ src/mongo_integration/mongo_drop_db.rs
@@ -2,9 +2,9 @@ use mongodb::{options::ClientOptions, Client};
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_drop_db(mongo_url: &str, db_name: &str) -> Result<(), mongodb::error::Error> {
-    let client_options = ClientOptions::parse(mongo_url).await?;
-    let client = Client::with_options(client_options)?;
-    let db = client.database(db_name);
-    db.drop(None).await?;
+    Client::with_options(ClientOptions::parse(mongo_url).await?)?
+        .database(db_name)
+        .drop(None)
+        .await?;
     Ok(())
 }
