5d3525cb0d8bd017534f60677a11c8eb64d0ee67
kuqmua <serezhashmakov@outlook.com>
2022-01-06 21:36:12 +0300

    add change to ImplFromForUpperStruct - starting implement support not named like types

--- proc_macro_libs/impl_from_for_upper_struct/src/lib.rs
+++ proc_macro_libs/impl_from_for_upper_struct/src/lib.rs
@@ -1,6 +1,8 @@
 use proc_macro::TokenStream;
 use quote::quote;
+
 use syn;
+use syn::Ident;
 
 /// struct and enum names must be like this
 /// pub struct StructNameError {
@@ -22,6 +24,30 @@ pub fn derive_impl_from_for_upper_struct(input: TokenStream) -> TokenStream {
     let ident = &ast.ident;
     let generated = variants.into_iter().map(|v| {
         let variant = v.ident;
+        let inner_enum_type: &Ident;
+        match &v.fields {
+            syn::Fields::Unnamed(fields_unnamed) => {
+                if fields_unnamed.unnamed.len() != 1 {
+                    panic!(
+                        "fields_unnamed.unnamed != 1, length is {}",
+                        fields_unnamed.unnamed.len()
+                    );
+                }
+                match &fields_unnamed.unnamed[0].ty {
+                    syn::Type::Path(type_path) => {
+                        if type_path.path.segments.len() != 1 {
+                            panic!(
+                                "type_path.path.segments != 1, length is {}",
+                                type_path.path.segments.len()
+                            );
+                        }
+                        inner_enum_type = &type_path.path.segments[0].ident;
+                    }
+                    _ => panic!("fields_unnamed.unnamed[0].ty is not syn::Type::Path"),
+                }
+            }
+            _ => panic!("v.fields is not syn::Fields::Unnamed"),
+        }
         let string_ident = ident.to_string();
         let enum_handle = "Enum";
         match string_ident.find(enum_handle) {
@@ -32,8 +58,8 @@ pub fn derive_impl_from_for_upper_struct(input: TokenStream) -> TokenStream {
             Some(index) => {
                 let struct_ident = syn::Ident::new(&&string_ident[..index], ident.span());
                 quote! {
-                    impl From<#variant> for #struct_ident {
-                        fn from(error: #variant) -> Self {
+                    impl From<#inner_enum_type> for #struct_ident {
+                        fn from(error: #inner_enum_type) -> Self {
                             #struct_ident {
                                 source: Box::new(#ident::#variant(
                                     error,
