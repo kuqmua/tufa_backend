670bd2ecffd12c4212f281f6593d66b75e998fa3
kuqmua <serezhashmakov@outlook.com>
2022-01-06 19:48:28 +0300

    cleaning mongo_check_collection_is_empty

--- src/mongo_integration/mongo_check_collection_is_empty.rs
+++ src/mongo_integration/mongo_check_collection_is_empty.rs
@@ -4,19 +4,15 @@ use mongodb::{options::ClientOptions, Client};
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_check_collection_is_empty(
     mongo_url: &str,
-    db_name_handle: &str,
-    db_collection_handle: &str,
+    db_name: &str,
+    db_collection_name: &str,
 ) -> Result<bool, mongodb::error::Error> {
-    let client_options = ClientOptions::parse(mongo_url).await?;
-    let client = Client::with_options(client_options)?;
-    let db = client.database(db_name_handle); //declare db name. there is no create db method in mongo
-    let documents_number = db
-        .collection::<Document>(db_collection_handle)
-        .count_documents(None, None)
-        .await?;
-    if documents_number == 0 {
-        Ok(true)
-    } else {
-        Ok(false)
-    }
+    Ok(
+        Client::with_options(ClientOptions::parse(mongo_url).await?)?
+            .database(db_name) //declare db name. there is no create db method in mongo
+            .collection::<Document>(db_collection_name)
+            .count_documents(None, None)
+            .await?
+            == 0,
+    )
 }
