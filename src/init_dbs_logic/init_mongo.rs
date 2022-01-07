98aa97de5c1a38c0681b4af985a10e73eb076600
kuqmua <serezhashmakov@outlook.com>
2022-01-07 15:22:09 +0300

    little tweaks

--- src/init_dbs_logic/init_mongo.rs
+++ src/init_dbs_logic/init_mongo.rs
@@ -34,74 +34,64 @@ pub enum InitMongoErrorEnum {
 pub async fn init_mongo(
     providers_json_local_data_hashmap: HashMap<ProviderKind, Vec<String>>,
 ) -> Result<(), InitMongoError> {
-    let client_options_result = ClientOptions::parse(&mongo_get_db_url()).await;
-    match client_options_result {
+    let client_options = ClientOptions::parse(&mongo_get_db_url()).await?;
+    match Client::with_options(client_options) {
         Err(e) => Err(InitMongoError {
             source: Box::new(InitMongoErrorEnum::Client(e)),
         }),
-        Ok(client_options) => {
-            match Client::with_options(client_options) {
-                Err(e) => Err(InitMongoError {
-                    source: Box::new(InitMongoErrorEnum::Client(e)),
-                }),
-                Ok(client) => {
-                    let db = client.database(&CONFIG.mongo_providers_logs_db_name); //<- todo this is incorrect name
-                    let mut vec_of_futures =
-                        Vec::with_capacity(providers_json_local_data_hashmap.len());
-                    for pk in providers_json_local_data_hashmap.keys() {
-                        vec_of_futures.push(async {
-                            (
-                                *pk,
-                                db.collection::<Document>(&format!("{}", *pk))
-                                    .count_documents(None, None)
-                                    .await,
-                            )
-                        });
+        Ok(client) => {
+            let db = client.database(&CONFIG.mongo_providers_logs_db_name); //<- todo this is incorrect name
+            let mut vec_of_futures = Vec::with_capacity(providers_json_local_data_hashmap.len());
+            for pk in providers_json_local_data_hashmap.keys() {
+                vec_of_futures.push(async {
+                    (
+                        *pk,
+                        db.collection::<Document>(&format!("{}", *pk))
+                            .count_documents(None, None)
+                            .await,
+                    )
+                });
+            }
+            let result_vec = join_all(vec_of_futures).await;
+            for (pk, result) in result_vec {
+                match result {
+                    //todo filter
+                    Err(e) => {
+                        return Err(InitMongoError {
+                            source: Box::new(InitMongoErrorEnum::CollectionOperation((pk, e))),
+                        })
                     }
-                    let result_vec = join_all(vec_of_futures).await;
-                    for (pk, result) in result_vec {
-                        match result {
-                            //todo filter
-                            Err(e) => {
-                                return Err(InitMongoError {
-                                    source: Box::new(InitMongoErrorEnum::CollectionOperation((
-                                        pk, e,
-                                    ))),
-                                })
-                            }
-                            Ok(documents_number) => {
-                                if documents_number > 0 {
-                                    return Err(InitMongoError {
-                                        source: Box::new(InitMongoErrorEnum::CollectionIsNotEmpty(
-                                            (pk, documents_number),
-                                        )),
-                                    });
-                                }
-                            }
+                    Ok(documents_number) => {
+                        if documents_number > 0 {
+                            return Err(InitMongoError {
+                                source: Box::new(InitMongoErrorEnum::CollectionIsNotEmpty((
+                                    pk,
+                                    documents_number,
+                                ))),
+                            });
                         }
                     }
-                    let mut vec_of_futures =
-                        Vec::with_capacity(providers_json_local_data_hashmap.len());
-                    for (pk, data_vec) in &providers_json_local_data_hashmap {
-                        vec_of_futures.push(async {
+                }
+            }
+            let mut vec_of_futures = Vec::with_capacity(providers_json_local_data_hashmap.len());
+            for (pk, data_vec) in &providers_json_local_data_hashmap {
+                vec_of_futures.push(async {
                             let pk_cloned = *pk;
                             let collection = db.collection(&format!("{}", pk_cloned));
                             let docs: Vec<Document> = data_vec.iter().map(|data| doc! { &CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: data }).collect();
                             (pk_cloned, collection.insert_many(docs, None).await)
                         });
-                    }
-                    let result_vec = join_all(vec_of_futures).await;
-                    //todo: db partially initialized, print some warning
-                    for (pk, result) in result_vec {
-                        if let Err(e) = result {
-                            return Err(InitMongoError {
-                                source: Box::new(InitMongoErrorEnum::CollectionOperation((pk, e))),
-                            });
-                        }
-                    }
-                    Ok(())
+            }
+            let result_vec = join_all(vec_of_futures).await;
+            //todo: db partially initialized, print some warning
+            for (pk, result) in result_vec {
+                if let Err(e) = result {
+                    return Err(InitMongoError {
+                        source: Box::new(InitMongoErrorEnum::CollectionOperation((pk, e))),
+                    });
                 }
             }
+            Ok(())
         }
     }
 }
