e5f82bd88857b0528fcbfba2717f44a8678df98d
kuqmua <serezhashmakov@outlook.com>
2022-01-08 20:54:54 +0300

    few tables

--- src/postgres_integration/schemas/providers_link_parts_schema.rs
+++ src/postgres_integration/schemas/providers_link_parts_schema.rs
@@ -5,3 +5,59 @@ table! {
         link_part -> Text,
     }
 }
+
+table! {
+    arxiv_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    biorxiv_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    github_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    habr_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    medrxiv_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    reddit_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
+
+table! {
+    twitter_link_parts (id) {
+        id -> Integer,
+        provider_kind -> Text,
+        link_part -> Text,
+    }
+}
