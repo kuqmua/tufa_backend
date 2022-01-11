0688fa1567fcdeca7c504264f60ebb71bd089048
kuqmua <serezhashmakov@outlook.com>
2022-01-11 20:13:14 +0300

    add connect_timeout into postgres_check_availability

--- src/postgres_integration/postgres_check_availability.rs
+++ src/postgres_integration/postgres_check_availability.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::time::Duration;
 
 use sqlx::postgres::PgPoolOptions;
 use sqlx::Error;
@@ -15,6 +16,7 @@ pub async fn postgres_check_availability(
 ) -> Result<(), PostgresCheckAvailabilityError> {
     PgPoolOptions::new()
         .max_connections(1)
+        .connect_timeout(Duration::from_millis(1000))
         .connect(postgres_url).await?;
     Ok(())
 }
