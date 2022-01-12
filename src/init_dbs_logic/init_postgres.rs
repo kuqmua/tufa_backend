359d425ed87034c1b2e60afd390169388b7ba7ee
kuqmua <serezhashmakov@outlook.com>
2022-01-12 20:04:49 +0300

    init_postgres link parts insertion

--- src/init_dbs_logic/init_postgres.rs
+++ src/init_dbs_logic/init_postgres.rs
@@ -35,14 +35,17 @@ pub async fn init_postgres(
     // let mut tasks_vec = Vec::with_capacity(providers_json_local_data_hashmap.len());
     let tasks_vec = providers_json_local_data_hashmap.iter().map(|(pk, string_vec)|{
         async {
-            (*pk, sqlx::query("INSERT INTO providers_link_parts (link_part) VALUES ('test_link');").execute(&db).await)
+            let mut values_string = String::from("");
+            for link_part in string_vec.clone() {
+                values_string.push_str(&format!("('{}'),", link_part));
+            }
+            if !values_string.is_empty() {
+                values_string.pop();
+            }
+            let query_string = format!("INSERT INTO providers_link_parts (link_part) VALUES {};", values_string);
+            (*pk, sqlx::query(&query_string).execute(&db).await)
             }
     });
-    // for (pk, string_vec) in providers_json_local_data_hashmap {
-    //     tasks_vec.push(async {
-    //     (*pk, sqlx::query("INSERT INTO providers_link_parts (link_part) VALUES ('test_link');").execute(&db).await)
-    //     });
-    // }
     let result_vec = join_all(tasks_vec).await; //todo: add state of success/unsuccess
     let mut error_hashmap = HashMap::new();
     for (pk, result) in result_vec {
@@ -53,13 +56,5 @@ pub async fn init_postgres(
     if !error_hashmap.is_empty() {
         return Err(PostgresInitError { source: Box::new(PostgresInitErrorEnum::InsertQueries(error_hashmap))})
     }
-    // println!("sommething {:#?}", something);
-    // match todo_connection(&postgres_get_db_url()) {
-    //     Err(e) => Err(PostgresInitError::TodoError(e)),
-    //     Ok(pg_connection) => {
-    //         // Todo: all tables initialization as one transaction?
-    //         todo!()
-    //     }
-    // }
-    todo!()
+    Ok(())
 }   
