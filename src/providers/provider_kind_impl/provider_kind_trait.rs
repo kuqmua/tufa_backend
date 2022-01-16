5636da857b07dec91e8d979f5ea9a3517c7ddd61
kuqmua <serezhashmakov@outlook.com>
2022-01-16 17:20:12 +0300

    some tweaks

--- src/providers/provider_kind_impl/provider_kind_trait.rs
+++ src/providers/provider_kind_impl/provider_kind_trait.rs
@@ -121,18 +121,17 @@ impl ProviderKindTrait for ProviderKind {
     #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
     fn generate_hashmap_with_empty_string_vecs_for_enabled_providers(
     ) -> HashMap<ProviderKind, Vec<String>> {
-        let mut hashmap_with_empty_vecs = HashMap::<ProviderKind, Vec<String>>::with_capacity(
-            ProviderKind::get_enabled_providers_vec().len(),
-        );
-        for provider_kind in ProviderKind::get_enabled_providers_vec().iter() {
-            hashmap_with_empty_vecs.insert(*provider_kind, Vec::<String>::new());
-        }
-        hashmap_with_empty_vecs
+        ProviderKind::get_enabled_providers_vec()
+            .iter()
+            .map(|pk| (*pk, Vec::<String>::new()))
+            .collect()
     }
 
     #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
     fn get_enabled_providers_vec() -> Vec<ProviderKind> {
-        ProviderKind::iter().filter(|provider_kind| provider_kind.is_enabled()).collect()
+        ProviderKind::iter()
+            .filter(|provider_kind| provider_kind.is_enabled())
+            .collect()
     }
 
     #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
