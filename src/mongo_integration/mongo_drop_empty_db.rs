b64ce7a4b0cb527fb6b053886f1ad7b5a30c2501
kuqmua <serezhashmakov@outlook.com>
2022-01-19 21:55:27 +0300

    mongo_drop_empty_db error handling with box

--- src/mongo_integration/mongo_drop_empty_db.rs
+++ src/mongo_integration/mongo_drop_empty_db.rs
@@ -1,10 +1,20 @@
+use std::fmt;
+
+use mongodb::error::Error;
 use mongodb::{options::ClientOptions, Client};
+
+#[derive(thiserror::Error, displaydoc::Display, Debug, BoxErrFromErrDerive, ImplDisplayDerive)]
+pub struct MongoDropEmptyDbError {
+    /// mongo drop empty database error `{0}`
+    #[source]
+    source: Box<Error>,
+}
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_drop_empty_db(
     mongo_url: &str,
     db_name: &str,
-) -> Result<bool, mongodb::error::Error> {
+) -> Result<bool, MongoDropEmptyDbError> {
     let db = Client::with_options(ClientOptions::parse(mongo_url).await?)?.database(db_name);
     let collections_names_list = db.list_collection_names(None).await?;
     if !collections_names_list.is_empty() {
