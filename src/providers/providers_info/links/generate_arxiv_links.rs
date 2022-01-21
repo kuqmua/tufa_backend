0d68ec56c2f6fbd035fc5e43b8cd15ad3e923c36
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:24:36 +0300

    generate_arxiv_links functional way

--- src/providers/providers_info/links/generate_arxiv_links.rs
+++ src/providers/providers_info/links/generate_arxiv_links.rs
@@ -2,10 +2,5 @@ use crate::constants::project_constants::ARXIV_LINK_FIRST_PART;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_arxiv_links(arxiv_names: Vec<String>) -> Vec<String> {
-    //example http://export.arxiv.org/rss/astro-ph.CO
-    let mut arxiv_links: Vec<String> = Vec::with_capacity(arxiv_names.len());
-    for value in arxiv_names {
-        arxiv_links.push(format!("{}{}", ARXIV_LINK_FIRST_PART, value));
-    }
-    arxiv_links
+    arxiv_names.iter().map(|str| format!("{}{}", ARXIV_LINK_FIRST_PART, str)).collect()
 }
