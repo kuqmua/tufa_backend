6f19c0c640d34b83d98e19356e410690637dce89
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:16:23 +0300

    little tweaks

--- src/tests/continuous_integration/ci_check_compromised_env_vars.rs
+++ src/tests/continuous_integration/ci_check_compromised_env_vars.rs
@@ -7,10 +7,7 @@ use crate::config_mods::common_env_var_enum::CommonEnvVar;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 fn ci_check_compromised_env_vars() {
-    let mut was_dotenv_enable = false;
-    if dotenv().is_ok() {
-        was_dotenv_enable = true;
-    }
+    let was_dotenv_enable = dotenv().is_ok();
     for env_var_kind in CommonEnvVar::into_array().iter() {
         env_var_kind.check_compromised_env_vars(was_dotenv_enable);
     }
