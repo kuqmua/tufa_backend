d3c4389c42e8a5842a7256d90731ef3b6df9ae16
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:30:56 +0300

    some tweaks

--- src/providers/providers_info/links/generate_arxiv_links.rs
+++ src/providers/providers_info/links/generate_arxiv_links.rs
@@ -2,5 +2,6 @@ use crate::constants::project_constants::ARXIV_LINK_FIRST_PART;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_arxiv_links(arxiv_names: Vec<String>) -> Vec<String> {
-    arxiv_names.iter().map(|str| format!("{}{}", ARXIV_LINK_FIRST_PART, str)).collect()
+    //example http://export.arxiv.org/rss/astro-ph.CO
+    arxiv_names.iter().map(|name| format!("{}{}", ARXIV_LINK_FIRST_PART, name)).collect()
 }
--- src/providers/providers_info/links/generate_biorxiv_links.rs
+++ src/providers/providers_info/links/generate_biorxiv_links.rs
@@ -3,9 +3,5 @@ use crate::constants::project_constants::BIORXIV_LINK_FIRST_PART;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_biorxiv_links(biorxiv_names: Vec<String>) -> Vec<String> {
     //example http://connect.biorxiv.org/biorxiv_xml.php?subject=animal_behavior_and_cognition
-    let mut biorxiv_links: Vec<String> = Vec::with_capacity(biorxiv_names.len());
-    for value in biorxiv_names {
-        biorxiv_links.push(format!("{}{}", BIORXIV_LINK_FIRST_PART, value));
-    }
-    biorxiv_links
+    biorxiv_names.iter().map(|name| format!("{}{}", BIORXIV_LINK_FIRST_PART, name)).collect()
 }
