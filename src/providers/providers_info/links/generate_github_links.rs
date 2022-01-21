619ea84a30c562c94d734b057177869588b5754f
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:33:22 +0300

    generate_github_links functional way

--- src/providers/providers_info/links/generate_github_links.rs
+++ src/providers/providers_info/links/generate_github_links.rs
@@ -6,12 +6,10 @@ use crate::constants::project_constants::GITHUB_LINK_SECOND_PART;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_github_links(github_names: Vec<String>) -> Vec<String> {
     //https://github.com/kuqmua.private.atom?token=EXAMPLE_FROM_CONFIG
-    let mut github_links: Vec<String> = Vec::with_capacity(github_names.len());
-    for github_name in github_names {
-        github_links.push(format!(
+    github_names.iter().map(|name| 
+        format!(
             "{}{}{}{}",
-            GITHUB_LINK_FIRST_PART, github_name, GITHUB_LINK_SECOND_PART, CONFIG.github_token
-        ));
-    }
-    github_links
+            GITHUB_LINK_FIRST_PART, name, GITHUB_LINK_SECOND_PART, CONFIG.github_token
+        )
+    ).collect()
 }
