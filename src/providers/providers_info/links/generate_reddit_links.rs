18a90906f633381030d0e133c144fea49fd08330
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:38:18 +0300

    generate_reddit_links functional way

--- src/providers/providers_info/links/generate_reddit_links.rs
+++ src/providers/providers_info/links/generate_reddit_links.rs
@@ -4,12 +4,10 @@ use crate::constants::project_constants::REDDIT_LINK_SECOND_PART;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_reddit_links(subreddits_names: Vec<String>) -> Vec<String> {
     //example https://www.reddit.com/r/3Dprinting/new.json
-    let mut reddit_links: Vec<String> = Vec::with_capacity(subreddits_names.len());
-    for subreddit_name in subreddits_names {
-        reddit_links.push(format!(
+    subreddits_names.iter().map(|name| 
+        format!(
             "{}{}{}",
-            REDDIT_LINK_FIRST_PART, subreddit_name, REDDIT_LINK_SECOND_PART
-        ));
-    }
-    reddit_links
+            REDDIT_LINK_FIRST_PART, name, REDDIT_LINK_SECOND_PART
+        )
+    ).collect()
 }
