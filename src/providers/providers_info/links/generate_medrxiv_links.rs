7f9be43c3254a34d191368ad06b902247ebdaef2
kuqmua <serezhashmakov@outlook.com>
2022-01-21 21:36:17 +0300

    generate_medrxiv_links functional way

--- src/providers/providers_info/links/generate_medrxiv_links.rs
+++ src/providers/providers_info/links/generate_medrxiv_links.rs
@@ -3,9 +3,5 @@ use crate::constants::project_constants::MEDRXIV_LINK_FIRST_PART;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn generate_medrxiv_links(medrxiv_names: Vec<String>) -> Vec<String> {
     //example http://connect.medrxiv.org/medrxiv_xml.php?subject=Addiction_Medicine
-    let mut medrxiv_links: Vec<String> = Vec::with_capacity(medrxiv_names.len());
-    for value in medrxiv_names {
-        medrxiv_links.push(format!("{}{}", MEDRXIV_LINK_FIRST_PART, value));
-    }
-    medrxiv_links
+    medrxiv_names.iter().map(|name| format!("{}{}", MEDRXIV_LINK_FIRST_PART, name)).collect()
 }
