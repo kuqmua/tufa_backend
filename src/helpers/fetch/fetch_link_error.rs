97544d2863808f18bb890fe9a77de0a895dd5d1d
kuqmua <serezhashmakov@outlook.com>
2022-01-22 20:43:05 +0300

    little tweak

--- src/helpers/fetch/fetch_link_error.rs
+++ src/helpers/fetch/fetch_link_error.rs
@@ -1,13 +1,11 @@
 use std::fmt;
 
-use strum_macros::Display;
-
 #[derive(Debug, ImplDisplayDerive)]
 pub struct FetchLinkError {
     pub source: Box<FetchLinkErrorEnum>,
 }
 
-#[derive(Debug, Display)] //Debug only for prints
+#[derive(Debug)]
 pub enum FetchLinkErrorEnum {
     ReqwestBlockingGet(reqwest::Error),
     StatusCode(reqwest::StatusCode),
