0eb288295274c159acb929167a9b591ae3c47860
kuqmua <serezhashmakov@outlook.com>
2022-01-23 14:37:42 +0300

    mongo_insert_docs_in_empty_collection error handling with Box

--- src/mongo_integration/mongo_insert_docs_in_empty_collection.rs
+++ src/mongo_integration/mongo_insert_docs_in_empty_collection.rs
@@ -11,16 +11,38 @@ use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
 use crate::prints::print_colorful_message::print_colorful_message;
 use crate::prints::print_type_enum::PrintType;
 
-#[derive(Debug, Clone)]
-pub enum MongoInsertDocsInEmptyCollection {
-    MongoError(mongodb::error::Error),
-    NotEmpty(u64),
+#[derive(Debug)]
+pub struct MongoInsertDocsInEmptyCollectionError {
+    pub source: Box<MongoInsertDocsInEmptyCollectionErrorEnum>
 }
 
-impl From<mongodb::error::Error> for MongoInsertDocsInEmptyCollection {
-    fn from(e: mongodb::error::Error) -> Self {
-        MongoInsertDocsInEmptyCollection::MongoError(e)
-    }
+#[derive(Debug)]
+pub enum MongoInsertDocsInEmptyCollectionErrorEnum {
+    ClientOptionsParse(ClientOptionsParseError),
+    ClientWithOptions(ClientWithOptionsError),
+    CountDocuments(CountDocumentsError),
+    NotEmpty(u64),
+    CollectionInsertMany(CollectionInsertManyError),
+}
+
+#[derive(Debug)]
+pub struct ClientOptionsParseError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ClientWithOptionsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct CountDocumentsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct CollectionInsertManyError {
+    pub source: mongodb::error::Error,
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
@@ -28,12 +50,41 @@ pub async fn mongo_insert_docs_in_empty_collection(
     db_name_handle: &str,
     db_collection_handle: String,
     vec_of_values: Vec<String>,
-) -> Result<(), MongoInsertDocsInEmptyCollection> {
-    let client_options = ClientOptions::parse(&mongo_get_db_url()).await?;
-    let client = Client::with_options(client_options)?;
+) -> Result<(), MongoInsertDocsInEmptyCollectionError> {
+    //
+    let client_options: ClientOptions;
+    match ClientOptions::parse(mongo_get_db_url()).await {
+        Err(e) => {
+            return Err(MongoInsertDocsInEmptyCollectionError {
+                source: Box::new(MongoInsertDocsInEmptyCollectionErrorEnum::ClientOptionsParse(
+                    ClientOptionsParseError { source: e },
+                )),
+            })
+        }
+        Ok(cl) => client_options = cl,
+    }
+    let client: Client;
+    match Client::with_options(client_options) {
+        Err(e) => {
+            return Err(MongoInsertDocsInEmptyCollectionError {
+                source: Box::new(MongoInsertDocsInEmptyCollectionErrorEnum::ClientWithOptions(
+                    ClientWithOptionsError { source: e },
+                )),
+            })
+        }
+        Ok(c) => client = c,
+    }
     let db = client.database(db_name_handle);
     let collection = db.collection(&db_collection_handle);
-    let documents_number = collection.count_documents(None, None).await?;
+    let documents_number: u64;
+    match collection.count_documents(None, None).await {
+        Err(e) => return Err(MongoInsertDocsInEmptyCollectionError {
+            source: Box::new(MongoInsertDocsInEmptyCollectionErrorEnum::CountDocuments(
+                CountDocumentsError { source: e },
+            )),
+        }),
+        Ok(dn) => documents_number = dn,
+    }
     if documents_number > 0 {
         print_colorful_message(
             None,
@@ -42,13 +93,24 @@ pub async fn mongo_insert_docs_in_empty_collection(
             line!().to_string(),
             "collection is not empty, docs did not inserted".to_string(),
         );
-        Err(MongoInsertDocsInEmptyCollection::NotEmpty(documents_number))
+        Err(MongoInsertDocsInEmptyCollectionError {
+            source: Box::new(
+                MongoInsertDocsInEmptyCollectionErrorEnum::NotEmpty(documents_number)
+            )
+        }
+        )
     } else {
         let mut docs: Vec<Document> = Vec::with_capacity(vec_of_values.len());
         for value in &vec_of_values {
             docs.push(doc! { &CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: value });
         }
-        collection.insert_many(docs, None).await?;
+        if let Err(e) = collection.insert_many(docs, None).await {
+            return Err(MongoInsertDocsInEmptyCollectionError {
+                source: Box::new(MongoInsertDocsInEmptyCollectionErrorEnum::CollectionInsertMany(
+                    CollectionInsertManyError { source: e },
+                )),
+            });
+        }
         Ok(())
     }
 }
