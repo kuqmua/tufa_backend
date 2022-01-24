7708dbc167016ec5dfafc2e6055db2686699f8d4
kuqmua <serezhashmakov@outlook.com>
2022-01-24 21:20:18 +0300

    mongo_get_documents_as_string_vector refactoring

--- src/mongo_integration/mongo_get_documents_as_string_vector.rs
+++ src/mongo_integration/mongo_get_documents_as_string_vector.rs
@@ -10,9 +10,31 @@ pub struct MongoGetDocumentsAsStringVectorError {
 
 #[derive(Debug, ImplFromForUpperStruct)]
 pub enum MongoGetDocumentsAsStringVectorErrorEnum {
-    MongoError(mongodb::error::Error),
+    CollectionAggregate(CollectionAggregateError),
+    CursorTryNext(CursorTryNextError),
     WrongBsonType(mongodb::bson::Bson),
     NoKeyInDocument(String),
+}
+
+//not a good solution, rewrite later
+impl From<mongodb::error::Error> for MongoGetDocumentsAsStringVectorError {
+    fn from(e: mongodb::error::Error) -> Self {
+        MongoGetDocumentsAsStringVectorError { 
+            source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::CursorTryNext(CursorTryNextError{
+                source: e
+            })) 
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CollectionAggregateError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct CursorTryNextError {
+    pub source: mongodb::error::Error,
 }
 
 pub async fn mongo_get_documents_as_string_vector(
@@ -20,30 +42,41 @@ pub async fn mongo_get_documents_as_string_vector(
     db_collection_document_field_name_handle: &str,
     option_aggregation: Option<Document>,
 ) -> Result<Vec<String>, MongoGetDocumentsAsStringVectorError> {
-    let mut cursor = collection.aggregate(option_aggregation, None).await?;
-    let mut vec_of_strings: Vec<String> = Vec::new();
-    while let Some(document) = cursor.try_next().await? {
-        match document.get(db_collection_document_field_name_handle) {
-            None => {
-                return Err(MongoGetDocumentsAsStringVectorError {
-                    source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::NoKeyInDocument(
-                        db_collection_document_field_name_handle.to_string(),
-                    )),
-                })
+    match collection.aggregate(option_aggregation, None).await {
+        Err(e) => {
+            return Err(MongoGetDocumentsAsStringVectorError {
+                source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::CollectionAggregate(
+                    CollectionAggregateError { source: e }
+                )),
+            });
+        },
+        Ok(mut cursor) => {
+            let mut vec_of_strings: Vec<String> = Vec::new();
+            //dont know yet how to convert this expression into for explicit way
+            while let Some(document) = cursor.try_next().await? {
+                match document.get(db_collection_document_field_name_handle) {
+                    None => {
+                        return Err(MongoGetDocumentsAsStringVectorError {
+                            source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::NoKeyInDocument(
+                                db_collection_document_field_name_handle.to_string(),
+                            )),
+                        })
+                    }
+                    Some(bson_handle) => match bson_handle {
+                        mongodb::bson::Bson::String(value) => {
+                            vec_of_strings.push(value.to_string());
+                        }
+                        other_bson_type => {
+                            return Err(MongoGetDocumentsAsStringVectorError {
+                                source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::WrongBsonType(
+                                    other_bson_type.clone(),
+                                )),
+                            });
+                        }
+                    },
+                }
             }
-            Some(bson_handle) => match bson_handle {
-                mongodb::bson::Bson::String(value) => {
-                    vec_of_strings.push(value.to_string());
-                }
-                other_bson_type => {
-                    return Err(MongoGetDocumentsAsStringVectorError {
-                        source: Box::new(MongoGetDocumentsAsStringVectorErrorEnum::WrongBsonType(
-                            other_bson_type.clone(),
-                        )),
-                    });
-                }
-            },
-        }
+            Ok(vec_of_strings)
+        },
     }
-    Ok(vec_of_strings)
 }
