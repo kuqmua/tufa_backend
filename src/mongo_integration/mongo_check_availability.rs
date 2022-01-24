2ac44eeb5e1044f7aa95fce437b26dc89be6edeb
kuqmua <serezhashmakov@outlook.com>
2022-01-24 20:23:21 +0300

    mongo_check_availability refactoring

--- src/mongo_integration/mongo_check_availability.rs
+++ src/mongo_integration/mongo_check_availability.rs
@@ -1,22 +1,67 @@
-use mongodb::error::Error;
 use mongodb::{options::ClientOptions, Client};
-
-use std::fmt;
 
 use crate::config_mods::lazy_static_config::CONFIG;
 
-#[derive(thiserror::Error, displaydoc::Display, Debug, BoxErrFromErrDerive, ImplDisplayDerive)]
+#[derive(Debug)]
 pub struct MongoCheckAvailabilityError {
-    /// mongo check availability error `{0}`
-    #[source]
-    source: Box<Error>,
+    source: Box<MongoCheckAvailabilityErrorEnum>,
+}
+
+#[derive(Debug)]
+pub enum MongoCheckAvailabilityErrorEnum {
+    ClientOptionsParse(ClientOptionsParseError),
+    ClientWithOptions(ClientWithOptionsError),
+    ListCollectionNames(ListCollectionNamesError),
+}
+
+#[derive(Debug)]
+pub struct ClientOptionsParseError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ClientWithOptionsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ListCollectionNamesError {
+    pub source: mongodb::error::Error,
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_check_availability(mongo_url: &str) -> Result<(), MongoCheckAvailabilityError> {
-    Client::with_options(ClientOptions::parse(mongo_url).await?)?
-        .database(&CONFIG.mongo_providers_logs_db_name)
-        .list_collection_names(None)
-        .await?;
-    Ok(())
+    match ClientOptions::parse(mongo_url).await {
+        Err(e) => {
+            return Err(MongoCheckAvailabilityError {
+                source: Box::new(MongoCheckAvailabilityErrorEnum::ClientOptionsParse(
+                    ClientOptionsParseError { source: e },
+                )),
+            });
+        },
+        Ok(client_options) => {
+            match Client::with_options(client_options) {
+                Err(e) => {
+                    return Err(MongoCheckAvailabilityError {
+                        source: Box::new(MongoCheckAvailabilityErrorEnum::ClientWithOptions(
+                            ClientWithOptionsError { source: e },
+                        )),
+                    });
+                },
+                Ok(client) => {
+                    if let Err(e) = client.database(&CONFIG.mongo_providers_logs_db_name)
+                    .list_collection_names(None)
+                    .await {
+                        return Err(MongoCheckAvailabilityError {
+                            source: Box::new(MongoCheckAvailabilityErrorEnum::ListCollectionNames(
+                                ListCollectionNamesError { source: e },
+                            )),
+                        });
+                    }
+                    Ok(())
+                },
+            }
+        },
+    }
+    
 }
