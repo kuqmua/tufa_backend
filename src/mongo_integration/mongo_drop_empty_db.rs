867276987abc84410735ae8cef094a7ce9dbe5ae
kuqmua <serezhashmakov@outlook.com>
2022-01-24 21:53:40 +0300

    mongo_drop_empty_db error handling

--- src/mongo_integration/mongo_drop_empty_db.rs
+++ src/mongo_integration/mongo_drop_empty_db.rs
@@ -1,25 +1,94 @@
-use std::fmt;
-
-use mongodb::error::Error;
 use mongodb::{options::ClientOptions, Client};
 
-#[derive(thiserror::Error, displaydoc::Display, Debug, BoxErrFromErrDerive, ImplDisplayDerive)]
+#[derive(Debug)]
 pub struct MongoDropEmptyDbError {
-    /// mongo drop empty database error `{0}`
-    #[source]
-    source: Box<Error>,
+    source: Box<MongoDropEmptyDbErrorEnum>,
+}
+
+#[derive(Debug)]
+pub enum MongoDropEmptyDbErrorEnum {
+    ClientOptionsParse(ClientOptionsParseError),
+    ClientWithOptions(ClientWithOptionsError),
+    ListCollectionNames(ListCollectionNamesError),
+    CollectionNamesListIsEmpty(CollectionNamesListIsEmptyError),
+    DatabaseDrop(DatabaseDropError),
+}
+
+#[derive(Debug)]
+pub struct ClientOptionsParseError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ClientWithOptionsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct DatabaseDropError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ListCollectionNamesError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct CollectionNamesListIsEmptyError {
+    pub source: String,
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub async fn mongo_drop_empty_db(
     mongo_url: &str,
     db_name: &str,
-) -> Result<bool, MongoDropEmptyDbError> {
-    let db = Client::with_options(ClientOptions::parse(mongo_url).await?)?.database(db_name);
-    let collections_names_list = db.list_collection_names(None).await?;
-    if !collections_names_list.is_empty() {
-        return Ok(false);
+) -> Result<(), MongoDropEmptyDbError> {
+    match ClientOptions::parse(mongo_url).await {
+        Err(e) => {
+            return Err(MongoDropEmptyDbError {
+                source: Box::new(MongoDropEmptyDbErrorEnum::ClientOptionsParse(
+                    ClientOptionsParseError { source: e },
+                )),
+            })
+        }
+        Ok(client_options) => match Client::with_options(client_options) {
+            Err(e) => {
+                return Err(MongoDropEmptyDbError {
+                    source: Box::new(MongoDropEmptyDbErrorEnum::ClientWithOptions(
+                        ClientWithOptionsError { source: e },
+                    )),
+                })
+            }
+            Ok(client) => {
+                let db = client.database(db_name);
+                match db.list_collection_names(None).await {
+                    Err(e) => {
+                        return Err(MongoDropEmptyDbError {
+                            source: Box::new(MongoDropEmptyDbErrorEnum::ListCollectionNames(
+                                ListCollectionNamesError { source: e },
+                            )),
+                        })
+                    },
+                    Ok(collections_names_list) => {
+                        if !collections_names_list.is_empty() {
+                            return Err(MongoDropEmptyDbError {
+                                source: Box::new(MongoDropEmptyDbErrorEnum::CollectionNamesListIsEmpty(
+                                    CollectionNamesListIsEmptyError { source: db_name.to_string() },
+                                )),
+                            })
+                        }
+                        if let Err(e) =  db.drop(None).await {
+                            return Err(MongoDropEmptyDbError {
+                                source: Box::new(MongoDropEmptyDbErrorEnum::DatabaseDrop(DatabaseDropError {
+                                    source: e,
+                                })),
+                            })
+                        }
+                        Ok(())
+                    },
+                }
+            }
+        },
     }
-    db.drop(None).await?;
-    Ok(true)
 }
