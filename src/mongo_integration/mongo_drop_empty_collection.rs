1f3f5659a80704c1b2dbd077f04496227b2e07cf
kuqmua <serezhashmakov@outlook.com>
2022-01-25 19:39:50 +0300

    mongo_drop_empty_collection error handling

--- src/mongo_integration/mongo_drop_empty_collection.rs
+++ src/mongo_integration/mongo_drop_empty_collection.rs
@@ -1,15 +1,40 @@
+use mongodb::Collection;
 use mongodb::bson::Document;
 use mongodb::{options::ClientOptions, Client};
 
+//
 #[derive(Debug)]
-pub enum MongoDropEmptyCollectionError {
-    MongoError(mongodb::error::Error),
-    NotEmpty(u64),
+pub struct MongoDropEmptyCollectionError {
+    pub source: Box<MongoDropEmptyCollectionErrorEnum>,
 }
-impl From<mongodb::error::Error> for MongoDropEmptyCollectionError {
-    fn from(e: mongodb::error::Error) -> Self {
-        MongoDropEmptyCollectionError::MongoError(e)
-    }
+
+#[derive(Debug)]
+pub enum MongoDropEmptyCollectionErrorEnum {
+    ClientOptionsParse(ClientOptionsParseError),
+    ClientWithOptions(ClientWithOptionsError),
+    CountDocuments(CountDocumentsError),
+    NotEmpty(u64),
+    DatabaseDrop(DatabaseDropError),
+}
+
+#[derive(Debug)]
+pub struct ClientOptionsParseError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct ClientWithOptionsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct CountDocumentsError {
+    pub source: mongodb::error::Error,
+}
+
+#[derive(Debug)]
+pub struct DatabaseDropError {
+    pub source: mongodb::error::Error,
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
@@ -18,14 +43,63 @@ pub async fn mongo_drop_empty_collection(
     db_name: &str,
     db_collection_name: &str,
 ) -> Result<(), MongoDropEmptyCollectionError> {
-    let collection = Client::with_options(ClientOptions::parse(mongo_url).await?)?
-        .database(db_name)
-        .collection::<Document>(db_collection_name);
-    let documents_number = collection.count_documents(None, None).await?;
-    if documents_number > 0 {
-        Err(MongoDropEmptyCollectionError::NotEmpty(documents_number))
-    } else {
-        collection.drop(None).await?;
-        Ok(())
+    //
+    match ClientOptions::parse(mongo_url).await {
+        Err(e) => {
+            return Err(MongoDropEmptyCollectionError {
+                source: Box::new(
+                    MongoDropEmptyCollectionErrorEnum::ClientOptionsParse(
+                        ClientOptionsParseError { source: e },
+                    ),
+                ),
+            })
+        }
+        Ok(client_options) => match Client::with_options(client_options) {
+            Err(e) => {
+                return Err(MongoDropEmptyCollectionError {
+                    source: Box::new(
+                        MongoDropEmptyCollectionErrorEnum::ClientWithOptions(
+                            ClientWithOptionsError { source: e },
+                        ),
+                    ),
+                })
+            }
+            Ok(client) => {
+                let collection: Collection<Document> = client
+                    .database(db_name)
+                    .collection(&db_collection_name);
+                match collection.count_documents(None, None).await {
+                    Err(e) => {
+                        return Err(MongoDropEmptyCollectionError {
+                            source: Box::new(
+                                MongoDropEmptyCollectionErrorEnum::CountDocuments(
+                                    CountDocumentsError { source: e },
+                                ),
+                            ),
+                        })
+                    }
+                    Ok(documents_number) => {
+                        if documents_number > 0 {
+                            return Err(MongoDropEmptyCollectionError {
+                                source: Box::new(
+                                    MongoDropEmptyCollectionErrorEnum::NotEmpty(
+                                        documents_number,
+                                    ),
+                                ),
+                            });
+                        } else {
+                            if let Err(e) = collection.drop(None).await {
+                                return Err(MongoDropEmptyCollectionError {
+                                    source: Box::new(MongoDropEmptyCollectionErrorEnum::DatabaseDrop(
+                                        DatabaseDropError { source: e },
+                                    )),
+                                });
+                            }
+                            Ok(())
+                        }
+                    }
+                }
+            }
+        },
     }
 }
