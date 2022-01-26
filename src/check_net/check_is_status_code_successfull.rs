a47b1826ab8d63177ab35c48c79de2fa6af40479
kuqmua <serezhashmakov@outlook.com>
2022-01-26 20:31:14 +0300

    check_is_status_code_successfull tweaks

--- src/check_net/check_is_status_code_successfull.rs
+++ src/check_net/check_is_status_code_successfull.rs
@@ -1,20 +1,15 @@
 use reqwest::StatusCode;
-use std::fmt;
 
-#[derive(thiserror::Error, displaydoc::Display, Debug, ImplDisplayDerive)]
+#[derive(Debug)]
 pub struct StatusCodeError {
-    /// check status code error `{0}`
-    source: Box<StatusCodeWrapper>,
+    source: Box<StatusCode>,
 }
-
-#[derive(thiserror::Error, displaydoc::Display, Debug, ImplDisplayDerive)]
-pub struct StatusCodeWrapper(StatusCode);
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn check_is_status_code_successfull(status_code: StatusCode) -> Result<(), StatusCodeError> {
     if !StatusCode::is_success(&status_code) {
         return Err(StatusCodeError {
-            source: Box::new(StatusCodeWrapper(status_code)),
+            source: Box::new(status_code),
         });
     }
     Ok(())
