6c3ae3b6b8e7a31b0378d9378a621d8b31b220b4
kuqmua <serezhashmakov@outlook.com>
2022-01-27 23:04:50 +0300

    InitDbsError add line

--- src/init_dbs_logic/init_dbs.rs
+++ src/init_dbs_logic/init_dbs.rs
@@ -4,6 +4,7 @@ use super::init_dbs_with_providers_link_parts::InitDbsProvidersLinkPartsError;
 #[derive(Debug)]
 pub struct InitDbsError {
     pub source: Box<InitDbsErrorEnum>,
+    line: String
 }
 #[derive(Debug)]
 pub enum InitDbsErrorEnum {
@@ -15,6 +16,7 @@ pub async fn init_dbs() -> Result<(), InitDbsError> {
     match init_dbs_with_providers_link_parts().await {
         Err(e) => Err(InitDbsError {
             source: Box::new(InitDbsErrorEnum::InitDbsProvidersLinkParts(e)),
+            line: format!("{} {}", line!().to_string(), file!().to_string())
         }),
         Ok(_) => Ok(()),
     }
