f07e1d2c3b3c201dc60ea828509ee11cbe196111
kuqmua <serezhashmakov@outlook.com>
2022-01-27 23:25:49 +0300

    MongoDropDbError add line

--- src/mongo_integration/mongo_drop_db.rs
+++ src/mongo_integration/mongo_drop_db.rs
@@ -3,6 +3,7 @@ use mongodb::{options::ClientOptions, Client};
 #[derive(Debug)]
 pub struct MongoDropDbError {
     pub source: Box<MongoDropDbErrorEnum>,
+    line: String
 }
 
 #[derive(Debug)]
@@ -15,16 +16,19 @@ pub enum MongoDropDbErrorEnum {
 #[derive(Debug)]
 pub struct ClientOptionsParseError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct ClientWithOptionsError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct DatabaseDropError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
@@ -33,24 +37,27 @@ pub async fn mongo_drop_db(mongo_url: &str, db_name: &str) -> Result<(), MongoDr
         Err(e) => {
             return Err(MongoDropDbError {
                 source: Box::new(MongoDropDbErrorEnum::ClientOptionsParse(
-                    ClientOptionsParseError { source: e },
+                    ClientOptionsParseError { source: e,line: format!("{} {}", line!().to_string(), file!().to_string()) },
                 )),
+                line: format!("{} {}", line!().to_string(), file!().to_string())
             })
         }
         Ok(client_options) => match Client::with_options(client_options) {
             Err(e) => {
                 return Err(MongoDropDbError {
                     source: Box::new(MongoDropDbErrorEnum::ClientWithOptions(
-                        ClientWithOptionsError { source: e },
+                        ClientWithOptionsError { source: e,line: format!("{} {}", line!().to_string(), file!().to_string()) },
                     )),
+                    line: format!("{} {}", line!().to_string(), file!().to_string())
                 })
             }
             Ok(client) => {
                 if let Err(e) = client.database(db_name).drop(None).await {
                     return Err(MongoDropDbError {
                         source: Box::new(MongoDropDbErrorEnum::DatabaseDrop(DatabaseDropError {
-                            source: e,
+                            source: e, line: format!("{} {}", line!().to_string(), file!().to_string())
                         })),
+                        line: format!("{} {}", line!().to_string(), file!().to_string())
                     });
                 }
                 Ok(())
