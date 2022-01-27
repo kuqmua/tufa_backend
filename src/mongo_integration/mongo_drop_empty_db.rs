da7c1974e5321a3b1c16a867c25ec15aa7aa40ad
kuqmua <serezhashmakov@outlook.com>
2022-01-27 23:28:30 +0300

    MongoDropEmptyDbError add line

--- src/mongo_integration/mongo_drop_empty_db.rs
+++ src/mongo_integration/mongo_drop_empty_db.rs
@@ -3,6 +3,7 @@ use mongodb::{options::ClientOptions, Client};
 #[derive(Debug)]
 pub struct MongoDropEmptyDbError {
     source: Box<MongoDropEmptyDbErrorEnum>,
+    line: String
 }
 
 #[derive(Debug)]
@@ -17,26 +18,31 @@ pub enum MongoDropEmptyDbErrorEnum {
 #[derive(Debug)]
 pub struct ClientOptionsParseError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct ClientWithOptionsError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct ListCollectionNamesError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct CollectionNamesListIsEmptyError {
     pub source: String,
+    line: String
 }
 
 #[derive(Debug)]
 pub struct DatabaseDropError {
     pub source: mongodb::error::Error,
+    line: String
 }
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
@@ -48,16 +54,18 @@ pub async fn mongo_drop_empty_db(
         Err(e) => {
             return Err(MongoDropEmptyDbError {
                 source: Box::new(MongoDropEmptyDbErrorEnum::ClientOptionsParse(
-                    ClientOptionsParseError { source: e },
+                    ClientOptionsParseError { source: e,line: format!("{} {}", line!().to_string(), file!().to_string()) },
                 )),
+                line: format!("{} {}", line!().to_string(), file!().to_string())
             })
         }
         Ok(client_options) => match Client::with_options(client_options) {
             Err(e) => {
                 return Err(MongoDropEmptyDbError {
                     source: Box::new(MongoDropEmptyDbErrorEnum::ClientWithOptions(
-                        ClientWithOptionsError { source: e },
+                        ClientWithOptionsError { source: e, line: format!("{} {}", line!().to_string(), file!().to_string()) },
                     )),
+                    line: format!("{} {}", line!().to_string(), file!().to_string())
                 })
             }
             Ok(client) => {
@@ -66,8 +74,9 @@ pub async fn mongo_drop_empty_db(
                     Err(e) => {
                         return Err(MongoDropEmptyDbError {
                             source: Box::new(MongoDropEmptyDbErrorEnum::ListCollectionNames(
-                                ListCollectionNamesError { source: e },
+                                ListCollectionNamesError { source: e,line: format!("{} {}", line!().to_string(), file!().to_string()) },
                             )),
+                            line: format!("{} {}", line!().to_string(), file!().to_string())
                         })
                     }
                     Ok(collections_names_list) => {
@@ -77,16 +86,19 @@ pub async fn mongo_drop_empty_db(
                                     MongoDropEmptyDbErrorEnum::CollectionNamesListIsEmpty(
                                         CollectionNamesListIsEmptyError {
                                             source: db_name.to_string(),
+                                            line: format!("{} {}", line!().to_string(), file!().to_string())
                                         },
                                     ),
                                 ),
+                                line: format!("{} {}", line!().to_string(), file!().to_string())
                             });
                         }
                         if let Err(e) = db.drop(None).await {
                             return Err(MongoDropEmptyDbError {
                                 source: Box::new(MongoDropEmptyDbErrorEnum::DatabaseDrop(
-                                    DatabaseDropError { source: e },
+                                    DatabaseDropError { source: e, line: format!("{} {}", line!().to_string(), file!().to_string()) },
                                 )),
+                                line: format!("{} {}", line!().to_string(), file!().to_string())
                             });
                         }
                         Ok(())
