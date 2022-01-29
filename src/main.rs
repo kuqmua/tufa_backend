eac4ee2cef004612e8d617bc822f348c847b17cc
kuqmua <serezhashmakov@outlook.com>
2022-01-29 12:19:49 +0300

    git_info_trait

--- src/main.rs
+++ src/main.rs
@@ -164,6 +164,7 @@ mod tests {
 mod traits {
     pub mod enum_extention;
     pub mod env_var_typed_trait;
+    pub mod git_info_trait;
     pub mod provider_kind_from_config_trait;
     pub mod provider_kind_trait;
 }
