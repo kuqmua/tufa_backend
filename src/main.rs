eb47cf72b0e3c00fc2b52ba1cfb4c59f4597beac
kuqmua <serezhashmakov@outlook.com>
2022-01-30 18:00:49 +0300

    remove rss_filter_fetched_and_parsed_posts

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ mod constants {
 mod fetch {
     pub mod parse_github_html;
     pub mod rss_async_write_fetch_error_logs_into_files_wrapper;
-    pub mod rss_filter_fetched_and_parsed_posts;
     pub mod rss_handle_error_status_code;
     pub mod rss_metainfo_fetch_structures;
     pub mod rss_parse_string_into_struct;
