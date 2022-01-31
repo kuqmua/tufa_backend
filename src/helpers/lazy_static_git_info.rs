49397ae87c029f1e77207142b3e465342c9176be
kuqmua <serezhashmakov@outlook.com>
2022-01-31 20:34:34 +0300

    get_git_info rewritten. GitCommitInfo as return type

--- src/helpers/lazy_static_git_info.rs
+++ src/helpers/lazy_static_git_info.rs
@@ -1,5 +1,5 @@
-use crate::helpers::get_git_info::get_git_info;
+use crate::helpers::get_git_info::{get_git_info, GitCommitInfo};
 
 lazy_static! {
-    pub static ref GIT_INFO: String = get_git_info();
+    pub static ref GIT_INFO: GitCommitInfo = get_git_info();
 }
