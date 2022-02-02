b6dde9dfc6f9066c40b8e8ff5679a054d57c735b
kuqmua <serezhashmakov@outlook.com>
2022-02-02 18:09:23 +0300

    little tweaks

--- src/helpers/fetch/blocking_fetch_link.rs
+++ src/helpers/fetch/blocking_fetch_link.rs
@@ -4,14 +4,12 @@ use crate::helpers::fetch::fetch_link_error::FetchLinkErrorEnum;
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 pub fn blocking_fetch_link(link: &str) -> Result<String, FetchLinkError> {
     match reqwest::blocking::get(link) {
-        Err(e) => {
-            return Err(FetchLinkError {
-                source: Box::new(FetchLinkErrorEnum::ReqwestBlockingGet(e)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            })
-        }
+        Err(e) => Err(FetchLinkError {
+            source: Box::new(FetchLinkErrorEnum::ReqwestBlockingGet(e)),
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        }),
         Ok(res) => {
             if res.status() != reqwest::StatusCode::OK {
                 return Err(FetchLinkError {
@@ -22,14 +20,12 @@ pub fn blocking_fetch_link(link: &str) -> Result<String, FetchLinkError> {
                 });
             }
             match res.text() {
-                Err(e) => {
-                    return Err(FetchLinkError {
-                        source: Box::new(FetchLinkErrorEnum::ResponseText(e)),
-                        file: file!(),
-                        line: line!(),
-                        column: column!(),
-                    })
-                }
+                Err(e) => Err(FetchLinkError {
+                    source: Box::new(FetchLinkErrorEnum::ResponseText(e)),
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                }),
                 Ok(text) => Ok(text),
             }
         }
