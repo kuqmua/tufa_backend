be617258914e1034b3c736892b9a2f4999dc1ef1
kuqmua <serezhashmakov@outlook.com>
2022-02-03 19:33:00 +0300

    drop_mongo_provider_logs_collection_if_need rewritten

--- src/logs_logic/drop_mongo_provider_logs_collection_if_need.rs
+++ src/logs_logic/drop_mongo_provider_logs_collection_if_need.rs
@@ -10,35 +10,32 @@ use crate::config_mods::lazy_static_config::CONFIG;
 use crate::traits::provider_kind_from_config_trait::ProviderKindFromConfigTrait;
 
 pub async fn drop_mongo_provider_logs_collection_if_need(
-    pk: ProviderKind,
+    pk: &ProviderKind,
     mongo_url: String,
-) -> (ProviderKind, Result<(), MongoDropEmptyCollectionError>) {
-    if pk.is_cleaning_warning_logs_directory_enabled() {
-        let db_collection_name = &format!(
-            "{:#?}{}",
-            pk, &CONFIG.mongo_providers_logs_db_collection_handle_second_part
-        );
-        //using different (old) tokio runtime 0.2.25
-        let future_possible_drop_collection = mongo_drop_empty_collection(
-            &mongo_url,
-            &CONFIG.mongo_providers_logs_db_name,
-            db_collection_name,
-        )
-        .await;
-        match future_possible_drop_collection {
-            Ok(()) => (pk, Ok(())),
-            Err(e) => {
-                print_colorful_message(
-                    Some(&pk),
-                    PrintType::WarningHigh,
-                    file!().to_string(),
-                    line!().to_string(),
-                    format!("drop fail with error {:#?}", e),
-                );
-                (pk, Err(e))
-            }
-        }
-    } else {
-        (pk, Ok(()))
+) -> Result<(), MongoDropEmptyCollectionError> {
+    if !pk.is_cleaning_warning_logs_directory_enabled() {
+        return Ok(());
     }
+    let db_collection_name = &format!(
+        "{:#?}{}",
+        pk, &CONFIG.mongo_providers_logs_db_collection_handle_second_part
+    );
+    //using different (old) tokio runtime 0.2.25
+    let future_possible_drop_collection = mongo_drop_empty_collection(
+        &mongo_url,
+        &CONFIG.mongo_providers_logs_db_name,
+        db_collection_name,
+    )
+    .await;
+    if let Err(e) = future_possible_drop_collection {
+        print_colorful_message(
+            Some(&pk),
+            PrintType::WarningHigh,
+            file!().to_string(),
+            line!().to_string(),
+            format!("drop fail with error {:#?}", e),
+        );
+        return Err(e);
+    }
+    Ok(())
 }
