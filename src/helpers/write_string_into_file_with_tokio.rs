4d6c2dd80b4997a218a3cd86747ec5ee540ea117
kuqmua <serezhashmakov@outlook.com>
2022-02-03 21:31:13 +0300

    remove file line column from WriteStringIntoFileWithTokioError

--- src/helpers/write_string_into_file_with_tokio.rs
+++ src/helpers/write_string_into_file_with_tokio.rs
@@ -8,35 +8,26 @@ pub struct WriteStringIntoFileWithTokioError {
     pub source: Box<WriteStringIntoFileWithTokioErrorEnum>,
 }
 
-#[derive(thiserror::Error, Debug, ImplDisplayDerive)] //, ImplFromForUpperStruct
+#[derive(thiserror::Error, Debug, ImplDisplayDerive)]
 pub enum WriteStringIntoFileWithTokioErrorEnum {
-    StdFsCreateDirAll(StdFsCreateDirAllStruct),
-    TokioFsFileOpen(TokioFsFileOpenStruct),
-    FileWriteAll(FileWriteAllStruct),
-}
-
-#[derive(Debug)]
-pub struct StdFsCreateDirAllStruct {
-    pub source: std::io::Error,
-    pub file: &'static str,
-    pub line: u32,
-    pub column: u32,
-}
-
-#[derive(Debug)]
-pub struct TokioFsFileOpenStruct {
-    pub source: std::io::Error,
-    pub file: &'static str,
-    pub line: u32,
-    pub column: u32,
-}
-
-#[derive(Debug)]
-pub struct FileWriteAllStruct {
-    pub source: std::io::Error,
-    pub file: &'static str,
-    pub line: u32,
-    pub column: u32,
+    StdFsCreateDirAll{
+        source: std::io::Error,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    TokioFsFileOpen {
+        source: std::io::Error,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    FileWriteAll {
+        source: std::io::Error,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
 }
 
 pub async fn write_string_into_file_with_tokio(
@@ -46,39 +37,36 @@ pub async fn write_string_into_file_with_tokio(
     if let Some(prefix) = path.parent() {
         if let Err(e) = std::fs::create_dir_all(prefix) {
             return Err(WriteStringIntoFileWithTokioError {
-                source: Box::new(WriteStringIntoFileWithTokioErrorEnum::StdFsCreateDirAll(
-                    StdFsCreateDirAllStruct {
+                source: Box::new(WriteStringIntoFileWithTokioErrorEnum::StdFsCreateDirAll {
                         source: e,
                         file: file!(),
                         line: line!(),
                         column: column!(),
                     },
-                )),
+                ),
             });
         }
     }
     match tokio::fs::File::open(path).await {
         Err(e) => Err(WriteStringIntoFileWithTokioError {
-            source: Box::new(WriteStringIntoFileWithTokioErrorEnum::TokioFsFileOpen(
-                TokioFsFileOpenStruct {
+            source: Box::new(WriteStringIntoFileWithTokioErrorEnum::TokioFsFileOpen {
                     source: e,
                     file: file!(),
                     line: line!(),
                     column: column!(),
                 },
-            )),
+            ),
         }),
         Ok(mut file) => {
             if let Err(e) = file.write_all(stringified_json.as_bytes()).await {
                 return Err(WriteStringIntoFileWithTokioError {
-                    source: Box::new(WriteStringIntoFileWithTokioErrorEnum::FileWriteAll(
-                        FileWriteAllStruct {
+                    source: Box::new(WriteStringIntoFileWithTokioErrorEnum::FileWriteAll {
                             source: e,
                             file: file!(),
                             line: line!(),
                             column: column!(),
                         },
-                    )),
+                    ),
                 });
             }
             Ok(())
