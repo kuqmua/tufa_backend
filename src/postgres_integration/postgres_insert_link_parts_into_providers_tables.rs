3e9780e3a42f56b6873ef192610c8392472d4c08
kuqmua <serezhashmakov@outlook.com>
2022-02-03 19:28:45 +0300

    little tweaks

--- src/postgres_integration/postgres_insert_link_parts_into_providers_tables.rs
+++ src/postgres_integration/postgres_insert_link_parts_into_providers_tables.rs
@@ -19,25 +19,23 @@ pub async fn postgres_insert_link_parts_into_providers_tables(
     providers_json_local_data_hashmap: &HashMap<ProviderKind, Vec<String>>,
     pool: &Pool<Postgres>,
 ) -> Result<(), PostgresInsertLinkPartsIntoProvidersTablesError> {
-    let insertion_tasks_vec =
-        providers_json_local_data_hashmap
-            .iter()
-            .map(|(pk, string_vec)| async {
-                let mut values_string = String::from("");
-                for link_part in string_vec.clone() {
-                    values_string.push_str(&format!("('{}'),", link_part));
-                }
-                if !values_string.is_empty() {
-                    values_string.pop();
-                }
-                let query_string = format!(
-                    "INSERT INTO {} (link_part) VALUES {};",
-                    pk.get_postgres_table_name(),
-                    values_string
-                );
-                (*pk, sqlx::query(&query_string).execute(pool).await)
-            });
-    let insertion_error_hashmap = join_all(insertion_tasks_vec)
+    let insertion_error_hashmap = join_all(providers_json_local_data_hashmap
+        .iter()
+        .map(|(pk, string_vec)| async {
+            let mut values_string = String::from("");
+            for link_part in string_vec.clone() {
+                values_string.push_str(&format!("('{}'),", link_part));
+            }
+            if !values_string.is_empty() {
+                values_string.pop();
+            }
+            let query_string = format!(
+                "INSERT INTO {} (link_part) VALUES {};",
+                pk.get_postgres_table_name(),
+                values_string
+            );
+            (*pk, sqlx::query(&query_string).execute(pool).await)
+        }))
         .await
         .into_iter()
         .filter_map(|(pk, result)| {
