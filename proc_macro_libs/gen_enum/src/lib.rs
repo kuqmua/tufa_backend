6313367aca557b8e21d4bef775a51dae37a8bb28
kuqmua <serezhashmakov@outlook.com>
2022-02-04 22:35:03 +0300

    gen enum enum_variant_ident

--- proc_macro_libs/gen_enum/src/lib.rs
+++ proc_macro_libs/gen_enum/src/lib.rs
@@ -2,17 +2,32 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn;
 
+use convert_case::Case;
+use convert_case::Casing;
+
 #[proc_macro_derive(GenEnum)]
 pub fn derive_gen_enum(input: TokenStream) -> TokenStream {
     let ast: syn::DeriveInput =
         syn::parse(input).expect("derive_gen_enum syn::parse(input) failed");
-    let len = match ast.data.clone() {
-        syn::Data::Struct(_) => {
-            // println!("ast {:#?}", ast);
+    let ident = &ast.ident;
+    let len = match ast.data {
+        syn::Data::Struct(datastruct) => {
+            for field in datastruct.fields {
+                match field.ident {
+                    None => panic!("field.ident is None"),
+                    Some(field_ident) => {
+                        let enum_variant_ident = syn::Ident::new(
+                            &format!("{}", field_ident).to_case(Case::Pascal),
+                            ident.span(),
+                        );
+                        println!("enum_variant_ident {:#?}", enum_variant_ident);
+                    }
+                }
+            }
         }
         _ => panic!("GenEnum only works on Struct"),
     };
-    let ident = &ast.ident;
+
     println!("/////");
     println!("{}", ident);
     println!("/////");
