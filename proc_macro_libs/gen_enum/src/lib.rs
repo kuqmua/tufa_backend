b4b453982fb4ea590a0aba2cfaf5ea069f158e49
kuqmua <serezhashmakov@outlook.com>
2022-02-04 22:05:11 +0300

    starting with derive_gen_enum

--- proc_macro_libs/gen_enum/src/lib.rs
+++ proc_macro_libs/gen_enum/src/lib.rs
@@ -1,8 +1,35 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(GenEnum)]
+pub fn derive_gen_enum(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("derive_gen_enum syn::parse(input) failed");
+    let len = match ast.data.clone() {
+        syn::Data::Struct(_) => {
+            // println!("ast {:#?}", ast);
+        }
+        _ => panic!("GenEnum only works on Struct"),
+    };
+    let ident = &ast.ident;
+    println!("/////");
+    println!("{}", ident);
+    println!("/////");
+    let enum_ident = syn::Ident::new(&format!("{}Enum", ident), ident.span());
+    println!("{}", enum_ident);
+    // #[derive(Debug)]
+    let gen = quote! {
+        pub enum #enum_ident {
+            One,
+            Two
+        }
+    };
+    gen.into()
 }
+
+// impl EnumExtenstion for #name {
+//     fn get_length() -> usize {
+//         #len
+//     }
+// }
