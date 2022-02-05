b58429e060ed93f72144e4bf72c0da07e1d5cb3b
kuqmua <serezhashmakov@outlook.com>
2022-02-05 20:16:39 +0300

    little cleaning

--- proc_macro_libs/init_from_env/src/lib.rs
+++ proc_macro_libs/init_from_env/src/lib.rs
@@ -36,80 +36,71 @@ pub fn derive_init_from_env(input: TokenStream) -> TokenStream {
         _ => panic!("InitFromEnv only works on Struct"),
     };
     let generated_functions = match ast.data.clone() {
-        syn::Data::Struct(datastruct) => {
-            let generated = datastruct.fields.into_iter().map(|field| {
-                let enum_variant_ident_pascal_case: Ident;
-                let enum_variant_ident_snake_case: Ident;
-                match field.ident.clone() {
-                    None => panic!("field.ident is None"),
-                    Some(field_ident) => {
-                        enum_variant_ident_pascal_case = syn::Ident::new(
-                            &format!("{}", field_ident).to_case(Case::Pascal),
-                            ident.span(),
-                        );
-                        enum_variant_ident_snake_case =
-                            syn::Ident::new(&format!("{}", field_ident), ident.span());
-                    }
-                };
-                let enum_variant_type: Path;
-                let enum_variant_type_as_string: LitStr;
-                match field.ty.clone() {
-                    syn::Type::Path(type_path) => {
-                        enum_variant_type = type_path.path.clone();
-                        //todo: remove :? later
-                        enum_variant_type_as_string = syn::LitStr::new(&format!("{:?}", type_path), ident.span());
+        syn::Data::Struct(datastruct) => datastruct.fields.into_iter().map(|field| {
+            let enum_variant_ident_pascal_case: Ident;
+            let enum_variant_ident_value: Ident;
+            let env_var_name: Ident;
+            let env_var_name_as_snake_case_string: LitStr;
+            match field.ident.clone() {
+                None => panic!("field.ident is None"),
+                Some(field_ident) => {
+                    enum_variant_ident_pascal_case = syn::Ident::new(
+                        &format!("{}", field_ident).to_case(Case::Pascal),
+                        ident.span(),
+                    );
+                    enum_variant_ident_value =
+                        syn::Ident::new(&format!("{}_value", field_ident), ident.span());
+                    env_var_name = syn::Ident::new(
+                        &format!("{}", field_ident)
+                            .to_case(Case::Snake)
+                            .to_uppercase(),
+                        ident.span(),
+                    );
+                    env_var_name_as_snake_case_string =
+                        syn::LitStr::new(&format!("{}", field_ident), ident.span());
+                }
+            };
+            let enum_variant_type: Path;
+            let enum_variant_type_as_string: LitStr;
+            match field.ty.clone() {
+                syn::Type::Path(type_path) => {
+                    enum_variant_type = type_path.path.clone();
+                    //todo: remove :? later
+                    enum_variant_type_as_string =
+                        syn::LitStr::new(&format!("{:?}", type_path), ident.span());
+                }
+                _ => panic!("field.ty is not a syn::Type::Path!"),
+            };
+            let env_var_name_as_screaming_snake_case_string =
+                syn::LitStr::new(&format!("{}", env_var_name), ident.span());
+            quote! {
+                let #enum_variant_ident_value: #enum_variant_type;
+                match std::env::var(#env_var_name_as_screaming_snake_case_string) {
+                    Ok(string_handle) => {
+                        match string_handle.parse::<#enum_variant_type>() {
+                            Err(e) => {
+                                return Err(
+                                    #error_ident {
+                                        source: Box::new(
+                                            #error_enum_ident::#enum_variant_ident_pascal_case {
+                                                env_var_name: #env_var_name_as_snake_case_string,
+                                                expected_env_var_type: #enum_variant_type_as_string,
+                                                file: "test",
+                                                line: 32,
+                                                column: 32,
+                                            }
+                                        ),
+                                        was_dotenv_enable,
+                                    }
+                                );
+                            },
+                            Ok(handle) => {
+                                #enum_variant_ident_value = handle;
+                            },
+                        }
                     },
-                    _ => panic!("field.ty is not a syn::Type::Path!"),
-                };
-                let enum_variant_ident_value = syn::Ident::new(
-                    &format!("{}_value", enum_variant_ident_snake_case),
-                    ident.span(),
-                );
-                let env_var_name: Ident;
-                let env_var_name_as_snake_case_string: LitStr;
-                match field.ident {
-                    None => panic!("field.ident is None"),
-                    Some(field_ident) => {
-                        env_var_name = syn::Ident::new(
-                            &format!("{}", field_ident)
-                                .to_case(Case::Snake)
-                                .to_uppercase(),
-                            ident.span(),
-                        );
-                        env_var_name_as_snake_case_string =
-                            syn::LitStr::new(&format!("{}", field_ident), ident.span());
-                    }
-                };
-                let env_var_name_as_screaming_snake_case_string =
-                    syn::LitStr::new(&format!("{}", env_var_name), ident.span());
-                quote! {
-                    let #enum_variant_ident_value: #enum_variant_type;
-                    match std::env::var(#env_var_name_as_screaming_snake_case_string) {
-                        Ok(string_handle) => {
-                            match string_handle.parse::<#enum_variant_type>() {
-                                Err(e) => {
-                                    return Err(
-                                        #error_ident {
-                                            source: Box::new(
-                                                #error_enum_ident::#enum_variant_ident_pascal_case {
-                                                    env_var_name: #env_var_name_as_snake_case_string,
-                                                    expected_env_var_type: #enum_variant_type_as_string,
-                                                    file: "test",
-                                                    line: 32,
-                                                    column: 32,
-                                                }
-                                            ),
-                                            was_dotenv_enable,
-                                        }
-                                    );
-                                },
-                                Ok(handle) => {
-                                    #enum_variant_ident_value = handle;
-                                },
-                            }
-                        },
-                        Err(e) => {
-                            return Err(
+                    Err(e) => {
+                        return Err(
                             #error_ident {
                                 source: Box::new(
                                     #error_enum_ident::#enum_variant_ident_pascal_case {
@@ -122,12 +113,11 @@ pub fn derive_init_from_env(input: TokenStream) -> TokenStream {
                                 ),
                                 was_dotenv_enable,
                             }
-                        );},
-                    }
+                        );
+                    },
                 }
-            });
-            generated
-        }
+            }
+        }),
         _ => panic!("GenEnum only works on Struct"),
     };
     let generated_enum_error_variants = match ast.data {
