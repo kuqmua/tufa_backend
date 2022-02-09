e27c97a24e55def52adcb28f2f15144750f54456
kuqmua <serezhashmakov@outlook.com>
2022-02-09 21:22:46 +0300

    ConfigStruct::new().wrap_config_checks()

--- src/config_mods/lazy_static_config.rs
+++ src/config_mods/lazy_static_config.rs
@@ -1,8 +1,8 @@
+use crate::traits::wrap_config_checks_trait::WrapConfigChecks;
 use crate::config_mods::config_struct::ConfigStruct;
 use crate::constants::project_constants::LOAD_CONFIG_FILE_ERROR_MESSAGE;
 
 lazy_static! {
     pub static ref CONFIG: ConfigStruct =
-        ConfigStruct::new().expect(LOAD_CONFIG_FILE_ERROR_MESSAGE);
-        //todo: add wrap_config_checks(handle_config)
+        ConfigStruct::new().expect(LOAD_CONFIG_FILE_ERROR_MESSAGE).wrap_config_checks().expect("wrap_config_checks error");
 }
