84791b6d05684734e39e455a5b1ab041a1180071
kuqmua <serezhashmakov@outlook.com>
2022-02-09 20:47:11 +0300

    rewrite ci_check_compromised_env_vars

--- src/tests/continuous_integration/ci_check_compromised_env_vars.rs
+++ src/tests/continuous_integration/ci_check_compromised_env_vars.rs
@@ -1,14 +1,9 @@
-use dotenv::dotenv;
-
-use crate::traits::enum_extention::EnumExtenstion;
-
-use crate::config_mods::common_env_var_enum::CommonEnvVar;
+use crate::config_mods::config_struct::ConfigStruct;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
 #[test]
 fn ci_check_compromised_env_vars() {
-    let was_dotenv_enable = dotenv().is_ok();
-    for env_var_kind in CommonEnvVar::into_array().iter() {
-        env_var_kind.check_compromised_env_vars(was_dotenv_enable);
-    }
+    let config_from_env = ConfigStruct::new().expect("cannot create ConfigStruct::new()");
+    let default_config = ConfigStruct::default();
+    assert_eq!(config_from_env, default_config)
 }
