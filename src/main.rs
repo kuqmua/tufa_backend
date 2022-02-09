87669cdc29e191ccab6c1d76b23a85fce97206c5
kuqmua <serezhashmakov@outlook.com>
2022-02-09 20:48:52 +0300

    remove common_env_var_enum

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,6 @@ pub mod config_mods {
         pub mod check_valid_i64_providers_links_limits_for_mongo;
         // pub mod wrap_config_checks;//stay
     }
-    // pub mod common_env_var_enum;//stay
     pub mod config_struct;
     pub mod lazy_static_config;
 }
