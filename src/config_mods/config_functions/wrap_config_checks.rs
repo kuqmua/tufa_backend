3f0c3dcb4c7437234311ca6308dbefd7f0eb2a7c
kuqmua <serezhashmakov@outlook.com>
2022-02-10 21:22:29 +0300

    wrap_config_checks impl

--- src/config_mods/config_functions/wrap_config_checks.rs
+++ src/config_mods/config_functions/wrap_config_checks.rs
@@ -11,9 +11,98 @@ pub struct WrapConfigChecksError {
 
 #[derive(Debug)]
 pub enum WrapConfigChecksErrorEnum {
-    //todo
-    Somthing {
-        source: bool,//temp
+    GithubName {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    GithubToken {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    RedditUserAgent {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    RedditClientId {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    RedditClientSecret {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    RedditUsername {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    RedditPassword {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    MongoLogin {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    MongoPassword {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    MongoIp {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    MongoPort {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    LogFileExtension {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    PathToProviderLinkPartsFolder {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    ProvidersDbCollectionDocumentFieldName {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    WarningLogsDirectoryName {
+        source: String,
+        file: &'static str,
+        line: u32,
+        column: u32,
+    },
+    LinksLimitProviderse {
+        source: i64,
         file: &'static str,
         line: u32,
         column: u32,
@@ -22,198 +111,195 @@ pub enum WrapConfigChecksErrorEnum {
 
 impl WrapConfigChecks for ConfigStruct {
     fn wrap_config_checks(self) -> Result<Self, WrapConfigChecksError> {
-        // if !config.github_authorization.github_name.is_empty() {
-        //     let error: Result<ConfigStruct, ConfigError> =
-        //         Err(ConfigError::Message("github_name is not valid".to_string()));
-        //     drop(error);
-        // }
-        // if !config.github_authorization.github_token.is_empty() {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "github_token is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .reddit_authorization
-        //     .reddit_user_agent
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "reddit_user_agent is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .reddit_authorization
-        //     .reddit_client_id
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "reddit_client_id is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .reddit_authorization
-        //     .reddit_client_secret
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "reddit_client_secret is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .reddit_authorization
-        //     .reddit_username
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "reddit_username is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .reddit_authorization
-        //     .reddit_password
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "reddit_password is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        //     .mongo_params.mongo_authorization
-        //     .mongo_login
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "mongo_login is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        // .mongo_params
-        //     .mongo_authorization
-        //     .mongo_password
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "mongo_password is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        // .mongo_params
-        //     .mongo_authorization
-        //     .mongo_ip
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "mongo_ip is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        // .mongo_params
-        //     .mongo_authorization
-        //     .mongo_port
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "mongo_port is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !config
-        // .mongo_params
-        //     .mongo_authorization
-        //     .mongo_params
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "mongo_params is not valid".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // //
-        // if config.mongo_params.log_file_extension.is_empty() {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "log_file_extension is not empty".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config
-        //     .mongo_params
-        //     .path_to_provider_link_parts_folder
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "path_to_provider_link_parts_folder is empty"
-        //             .to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config
-        //     .mongo_params
-        //     .providers_db_collection_document_field_name
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "db_collection_document_field_name is empty"
-        //             .to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config
-        //     .mongo_params
-        //     .providers_db_collection_second_part
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "db_collection_second_part is empty"
-        //             .to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config
-        //     .mongo_params
-        //     .providers_db_name
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "db_name is empty".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config
-        //     .params
-        //     .unhandled_successd_success_are_there_items_initialized_posts_dir
-        //     .is_empty()
-        // {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //             "unhandled_successd_success_are_there_items_initialized_posts_dir is empty".to_string(),
-        //         ));
-        //     drop(error);
-        // }
-        // if config.params.warning_logs_directory_name.is_empty() {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "warning_logs_directory_name is empty".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if config.params.links_limit_providers > 0 {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "links_limit_providers <= 0".to_string(),
-        //     ));
-        //     drop(error);
-        // }
-        // if !ConfigStruct::check_valid_i64_providers_links_limits_for_mongo(&config) {
-        //     let error: Result<ConfigStruct, ConfigError> = Err(ConfigError::Message(
-        //         "providers_links_limits are not valid".to_string(),
-        //     ));
-        //     drop(error);
+        if !self.github_name.is_empty() {
+                return Err(WrapConfigChecksError {
+                    source: Box::new(WrapConfigChecksErrorEnum::GithubName {
+                        source: self.github_name,
+                        file: file!(),
+                        line: line!(),
+                        column: column!(),
+                    })
+                });
+        }
+        if !self.github_token.is_empty() {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::GithubToken {
+                    source: self.github_token,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .reddit_user_agent
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::RedditUserAgent {
+                    source: self.reddit_user_agent,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .reddit_client_id
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::RedditClientId {
+                    source: self.reddit_client_id,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .reddit_client_secret
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::RedditClientSecret {
+                    source: self.reddit_client_secret,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .reddit_username
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::RedditUsername {
+                    source: self.reddit_username,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .reddit_password
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::RedditPassword {
+                    source: self.reddit_password,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .mongo_login
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::MongoLogin {
+                    source: self.mongo_login,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .mongo_password
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::MongoPassword {
+                    source: self.mongo_password,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .mongo_ip
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::MongoIp {
+                    source: self.mongo_ip,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if !self
+            .mongo_port
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::MongoPort {
+                    source: self.mongo_port,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if self.log_file_extension.is_empty() {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::LogFileExtension {
+                    source: self.log_file_extension,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if self
+            .path_to_provider_link_parts_folder
+            .is_empty()
+        {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::PathToProviderLinkPartsFolder {
+                    source: self.path_to_provider_link_parts_folder,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if self.warning_logs_directory_name.is_empty() {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::WarningLogsDirectoryName {
+                    source: self.warning_logs_directory_name,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        if self.links_limit_providers > 0 {
+            return Err(WrapConfigChecksError {
+                source: Box::new(WrapConfigChecksErrorEnum::LinksLimitProviderse {
+                    source: self.links_limit_providers,
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                })
+            });
+        }
+        // if !ConfigStruct::check_valid_i64_providers_links_limits_for_mongo(&self) {
+        //     return Err(WrapConfigChecksError {
+        //         source: Box::new(WrapConfigChecksErrorEnum::GithubName {
+        //             source: self.github_name,
+        //             file: file!(),
+        //             line: line!(),
+        //             column: column!(),
+        //         })
+        //     });
         // }
         Ok(self)
     }
