f4b032f227ae6abf65c12a18d1f2aefb1073a0cb
kuqmua <serezhashmakov@outlook.com>
2022-02-12 19:50:28 +0400

    get_git_source_file_link change source type

--- src/helpers/get_git_source_file_link.rs
+++ src/helpers/get_git_source_file_link.rs
@@ -1,7 +1,7 @@
 use super::lazy_static_git_info::GIT_INFO;
 
 #[deny(clippy::indexing_slicing, clippy::unwrap_used)]
-pub fn get_git_source_file_link(file: String, line: String) -> String {
+pub fn get_git_source_file_link(file: String, line: u32) -> String {
     format!(
         "https://{}/blob/{}/{}#L{}",
         GIT_INFO.repo_link, GIT_INFO.commit_id, file, line
