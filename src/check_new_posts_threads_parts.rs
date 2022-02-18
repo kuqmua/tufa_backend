d0a096f82946f5787caf3ac8a5b4af1816212e0b
kuqmua <serezhashmakov@outlook.com>
2022-02-18 21:31:17 +0300

    check_new_posts_threads_parts rewritten

--- src/check_new_posts_threads_parts.rs
+++ src/check_new_posts_threads_parts.rs
@@ -1,9 +1,5 @@
 use std::collections::HashMap;
 
-use std::sync::Arc;
-
-use tokio::sync::Mutex;
-
 use futures::future::join_all;
 
 use crate::fetch::info_structures::common_rss_structures::CommonRssPostStruct;
@@ -11,44 +7,30 @@ use crate::providers::provider_kind_impl::functions::rss_part::RssPartErrorEnum;
 
 use crate::providers::provider_kind_enum::ProviderKind;
 
-use crate::helpers::get_git_commit_string::get_git_commit_string;
-use crate::helpers::resource::Resource;
-
-use crate::traits::git_info_trait::GitInfo;
 use crate::traits::provider_kind_trait::ProviderKindTrait;
 
 use crate::providers::provider_kind_impl::functions::rss_part::rss_part;
 
-#[derive(GitInfoDerive)]
-pub enum ResourceError {
-    NoLinkParts(Resource),
-    Other,
-}
-
 #[deny(clippy::indexing_slicing)]
 pub async fn check_new_posts_threads_parts(
     providers_link_parts: HashMap<ProviderKind, Vec<String>>,
-) -> Result<HashMap<ProviderKind, Result<Vec<CommonRssPostStruct>, RssPartErrorEnum>>, ResourceError>
+) -> HashMap<
+    ProviderKind, 
+    Result<Vec<CommonRssPostStruct>, RssPartErrorEnum>
+    >
 {
-    let posts_and_errors_arc_mutex = Arc::new(Mutex::new(HashMap::with_capacity(
-        providers_link_parts.len(),
-    )));
-    let tasks_vec = providers_link_parts.into_iter().map(|(pk, link_parts)| {
-        let posts_and_errors_handle_arc = Arc::clone(&posts_and_errors_arc_mutex);
-        async move {
-            match rss_part(pk, pk.generate_provider_links(link_parts)).await {
-                Ok(posts_vec) => {
-                        let mut posts_handle_locked = posts_and_errors_handle_arc.lock().await;
-                        posts_handle_locked.insert(pk, Ok(posts_vec));
-                }
-                Err(e) => {
-                    let mut posts_handle_locked = posts_and_errors_handle_arc.lock().await;
-                    posts_handle_locked.insert(pk, Err(*e));
-                }
+    let tasks_vec = providers_link_parts.into_iter().map(|(pk, link_parts)| async move  {
+        match rss_part(pk, pk.generate_provider_links(link_parts)).await {
+            Ok(posts_vec) => {
+                    (pk, Ok(posts_vec))
+            }
+            Err(e) => {
+                (pk, Err(*e))
             }
         }
     });
-    let _ = join_all(tasks_vec).await;
-    let posts_and_errors_to_return = posts_and_errors_arc_mutex.lock().await.drain().collect();
-    Ok(posts_and_errors_to_return)
+    let posts_and_errors_to_return = join_all(tasks_vec).await.into_iter().map(|(pk, s)|{
+        (pk, s)
+    }).collect();
+    posts_and_errors_to_return
 }
