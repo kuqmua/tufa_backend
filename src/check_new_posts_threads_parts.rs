c3f2932fdf20c3ca7070a5e6b5c757f7c65ec031
kuqmua <serezhashmakov@outlook.com>
2022-02-18 20:21:20 +0300

    cargo fmt

--- src/check_new_posts_threads_parts.rs
+++ src/check_new_posts_threads_parts.rs
@@ -30,18 +30,18 @@ pub async fn check_new_posts_threads_parts(
     providers_link_parts: HashMap<ProviderKind, Vec<String>>,
 ) -> Result<HashMap<ProviderKind, Result<Vec<CommonRssPostStruct>, RssPartErrorEnum>>, ResourceError>
 {
-    
-    let posts_and_errors_arc_mutex = Arc::new(Mutex::new(HashMap::with_capacity(//maybe it needs only Muxex? without Arc?
+    let posts_and_errors_arc_mutex = Arc::new(Mutex::new(HashMap::with_capacity(
+        //maybe it needs only Muxex? without Arc?
         providers_link_parts.len(),
     )));
     //check if provider_names are unique
-    let tasks_vec = providers_link_parts.into_iter().map(|(pk, link_parts)|{
+    let tasks_vec = providers_link_parts.into_iter().map(|(pk, link_parts)| {
         let posts_and_errors_handle_arc = Arc::clone(&posts_and_errors_arc_mutex);
         async move {
-             providers_new_posts_check(
+            providers_new_posts_check(
                 pk,
                 pk.generate_provider_links(link_parts),
-                posts_and_errors_handle_arc
+                posts_and_errors_handle_arc,
             )
             .await;
         }
