8dfd6a4e46c77d5668cb91b00a553a53d4f6d603
kuqmua <serezhashmakov@outlook.com>
2022-02-22 18:46:01 +0300

    return gen_enum_without_values

---
+++ proc_macro_libs/gen_enum_without_values/src/lib.rs
@@ -0,0 +1,41 @@
+use convert_case::Case;
+use convert_case::Casing;
+
+use syn;
+
+use proc_macro::TokenStream;
+
+use quote::quote;
+
+#[proc_macro_derive(GenEnumWithoutValuesDerive)]
+pub fn derive_gen_enum_without_values(input: TokenStream) -> TokenStream {
+    let ast: syn::DeriveInput =
+        syn::parse(input).expect("derive_gen_enum_without_values syn::parse(input) failed");
+    let ident = &ast.ident;
+    let generated = match ast.data {
+        syn::Data::Struct(datastruct) => {
+            let generated = datastruct.fields.into_iter().map(|field| {
+                let enum_variant_ident = match field.ident {
+                    None => panic!("field.ident is None"),
+                    Some(field_ident) => syn::Ident::new(
+                        &format!("{}", field_ident).to_case(Case::Pascal),
+                        ident.span(),
+                    ),
+                };
+                quote! {
+                    #enum_variant_ident,
+                }
+            });
+            generated
+        }
+        _ => panic!("GenEnum only works on Struct"),
+    };
+    let enum_ident = syn::Ident::new(&format!("{}Enum", ident), ident.span());
+    let gen = quote! {
+         #[derive(Debug, strum_macros::Display, EnumIter, EnumExtenstion)]
+        pub enum #enum_ident {
+            #(#generated)*
+        }
+    };
+    gen.into()
+}
