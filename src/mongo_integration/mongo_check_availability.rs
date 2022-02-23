e167123bbd491c4c3023063070b312c35fa90d41
kuqmua <serezhashmakov@outlook.com>
2022-02-23 17:28:13 +0300

    mongo client_options.connect_timeout

--- src/mongo_integration/mongo_check_availability.rs
+++ src/mongo_integration/mongo_check_availability.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use mongodb::{options::ClientOptions, Client};
 
 use crate::config_mods::lazy_static_config::CONFIG;
@@ -40,36 +42,39 @@ pub async fn mongo_check_availability(
                 },
             },
         )),
-        Ok(client_options) => match Client::with_options(client_options) {
-            Err(e) => Err(Box::new(
-                MongoCheckAvailabilityErrorEnum::ClientWithOptions {
-                    source: e,
-                    where_was: WhereWas {
-                        file: file!(),
-                        line: line!(),
-                        column: column!(),
-                    },
-                },
-            )),
-            Ok(client) => {
-                if let Err(e) = client
-                    .database(&CONFIG.mongo_providers_logs_db_name)
-                    .list_collection_names(None)
-                    .await
-                {
-                    return Err(Box::new(
-                        MongoCheckAvailabilityErrorEnum::ListCollectionNames {
-                            source: e,
-                            where_was: WhereWas {
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            },
+        Ok(mut client_options) => {
+            client_options.connect_timeout = Some(Duration::from_millis(1000));
+            match Client::with_options(client_options) {
+                Err(e) => Err(Box::new(
+                    MongoCheckAvailabilityErrorEnum::ClientWithOptions {
+                        source: e,
+                        where_was: WhereWas {
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
                         },
-                    ));
+                    },
+                )),
+                Ok(client) => {
+                    if let Err(e) = client
+                        .database(&CONFIG.mongo_providers_logs_db_name)
+                        .list_collection_names(None)
+                        .await
+                    {
+                        return Err(Box::new(
+                            MongoCheckAvailabilityErrorEnum::ListCollectionNames {
+                                source: e,
+                                where_was: WhereWas {
+                                    file: file!(),
+                                    line: line!(),
+                                    column: column!(),
+                                },
+                            },
+                        ));
+                    }
+                    Ok(())
                 }
-                Ok(())
             }
-        },
+        }
     }
 }
