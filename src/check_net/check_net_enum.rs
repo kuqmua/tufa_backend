367dc629b0f8ff1b4ccfd2244b5026380f4f8a94
kuqmua <serezhashmakov@outlook.com>
2022-02-23 15:50:22 +0300

    impl CheckNet get_url and check

--- src/check_net/check_net_enum.rs
+++ src/check_net/check_net_enum.rs
@@ -1,5 +1,55 @@
+use crate::config_mods::lazy_static_config::CONFIG;
+
+use crate::mongo_integration::mongo_get_db_url::mongo_get_db_url;
+
+use crate::postgres_integration::postgres_get_db_url::postgres_get_db_url;
+
+use crate::check_net::check_net_availability::check_net_availability;
+use crate::mongo_integration::mongo_check_availability::mongo_check_availability;
+use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
+
+use crate::check_net::check_net_availability::CheckNetAvailabilityErrorEnum;
+use crate::mongo_integration::mongo_check_availability::MongoCheckAvailabilityErrorEnum;
+use crate::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityError;
+
 pub enum CheckNet {
-    StartingCheckLink,
-    MongoUrl,
-    PostgersUrl,
+    Net,
+    Postgres,
+    Mongo,
+}
+
+pub enum CheckNetError {
+    Net(CheckNetAvailabilityErrorEnum),
+    Postgres(PostgresCheckAvailabilityError),
+    Mongo(MongoCheckAvailabilityErrorEnum),
+}
+
+impl CheckNet {
+    pub fn get_url(&self) -> String {
+        match self {
+            CheckNet::Net => CONFIG.starting_check_link.clone(),
+            CheckNet::Mongo => postgres_get_db_url(),
+            CheckNet::Postgres => mongo_get_db_url(),
+        }
+    }
+    pub async fn check(&self) -> Result<(), CheckNetError> {
+        match self {
+            CheckNet::Net => {
+                if let Err(e) = check_net_availability(&self.get_url()).await {
+                    return Err(CheckNetError::Net(*e));
+                }
+            }
+            CheckNet::Postgres => {
+                if let Err(e) = postgres_check_availability(&self.get_url()).await {
+                    return Err(CheckNetError::Postgres(*e));
+                }
+            }
+            CheckNet::Mongo => {
+                if let Err(e) = mongo_check_availability(&self.get_url()).await {
+                    return Err(CheckNetError::Mongo(*e));
+                }
+            }
+        }
+        Ok(())
+    }
 }
