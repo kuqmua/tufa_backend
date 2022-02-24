ddac806e6358080d6b2250c916d46fbd919c6f70
kuqmua <serezhashmakov@outlook.com>
2022-02-24 21:44:02 +0300

    impl fmt::Display for CheckNetWrapperError thoughts

--- src/check_net/check_net_wrapper.rs
+++ src/check_net/check_net_wrapper.rs
@@ -10,14 +10,73 @@ use crate::traits::where_was_trait::WhereWasTrait;
 
 use super::check_net_enum::CheckNetError;
 
+use crate::check_net::check_net_availability::CheckNetAvailabilityErrorEnum;
+
 #[derive(Debug)]
 pub struct CheckNetWrapperError {
     pub source: Vec<CheckNetError>,
     pub where_was: WhereWas,
 }
 
+// CheckNetWrapperError {
+//     source: [
+//         Postgres(
+//             PostgresCheckAvailabilityError {
+//                 source: PoolTimedOut,
+//                 where_was: WhereWas {
+//                     file: "src/postgres_integration/postgres_check_availability.rs",
+//                     line: 34,
+//                     column: 25,
+//                 },
+//             },
+//         ),
+//         Mongo(
+//             ClientOptionsParse {
+//                 source: Error {
+//                     kind: InvalidArgument {
+//                         message: "invalid connection string scheme: postgres",
+//                     },
+//                     labels: {},
+//                 },
+//                 where_was: WhereWas {
+//                     file: "src/mongo_integration/mongo_check_availability.rs",
+//                     line: 40,
+//                     column: 29,
+//                 },
+//             },
+//         ),
+//     ],
+//     where_was: WhereWas {
+//         file: "src/check_net/check_net_wrapper.rs",
+//         line: 38,
+//         column: 25,
+//     },
+// }
 impl fmt::Display for CheckNetWrapperError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // enum Something {
+        //     TODO! how to write from WhereWas conversion properly?
+        // }
+        // for i in &self.source {
+        //     match i {
+        //         CheckNetError::Net(e) => {
+        //             match e {
+        //                 CheckNetAvailabilityErrorEnum::CheckLinkStatusCodeError { source, where_was } => {
+
+        //                 },
+        //                 CheckNetAvailabilityErrorEnum::StatusCodeError { source, where_was } => {
+                            
+        //                 },
+        //             }
+        //         },
+        //         CheckNetError::Postgres(e) => {
+
+        //         },
+        //         CheckNetError::Mongo(e) => {
+
+        //         },
+        //     }
+        // }
         write!(f, "{}\n{})", self.where_was.source_place(), self.where_was.github_source_place())
     }
 }
