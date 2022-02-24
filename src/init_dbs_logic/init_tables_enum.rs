1cd1c8bac2be2ea33bb802b350ddcdf07fc8a638
kuqmua <serezhashmakov@outlook.com>
2022-02-24 20:14:06 +0300

    InitTablesEnum {     pub async fn init impl

--- src/init_dbs_logic/init_tables_enum.rs
+++ src/init_dbs_logic/init_tables_enum.rs
@@ -1,14 +1,32 @@
 use strum_macros::EnumIter;
 
+use crate::init_dbs_logic::init_dbs_with_providers_link_parts::init_dbs_with_providers_link_parts;
+
+use crate::init_dbs_logic::init_dbs::InitDbsErrorEnum;
+
+use crate::helpers::where_was::WhereWas;
+
 #[derive(Debug, EnumIter)]
 pub enum InitTablesEnum {
     ProvidersLinkParts,
 }
 
 impl InitTablesEnum {
-    pub fn init(&self) {
+    pub async fn init(&self) -> Result<(), Box<InitDbsErrorEnum>> {
         match self {
-            InitTablesEnum::ProvidersLinkParts => todo!(),
+            InitTablesEnum::ProvidersLinkParts => {
+                if let Err(e) = init_dbs_with_providers_link_parts().await {
+                    return Err(Box::new(InitDbsErrorEnum::InitDbsProvidersLinkParts {
+                        source: e,
+                        where_was: WhereWas {
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                    }));
+                }
+            },
         }
+        Ok(())
     }
 }
