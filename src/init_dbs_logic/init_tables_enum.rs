1148350583f71af3a9533bec5e68839de6b834e5
kuqmua <serezhashmakov@outlook.com>
2022-02-24 19:56:02 +0300

    impl InitTablesEnum {     pub fn init

--- src/init_dbs_logic/init_tables_enum.rs
+++ src/init_dbs_logic/init_tables_enum.rs
@@ -1,4 +1,14 @@
-#[derive(Debug)]
+use strum_macros::EnumIter;
+
+#[derive(Debug, EnumIter)]
 pub enum InitTablesEnum {
     ProvidersLinkParts,
+}
+
+impl InitTablesEnum {
+    pub fn init(&self) {
+        match self {
+            InitTablesEnum::ProvidersLinkParts => todo!(),
+        }
+    }
 }
