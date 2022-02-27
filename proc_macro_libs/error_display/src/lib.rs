fea9f1c6240c158313555a19f5ef08bf1ec39396
kuqmua <serezhashmakov@outlook.com>
2022-02-27 15:06:58 +0300

    lil tweaks

--- proc_macro_libs/error_display/src/lib.rs
+++ proc_macro_libs/error_display/src/lib.rs
@@ -10,38 +10,34 @@ pub fn derive_error_display(input: TokenStream) -> TokenStream {
         syn::parse(input).expect("derive_error_display syn::parse(input) failed");
     // println!("{:#?}", ast.data);
     let ident = &ast.ident;
-    match ast.data {
+    let gen = match ast.data {
         syn::Data::Struct(_) => quote! {
-            impl fmt::Display for #ident {
-                fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                    if CONFIG.is_show_source_place_enabled && CONFIG.is_show_github_source_place_enabled {
-                        write!(
-                            f,
-                            "{}\n{}\n{}",
-                            self.where_was.source_place_with_readable_time(),
-                            self.where_was.github_source_place_with_readable_time(),
-                            self.source
-                        )
-                    } else if CONFIG.is_show_source_place_enabled {
-                        write!(
-                            f,
-                            "{}\n{}",
-                            self.where_was.source_place_with_readable_time(),
-                            self.source
-                        )
-                    } else if CONFIG.is_show_github_source_place_enabled {
-                        write!(
-                            f,
-                            "{}\n{}",
-                            self.where_was.github_source_place_with_readable_time(),
-                            self.source
-                        )
-                    } else {
-                        write!(f, "{}", self.source)
-                    }
-                }
+            if CONFIG.is_show_source_place_enabled && CONFIG.is_show_github_source_place_enabled {
+                write!(
+                    f,
+                    "{}\n{}\n{}",
+                    self.where_was.source_place_with_readable_time(),
+                    self.where_was.github_source_place_with_readable_time(),
+                    self.source
+                )
+            } else if CONFIG.is_show_source_place_enabled {
+                write!(
+                    f,
+                    "{}\n{}",
+                    self.where_was.source_place_with_readable_time(),
+                    self.source
+                )
+            } else if CONFIG.is_show_github_source_place_enabled {
+                write!(
+                    f,
+                    "{}\n{}",
+                    self.where_was.github_source_place_with_readable_time(),
+                    self.source
+                )
+            } else {
+                write!(f, "{}", self.source)
             }
-        }.into(),
+        },
         syn::Data::Enum(data_enum) => {
             let generated = data_enum.variants.into_iter().map(|v| {
                 let variant = v.ident;
@@ -77,17 +73,21 @@ pub fn derive_error_display(input: TokenStream) -> TokenStream {
                 }
             });
             quote! {
-                impl fmt::Display for #ident {
-                    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                        match self {
-                            #(#generated)*
-                        }
-                    }
+                match self {
+                    #(#generated)*
                 }
-            }.into()
-        },
+            }
+        }
         _ => panic!("EnumIntoArray only works on enum and struct"),
+    };
+    quote! {
+        impl fmt::Display for #ident {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                #gen
+            }
+        }
     }
+    .into()
 }
 
 // if CONFIG.is_show_source_place_enabled && CONFIG.is_show_github_source_place_enabled
