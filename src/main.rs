a8d89b9099644658619950d80655e3a0572e1b33
kuqmua <serezhashmakov@outlook.com>
2022-02-28 18:33:06 +0300

    actix web start

--- src/main.rs
+++ src/main.rs
@@ -169,12 +169,24 @@ extern crate provider_kind_from_config;
 
 extern crate dotenv;
 
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
+    entry::entry();
+    format!("Hello {}! id:{}", name, id)
+}
+
 #[deny(
     clippy::indexing_slicing,
     clippy::unwrap_used,
     clippy::integer_arithmetic,
     clippy::float_arithmetic
 )]
-fn main() {
-    entry::entry();
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
