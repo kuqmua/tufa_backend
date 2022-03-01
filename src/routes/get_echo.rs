bf7524677508a781660bf5c63151a7a2fa74830c
kuqmua <serezhashmakov@outlook.com>
2022-03-01 21:04:40 +0300

    remove body from get_echo coz get cannot have body. still dont know how to use it in actix

--- src/routes/get_echo.rs
+++ src/routes/get_echo.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, HttpResponse, Responder};
 
 #[get("/get_echo")]
-async fn get_echo(req_body: String) -> impl Responder {
+async fn get_echo() -> impl Responder {
     println!("get_echo");
-    HttpResponse::Ok().body(req_body)
+    HttpResponse::Ok().body("get_echo")
 }
