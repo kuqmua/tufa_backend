3d95cfe90d7ae60d0874e0a7c868d6eef3646e5c
kuqmua <serezhashmakov@outlook.com>
2022-03-02 20:56:10 +0300

    mongo_drop_collection type error

--- src/mongo_integration/mongo_drop_collection.rs
+++ src/mongo_integration/mongo_drop_collection.rs
@@ -9,11 +9,6 @@ use crate::helpers::where_was::WhereWas;
 use crate::config_mods::lazy_static_config::CONFIG;
 
 #[derive(Debug)]
-pub struct MongoDropCollectionError {
-    pub source: Box<MongoDropCollectionErrorEnum>,
-}
-
-#[derive(Debug)]
 pub enum MongoDropCollectionErrorEnum {
     ClientOptionsParse {
         source: mongodb::error::Error,
@@ -39,10 +34,20 @@ pub async fn mongo_drop_collection(
     mongo_url: &str,
     db_name: &str,
     db_collection_name: &str,
-) -> Result<(), MongoDropCollectionError> {
+) -> Result<(), Box<MongoDropCollectionErrorEnum>> {
     match ClientOptions::parse(mongo_url).await {
-        Err(e) => Err(MongoDropCollectionError {
-            source: Box::new(MongoDropCollectionErrorEnum::ClientOptionsParse {
+        Err(e) => Err(Box::new(MongoDropCollectionErrorEnum::ClientOptionsParse {
+            source: e,
+            where_was: WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            },
+        })),
+        Ok(client_options) => match Client::with_options(client_options) {
+            Err(e) => Err(Box::new(MongoDropCollectionErrorEnum::ClientWithOptions {
                 source: e,
                 where_was: WhereWas {
                     time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
@@ -51,37 +56,21 @@ pub async fn mongo_drop_collection(
                     line: line!(),
                     column: column!(),
                 },
-            }),
-        }),
-        Ok(client_options) => match Client::with_options(client_options) {
-            Err(e) => Err(MongoDropCollectionError {
-                source: Box::new(MongoDropCollectionErrorEnum::ClientWithOptions {
-                    source: e,
-                    where_was: WhereWas {
-                        time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                            .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                        file: file!(),
-                        line: line!(),
-                        column: column!(),
-                    },
-                }),
-            }),
+            })),
             Ok(client) => {
                 let collection: Collection<Document> =
                     client.database(db_name).collection(db_collection_name);
                 if let Err(e) = collection.drop(None).await {
-                    return Err(MongoDropCollectionError {
-                        source: Box::new(MongoDropCollectionErrorEnum::DatabaseDrop {
-                            source: e,
-                            where_was: WhereWas {
-                                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            },
-                        }),
-                    });
+                    return Err(Box::new(MongoDropCollectionErrorEnum::DatabaseDrop {
+                        source: e,
+                        where_was: WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                    }));
                 }
                 Ok(())
             }
