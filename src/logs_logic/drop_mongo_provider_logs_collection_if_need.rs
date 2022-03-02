fab1fccbbbc1fe2080d2264600a923de3eda6771
kuqmua <serezhashmakov@outlook.com>
2022-03-02 21:04:39 +0300

    drop_mongo_provider_logs_collection_if_need types error

--- src/logs_logic/drop_mongo_provider_logs_collection_if_need.rs
+++ src/logs_logic/drop_mongo_provider_logs_collection_if_need.rs
@@ -1,7 +1,7 @@
 use crate::providers::provider_kind_enum::ProviderKind;
 
 use crate::mongo_integration::mongo_drop_empty_collection::mongo_drop_empty_collection;
-use crate::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionError;
+use crate::mongo_integration::mongo_drop_empty_collection::MongoDropEmptyCollectionErrorEnum;
 
 use crate::prints::print_colorful_message::print_colorful_message;
 use crate::prints::print_type_enum::PrintType;
@@ -20,22 +20,19 @@ use crate::helpers::get_git_source_file_link::get_git_source_file_link;
 pub async fn drop_mongo_provider_logs_collection_if_need(
     pk: &ProviderKind,
     mongo_url: String,
-) -> Result<(), MongoDropEmptyCollectionError> {
+) -> Result<(), Box<MongoDropEmptyCollectionErrorEnum>> {
     if !pk.is_cleaning_warning_logs_directory_enabled() {
         return Ok(());
     }
-    let db_collection_name = &format!(
-        "{pk:#?}{}",
-        &CONFIG.mongo_providers_logs_db_collection_handle_second_part
-    );
-    //using different (old) tokio runtime 0.2.25
-    let future_possible_drop_collection = mongo_drop_empty_collection(
+    if let Err(e) = mongo_drop_empty_collection(
         &mongo_url,
         &CONFIG.mongo_providers_logs_db_name,
-        db_collection_name,
+        &format!(
+            "{pk:#?}{}",
+            &CONFIG.mongo_providers_logs_db_collection_handle_second_part
+        ),
     )
-    .await;
-    if let Err(e) = future_possible_drop_collection {
+    .await {
         print_colorful_message(
             Some(pk),
             PrintType::WarningHigh,
