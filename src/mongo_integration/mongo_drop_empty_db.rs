b364ca7d187b6d43337d91e177c29b4be3cca1f7
kuqmua <serezhashmakov@outlook.com>
2022-03-02 21:06:23 +0300

    mongo_drop_empty_db error type

--- src/mongo_integration/mongo_drop_empty_db.rs
+++ src/mongo_integration/mongo_drop_empty_db.rs
@@ -7,11 +7,6 @@ use crate::helpers::where_was::WhereWas;
 use crate::config_mods::lazy_static_config::CONFIG;
 
 #[derive(Debug)]
-pub struct MongoDropEmptyDbError {
-    source: Box<MongoDropEmptyDbErrorEnum>,
-}
-
-#[derive(Debug)]
 pub enum MongoDropEmptyDbErrorEnum {
     ClientOptionsParse {
         source: mongodb::error::Error,
@@ -44,10 +39,20 @@ pub enum MongoDropEmptyDbErrorEnum {
 pub async fn mongo_drop_empty_db(
     mongo_url: &str,
     db_name: &str,
-) -> Result<(), MongoDropEmptyDbError> {
+) -> Result<(), Box<MongoDropEmptyDbErrorEnum>> {
     match ClientOptions::parse(mongo_url).await {
-        Err(e) => Err(MongoDropEmptyDbError {
-            source: Box::new(MongoDropEmptyDbErrorEnum::ClientOptionsParse {
+        Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ClientOptionsParse {
+            source: e,
+            where_was: WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            },
+        })),
+        Ok(client_options) => match Client::with_options(client_options) {
+            Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ClientWithOptions {
                 source: e,
                 where_was: WhereWas {
                     time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
@@ -56,60 +61,25 @@ pub async fn mongo_drop_empty_db(
                     line: line!(),
                     column: column!(),
                 },
-            }),
-        }),
-        Ok(client_options) => match Client::with_options(client_options) {
-            Err(e) => Err(MongoDropEmptyDbError {
-                source: Box::new(MongoDropEmptyDbErrorEnum::ClientWithOptions {
-                    source: e,
-                    where_was: WhereWas {
-                        time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                            .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                        file: file!(),
-                        line: line!(),
-                        column: column!(),
-                    },
-                }),
-            }),
+            })),
             Ok(client) => {
                 let db = client.database(db_name);
                 match db.list_collection_names(None).await {
-                    Err(e) => Err(MongoDropEmptyDbError {
-                        source: Box::new(MongoDropEmptyDbErrorEnum::ListCollectionNames {
-                            source: e,
-                            where_was: WhereWas {
-                                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            },
-                        }),
-                    }),
+                    Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ListCollectionNames {
+                        source: e,
+                        where_was: WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                    })),
                     Ok(collections_names_list) => {
                         if !collections_names_list.is_empty() {
-                            return Err(MongoDropEmptyDbError {
-                                source: Box::new(
-                                    MongoDropEmptyDbErrorEnum::CollectionNamesListIsEmpty {
-                                        source: db_name.to_string(),
-                                        where_was: WhereWas {
-                                            time: DateTime::<Utc>::from_utc(
-                                                Local::now().naive_utc(),
-                                                Utc,
-                                            )
-                                            .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                                            file: file!(),
-                                            line: line!(),
-                                            column: column!(),
-                                        },
-                                    },
-                                ),
-                            });
-                        }
-                        if let Err(e) = db.drop(None).await {
-                            return Err(MongoDropEmptyDbError {
-                                source: Box::new(MongoDropEmptyDbErrorEnum::DatabaseDrop {
-                                    source: e,
+                            return Err(Box::new(
+                                MongoDropEmptyDbErrorEnum::CollectionNamesListIsEmpty {
+                                    source: db_name.to_string(),
                                     where_was: WhereWas {
                                         time: DateTime::<Utc>::from_utc(
                                             Local::now().naive_utc(),
@@ -120,8 +90,23 @@ pub async fn mongo_drop_empty_db(
                                         line: line!(),
                                         column: column!(),
                                     },
-                                }),
-                            });
+                                },
+                            ));
+                        }
+                        if let Err(e) = db.drop(None).await {
+                            return Err(Box::new(MongoDropEmptyDbErrorEnum::DatabaseDrop {
+                                source: e,
+                                where_was: WhereWas {
+                                    time: DateTime::<Utc>::from_utc(
+                                        Local::now().naive_utc(),
+                                        Utc,
+                                    )
+                                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                                    file: file!(),
+                                    line: line!(),
+                                    column: column!(),
+                                },
+                            }));
                         }
                         Ok(())
                     }
