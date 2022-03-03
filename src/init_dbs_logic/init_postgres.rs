fac8bcd8833c53196e2aee2cb70a96346905452e
kuqmua <serezhashmakov@outlook.com>
2022-03-03 21:30:22 +0300

    fix

--- src/init_dbs_logic/init_postgres.rs
+++ src/init_dbs_logic/init_postgres.rs
@@ -91,7 +91,7 @@ pub async fn init_postgres(
             {
                 return Err(PostgresInitError {
                     source: Box::new(PostgresInitErrorEnum::CreateTableQueries {
-                        source: e,
+                        source: *e,
                         where_was: WhereWas {
                             time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
                                 .with_timezone(&FixedOffset::east(CONFIG.timezone)),
