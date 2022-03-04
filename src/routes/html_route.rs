88fe98b7edcebca368f0b8fc3c74da07e188105a
kuqmua <serezhashmakov@outlook.com>
2022-03-04 18:52:39 +0300

    working html_route

--- src/routes/html_route.rs
+++ src/routes/html_route.rs
@@ -1,9 +1,27 @@
 
 
-use actix_web::{get, web, Responder};
+use actix_web::{get, web, Responder, HttpResponse, http::header::ContentType};
 
-#[get("/html/{name}/index.html")]
+#[get("/html/{name}")]
 async fn html_route(name: web::Path<String>) -> impl Responder {
     println!("html_route");
-    format!("todo html {name}")
+    let f = format!(
+        r#"<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <!-- This is equivalent to a HTTP header -->
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>title</title>
+        </head>
+        <!-- [...] -->
+        <body>
+        <div style="display: flex; justify-content: center; width: 100%; height: 100%;">
+        <div style="background-color: red; width: 100px; height: 100px;">{name}</div>
+        </div>
+        </body>
+        </html>"#
+    );
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(f)
 }
