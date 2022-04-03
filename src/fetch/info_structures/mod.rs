86ac5b3f3f063e9635812dcc10d6703da1a17e92
kuqmua <serezhashmakov@outlook.com>
2022-04-03 15:31:34 +0300

    mod common_rss_structures file

--- src/fetch/info_structures/mod.rs
+++ src/fetch/info_structures/mod.rs
@@ -1,10 +1,2 @@
-pub mod structs_for_parsing {
-    pub mod arxiv_struct_for_parsing;
-    pub mod biorxiv_struct_for_parsing;
-    pub mod github_struct_for_parsing;
-    pub mod habr_struct_for_parsing;
-    pub mod medrxiv_struct_for_parsing;
-    pub mod reddit_struct_for_parsing;
-    pub mod twitter_struct_for_parsing;
-}
 pub mod common_rss_structures;
+pub mod structs_for_parsing;
