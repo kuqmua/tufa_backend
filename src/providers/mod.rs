b5f211bf7bbce9420d9c3efe260ac702d5d055c9
kuqmua <serezhashmakov@outlook.com>
2022-04-03 15:12:19 +0300

    mod providers_info

--- src/providers/mod.rs
+++ src/providers/mod.rs
@@ -1,18 +1,4 @@
-pub mod providers_info {
-    pub mod links {
-        pub mod generate_arxiv_links;
-        pub mod generate_biorxiv_links;
-        pub mod generate_github_links;
-        pub mod generate_habr_links;
-        pub mod generate_medrxiv_links;
-        pub mod generate_reddit_links;
-        pub mod generate_twitter_links;
-    }
-    pub mod get_local_providers_link_parts;
-    pub mod get_providers_link_parts;
-    pub mod get_twitter_provider_name;
-    pub mod providers_init_json_schema;
-}
 pub mod check_providers_link_parts_on_empty;
 pub mod get_providers_posts;
 pub mod provider_kind;
+pub mod providers_info;
