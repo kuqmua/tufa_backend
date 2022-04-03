a970f156e8ef117c7aa0a7f5d5f46fa216f92927
kuqmua <serezhashmakov@outlook.com>
2022-04-03 15:29:11 +0300

    mod config_functions mod

---
+++ src/config_mods/config_functions/mod.rs
@@ -0,0 +1,2 @@
+pub mod check_valid_i64_providers_links_limits_for_mongo;
+pub mod wrap_config_checks; //stay
--- src/config_mods/mod.rs
+++ src/config_mods/mod.rs
@@ -1,7 +1,3 @@
-
-pub mod config_functions {
-    pub mod check_valid_i64_providers_links_limits_for_mongo;
-    pub mod wrap_config_checks; //stay
-}
+pub mod config_functions;
 pub mod config_struct;
 pub mod lazy_static_config;
