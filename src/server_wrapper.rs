316970a63d897f977cb1e9224c472d858264a3ba
kuqmua <serezhashmakov@outlook.com>
2022-04-08 22:33:41 +0300

    tokio::spawn(run_worker_until_stopped(configuration));

--- src/server_wrapper.rs
+++ src/server_wrapper.rs
@@ -1,3 +1,9 @@
+use crate::config_mods::lazy_static_config::CONFIG;
+use crate::configuration;
+use crate::configuration::ApplicationSettings;
+use crate::configuration::DatabaseSettings;
+use crate::configuration::EmailClientSettings;
+use crate::configuration::Settings;
 use crate::helpers::get_server_address::get_server_address;
 use crate::issue_delivery_worker::run_worker_until_stopped;
 use crate::routes::get_providers_posts_route::get_providers_posts_route;
@@ -5,44 +11,55 @@ use crate::startup::Application;
 use crate::startup::ApplicationBuildErrorEnum;
 use crate::telemetry::get_subscriber;
 use crate::telemetry::init_subscriber;
-use actix_web::web;
-use actix_web::App;
-use actix_web::HttpServer;
+use secrecy::Secret;
 use std::fmt::{Debug, Display};
 use tokio::task::JoinError;
 
 #[actix_web::main] // or #[tokio::main]
-pub async fn server_wrapper() {
-    // HttpServer::new(|| {
-    //     App::new()
-    //         // .route("/", web::get().to(|| async { "Hello World!" }))
-    //         .route(
-    //             "/get_providers_posts",
-    //             web::post().to(get_providers_posts_route),
-    //         )
-    // })
-    // .bind(get_server_address())?
-    // .run()
-    // .await
+pub async fn server_wrapper() -> Result<(), Box<ApplicationBuildErrorEnum>> {
     let subscriber = get_subscriber(
         "session_based_authentication".into(),
         "info".into(),
         std::io::stdout,
     );
-    // subscriber
     init_subscriber(subscriber);
     // let configuration = get_configuration().expect("Failed to read configuration.");
     let application = match Application::build().await {
         Ok(app) => app,
-        Err(e) => panic!("error"),
+        Err(e) => return Err(e),
     };
     let application_task = tokio::spawn(application.run_until_stopped());
-    // let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
-    // tokio::select! {
-    //     o = application_task => report_exit("API", o),
-    //     o = worker_task => report_exit("Background worker", o),
-    // };
-    // return None;
+    let configuration = Settings {
+        database: DatabaseSettings {
+            host: CONFIG.postgres_ip.clone(),
+            port: CONFIG.postgres_port,
+            username: CONFIG.postgres_login.clone(),
+            password: Secret::new(CONFIG.postgres_password.clone()),
+            database_name: CONFIG.postgres_db.clone(),
+            require_ssl: CONFIG.require_ssl,
+        },
+        application: ApplicationSettings {
+            port: 8080,
+            host: "127.0.0.1".to_string(),
+            base_url: "http://127.0.0.1".to_string(),
+            hmac_secret: Secret::new(
+                "super-long-and-secret-random-key-needed-to-verify-message-integrity".to_string(),
+            ),
+        },
+        email_client: EmailClientSettings {
+            base_url: CONFIG.base_url.clone(),
+            sender_email: "test@gmail.com".to_string(),
+            authorization_token: Secret::new("my-secret-token".to_string()),
+            timeout_milliseconds: 10000,
+        },
+        redis_uri: Secret::new("redis://127.0.0.1:6379".to_string()),
+    };
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
+    tokio::select! {
+        o = application_task => report_exit("API", o),
+        o = worker_task => report_exit("Background worker", o),
+    };
+    Ok(())
 }
 
 fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
