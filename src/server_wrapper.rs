065531d04c2f81d08f597b1efed4dc941d016c23
kuqmua <serezhashmakov@outlook.com>
2022-04-09 11:26:54 +0300

    ServerWrapperErrorEnum

--- src/server_wrapper.rs
+++ src/server_wrapper.rs
@@ -8,18 +8,29 @@ use crate::startup::Application;
 use crate::startup::ApplicationBuildErrorEnum;
 use crate::telemetry::get_subscriber;
 use crate::telemetry::init_subscriber;
+use crate::telemetry::InitSubcriberErrorEnum;
 use secrecy::Secret;
 use std::fmt::{Debug, Display};
 use tokio::task::JoinError;
 
+#[derive(Debug)]
+pub enum ServerWrapperErrorEnum {
+    InitSubcriber { source: InitSubcriberErrorEnum },
+    ApplicationBuild { source: ApplicationBuildErrorEnum },
+}
+
 #[actix_web::main] // or #[tokio::main]
-pub async fn server_wrapper() -> Result<(), Box<ApplicationBuildErrorEnum>> {
+pub async fn server_wrapper() -> Result<(), Box<ServerWrapperErrorEnum>> {
     let subscriber = get_subscriber(
         "session_based_authentication".into(),
         "info".into(),
         std::io::stdout,
     );
-    init_subscriber(subscriber);
+    if let Err(e) = init_subscriber(subscriber) {
+        return Err(Box::new(ServerWrapperErrorEnum::InitSubcriber {
+            source: e,
+        }));
+    };
     let configuration = Settings {
         database: DatabaseSettings {
             host: CONFIG.postgres_ip.clone(),
@@ -45,7 +56,11 @@ pub async fn server_wrapper() -> Result<(), Box<ApplicationBuildErrorEnum>> {
     };
     let application = match Application::build(configuration.clone()).await {
         Ok(app) => app,
-        Err(e) => return Err(e),
+        Err(e) => {
+            return Err(Box::new(ServerWrapperErrorEnum::ApplicationBuild {
+                source: *e,
+            }))
+        }
     };
     let application_task = tokio::spawn(application.run_until_stopped());
     let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
