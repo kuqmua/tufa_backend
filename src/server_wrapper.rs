e384cc68e57cdb1f70875ef8b67bbb1275c34f65
kuqmua <serezhashmakov@outlook.com>
2022-04-10 09:30:13 +0300

    fixed server wrapper, run and build application for integration tests usage

--- src/server_wrapper.rs
+++ src/server_wrapper.rs
@@ -1,5 +1,4 @@
 use crate::config_mods::lazy_static_config::CONFIG;
-use crate::configuration::get_configuration;
 use crate::configuration::ApplicationSettings;
 use crate::configuration::DatabaseSettings;
 use crate::configuration::EmailClientSettings;
@@ -7,59 +6,39 @@ use crate::configuration::Settings;
 use crate::issue_delivery_worker::run_worker_until_stopped;
 use crate::startup::Application;
 use crate::startup::ApplicationBuildErrorEnum;
-use crate::telemetry::{get_subscriber, init_subscriber};
 use secrecy::Secret;
 use std::fmt::{Debug, Display};
 use tokio::task::JoinError;
 
-// #[actix_web::main] // or #[tokio::main]
-// pub async fn server_wrapper() -> Result<(), Box<ApplicationBuildErrorEnum>> {
-//     let configuration = Settings {
-//         database: DatabaseSettings {
-//             host: CONFIG.postgres_ip.clone(),
-//             port: CONFIG.postgres_port,
-//             username: CONFIG.postgres_login.clone(),
-//             password: Secret::new(CONFIG.postgres_password.clone()),
-//             database_name: CONFIG.postgres_db.clone(),
-//             require_ssl: CONFIG.require_ssl,
-//         },
-//         application: ApplicationSettings {
-//             port: CONFIG.server_port,
-//             host: CONFIG.server_ip.clone(),
-//             base_url: CONFIG.base_url.clone(),
-//             hmac_secret: Secret::new(CONFIG.hmac_secret.clone()),
-//         },
-//         email_client: EmailClientSettings {
-//             base_url: CONFIG.base_url.clone(),
-//             sender_email: "test@gmail.com".to_string(),
-//             authorization_token: Secret::new("my-secret-token".to_string()),
-//             timeout_milliseconds: 10000,
-//         },
-//         redis_uri: Secret::new("redis://127.0.0.1:6379".to_string()),
-//     };
-//     let application = match Application::build(configuration.clone()).await {
-//         Ok(app) => app,
-//         Err(e) => return Err(e),
-//     };
-//     let application_task = tokio::spawn(application.run_until_stopped());
-//     let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
-//     tokio::select! {
-//         o = application_task => report_exit("API", o),
-//         o = worker_task => report_exit("Background worker", o),
-//     };
-//     Ok(())
-// }
-
-#[tokio::main]
-pub async fn server_wrapper() -> anyhow::Result<()> {
-    let subscriber = get_subscriber(
-        "session_based_authentication".into(),
-        "info".into(),
-        std::io::stdout,
-    );
-    init_subscriber(subscriber);
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration.clone()).await?;
+#[actix_web::main] // or #[tokio::main]
+pub async fn server_wrapper() -> Result<(), Box<ApplicationBuildErrorEnum>> {
+    let configuration = Settings {
+        database: DatabaseSettings {
+            host: CONFIG.postgres_ip.clone(),
+            port: CONFIG.postgres_port,
+            username: CONFIG.postgres_login.clone(),
+            password: Secret::new(CONFIG.postgres_password.clone()),
+            database_name: CONFIG.postgres_db.clone(),
+            require_ssl: CONFIG.require_ssl,
+        },
+        application: ApplicationSettings {
+            port: CONFIG.server_port,
+            host: CONFIG.server_ip.clone(),
+            base_url: CONFIG.base_url.clone(),
+            hmac_secret: Secret::new(CONFIG.hmac_secret.clone()),
+        },
+        email_client: EmailClientSettings {
+            base_url: CONFIG.base_url.clone(),
+            sender_email: "test@gmail.com".to_string(),
+            authorization_token: Secret::new("my-secret-token".to_string()),
+            timeout_milliseconds: 10000,
+        },
+        redis_uri: Secret::new("redis://127.0.0.1:6379".to_string()),
+    };
+    let application = match Application::build(configuration.clone()).await {
+        Ok(app) => app,
+        Err(e) => return Err(e),
+    };
     let application_task = tokio::spawn(application.run_until_stopped());
     let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
     tokio::select! {
