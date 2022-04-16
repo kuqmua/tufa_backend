906b9fb03357bd5c375149e8553d67211ca08395
kuqmua <serezhashmakov@outlook.com>
2022-04-16 19:30:27 +0300

    JsonExample

--- src/routes/json_example.rs
+++ src/routes/json_example.rs
@@ -2,19 +2,19 @@ use actix_web::{web, Responder};
 use serde::Serialize;
 
 #[derive(Serialize)]
-struct Country {
+struct JsonExample {
     first: String,
     second: String,
 }
 
 pub async fn json_example() -> impl Responder {
     println!("json example");
-    let mut vec: Vec<Country> = Vec::new();
-    vec.push(Country {
+    let mut vec: Vec<JsonExample> = Vec::new();
+    vec.push(JsonExample {
         first: "1".to_string(),
         second: "value1".to_string(),
     });
-    vec.push(Country {
+    vec.push(JsonExample {
         first: "2".to_string(),
         second: "value2".to_string(),
     });
