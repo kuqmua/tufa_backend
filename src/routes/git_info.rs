b7c9d6ebefb66873f810c8ddb30cb2ae5fea1360
kuqmua <serezhashmakov@outlook.com>
2022-04-18 22:04:52 +0300

    commit_string

--- src/routes/git_info.rs
+++ src/routes/git_info.rs
@@ -1,6 +1,7 @@
 use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
 use tufa_common::helpers::git::lazy_static_git_info::GIT_INFO;
+use tufa_common::helpers::git::get_git_commit_string::get_git_commit_string;
 
 pub struct GitCommitInfo {
     pub commit_message: String,
@@ -13,6 +14,7 @@ pub async fn git_info() -> HttpResponse {
     let commit_id = GIT_INFO.commit_id.clone();
     let branch = GIT_INFO.branch.clone();
     let repo_link = GIT_INFO.repo_link.clone();
+    let commit_string = get_git_commit_string();
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
@@ -24,10 +26,11 @@ pub async fn git_info() -> HttpResponse {
 </head>
 <body>
 <div style="display:flex; justify-content: center; align-items: center; flex-direction: column; width: 100%; height: 100%;">
-<div>Commit message:{commit_message}</div>
+<div>Commit message: {commit_message}</div>
 <div>Commit id: {commit_id}</div>
 <div>Branch: {branch}</div>
 <div>Repo link: {repo_link}</div>
+<div>Commit string: {commit_string}</div>
 </div>
     
 </body>
