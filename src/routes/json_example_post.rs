8d1a86dd34b51f7c24e5c5fd398aa9d601d88bef
kuqmua <serezhashmakov@outlook.com>
2022-05-10 21:57:28 +0300

    changed json_example_post return

--- src/routes/json_example_post.rs
+++ src/routes/json_example_post.rs
@@ -1,10 +1,7 @@
-use actix_web::{web, Responder};
+use actix_web::{web, HttpResponse, Responder};
 use tufa_common::json_example::JsonExample;
 
 pub async fn json_example_post(json: web::Json<JsonExample>) -> impl Responder {
     println!("json example {:#?}", json);
-    web::Json(JsonExample {
-        first: "first_value_json_example".to_string(),
-        second: "second_value_json_example".to_string(),
-    })
+    HttpResponse::Ok().finish()
 }
