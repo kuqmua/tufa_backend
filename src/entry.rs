096120b1418824a34a0d036dd9e0440ddc33a557
kuqmua <serezhashmakov@outlook.com>
2022-08-07 12:54:17 +0300

    if let true = CONFIG.is_preparation_enabled {

--- src/entry.rs
+++ src/entry.rs
@@ -5,11 +5,8 @@ use crate::prints::print_colorful_message::print_colorful_message;
 use crate::prints::print_type_enum::PrintType;
 use crate::project_constants::PROJECT_NAME;
 use crate::server_wrapper::server_wrapper;
-use crate::telemetry::get_subscriber;
 use crate::telemetry::get_subscriber::get_subscriber;
-use crate::telemetry::init_subscriber;
 use crate::telemetry::init_subscriber::init_subscriber;
-use std::time::Instant;
 
 #[deny(
     clippy::indexing_slicing,
@@ -33,7 +30,6 @@ pub fn entry() {
             );
         }
         Ok(runtime) => {
-            //disable tracing to remove useless spam for now
             if let true = CONFIG.is_tracing_enabled {
                 if let Err(e) = init_subscriber(get_subscriber(
                     PROJECT_NAME.into(),
@@ -50,16 +46,18 @@ pub fn entry() {
                     return;
                 };
             }
-            if let Err(e) = runtime.block_on(preparation()) {
-                println!("{e}");
-                // print_colorful_message(
-                //     None,
-                //     PrintType::Error,
-                //     vec![format!("{}:{}:{}", file!(), line!(), column!())],
-                //     vec![GIT_INFO.get_git_source_file_link(file!(), line!())],
-                //     format!("preparation failed, error:\n {}", *e),
-                // );
-                return;
+            if let true = CONFIG.is_preparation_enabled {
+                if let Err(e) = runtime.block_on(preparation()) {
+                    println!("{e}");
+                    // print_colorful_message(
+                    //     None,
+                    //     PrintType::Error,
+                    //     vec![format!("{}:{}:{}", file!(), line!(), column!())],
+                    //     vec![GIT_INFO.get_git_source_file_link(file!(), line!())],
+                    //     format!("preparation failed, error:\n {}", *e),
+                    // );
+                    return;
+                }
             }
             if let Err(e) = server_wrapper() {
                 print_colorful_message(
@@ -69,6 +67,7 @@ pub fn entry() {
                     vec![GIT_INFO.get_git_source_file_link(file!(), line!())],
                     format!("Cannot run actix-web HttpServer, error: {:#?}", e),
                 );
+                return;
             }
         }
     }
