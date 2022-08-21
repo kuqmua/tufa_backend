ef251bd4d9d6a254c437083a764828c47e49a5be
kuqmua <serezhashmakov@outlook.com>
2022-08-21 14:46:45 +0300

    prepera server and check availability functions

---
+++ src/preparation/check_availability.rs
@@ -0,0 +1,360 @@
+use crate::config_mods::lazy_static_config::CONFIG;
+use crate::helpers::mongo::get_mongo_url::get_mongo_url;
+use crate::helpers::postgres::get_postgres_url::get_postgres_url;
+use crate::helpers::where_was::WhereWas;
+use crate::helpers::where_was::WhereWasOneOrFew;
+use crate::mongo_integration::mongo_check_availability::mongo_check_availability;
+use crate::mongo_integration::mongo_check_availability::MongoCheckAvailabilityError;
+use crate::net_check::net_check_availability::net_check_availability;
+use crate::net_check::net_check_availability::NetCheckAvailabilityError;
+use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
+use crate::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityError;
+use crate::traits::tufa_error::TufaError;
+use chrono::DateTime;
+use chrono::FixedOffset;
+use chrono::Local;
+use chrono::Utc;
+use futures::join;
+use std::fmt::Display;
+// use init_error_with_tracing::DeriveInitErrorWithTracing;
+
+//DeriveInitErrorWithTracing
+#[derive(Debug)]
+pub struct CheckAvailabilityError {
+    source: CheckAvailabilityErrorEnum,
+    where_was: Vec<WhereWasOneOrFew>,
+}
+
+impl TufaError for CheckAvailabilityError {
+    fn get_source(&self) -> String {
+        format!("{}", self.source)
+    }
+    fn get_where_was(&self) -> String {
+        match CONFIG.is_debug_implementation_enable {
+            true => format!("{:#?}", self.where_was),
+            false => {
+                let mut content =
+                    self.where_was
+                        .clone()
+                        .iter()
+                        .fold(String::from(""), |mut acc, elem| {
+                            acc.push_str(&format!("{},", elem));
+                            acc
+                        });
+                if !content.is_empty() {
+                    content.pop();
+                }
+                content
+            }
+        }
+    }
+}
+
+impl CheckAvailabilityError {
+    pub fn with_tracing(
+        source: CheckAvailabilityErrorEnum,
+        where_was: Vec<crate::helpers::where_was::WhereWasOneOrFew>,
+    ) -> Self {
+        if where_was.len() == 1 {
+            if let Some(first_value) = where_was.get(0) {
+                match first_value {
+                    crate::helpers::where_was::WhereWasOneOrFew::One(where_was_one) => {
+                        match crate::config_mods::lazy_static_config::CONFIG.source_place_type {
+                            crate::config_mods::source_place_type::SourcePlaceType::Source => {
+                                tracing::error!(
+                                    error = format!("{}", source.get_source()),
+                                    children_source = format!("{}", source.get_where_was()),
+                                    source_place = where_was_one.source_place(),
+                                );
+                            }
+                            crate::config_mods::source_place_type::SourcePlaceType::Github => {
+                                tracing::error!(
+                                    error = format!("{}", source.get_source()),
+                                    children_source = format!("{}", source.get_where_was()),
+                                    github_source_place = where_was_one.github_source_place(),
+                                );
+                            }
+                            crate::config_mods::source_place_type::SourcePlaceType::None => {
+                                tracing::error!(error = format!("{}", source));
+                            }
+                        }
+                    }
+                    crate::helpers::where_was::WhereWasOneOrFew::Few(hs_where_was) => {
+                        tracing::error!(error = "todo WhereWasOneOrFew::Few",)
+                    }
+                }
+            }
+            //todo next elements
+        }
+        Self { source, where_was }
+    }
+}
+
+#[derive(Debug)]
+pub enum CheckAvailabilityErrorEnum {
+    Net(NetCheckAvailabilityError),
+    Postgres(PostgresCheckAvailabilityError),
+    Mongo(MongoCheckAvailabilityError),
+    NetAndMongo {
+        net_source: Box<NetCheckAvailabilityError>,
+        mongo_source: Box<MongoCheckAvailabilityError>,
+    },
+    NetAndPostgres {
+        net_source: Box<NetCheckAvailabilityError>,
+        postgres_source: Box<PostgresCheckAvailabilityError>,
+    },
+    MongoAndPostgres {
+        mongo_source: Box<MongoCheckAvailabilityError>,
+        postgres_source: Box<PostgresCheckAvailabilityError>,
+    },
+    NetAndMongoAndPostgres {
+        net_source: Box<NetCheckAvailabilityError>,
+        mongo_source: Box<MongoCheckAvailabilityError>,
+        postgres_source: Box<PostgresCheckAvailabilityError>,
+    },
+}
+
+impl TufaError for CheckAvailabilityErrorEnum {
+    fn get_source(&self) -> String {
+        match self {
+            CheckAvailabilityErrorEnum::Net(e) => e.get_source(),
+            CheckAvailabilityErrorEnum::Postgres(e) => e.get_source(),
+            CheckAvailabilityErrorEnum::Mongo(e) => e.get_source(),
+            CheckAvailabilityErrorEnum::NetAndMongo {
+                net_source,
+                mongo_source,
+            } => format!("{}, {}", *net_source, *mongo_source), //todo iteration on vec instead of format
+            CheckAvailabilityErrorEnum::NetAndPostgres {
+                net_source,
+                postgres_source,
+            } => format!("{}, {}", *net_source, *postgres_source), //todo iteration on vec instead of format
+            CheckAvailabilityErrorEnum::MongoAndPostgres {
+                mongo_source,
+                postgres_source,
+            } => format!("{}, {}", *mongo_source, *postgres_source), //todo iteration on vec instead of format
+            CheckAvailabilityErrorEnum::NetAndMongoAndPostgres {
+                net_source,
+                mongo_source,
+                postgres_source,
+            } => format!("{}, {}, {}", *net_source, *mongo_source, *postgres_source), //todo iteration on vec instead of format
+        }
+    }
+    fn get_where_was(&self) -> String {
+        match self {
+            CheckAvailabilityErrorEnum::Net(e) => e.get_where_was(),
+            CheckAvailabilityErrorEnum::Postgres(e) => e.get_where_was(),
+            CheckAvailabilityErrorEnum::Mongo(e) => e.get_where_was(),
+            CheckAvailabilityErrorEnum::NetAndMongo {
+                net_source,
+                mongo_source,
+            } => format!(
+                "{}, {}",
+                net_source.get_where_was(),
+                mongo_source.get_where_was()
+            ), //todo
+            CheckAvailabilityErrorEnum::NetAndPostgres {
+                net_source,
+                postgres_source,
+            } => format!(
+                "{}, {}",
+                net_source.get_where_was(),
+                postgres_source.get_where_was()
+            ), //todo
+            CheckAvailabilityErrorEnum::MongoAndPostgres {
+                mongo_source,
+                postgres_source,
+            } => format!(
+                "{}, {}",
+                mongo_source.get_where_was(),
+                postgres_source.get_where_was()
+            ), //todo
+            CheckAvailabilityErrorEnum::NetAndMongoAndPostgres {
+                net_source,
+                mongo_source,
+                postgres_source,
+            } => format!(
+                "{}, {}, {}",
+                net_source.get_where_was(),
+                mongo_source.get_where_was(),
+                postgres_source.get_where_was()
+            ), //todo
+        }
+    }
+}
+
+impl Display for CheckAvailabilityError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match CONFIG.is_debug_implementation_enable {
+            true => write!(f, "{:#?}", self),
+            false => write!(f, "{:?} {}", self.where_was, self.source),
+        }
+    }
+}
+
+impl Display for CheckAvailabilityErrorEnum {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match CONFIG.is_debug_implementation_enable {
+            true => write!(f, "{:#?}", self),
+            false => match self {
+                CheckAvailabilityErrorEnum::Net(source) => {
+                    write!(f, "{}", *source)
+                }
+                CheckAvailabilityErrorEnum::Postgres(source) => {
+                    write!(f, "{}", *source)
+                }
+                CheckAvailabilityErrorEnum::Mongo(source) => {
+                    write!(f, "{}", *source)
+                }
+                CheckAvailabilityErrorEnum::NetAndMongo {
+                    net_source,
+                    mongo_source,
+                } => {
+                    write!(f, "{}\n{}", net_source, mongo_source)
+                }
+                CheckAvailabilityErrorEnum::NetAndPostgres {
+                    net_source,
+                    postgres_source,
+                } => {
+                    write!(f, "{}\n{}", net_source, postgres_source)
+                }
+                CheckAvailabilityErrorEnum::MongoAndPostgres {
+                    mongo_source,
+                    postgres_source,
+                } => {
+                    write!(f, "{}\n{}", mongo_source, postgres_source)
+                }
+                CheckAvailabilityErrorEnum::NetAndMongoAndPostgres {
+                    net_source,
+                    mongo_source,
+                    postgres_source,
+                } => {
+                    write!(f, "{}\n{}\n{}", net_source, mongo_source, postgres_source)
+                }
+            },
+        }
+    }
+}
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub async fn check_availability() -> Result<(), Box<CheckAvailabilityError>> {
+    let net_url = &CONFIG.starting_check_link.clone();
+    let postgres_url = &get_postgres_url();
+    let mongo_url = &get_mongo_url();
+    match join!(
+        net_check_availability(net_url, false),
+        postgres_check_availability(postgres_url, false),
+        mongo_check_availability(mongo_url, false),
+    ) {
+        (Ok(_), Ok(_), Ok(_)) => Ok(()),
+        (Ok(_), Ok(_), Err(m)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            println!("{}", *m);
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::Mongo(*m),
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Ok(_), Err(p), Ok(_)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::Postgres(*p),
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Ok(_), Err(p), Err(m)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::MongoAndPostgres {
+                    mongo_source: m,
+                    postgres_source: p,
+                },
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Err(n), Ok(_), Ok(_)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::Net(*n),
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Err(n), Ok(_), Err(m)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::NetAndMongo {
+                    net_source: n,
+                    mongo_source: m,
+                },
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Err(n), Err(p), Ok(_)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::NetAndPostgres {
+                    net_source: n,
+                    postgres_source: p,
+                },
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+        (Err(n), Err(p), Err(m)) => {
+            let where_was = WhereWas {
+                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            };
+            Err(Box::new(CheckAvailabilityError::with_tracing(
+                CheckAvailabilityErrorEnum::NetAndMongoAndPostgres {
+                    net_source: n,
+                    postgres_source: p,
+                    mongo_source: m,
+                },
+                vec![WhereWasOneOrFew::One(where_was)],
+            )))
+        }
+    }
+}
--- src/preparation/prepare_server.rs
+++ src/preparation/prepare_server.rs
@@ -5,21 +5,15 @@ use crate::helpers::where_was::WhereWas;
 use crate::helpers::where_was::WhereWasOneOrFew;
 use crate::init_dbs_logic::init_dbs::init_dbs;
 use crate::init_dbs_logic::init_tables_enum::InitTablesEnumError;
-use crate::mongo_integration::mongo_check_availability::mongo_check_availability;
-use crate::mongo_integration::mongo_check_availability::MongoCheckAvailabilityError;
-use crate::net_check::net_check_availability::net_check_availability;
-use crate::net_check::net_check_availability::NetCheckAvailabilityError;
-use crate::postgres_integration::postgres_check_availability::postgres_check_availability;
-use crate::postgres_integration::postgres_check_availability::PostgresCheckAvailabilityError;
+use crate::preparation::check_availability::check_availability;
+use crate::preparation::check_availability::CheckAvailabilityError;
 use crate::traits::tufa_error::TufaError;
 use chrono::DateTime;
 use chrono::FixedOffset;
 use chrono::Local;
 use chrono::Utc;
-use futures::join;
 use std::fmt::Display;
 // use init_error_with_tracing::DeriveInitErrorWithTracing;
-
 //DeriveInitErrorWithTracing
 #[derive(Debug)]
 pub struct PreparationError {
@@ -69,52 +63,14 @@ impl PreparationError {
 
 #[derive(Debug)]
 pub enum PreparationErrorEnum {
-    Net(NetCheckAvailabilityError),
-    Postgres(PostgresCheckAvailabilityError),
-    Mongo(MongoCheckAvailabilityError),
-    NetAndMongo {
-        net_source: Box<NetCheckAvailabilityError>,
-        mongo_source: Box<MongoCheckAvailabilityError>,
-    },
-    NetAndPostgres {
-        net_source: Box<NetCheckAvailabilityError>,
-        postgres_source: Box<PostgresCheckAvailabilityError>,
-    },
-    MongoAndPostgres {
-        mongo_source: Box<MongoCheckAvailabilityError>,
-        postgres_source: Box<PostgresCheckAvailabilityError>,
-    },
-    NetAndMongoAndPostgres {
-        net_source: Box<NetCheckAvailabilityError>,
-        mongo_source: Box<MongoCheckAvailabilityError>,
-        postgres_source: Box<PostgresCheckAvailabilityError>,
-    },
+    CheckAvailability(Box<CheckAvailabilityError>),
     InitDbs(Vec<Box<InitTablesEnumError>>),
 }
 
 impl TufaError for PreparationErrorEnum {
     fn get_source(&self) -> String {
         match self {
-            PreparationErrorEnum::Net(e) => e.get_source(),
-            PreparationErrorEnum::Postgres(e) => e.get_source(),
-            PreparationErrorEnum::Mongo(e) => e.get_source(),
-            PreparationErrorEnum::NetAndMongo {
-                net_source,
-                mongo_source,
-            } => format!("{}, {}", *net_source, *mongo_source), //todo iteration on vec instead of format
-            PreparationErrorEnum::NetAndPostgres {
-                net_source,
-                postgres_source,
-            } => format!("{}, {}", *net_source, *postgres_source), //todo iteration on vec instead of format
-            PreparationErrorEnum::MongoAndPostgres {
-                mongo_source,
-                postgres_source,
-            } => format!("{}, {}", *mongo_source, *postgres_source), //todo iteration on vec instead of format
-            PreparationErrorEnum::NetAndMongoAndPostgres {
-                net_source,
-                mongo_source,
-                postgres_source,
-            } => format!("{}, {}, {}", *net_source, *mongo_source, *postgres_source), //todo iteration on vec instead of format
+            PreparationErrorEnum::CheckAvailability(e) => e.get_source(),
             PreparationErrorEnum::InitDbs(e) => {
                 let mut content = e.iter().fold(String::from(""), |mut acc, elem| {
                     acc.push_str(&format!("{:?},", *elem)); //todo
@@ -129,43 +85,7 @@ impl TufaError for PreparationErrorEnum {
     }
     fn get_where_was(&self) -> String {
         match self {
-            PreparationErrorEnum::Net(e) => e.get_where_was(),
-            PreparationErrorEnum::Postgres(e) => e.get_where_was(),
-            PreparationErrorEnum::Mongo(e) => e.get_where_was(),
-            PreparationErrorEnum::NetAndMongo {
-                net_source,
-                mongo_source,
-            } => format!(
-                "{}, {}",
-                net_source.get_where_was(),
-                mongo_source.get_where_was()
-            ), //todo
-            PreparationErrorEnum::NetAndPostgres {
-                net_source,
-                postgres_source,
-            } => format!(
-                "{}, {}",
-                net_source.get_where_was(),
-                postgres_source.get_where_was()
-            ), //todo
-            PreparationErrorEnum::MongoAndPostgres {
-                mongo_source,
-                postgres_source,
-            } => format!(
-                "{}, {}",
-                mongo_source.get_where_was(),
-                postgres_source.get_where_was()
-            ), //todo
-            PreparationErrorEnum::NetAndMongoAndPostgres {
-                net_source,
-                mongo_source,
-                postgres_source,
-            } => format!(
-                "{}, {}, {}",
-                net_source.get_where_was(),
-                mongo_source.get_where_was(),
-                postgres_source.get_where_was()
-            ), //todo
+            PreparationErrorEnum::CheckAvailability(e) => e.get_where_was(),
             PreparationErrorEnum::InitDbs(e) => {
                 let mut content = e.iter().fold(String::from(""), |mut acc, elem| {
                     acc.push_str(&format!("{:?},", *elem)); //todo .get_where_was()
@@ -194,39 +114,8 @@ impl Display for PreparationErrorEnum {
         match CONFIG.is_debug_implementation_enable {
             true => write!(f, "{:#?}", self),
             false => match self {
-                PreparationErrorEnum::Net(source) => {
+                PreparationErrorEnum::CheckAvailability(source) => {
                     write!(f, "{}", *source)
-                }
-                PreparationErrorEnum::Postgres(source) => {
-                    write!(f, "{}", *source)
-                }
-                PreparationErrorEnum::Mongo(source) => {
-                    write!(f, "{}", *source)
-                }
-                PreparationErrorEnum::NetAndMongo {
-                    net_source,
-                    mongo_source,
-                } => {
-                    write!(f, "{}\n{}", net_source, mongo_source)
-                }
-                PreparationErrorEnum::NetAndPostgres {
-                    net_source,
-                    postgres_source,
-                } => {
-                    write!(f, "{}\n{}", net_source, postgres_source)
-                }
-                PreparationErrorEnum::MongoAndPostgres {
-                    mongo_source,
-                    postgres_source,
-                } => {
-                    write!(f, "{}\n{}", mongo_source, postgres_source)
-                }
-                PreparationErrorEnum::NetAndMongoAndPostgres {
-                    net_source,
-                    mongo_source,
-                    postgres_source,
-                } => {
-                    write!(f, "{}\n{}\n{}", net_source, mongo_source, postgres_source)
                 }
                 PreparationErrorEnum::InitDbs(source) => {
                     write!(f, "{:#?}", *source)
@@ -243,120 +132,18 @@ impl Display for PreparationErrorEnum {
     clippy::float_arithmetic
 )]
 pub async fn prepare_server() -> Result<(), Box<PreparationError>> {
-    let net_url = &CONFIG.starting_check_link.clone();
-    let postgres_url = &get_postgres_url();
-    let mongo_url = &get_mongo_url();
-    match join!(
-        net_check_availability(net_url, false),
-        postgres_check_availability(postgres_url, false),
-        mongo_check_availability(mongo_url, false),
-    ) {
-        (Ok(_), Ok(_), Ok(_)) => (),
-        (Ok(_), Ok(_), Err(m)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            println!("{}", *m);
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::Mongo(*m),
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Ok(_), Err(p), Ok(_)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::Postgres(*p),
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Ok(_), Err(p), Err(m)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::MongoAndPostgres {
-                    mongo_source: m,
-                    postgres_source: p,
-                },
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Err(n), Ok(_), Ok(_)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::Net(*n),
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Err(n), Ok(_), Err(m)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::NetAndMongo {
-                    net_source: n,
-                    mongo_source: m,
-                },
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Err(n), Err(p), Ok(_)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::NetAndPostgres {
-                    net_source: n,
-                    postgres_source: p,
-                },
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
-        (Err(n), Err(p), Err(m)) => {
-            let where_was = WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            };
-            return Err(Box::new(PreparationError::with_tracing(
-                PreparationErrorEnum::NetAndMongoAndPostgres {
-                    net_source: n,
-                    postgres_source: p,
-                    mongo_source: m,
-                },
-                vec![WhereWasOneOrFew::One(where_was)],
-            )));
-        }
+    if let Err(e) = check_availability().await {
+        let where_was = WhereWas {
+            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+            file: file!(),
+            line: line!(),
+            column: column!(),
+        };
+        return Err(Box::new(PreparationError::with_tracing(
+            PreparationErrorEnum::CheckAvailability(e),
+            vec![WhereWasOneOrFew::One(where_was)],
+        )));
     }
     //todo: add params dependency function to config after new to check. like if is_mongo_initialization_enabled is true but is_dbs_initialization_enabled is false so is_mongo_initialization_enabled is also false
     if !CONFIG.is_dbs_initialization_enabled
