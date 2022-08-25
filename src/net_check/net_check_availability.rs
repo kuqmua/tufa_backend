4b9cc5662a03f8a9cf6ffb3c2ab5653a75fbf29a
kuqmua <serezhashmakov@outlook.com>
2022-08-25 20:22:36 +0300

    NetCheckAvailabilityError proc macro usage

--- src/net_check/net_check_availability.rs
+++ src/net_check/net_check_availability.rs
@@ -1,127 +1,118 @@
 use crate::config_mods::lazy_static_config::CONFIG;
 use crate::helpers::git_info::GIT_INFO;
 use crate::helpers::where_was::WhereWas;
-use crate::helpers::where_was::WhereWasOneOrFew;
 use crate::traits::git_info_trait::GitInfo;
-use crate::traits::tufa_error::TufaError;
 use chrono::DateTime;
 use chrono::FixedOffset;
 use chrono::Local;
 use chrono::Utc;
 use git_info::GitInfoDerive;
-use init_error::DeriveInitError;
+use impl_display_for_error_struct::ImplDisplayForErrorStruct;
+use impl_display_for_simple_error_enum::ImplDisplayForSimpleErrorEnum;
+use impl_get_source_for_parent_error_struct::ImplGetSourceForParentErrorStruct;
+use impl_get_source_for_simple_error_enum::ImplGetSourceForSimpleErrorEnum;
+use impl_get_where_was_for_error_struct::ImplGetWhereWasForErrorStruct;
 use reqwest::StatusCode;
 use std::fmt;
-extern crate chrono;
-// use init_error_with_tracing::DeriveInitErrorWithTracing;
+
 //DeriveInitErrorWithTracing,
-#[derive(Debug, DeriveInitError)]
+// DeriveInitError
+#[derive(
+    Debug,
+    ImplDisplayForErrorStruct,
+    ImplGetSourceForParentErrorStruct,
+    ImplGetWhereWasForErrorStruct,
+)]
 pub struct NetCheckAvailabilityError {
     source: NetCheckAvailabilityErrorEnum,
-    where_was: Vec<WhereWasOneOrFew>,
+    where_was: WhereWas,
 }
 
 impl NetCheckAvailabilityError {
-    pub fn with_tracing(
-        source: NetCheckAvailabilityErrorEnum,
-        where_was: Vec<crate::helpers::where_was::WhereWasOneOrFew>,
-    ) -> Self {
-        if where_was.len() == 1 {
-            if let Some(first_value) = where_was.get(0) {
-                match first_value {
-                    crate::helpers::where_was::WhereWasOneOrFew::One(where_was_one) => {
-                        //todo different formating for source impl
-                        match crate::config_mods::lazy_static_config::CONFIG.source_place_type {
-                            crate::config_mods::source_place_type::SourcePlaceType::Source => {
-                                tracing::error!(
-                                    error = format!("{}", source),
-                                    // children_source = format!("{}", &self.get_where_was()),
-                                    source = where_was_one.source_place(),
-                                );
-                            }
-                            crate::config_mods::source_place_type::SourcePlaceType::Github => {
-                                tracing::error!(
-                                    error = format!("{}", source),
-                                    // error = format!("{}", &self.get_source()),
-                                    // children_source = format!("{}", &self.get_where_was()),
-                                    github_source = where_was_one.github_source_place(),
-                                );
-                            }
-                            crate::config_mods::source_place_type::SourcePlaceType::None => {
-                                tracing::error!(error = format!("{}", source));
-                            }
-                        }
-                    }
-                    crate::helpers::where_was::WhereWasOneOrFew::Few(hs_where_was) => {
-                        tracing::error!(error = "todo WhereWasOneOrFew::Few",)
-                    }
-                }
+    pub fn with_tracing(source: NetCheckAvailabilityErrorEnum, where_was: WhereWas) -> Self {
+        match crate::config_mods::lazy_static_config::CONFIG.source_place_type {
+            crate::config_mods::source_place_type::SourcePlaceType::Source => {
+                tracing::error!(
+                    error = format!("{}", source),
+                    source = where_was.source_place(),
+                );
             }
-            //todo next elements
+            crate::config_mods::source_place_type::SourcePlaceType::Github => {
+                tracing::error!(
+                    error = format!("{}", source),
+                    github_source = where_was.github_source_place(),
+                );
+            }
+            crate::config_mods::source_place_type::SourcePlaceType::None => {
+                tracing::error!(error = format!("{}", source));
+            }
         }
+        Self { source, where_was }
+    }
+    pub fn new(source: NetCheckAvailabilityErrorEnum, where_was: WhereWas) -> Self {
         Self { source, where_was }
     }
 }
 
-impl TufaError for NetCheckAvailabilityError {
-    fn get_source(&self) -> String {
-        format!("{}", self.source)
-    }
-    fn get_where_was(&self) -> String {
-        match CONFIG.is_debug_implementation_enable {
-            true => format!("{:#?}", self.where_was),
-            false => {
-                let mut content =
-                    self.where_was
-                        .clone()
-                        .iter()
-                        .fold(String::from(""), |mut acc, elem| {
-                            acc.push_str(&format!("{},", elem));
-                            acc
-                        });
-                if !content.is_empty() {
-                    content.pop();
-                }
-                content
-            }
-        }
-    }
-}
+// impl TufaError for NetCheckAvailabilityError {
+//     fn get_source(&self) -> String {
+//         format!("{}", self.source)
+//     }
+//     fn get_where_was(&self) -> String {
+//         match CONFIG.is_debug_implementation_enable {
+//             true => format!("{:#?}", self.where_was),
+//             false => {
+//                 let mut content =
+//                     self.where_was
+//                         .clone()
+//                         .iter()
+//                         .fold(String::from(""), |mut acc, elem| {
+//                             acc.push_str(&format!("{},", elem));
+//                             acc
+//                         });
+//                 if !content.is_empty() {
+//                     content.pop();
+//                 }
+//                 content
+//             }
+//         }
+//     }
+// }
 
-impl fmt::Display for NetCheckAvailabilityError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match CONFIG.is_debug_implementation_enable {
-            true => write!(f, "{:#?}", self),
-            false => write!(f, "{}\n{:#?}", self.source, self.where_was),
-        }
-    }
-}
+// impl fmt::Display for NetCheckAvailabilityError {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match CONFIG.is_debug_implementation_enable {
+//             true => write!(f, "{:#?}", self),
+//             false => write!(f, "{}\n{:#?}", self.source, self.where_was),
+//         }
+//     }
+// }
 
-#[derive(Debug, GitInfoDerive)]
+#[derive(Debug, GitInfoDerive, ImplDisplayForSimpleErrorEnum, ImplGetSourceForSimpleErrorEnum)]
 pub enum NetCheckAvailabilityErrorEnum {
     ReqwestGet(reqwest::Error),
     Client(StatusCode),
     Server(StatusCode),
 }
 
-impl fmt::Display for NetCheckAvailabilityErrorEnum {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match CONFIG.is_debug_implementation_enable {
-            true => write!(f, "{:#?}", self),
-            false => match self {
-                NetCheckAvailabilityErrorEnum::ReqwestGet(e) => {
-                    write!(f, "{}", e)
-                }
-                NetCheckAvailabilityErrorEnum::Client(e) => {
-                    write!(f, "{}", e)
-                }
-                NetCheckAvailabilityErrorEnum::Server(e) => {
-                    write!(f, "{}", e)
-                }
-            },
-        }
-    }
-}
+// impl fmt::Display for NetCheckAvailabilityErrorEnum {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match CONFIG.is_debug_implementation_enable {
+//             true => write!(f, "{:#?}", self),
+//             false => match self {
+//                 NetCheckAvailabilityErrorEnum::ReqwestGet(e) => {
+//                     write!(f, "{}", e)
+//                 }
+//                 NetCheckAvailabilityErrorEnum::Client(e) => {
+//                     write!(f, "{}", e)
+//                 }
+//                 NetCheckAvailabilityErrorEnum::Server(e) => {
+//                     write!(f, "{}", e)
+//                 }
+//             },
+//         }
+//     }
+// }
 
 #[deny(
     clippy::indexing_slicing,
@@ -145,11 +136,11 @@ pub async fn net_check_availability(
             match should_trace {
                 true => Err(Box::new(NetCheckAvailabilityError::with_tracing(
                     NetCheckAvailabilityErrorEnum::ReqwestGet(e),
-                    vec![WhereWasOneOrFew::One(where_was)],
+                    where_was,
                 ))),
                 false => Err(Box::new(NetCheckAvailabilityError::new(
                     NetCheckAvailabilityErrorEnum::ReqwestGet(e),
-                    vec![WhereWasOneOrFew::One(where_was)],
+                    where_was,
                 ))),
             }
         }
@@ -167,13 +158,13 @@ pub async fn net_check_availability(
                     true => {
                         return Err(Box::new(NetCheckAvailabilityError::with_tracing(
                             NetCheckAvailabilityErrorEnum::Client(status),
-                            vec![WhereWasOneOrFew::One(where_was)],
+                            where_was,
                         )));
                     }
                     false => {
                         return Err(Box::new(NetCheckAvailabilityError::new(
                             NetCheckAvailabilityErrorEnum::Client(status),
-                            vec![WhereWasOneOrFew::One(where_was)],
+                            where_was,
                         )));
                     }
                 }
@@ -190,13 +181,13 @@ pub async fn net_check_availability(
                     true => {
                         return Err(Box::new(NetCheckAvailabilityError::with_tracing(
                             NetCheckAvailabilityErrorEnum::Server(status),
-                            vec![WhereWasOneOrFew::One(where_was)],
+                            where_was,
                         )));
                     }
                     false => {
                         return Err(Box::new(NetCheckAvailabilityError::new(
                             NetCheckAvailabilityErrorEnum::Server(status),
-                            vec![WhereWasOneOrFew::One(where_was)],
+                            where_was,
                         )));
                     }
                 }
