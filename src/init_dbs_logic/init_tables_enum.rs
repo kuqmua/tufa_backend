0f5c82f2ef052754515a52c94de06422d7d255e5
kuqmua <serezhashmakov@outlook.com>
2022-09-07 19:20:49 +0300

    impl InitTablesError {     pub fn with_tracing

--- src/init_dbs_logic/init_tables_enum.rs
+++ src/init_dbs_logic/init_tables_enum.rs
@@ -2,10 +2,13 @@ use crate::config_mods::lazy_static_config::CONFIG;
 use crate::helpers::where_was::WhereWas;
 use crate::init_dbs_logic::init_dbs_with_providers_link_parts::init_dbs_with_providers_link_parts;
 use crate::init_dbs_logic::init_dbs_with_providers_link_parts::InitDbsProvidersLinkPartsError;
+use crate::traits::get_source::GetSource;
 use chrono::DateTime;
 use chrono::FixedOffset;
 use chrono::Local;
 use chrono::Utc;
+use impl_get_where_was_for_error_struct::ImplGetWhereWasForErrorStruct;
+use init_error::InitError;
 use strum_macros::EnumIter;
 
 #[derive(Debug, EnumIter)]
@@ -13,12 +16,64 @@ pub enum InitTablesEnum {
     ProvidersLinkParts,
 }
 
+#[derive(Debug, ImplGetWhereWasForErrorStruct, InitError)]
+pub struct InitTablesError {
+    source: InitTablesErrorEnum,
+    where_was: WhereWas,
+}
+
 #[derive(Debug)]
-pub enum InitTablesEnumError {
-    ProvidersLinkParts {
-        source: InitDbsProvidersLinkPartsError,
-        where_was: WhereWas,
-    },
+pub enum InitTablesErrorEnum {
+    ProvidersLinkParts(InitDbsProvidersLinkPartsError),
+}
+
+impl crate::traits::get_source::GetSource for InitTablesErrorEnum {
+    fn get_source(&self) -> String {
+        match crate::config_mods::lazy_static_config::CONFIG.is_debug_implementation_enable {
+            true => format!("{:#?}", self),
+            false => {
+                let mut formatted = match self {
+                    InitTablesErrorEnum::ProvidersLinkParts(e) => e.get_source(),
+                };
+                if !formatted.is_empty() {
+                    formatted.pop();
+                }
+                formatted
+            }
+        }
+    }
+}
+
+impl InitTablesError {
+    pub fn with_tracing(source: InitTablesErrorEnum, where_was: WhereWas) -> Self {
+        match crate::config_mods::lazy_static_config::CONFIG.source_place_type {
+            crate::config_mods::source_place_type::SourcePlaceType::Source => {
+                tracing::error!(
+                    error = source.get_source(),
+                    source_place = where_was.source_place(),
+                );
+            }
+            crate::config_mods::source_place_type::SourcePlaceType::Github => {
+                tracing::error!(
+                    error = source.get_source(),
+                    github_source_place = where_was.github_source_place(),
+                );
+            }
+            crate::config_mods::source_place_type::SourcePlaceType::None => {
+                tracing::error!(error = source.get_source());
+            }
+        }
+        Self { source, where_was }
+    }
+}
+
+impl crate::traits::get_source::GetSource for InitTablesError {
+    fn get_source(&self) -> String {
+        match crate::config_mods::lazy_static_config::CONFIG.is_debug_implementation_enable {
+            true => format!("{:#?}", self.source),
+            false => self.source.get_source(),
+        }
+    }
 }
 
 impl InitTablesEnum {
@@ -28,20 +83,31 @@ impl InitTablesEnum {
         clippy::integer_arithmetic,
         clippy::float_arithmetic
     )]
-    pub async fn init(&self, should_trace: bool) -> Result<(), Box<InitTablesEnumError>> {
+    pub async fn init(&self, should_trace: bool) -> Result<(), Box<InitTablesError>> {
         match self {
             InitTablesEnum::ProvidersLinkParts => {
                 if let Err(e) = init_dbs_with_providers_link_parts(false).await {
-                    return Err(Box::new(InitTablesEnumError::ProvidersLinkParts {
-                        source: *e,
-                        where_was: WhereWas {
-                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                            file: file!(),
-                            line: line!(),
-                            column: column!(),
-                        },
-                    }));
+                    let where_was = WhereWas {
+                        time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                            .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                        file: file!(),
+                        line: line!(),
+                        column: column!(),
+                    };
+                    match should_trace {
+                        true => {
+                            return Err(Box::new(InitTablesError::with_tracing(
+                                InitTablesErrorEnum::ProvidersLinkParts(*e),
+                                where_was,
+                            )));
+                        }
+                        false => {
+                            return Err(Box::new(InitTablesError::new(
+                                InitTablesErrorEnum::ProvidersLinkParts(*e),
+                                where_was,
+                            )));
+                        }
+                    }
                 }
             }
         }
