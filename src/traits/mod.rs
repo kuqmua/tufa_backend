b701e5344ce4b1f60a16576ce43ac0a3e118329f
kuqmua <serezhashmakov@outlook.com>
2022-09-08 20:39:18 +0300

    cleaning

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -3,5 +3,4 @@ pub mod get_where_was;
 pub mod git_info_trait;
 pub mod print_type_trait;
 pub mod provider_kind_trait;
-pub mod tufa_error;
 pub mod wrap_config_checks_trait;
--- src/traits/tufa_error.rs
+++
@@ -1,6 +0,0 @@
-use crate::helpers::where_was::WhereWas;
-
-pub trait TufaError {
-    fn get_source(&self) -> String;
-    fn get_where_was(&self) -> String; //Vec<WhereWasOneOrFew>
-}
