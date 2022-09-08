6a3e381caf0fabac1c3c824293ab68d1db27d689
kuqmua <serezhashmakov@outlook.com>
2022-09-08 18:23:35 +0300

    lil tweaks

--- src/providers/providers_info/providers_init_json_schema.rs
+++ src/providers/providers_info/providers_init_json_schema.rs
@@ -1,4 +1,6 @@
-#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[derive(
+    Default, Debug, Clone, PartialEq, Eq, serde_derive::Serialize, serde_derive::Deserialize,
+)]
 pub struct ProvidersInitJsonSchema {
     pub data: Vec<String>,
 }
