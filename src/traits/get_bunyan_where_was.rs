a8f99f8edabac993ce5cfcce4836c8cb738fde14
kuqmua <serezhashmakov@outlook.com>
2022-09-10 14:00:55 +0300

    fn get_bunyan_format(&self) -> String

--- src/traits/get_bunyan_where_was.rs
+++ src/traits/get_bunyan_where_was.rs
@@ -11,8 +11,23 @@ where
 {
     fn get_bunyan_format(&self) -> String {
         match self.get_where_was_one_or_many() {
-            WhereWasOneOrMany::One(where_was) => String::from("todo"),
-            WhereWasOneOrMany::Many(vec_where_was_with_addition) => String::from("todo"),
+            WhereWasOneOrMany::One(where_was) => where_was.file_line_column(),
+            WhereWasOneOrMany::Many(vec_where_was_with_addition) => {
+                let mut formatted_into_string_vec = vec_where_was_with_addition
+                    .iter()
+                    .map(|where_was_with_addition| format!("{}, ", where_was_with_addition)) //maybe here use \n
+                    .collect::<Vec<String>>()
+                    .iter()
+                    .fold(String::from(""), |mut acc, elem| {
+                        acc.push_str(elem);
+                        acc
+                    });
+                if !formatted_into_string_vec.is_empty() {
+                    formatted_into_string_vec.pop();
+                    formatted_into_string_vec.pop();
+                }
+                format!("[{}]", formatted_into_string_vec)
+            }
         }
     }
 }
