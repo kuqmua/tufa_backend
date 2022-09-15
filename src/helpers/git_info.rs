c36de6195cb69b5580ee11748c3f21ab3f21e5a0
kuqmua <serezhashmakov@outlook.com>
2022-09-15 23:03:23 +0300

    lil tweaks

--- src/helpers/git_info.rs
+++ src/helpers/git_info.rs
@@ -1,23 +1,6 @@
 use crate::project_constants::PROJECT_NAME;
 use lazy_static::lazy_static;
-use tufa_common::helpers::git::git_info::GitInformation;
-
-pub struct GitInformationWrapper {
-    pub data: GitInformation,
-}
-
-impl GitInformationWrapper {
-    pub fn init(repo_git_path: &str, repo_name: &str) -> Self {
-        GitInformationWrapper {
-            data: GitInformation::get_git_commit_info(repo_git_path, repo_name),
-        }
-    }
-}
-
-// lazy_static! {
-//     pub static ref GIT_INFO: GitInformation =
-//         GitInformation::get_git_commit_info("./", PROJECT_NAME);
-// }
+use tufa_common::helpers::git::git_info::GitInformationWrapper;
 
 lazy_static! {
     pub static ref GIT_INFO: GitInformationWrapper =
