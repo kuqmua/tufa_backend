b374501cd36370de4408bf7d579d760b8bd81cd0
kuqmua <serezhashmakov@outlook.com>
2022-10-08 12:45:18 +0300

    init_mongo to 4 different functions

--- src/init_dbs_logic/init_mongo.rs
+++ src/init_dbs_logic/init_mongo.rs
@@ -1,5 +1,15 @@
 use crate::lazy_static::config::CONFIG;
 use crate::lazy_static::git_info::GIT_INFO;
+use crate::mongo_integration::mongo_check_collection_is_not_empty::mongo_check_collections_is_not_empty;
+use crate::mongo_integration::mongo_check_collection_is_not_empty::MongoCheckCollectionsIsNotEmptyError;
+use crate::mongo_integration::mongo_client_options_parse::mongo_client_options_parse;
+use crate::mongo_integration::mongo_client_options_parse::MongoClientOptionsParseError;
+use crate::mongo_integration::mongo_client_with_options::mongo_client_with_options;
+use crate::mongo_integration::mongo_client_with_options::MongoClientWithOptionsError;
+
+use crate::mongo_integration::mongo_insert_many::mongo_insert_many;
+use crate::mongo_integration::mongo_insert_many::MongoInsertManyError;
+
 use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
 use crate::traits::provider_kind_trait::ProviderKindTrait;
 use chrono::DateTime;
@@ -8,7 +18,7 @@ use chrono::Local;
 use chrono::Utc;
 use futures::future::join_all;
 use impl_error_with_tracing_for_struct_with_get_source_without_get_where_was::ImplErrorWithTracingForStructWithGetSourceWithoutGetWhereWas;
-use impl_get_source_for_enum_without_method::ImplGetSourceForEnumWithoutMethod;
+use impl_get_source_for_enum_with_method::ImplGetSourceForEnumWithMethod;
 use impl_get_source_for_struct_with_method::ImplGetSourceForStructWithMethod;
 use impl_get_where_was_one_or_many_one_for_error_struct::ImplGetWhereWasOneOrManyOneForErrorStruct;
 use init_error::InitError;
@@ -35,29 +45,12 @@ pub struct InitMongoError {
     where_was: WhereWas,
 }
 
-#[derive(Debug, ImplGetSourceForEnumWithoutMethod)]
+#[derive(Debug, ImplGetSourceForEnumWithMethod)]
 pub enum InitMongoErrorEnum {
-    ClientOptionsParse(Error),
-    ClientWithOptions(Error),
-    CollectionCountDocumentsOrIsNotEmpty(
-        HashMap<ProviderKind, CollectionCountDocumentsOrIsNotEmpty>,
-    ),
-    InsertManyError(HashMap<ProviderKind, Error>),
-}
-
-#[derive(Debug)]
-pub enum CollectionCountDocumentsOrIsNotEmpty {
-    CountDocuments(Error),
-    IsNotEmpty(u64),
-}
-
-impl std::fmt::Display for CollectionCountDocumentsOrIsNotEmpty {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            CollectionCountDocumentsOrIsNotEmpty::CountDocuments(e) => write!(f, "{}", e),
-            CollectionCountDocumentsOrIsNotEmpty::IsNotEmpty(e) => write!(f, "{}", e),
-        }
-    }
+    ClientOptionsParse(MongoClientOptionsParseError),
+    ClientWithOptions(MongoClientWithOptionsError),
+    CollectionCountDocumentsOrIsNotEmpty(MongoCheckCollectionsIsNotEmptyError),
+    InsertManyError(MongoInsertManyError),
 }
 
 #[deny(
@@ -70,112 +63,106 @@ pub async fn init_mongo(
     providers_json_local_data_hashmap: HashMap<ProviderKind, Vec<String>>,
     should_trace: bool,
 ) -> Result<(), Box<InitMongoError>> {
-    match ClientOptions::parse(&CONFIG.get_mongo_url()).await {
-        Err(e) => Err(Box::new(InitMongoError::init_error_with_possible_trace(
-            InitMongoErrorEnum::ClientOptionsParse(e),
-            WhereWas {
-                time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                    .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                file: file!(),
-                line: line!(),
-                column: column!(),
-            },
-            &CONFIG.source_place_type,
-            &GIT_INFO.data,
-            should_trace,
-        ))),
-        Ok(client_options) => match Client::with_options(client_options) {
-            Err(e) => Err(Box::new(InitMongoError::init_error_with_possible_trace(
-                InitMongoErrorEnum::ClientWithOptions(e),
-                WhereWas {
-                    time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                        .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                    file: file!(),
-                    line: line!(),
-                    column: column!(),
-                },
-                &CONFIG.source_place_type,
-                &GIT_INFO.data,
-                should_trace,
-            ))),
-            Ok(client) => {
-                let db = client.database(&CONFIG.mongo_providers_link_parts_db_name);
-                let error_vec_count_documents =
-                    join_all(providers_json_local_data_hashmap.keys().map(|pk| async {
-                        (
-                            *pk,
-                            db.collection::<Document>(&pk.get_db_tag())
-                                .count_documents(None, None)
-                                .await,
-                        )
-                    }))
-                    .await
-                    .into_iter()
-                    .filter_map(|(pk, result)| match result {
-                        Err(e) => {
-                            Some((pk, CollectionCountDocumentsOrIsNotEmpty::CountDocuments(e)))
-                        }
-                        Ok(documents_number) => {
-                            if documents_number > 0 {
-                                return Some((
-                                    pk,
-                                    CollectionCountDocumentsOrIsNotEmpty::IsNotEmpty(
-                                        documents_number,
-                                    ),
-                                ));
-                            }
-                            None
-                        }
-                    })
-                    .collect::<HashMap<ProviderKind, CollectionCountDocumentsOrIsNotEmpty>>();
-                if !error_vec_count_documents.is_empty() {
-                    return Err(Box::new(InitMongoError::init_error_with_possible_trace(
-                        InitMongoErrorEnum::CollectionCountDocumentsOrIsNotEmpty(
-                            error_vec_count_documents,
-                        ),
-                        WhereWas {
-                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                            file: file!(),
-                            line: line!(),
-                            column: column!(),
-                        },
-                        &CONFIG.source_place_type,
-                        &GIT_INFO.data,
-                        should_trace,
-                    )));
-                }
-                drop(error_vec_count_documents);
-                let error_vec_insert_many = join_all(providers_json_local_data_hashmap.iter().map(|(pk, data_vec)| async {
-                                        let docs: Vec<Document> = data_vec.iter().map(|data| doc! { &CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: data }).collect();
-                                        (*pk, db.collection(&pk.get_db_tag()).insert_many(docs, None).await)
-                                    })).await
-                    .into_iter()
-                    .filter_map(|(pk, result)| {
-                        if let Err(e) = result {
-                            return Some((pk, e));
-                        }
-                        None
-                    })
-                    .collect::<HashMap<ProviderKind, Error>>();
-                if !error_vec_insert_many.is_empty() {
-                    return Err(Box::new(InitMongoError::init_error_with_possible_trace(
-                        InitMongoErrorEnum::InsertManyError(error_vec_insert_many),
-                        WhereWas {
-                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
-                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
-                            file: file!(),
-                            line: line!(),
-                            column: column!(),
-                        },
-                        &CONFIG.source_place_type,
-                        &GIT_INFO.data,
-                        should_trace,
-                    )));
-                }
-                Ok(())
-            }
-        },
-    }
-    //
+    todo!()
+    // match mongo_client_options_parse(false).await {
+    //     Err(e) => Err(Box::new(InitMongoError::init_error_with_possible_trace(
+    //         InitMongoErrorEnum::ClientOptionsParse(*e),
+    //         WhereWas {
+    //             time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+    //                 .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+    //             file: file!(),
+    //             line: line!(),
+    //             column: column!(),
+    //         },
+    //         &CONFIG.source_place_type,
+    //         &GIT_INFO.data,
+    //         should_trace,
+    //     ))),
+    //     Ok(client_options) => match mongo_client_with_options(client_options, false) {
+    //         Err(e) => Err(Box::new(InitMongoError::init_error_with_possible_trace(
+    //             InitMongoErrorEnum::ClientWithOptions(*e),
+    //             WhereWas {
+    //                 time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+    //                     .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+    //                 file: file!(),
+    //                 line: line!(),
+    //                 column: column!(),
+    //             },
+    //             &CONFIG.source_place_type,
+    //             &GIT_INFO.data,
+    //             should_trace,
+    //         ))),
+    //         Ok(client) => {
+    //             let db = client.database(&CONFIG.mongo_providers_link_parts_db_name);
+    //             if let Err(error_vec_count_documents) = mongo_check_collections_is_not_empty(
+    //                 providers_json_local_data_hashmap.clone(),
+    //                 &db,
+    //                 false,
+    //             )
+    //             .await
+    //             {
+    //                 return Err(Box::new(InitMongoError::init_error_with_possible_trace(
+    //                     InitMongoErrorEnum::CollectionCountDocumentsOrIsNotEmpty(
+    //                         *error_vec_count_documents,
+    //                     ),
+    //                     WhereWas {
+    //                         time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+    //                             .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+    //                         file: file!(),
+    //                         line: line!(),
+    //                         column: column!(),
+    //                     },
+    //                     &CONFIG.source_place_type,
+    //                     &GIT_INFO.data,
+    //                     should_trace,
+    //                 )));
+    //             }
+    //             if let Err(error_vec_insert_many) =
+    //                 mongo_insert_many(providers_json_local_data_hashmap, db, false).await
+    //             {
+    //                 return Err(Box::new(InitMongoError::init_error_with_possible_trace(
+    //                     InitMongoErrorEnum::InsertManyError(error_vec_insert_many),
+    //                     WhereWas {
+    //                         time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+    //                             .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+    //                         file: file!(),
+    //                         line: line!(),
+    //                         column: column!(),
+    //                     },
+    //                     &CONFIG.source_place_type,
+    //                     &GIT_INFO.data,
+    //                     should_trace,
+    //                 )));
+    //             }
+    //             // let error_vec_insert_many = join_all(providers_json_local_data_hashmap.iter().map(|(pk, data_vec)| async {
+    //             //                         let docs: Vec<Document> = data_vec.iter().map(|data| doc! { &CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: data }).collect();
+    //             //                         (*pk, db.collection(&pk.get_db_tag()).insert_many(docs, None).await)
+    //             //                     })).await
+    //             //     .into_iter()
+    //             //     .filter_map(|(pk, result)| {
+    //             //         if let Err(e) = result {
+    //             //             return Some((pk, e));
+    //             //         }
+    //             //         None
+    //             //     })
+    //             //     .collect::<HashMap<ProviderKind, Error>>();
+    //             // if !error_vec_insert_many.is_empty() {
+    //             //     return Err(Box::new(InitMongoError::init_error_with_possible_trace(
+    //             //         InitMongoErrorEnum::InsertManyError(error_vec_insert_many),
+    //             //         WhereWas {
+    //             //             time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+    //             //                 .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+    //             //             file: file!(),
+    //             //             line: line!(),
+    //             //             column: column!(),
+    //             //         },
+    //             //         &CONFIG.source_place_type,
+    //             //         &GIT_INFO.data,
+    //             //         should_trace,
+    //             //     )));
+    //             // }
+    //             Ok(())
+    //         }
+    //     },
+    // }
 }
