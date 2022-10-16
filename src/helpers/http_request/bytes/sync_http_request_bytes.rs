12a5fb70083a7b9671748bd83ae7ba6be7639085
kuqmua <serezhashmakov@outlook.com>
2022-10-16 16:11:06 +0300

    http request bytes

---
+++ src/helpers/http_request/bytes/sync_http_request_bytes.rs
@@ -0,0 +1,76 @@
+use super::http_request_bytes_error::HttpRequestBytesError;
+use crate::helpers::http_request::bytes::http_request_bytes_error::HttpRequestBytesErrorEnum;
+use crate::lazy_static::config::CONFIG;
+use crate::lazy_static::git_info::GIT_INFO;
+use chrono::DateTime;
+use chrono::FixedOffset;
+use chrono::Local;
+use chrono::Utc;
+use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use tufa_common::where_was::WhereWas;
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub fn sync_http_request_bytes(
+    link: &str,
+    should_trace: bool,
+) -> Result<actix_web::web::Bytes, Box<HttpRequestBytesError>> {
+    match reqwest::blocking::get(link) {
+        Err(e) => Err(Box::new(
+            HttpRequestBytesError::init_error_with_possible_trace(
+                HttpRequestBytesErrorEnum::ReqwestBlockingGet(e),
+                WhereWas {
+                    time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                        .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                },
+                &CONFIG.source_place_type,
+                &GIT_INFO.data,
+                should_trace,
+            ),
+        )),
+        Ok(res) => {
+            if let Err(e) = res.error_for_status_ref() {
+                return Err(Box::new(
+                    HttpRequestBytesError::init_error_with_possible_trace(
+                        HttpRequestBytesErrorEnum::StatusCode(e),
+                        WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                ));
+            }
+            match res.bytes() {
+                Err(e) => Err(Box::new(
+                    HttpRequestBytesError::init_error_with_possible_trace(
+                        HttpRequestBytesErrorEnum::ResponseBytes(e),
+                        WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                )),
+                Ok(bytes) => Ok(bytes),
+            }
+        }
+    }
+}
