76353a4de406e6e6f829f7b47d43ecbc095f0d6e
kuqmua <serezhashmakov@outlook.com>
2022-10-16 16:32:04 +0300

    http_request_content_length

---
+++ src/helpers/http_request/content_length/async_http_request_content_length.rs
@@ -0,0 +1,59 @@
+use super::http_request_content_length_error::HttpRequestContentLengthError;
+use crate::helpers::http_request::content_length::http_request_content_length_error::HttpRequestContentLengthErrorEnum;
+use crate::lazy_static::config::CONFIG;
+use crate::lazy_static::git_info::GIT_INFO;
+use chrono::DateTime;
+use chrono::FixedOffset;
+use chrono::Local;
+use chrono::Utc;
+use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use tufa_common::where_was::WhereWas;
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub async fn async_http_request_content_length(
+    link: &str,
+    should_trace: bool,
+) -> Result<Option<u64>, Box<HttpRequestContentLengthError>> {
+    match reqwest::get(link).await {
+        Err(e) => Err(Box::new(
+            HttpRequestContentLengthError::init_error_with_possible_trace(
+                HttpRequestContentLengthErrorEnum::ReqwestBlockingGet(e),
+                WhereWas {
+                    time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                        .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                },
+                &CONFIG.source_place_type,
+                &GIT_INFO.data,
+                should_trace,
+            ),
+        )),
+        Ok(res) => {
+            if let Err(e) = res.error_for_status_ref() {
+                return Err(Box::new(
+                    HttpRequestContentLengthError::init_error_with_possible_trace(
+                        HttpRequestContentLengthErrorEnum::StatusCode(e),
+                        WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                ));
+            }
+            Ok(res.content_length())
+        }
+    }
+}
--- src/helpers/http_request/mod.rs
+++ src/helpers/http_request/mod.rs
@@ -1,3 +1,4 @@
 pub mod bytes;
+pub mod content_length;
 pub mod json;
 pub mod text;
