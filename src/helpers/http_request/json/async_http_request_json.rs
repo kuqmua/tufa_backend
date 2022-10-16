f2babf5b0f30f28faaf81f2b74796e9afc1c2c89
kuqmua <serezhashmakov@outlook.com>
2022-10-16 15:52:23 +0300

    http_request json

---
+++ src/helpers/http_request/json/async_http_request_json.rs
@@ -0,0 +1,76 @@
+use super::http_request_json_error::HttpRequestJsonError;
+use crate::helpers::http_request::json::http_request_json_error::HttpRequestJsonErrorEnum;
+use crate::lazy_static::config::CONFIG;
+use crate::lazy_static::git_info::GIT_INFO;
+use chrono::DateTime;
+use chrono::FixedOffset;
+use chrono::Local;
+use chrono::Utc;
+use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use tufa_common::where_was::WhereWas;
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub async fn async_http_request_json(
+    link: &str,
+    should_trace: bool,
+) -> Result<String, Box<HttpRequestJsonError>> {
+    match reqwest::get(link).await {
+        Err(e) => Err(Box::new(
+            HttpRequestJsonError::init_error_with_possible_trace(
+                HttpRequestJsonErrorEnum::ReqwestBlockingGet(e),
+                WhereWas {
+                    time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                        .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                    file: file!(),
+                    line: line!(),
+                    column: column!(),
+                },
+                &CONFIG.source_place_type,
+                &GIT_INFO.data,
+                should_trace,
+            ),
+        )),
+        Ok(res) => {
+            if let Err(e) = res.error_for_status_ref() {
+                return Err(Box::new(
+                    HttpRequestJsonError::init_error_with_possible_trace(
+                        HttpRequestJsonErrorEnum::StatusCode(e),
+                        WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                ));
+            }
+            match res.json().await {
+                Err(e) => Err(Box::new(
+                    HttpRequestJsonError::init_error_with_possible_trace(
+                        HttpRequestJsonErrorEnum::ResponseJson(e),
+                        WhereWas {
+                            time: DateTime::<Utc>::from_utc(Local::now().naive_utc(), Utc)
+                                .with_timezone(&FixedOffset::east(CONFIG.timezone)),
+                            file: file!(),
+                            line: line!(),
+                            column: column!(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                )),
+                Ok(json) => Ok(json),
+            }
+        }
+    }
+}
