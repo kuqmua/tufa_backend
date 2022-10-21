a64007452b94eec6a7e6dab4dead2c2f9287e14d
kuqmua <serezhashmakov@outlook.com>
2022-10-21 20:48:54 +0300

    fix multipart method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     tufa_server::entry::entry();
+    println!("{:?}", location());
 }
+
+#[track_caller]
+fn location() -> core::panic::Location<'static> {
+    *core::panic::Location::caller()
+}
+
+// fn main() {
+//     println!("{:?}", location());
+//     println!("{:?}", location());
+// }
