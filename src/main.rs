ba672dfe073154faecbc9db5af53764dcbd41659
kuqmua <serezhashmakov@outlook.com>
2022-10-21 21:35:20 +0300

    location example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     tufa_server::entry::entry();
-    println!("{:?}", location());
+    let l = location();
+
+    let g = wrapper();
 }
 
 #[track_caller]
@@ -8,7 +10,37 @@ fn location() -> core::panic::Location<'static> {
     *core::panic::Location::caller()
 }
 
-// fn main() {
-//     println!("{:?}", location());
-//     println!("{:?}", location());
-// }
+fn wrapper() -> Result<(), FF> {
+    one()?;
+    two()?;
+    Ok(())
+}
+
+enum FF {
+    One(bool),
+    Two(u32),
+}
+
+impl From<bool> for FF {
+    fn from(e: bool) -> Self {
+        println!("1{:#?}", location());
+        FF::One(e)
+    }
+}
+
+impl From<u32> for FF {
+    fn from(e: u32) -> Self {
+        println!("2{:#?}", location());
+        FF::Two(e)
+    }
+}
+
+struct Two {}
+
+fn one() -> Result<(), bool> {
+    Err(true)
+}
+
+fn two() -> Result<(), u32> {
+    Err(32)
+}
