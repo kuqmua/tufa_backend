f245fbb430c6993be50e6d1c13db5a0dfda5c757
kuqmua <serezhashmakov@outlook.com>
2022-10-21 19:01:25 +0300

    additional declarations

--- src/helpers/http_request/get/mod.rs
+++ src/helpers/http_request/get/mod.rs
@@ -1,10 +1,21 @@
-pub mod bytes;
-// pub mod bytes_stream;
-// pub mod chunk;
-pub mod content_length;
-// pub mod error_for_status;
-pub mod json;
+// pub mod status;
+// pub mod headers;
+// pub mod headers_mut;
+// pub mod cookies;
+pub mod version;
+// pub mod url;
 pub mod remote_addr;
+// pub mod extenstions;
+// pub mod extenstions_mut;
+pub mod bytes;
+pub mod content_length;
+pub mod json;
 pub mod text;
 pub mod text_with_charset;
-pub mod version;
+// pub mod copy_to;//sync only
+// pub mod error_for_status;
+// pub mod error_for_status_ref;
+
+// pub mod chunk;//async only
+// pub mod bytes_stream;//async only
+// pub mod upgrade;//async only
