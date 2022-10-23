ef6bcca90bab21da31ab906796f2bcb318d2505e
kuqmua <serezhashmakov@outlook.com>
2022-10-23 17:08:51 +0300

    http response upgrade

--- src/helpers/http_request/get/mod.rs
+++ src/helpers/http_request/get/mod.rs
@@ -3,7 +3,7 @@ pub mod headers_mut;
 pub mod status;
 // pub mod cookies;
 pub mod bytes;
-pub mod bytes_stream;
+pub mod bytes_stream; //async only
 pub mod chunk;
 pub mod content_length;
 pub mod copy_to; //sync only
@@ -14,7 +14,7 @@ pub mod json;
 pub mod remote_addr;
 pub mod text;
 pub mod text_with_charset;
+pub mod upgrade;
 pub mod url;
-pub mod version; //async only //async only
-                 // pub mod upgrade;//async only
+pub mod version; //async only
                  // pub mod error_for_status_ref;//dont need it i think
