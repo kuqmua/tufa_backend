02cf177532577c5e63092107c29db88e1dcdf390
kuqmua <serezhashmakov@outlook.com>
2022-10-23 15:17:53 +0300

    copy_to

--- src/helpers/http_request/get/mod.rs
+++ src/helpers/http_request/get/mod.rs
@@ -4,6 +4,8 @@ pub mod status;
 // pub mod cookies;
 pub mod bytes;
 pub mod content_length;
+pub mod copy_to; //sync only
+pub mod error_for_status;
 pub mod extensions;
 pub mod extensions_mut;
 pub mod json;
@@ -12,9 +14,7 @@ pub mod text;
 pub mod text_with_charset;
 pub mod url;
 pub mod version;
-// pub mod copy_to;//sync only
-pub mod error_for_status;
-// pub mod error_for_status_ref;
+// pub mod error_for_status_ref;//dont need it i think
 
 // pub mod chunk;//async only
 // pub mod bytes_stream;//async only
