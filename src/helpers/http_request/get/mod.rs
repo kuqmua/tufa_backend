fd4a888db329ad2765038db8306061b2428ccdfe
kuqmua <serezhashmakov@outlook.com>
2022-10-23 16:32:44 +0300

    async_http_request_bytes_stream

--- src/helpers/http_request/get/mod.rs
+++ src/helpers/http_request/get/mod.rs
@@ -3,6 +3,8 @@ pub mod headers_mut;
 pub mod status;
 // pub mod cookies;
 pub mod bytes;
+pub mod bytes_stream;
+pub mod chunk;
 pub mod content_length;
 pub mod copy_to; //sync only
 pub mod error_for_status;
@@ -13,9 +15,6 @@ pub mod remote_addr;
 pub mod text;
 pub mod text_with_charset;
 pub mod url;
-pub mod version;
-// pub mod error_for_status_ref;//dont need it i think
-
-pub mod chunk; //async only
-               // pub mod bytes_stream;//async only
-               // pub mod upgrade;//async only
+pub mod version; //async only //async only
+                 // pub mod upgrade;//async only
+                 // pub mod error_for_status_ref;//dont need it i think
