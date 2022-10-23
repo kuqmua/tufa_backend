b6d0e52cc918863b7756036dc2c6edc72bc06233
kuqmua <serezhashmakov@outlook.com>
2022-10-23 17:23:45 +0300

    reqwest cookies commented but not done

---
+++ src/helpers/http_request/get/cookies/async_http_request_cookies.rs
@@ -0,0 +1,54 @@
+// use super::http_request_cookies_error::HttpRequestCookiesError;
+// use crate::helpers::http_request::get::cookies::http_request_cookies_error::HttpRequestCookiesErrorEnum;
+// use crate::lazy_static::config::CONFIG;
+// use crate::lazy_static::git_info::GIT_INFO;
+// use reqwest::RequestBuilder;
+// use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+// use tufa_common::where_was::WhereWas;
+
+// #[deny(
+//     clippy::indexing_slicing,
+//     clippy::unwrap_used,
+//     clippy::integer_arithmetic,
+//     clippy::float_arithmetic
+// )]
+// pub async fn async_http_request_cookies(
+//     request_builder: RequestBuilder,
+//     should_trace: bool,
+// ) -> Result<impl Iterator<Item = reqwest::cookie::Cookie<'a>> + 'a, Box<HttpRequestCookiesError>> {
+//     match request_builder.send().await {
+//         Err(e) => Err(Box::new(
+//             HttpRequestCookiesError::init_error_with_possible_trace(
+//                 HttpRequestCookiesErrorEnum::RequestBuilderSend(e),
+//                 WhereWas {
+//                     time: std::time::SystemTime::now()
+//                         .duration_since(std::time::UNIX_EPOCH)
+//                         .expect("cannot convert time to unix_epoch"),
+//                     location: *core::panic::Location::caller(),
+//                 },
+//                 &CONFIG.source_place_type,
+//                 &GIT_INFO.data,
+//                 should_trace,
+//             ),
+//         )),
+//         Ok(res) => {
+//             if let Err(e) = res.error_for_status_ref() {
+//                 return Err(Box::new(
+//                     HttpRequestCookiesError::init_error_with_possible_trace(
+//                         HttpRequestCookiesErrorEnum::StatusCode(e),
+//                         WhereWas {
+//                             time: std::time::SystemTime::now()
+//                                 .duration_since(std::time::UNIX_EPOCH)
+//                                 .expect("cannot convert time to unix_epoch"),
+//                             location: *core::panic::Location::caller(),
+//                         },
+//                         &CONFIG.source_place_type,
+//                         &GIT_INFO.data,
+//                         should_trace,
+//                     ),
+//                 ));
+//             }
+//             Ok(res.cookies())
+//         }
+//     }
+// }
--- src/helpers/http_request/get/mod.rs
+++ src/helpers/http_request/get/mod.rs
@@ -1,20 +1,20 @@
-pub mod headers;
-pub mod headers_mut;
-pub mod status;
-// pub mod cookies;
 pub mod bytes;
 pub mod bytes_stream; //async only
 pub mod chunk;
 pub mod content_length;
+pub mod cookies;
 pub mod copy_to; //sync only
 pub mod error_for_status;
 pub mod extensions;
 pub mod extensions_mut;
+pub mod headers;
+pub mod headers_mut;
 pub mod json;
 pub mod remote_addr;
+pub mod status;
 pub mod text;
 pub mod text_with_charset;
-pub mod upgrade;
+pub mod upgrade; //async only
 pub mod url;
-pub mod version; //async only
-                 // pub mod error_for_status_ref;//dont need it i think
+pub mod version;
+// pub mod error_for_status_ref;//dont need it i think
