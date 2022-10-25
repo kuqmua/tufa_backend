71bdb18f726c712db0e2cad848000811cc2c42d8
kuqmua <serezhashmakov@outlook.com>
2022-10-25 20:06:10 +0300

    pub mod request_builder_methods;

--- src/helpers/http_request/mod.rs
+++ src/helpers/http_request/mod.rs
@@ -1,25 +1,5 @@
 pub mod async_http_request_client_request_builder_prep;
 pub mod http_request_error;
 pub mod http_request_method;
+pub mod request_builder_methods;
 pub mod sync_http_request_client_request_builder_prep;
-
-pub mod bytes;
-pub mod bytes_stream; //async only
-pub mod chunk;
-pub mod content_length;
-pub mod cookies;
-pub mod copy_to; //sync only
-pub mod error_for_status;
-pub mod extensions;
-pub mod extensions_mut;
-pub mod headers;
-pub mod headers_mut;
-pub mod json;
-pub mod remote_addr;
-pub mod status;
-pub mod text;
-pub mod text_with_charset;
-pub mod upgrade; //async only
-pub mod url;
-pub mod version;
-// pub mod error_for_status_ref;//dont need it i think
---
+++ src/helpers/http_request/request_builder_methods/upgrade/async_http_request_upgrade.rs
@@ -0,0 +1,70 @@
+use super::http_request_upgrade_error::HttpRequestUpgradeError;
+use crate::helpers::http_request::request_builder_methods::upgrade::http_request_upgrade_error::HttpRequestUpgradeErrorEnum;
+use crate::lazy_static::config::CONFIG;
+use crate::lazy_static::git_info::GIT_INFO;
+use reqwest::RequestBuilder;
+use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
+use tufa_common::where_was::WhereWas;
+
+#[deny(
+    clippy::indexing_slicing,
+    clippy::unwrap_used,
+    clippy::integer_arithmetic,
+    clippy::float_arithmetic
+)]
+pub async fn async_http_request_upgrade(
+    request_builder: RequestBuilder,
+    should_trace: bool,
+) -> Result<reqwest::Upgraded, Box<HttpRequestUpgradeError>> {
+    match request_builder.send().await {
+        Err(e) => Err(Box::new(
+            HttpRequestUpgradeError::init_error_with_possible_trace(
+                HttpRequestUpgradeErrorEnum::RequestBuilderSend(e),
+                WhereWas {
+                    time: std::time::SystemTime::now()
+                        .duration_since(std::time::UNIX_EPOCH)
+                        .expect("cannot convert time to unix_epoch"),
+                    location: *core::panic::Location::caller(),
+                },
+                &CONFIG.source_place_type,
+                &GIT_INFO.data,
+                should_trace,
+            ),
+        )),
+        Ok(res) => {
+            if let Err(e) = res.error_for_status_ref() {
+                return Err(Box::new(
+                    HttpRequestUpgradeError::init_error_with_possible_trace(
+                        HttpRequestUpgradeErrorEnum::StatusCode(e),
+                        WhereWas {
+                            time: std::time::SystemTime::now()
+                                .duration_since(std::time::UNIX_EPOCH)
+                                .expect("cannot convert time to unix_epoch"),
+                            location: *core::panic::Location::caller(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                ));
+            }
+            match res.upgrade().await {
+                Err(e) => Err(Box::new(
+                    HttpRequestUpgradeError::init_error_with_possible_trace(
+                        HttpRequestUpgradeErrorEnum::ResponseUpgrade(e),
+                        WhereWas {
+                            time: std::time::SystemTime::now()
+                                .duration_since(std::time::UNIX_EPOCH)
+                                .expect("cannot convert time to unix_epoch"),
+                            location: *core::panic::Location::caller(),
+                        },
+                        &CONFIG.source_place_type,
+                        &GIT_INFO.data,
+                        should_trace,
+                    ),
+                )),
+                Ok(upgrade) => Ok(upgrade),
+            }
+        }
+    }
+}
