15d6f8b20e82ca0ba1046726c25262c6557e841e
kuqmua <serezhashmakov@outlook.com>
2022-10-25 21:04:30 +0300

    HttpRequestWrapperBytesError

--- src/helpers/http_request/wrappers/bytes/http_request_bytes_error.rs
+++ src/helpers/http_request/wrappers/bytes/http_request_bytes_error.rs
@@ -1,30 +1,28 @@
 use crate::helpers::http_request::http_request_error::HttpRequestClientRequestBuilderPrepError;
 use crate::helpers::http_request::request_builder_methods::bytes::http_request_bytes_error::HttpRequestBytesError;
 use crate::lazy_static::config::CONFIG;
-use impl_display_for_error_struct::ImplDisplayForErrorStruct;
-use impl_display_for_simple_error_enum::ImplDisplayForSimpleErrorEnum;
 use impl_error_with_tracing_for_struct_with_get_source_with_get_where_was::ImplErrorWithTracingForStructWithGetSourceWithGetWhereWas;
 use impl_get_source_with_method::ImplGetSourceWithMethod;
 use impl_get_where_was_one_or_many_with_method::ImplGetWhereWasOneOrManyWithMethod;
 use init_error::InitError;
+use tufa_common::traits::get_log_with_additional_where_was::GetLogWithAdditionalWhereWas;
 use tufa_common::traits::get_source::GetSource;
 use tufa_common::traits::where_was_trait::WhereWasTrait;
 use tufa_common::where_was::WhereWas;
 
-// #[derive(
-//     Debug,
-//     InitError,
-//     ImplErrorWithTracingForStructWithGetSourceWithGetWhereWas,
-//     ImplGetWhereWasOneOrManyWithMethod,
-//     ImplGetSourceWithMethod,
-//     // ImplDisplayForErrorStruct,
-// )]
-// pub struct HttpRequestWrapperBytesError {
-//     source: HttpRequestWrapperBytesErrorEnum,
-//     where_was: WhereWas,
-// }
+#[derive(
+    Debug,
+    InitError,
+    ImplErrorWithTracingForStructWithGetSourceWithGetWhereWas,
+    ImplGetWhereWasOneOrManyWithMethod,
+    ImplGetSourceWithMethod,
+)]
+pub struct HttpRequestWrapperBytesError {
+    source: HttpRequestWrapperBytesErrorEnum,
+    where_was: WhereWas,
+}
 
-#[derive(Debug, ImplGetSourceWithMethod)] //, ImplDisplayForSimpleErrorEnum
+#[derive(Debug, ImplGetSourceWithMethod, ImplGetWhereWasOneOrManyWithMethod)]
 pub enum HttpRequestWrapperBytesErrorEnum {
     Prep(HttpRequestClientRequestBuilderPrepError),
     Bytes(HttpRequestBytesError),
