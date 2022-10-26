9cfbfa455e703ab18de863c7f1ccb9eb3abd3cb8
kuqmua <serezhashmakov@outlook.com>
2022-10-26 22:36:35 +0300

    cleaning

--- src/helpers/http_request/http_request_error.rs
+++ src/helpers/http_request/http_request_error.rs
@@ -1,15 +1,8 @@
 use crate::lazy_static::config::CONFIG;
-use crate::lazy_static::git_info::GIT_INFO;
-use chrono::{DateTime, FixedOffset, Local, Utc};
-use impl_display_for_simple_error_enum::ImplDisplayForSimpleErrorEnum;
 use impl_error_with_tracing_for_struct_without_get_source::ImplErrorWithTracingForStructWithoutGetSource;
 use impl_get_source_without_method::ImplGetSourceWithoutMethod;
 use impl_get_where_was_one_or_many_one_for_error_struct::ImplGetWhereWasOneOrManyOneForErrorStruct;
-use impl_get_where_was_one_or_many_with_method::ImplGetWhereWasOneOrManyWithMethod;
 use init_error::InitError;
-use tufa_common::traits::get_log_with_additional_where_was::GetLogWithAdditionalWhereWas;
-use tufa_common::traits::get_source::GetSource;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
 use tufa_common::traits::where_was_trait::WhereWasTrait;
 use tufa_common::where_was::WhereWas;
 
