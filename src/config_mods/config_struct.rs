a80ff66d35669dfc55dd95d6c2aa5c9a8acd1490
kuqmua <serezhashmakov@outlook.com>
2022-11-06 17:18:40 +0300

    move resource to server

--- src/config_mods/config_struct.rs
+++ src/config_mods/config_struct.rs
@@ -3,7 +3,7 @@ use gen_enum_without_values::GenEnumWithoutValues;
 use init_from_env_with_panic_if_failed::InitFromEnvWithPanicIfFailedWithPanicIfFailedFromTufaCommon;
 use tufa_common::config_mods::source_place_type::SourcePlaceType;
 use tufa_common::config_mods::tracing_type::TracingType;
-use tufa_common::helpers::resource::Resource;
+use tufa_common::server::resource::Resource;
 
 #[derive(
     Debug,
