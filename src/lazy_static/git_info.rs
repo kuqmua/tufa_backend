5b66106bd259fe2b8b0f638714e1022e42bd5a17
kuqmua <serezhashmakov@outlook.com>
2022-11-06 20:10:07 +0300

    git in common

--- src/lazy_static/git_info.rs
+++ src/lazy_static/git_info.rs
@@ -1,6 +1,6 @@
 use crate::project_constants::PROJECT_NAME;
 use lazy_static::lazy_static;
-use tufa_common::helpers::git::git_info_wrapper::GitInformationWrapper;
+use tufa_common::common::git::git_info_wrapper::GitInformationWrapper;
 
 lazy_static! {
     pub static ref GIT_INFO: GitInformationWrapper =
