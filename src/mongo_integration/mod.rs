7799af204c3bf23291d136c6bafb5af73986448a
kuqmua <serezhashmakov@outlook.com>
2022-11-14 19:29:21 +0300

    mongo_check_db_is_empty moved

--- src/mongo_integration/mod.rs
+++ src/mongo_integration/mod.rs
@@ -1,5 +1,4 @@
 pub mod mongo_check_collection_is_not_empty;
-pub mod mongo_check_db_is_empty;
 pub mod mongo_client_options_parse;
 pub mod mongo_client_with_options;
 pub mod mongo_drop_collection;
