5f1e29a051dc002f257ba60745917687da3e6480
kuqmua <serezhashmakov@outlook.com>
2022-12-03 16:33:04 +0300

    GetGitCommitLink in GetGitHtmlInfo

--- src/routes/git/git_info_html.rs
+++ src/routes/git/git_info_html.rs
@@ -1,13 +1,12 @@
 use crate::global_variables::compile_time::git_info::GIT_INFO;
 use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
-use tufa_common::traits::get_git_commit_link::GetGitCommitLink;
 use tufa_common::traits::get_git_html_info::GetGitHtmlInfo;
 
 pub async fn git_info_html() -> HttpResponse {
     {
         HttpResponse::Ok()
             .content_type(ContentType::html())
-            .body(GIT_INFO.get_git_html_info(GIT_INFO.get_git_commit_link()))
+            .body(GIT_INFO.get_git_html_info())
     }
 }
