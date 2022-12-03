7f5d3fc076ee15a7bcbce1decc615a761c2f5260
kuqmua <serezhashmakov@outlook.com>
2022-12-03 13:42:02 +0300

    GIT_INFO_WITHOUT_LIFETIMES

--- src/global_variables/runtime/git_info_without_lifetimes.rs
+++ src/global_variables/runtime/git_info_without_lifetimes.rs
@@ -2,5 +2,5 @@ use crate::global_variables::compile_time::git_info::GIT_INFO;
 use once_cell::sync::Lazy;
 use tufa_common::common::git::git_info::GitInformationWithoutLifetimes;
 
-pub static RUNTIME_GIT_INFO: Lazy<GitInformationWithoutLifetimes> =
+pub static GIT_INFO_WITHOUT_LIFETIMES: Lazy<GitInformationWithoutLifetimes> =
     Lazy::new(|| GIT_INFO.get_runtime_version());
