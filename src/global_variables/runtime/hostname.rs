cd16f86954da2fbf36f49681f55fa17eb77026e7
kuqmua <serezhashmakov@outlook.com>
2023-01-24 19:39:53 +0300

    gethostname::gethostname() as runtime call + config.get_server_port()

--- src/global_variables/runtime/hostname.rs
+++
@@ -1,2 +0,0 @@
-pub static HOSTNAME: once_cell::sync::Lazy<String> =
-    once_cell::sync::Lazy::new(|| format!("{:?}", gethostname::gethostname()));
--- src/global_variables/runtime/mod.rs
+++ src/global_variables/runtime/mod.rs
@@ -1,4 +1,3 @@
 pub mod config;
 pub mod git_info_without_lifetimes;
-pub mod hostname;
 pub mod mongo_client_options;
