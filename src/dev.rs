2170f5e801e6a4709f0c12e8606729afbf0961c7
kuqmua <serezhashmakov@outlook.com>
2023-01-24 22:09:53 +0300

    cleaning

--- src/dev.rs
+++ src/dev.rs
@@ -1,10 +1,9 @@
 use tufa_common::dev::ThreeWrapperError;
-use tufa_common::traits::error_log::ErrorLog;
 use tufa_common::traits::get_code_occurence::GetCodeOccurenceAsString;
 use tufa_common::traits::separator_symbol::SeparatorSymbol;
 
 pub fn dev() {
-    let _f = one(true);
+    let _f = one();
     if let Err(e) = _f {
         println!("{}", e);
     }
@@ -18,14 +17,18 @@ pub struct OneWrapperError {
 
 impl std::fmt::Display for OneWrapperError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use tufa_common::traits::get_color::ErrorColorBold;
         let config =
             once_cell::sync::Lazy::force(&crate::global_variables::runtime::config::CONFIG);
         write!(
             f,
-            "{}{}{}",
-            self.source,
-            config.symbol(),
-            self.get_code_occurence_as_string(config),
+            "{}",
+            config.get_error_color_bold().paint(format!(
+                "{}{}{}",
+                self.source,
+                config.symbol(),
+                self.get_code_occurence_as_string(config),
+            ))
         )
     }
 }
@@ -48,55 +51,6 @@ impl tufa_common::traits::get_code_occurence::GetCodeOccurenceOldWay for OneWrap
         &self,
     ) -> &tufa_common::common::code_occurence::CodeOccurenceOldWay {
         &self.code_occurence
-    }
-}
-
-impl<ConfigGeneric>
-    tufa_common::traits::get_inner_source_and_code_occurence_vec::GetInnerSourceAndCodeOccurenceVec<
-        ConfigGeneric,
-    > for OneWrapperError
-where
-    ConfigGeneric: tufa_common::traits::fields::GetTimezone
-        + tufa_common::traits::fields::GetLogType
-        + tufa_common::traits::fields::GetSourcePlaceType
-        + tufa_common::traits::get_server_address::GetServerAddress,
-{
-    fn get_inner_source_and_code_occurence_vec(
-        &self,
-        config: &ConfigGeneric,
-    ) -> Vec<tufa_common::common::source_and_code_occurence::SourceAndCodeOccurenceAsString> {
-        let vec = self.source.get_inner_source_and_code_occurence_vec(config);
-        let mut sources_for_tracing: Vec<
-            Vec<(
-                tufa_common::common::source_and_code_occurence::Source,
-                Vec<tufa_common::common::source_and_code_occurence::Key>,
-            )>,
-        > = Vec::with_capacity(
-            vec.iter()
-                .map(|e| e.source.len())
-                .collect::<Vec<usize>>()
-                .iter()
-                .sum(),
-        );
-        let vec_capacity = vec.len() + 1;
-        let mut new_vec =
-            vec.into_iter()
-                .fold(Vec::with_capacity(vec_capacity), |mut acc, mut s| {
-                    s.source.iter().for_each(|v| {
-                        sources_for_tracing.push(v.clone());
-                    });
-                    s.add_one();
-                    acc.push(s);
-                    acc
-                });
-        new_vec.push(
-            tufa_common::common::source_and_code_occurence::SourceAndCodeOccurenceAsString {
-                source: sources_for_tracing,
-                code_occurence: self.get_code_occurence_as_string(config),
-                increment: 0,
-            },
-        );
-        new_vec
     }
 }
 
@@ -141,31 +95,9 @@ where
     }
 }
 
-impl<ConfigGeneric>
-    tufa_common::traits::get_inner_source_and_code_occurence_vec::GetInnerSourceAndCodeOccurenceVec<
-        ConfigGeneric,
-    > for OneWrapperErrorEnum
-where
-    ConfigGeneric: tufa_common::traits::fields::GetTimezone
-        + tufa_common::traits::fields::GetLogType
-        + tufa_common::traits::fields::GetSourcePlaceType
-        + tufa_common::traits::get_server_address::GetServerAddress,
-{
-    fn get_inner_source_and_code_occurence_vec(
-        &self,
-        config: &ConfigGeneric,
-    ) -> Vec<tufa_common::common::source_and_code_occurence::SourceAndCodeOccurenceAsString> {
-        match self {
-            OneWrapperErrorEnum::ThreeWrapper(i) => {
-                i.get_inner_source_and_code_occurence_vec(config)
-            }
-        }
-    }
-}
-
-pub fn one(should_trace: bool) -> Result<(), Box<OneWrapperError>> {
-    if let Err(e) = tufa_common::dev::three(false) {
-        let f = OneWrapperError {
+pub fn one() -> Result<(), Box<OneWrapperError>> {
+    if let Err(e) = tufa_common::dev::three() {
+        return Err(Box::new(OneWrapperError {
             source: OneWrapperErrorEnum::ThreeWrapper(*e),
             code_occurence: tufa_common::common::code_occurence::CodeOccurenceOldWay {
                 git_info: once_cell::sync::Lazy::force(&crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES).clone(),
@@ -175,12 +107,7 @@ pub fn one(should_trace: bool) -> Result<(), Box<OneWrapperError>> {
                     column!(),
                 ),
             }
-        };
-        //start removing trait ErrorLog
-        f.log(once_cell::sync::Lazy::force(
-            &crate::global_variables::runtime::config::CONFIG,
-        ));
-        return Err(Box::new(f));
+        }));
     }
     Ok(())
 }
