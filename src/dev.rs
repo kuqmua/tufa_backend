4f0db595f5346edb010b3efc7f7a8cbd4c0f2b6c
kuqmua <serezhashmakov@outlook.com>
2023-01-28 17:19:53 +0300

    error struct into enums

--- src/dev.rs
+++ src/dev.rs
@@ -17,18 +17,12 @@ pub fn dev() {
 }
 
 #[derive(Debug, Serialize, Deserialize, Error)]
-pub struct OneWrapperError {
-    source: OneWrapperErrorEnum,
-    code_occurence: tufa_common::common::code_occurence::CodeOccurenceOldWay,
-}
-
-impl std::fmt::Display for OneWrapperError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use tufa_common::traits::get_color::ErrorColorBold;
-        let config =
-            once_cell::sync::Lazy::force(&crate::global_variables::runtime::config::CONFIG);
-        write!(f, "{}", self.source)
-    }
+pub enum OneWrapperError {
+    #[error("{source}\n{code_occurence}")]
+    Something {
+        source: OneWrapperErrorEnum,
+        code_occurence: tufa_common::common::code_occurence::CodeOccurenceOldWay,
+    },
 }
 
 impl<ConfigGeneric> tufa_common::traits::error_display::ToStringHandle<ConfigGeneric>
@@ -39,11 +33,16 @@ where
         + tufa_common::traits::get_server_address::GetServerAddress,
 {
     fn to_string_handle(&self, config: &ConfigGeneric) -> String {
-        format!(
-            "{}\n{}",
-            self.source.to_string_handle(config),
-            self.get_code_occurence_as_string(config),
-        )
+        match self {
+            OneWrapperError::Something {
+                source,
+                code_occurence,
+            } => format!(
+                "{}\n{}",
+                source.to_string_handle(config),
+                self.get_code_occurence_as_string(config),
+            ),
+        }
     }
 }
 
@@ -54,7 +53,12 @@ where
         + tufa_common::traits::get_server_address::GetServerAddress,
 {
     fn get_source_as_string(&self, config: &ConfigGeneric) -> String {
-        self.source.get_source_as_string(config)
+        match self {
+            OneWrapperError::Something {
+                source,
+                code_occurence,
+            } => source.get_source_as_string(config),
+        }
     }
 }
 
@@ -62,7 +66,12 @@ impl tufa_common::traits::get_code_occurence::GetCodeOccurenceOldWay for OneWrap
     fn get_code_occurence_old_way(
         &self,
     ) -> &tufa_common::common::code_occurence::CodeOccurenceOldWay {
-        &self.code_occurence
+        match self {
+            OneWrapperError::Something {
+                source,
+                code_occurence,
+            } => code_occurence,
+        }
     }
 }
 
@@ -115,8 +124,8 @@ where
 
 pub fn one() -> Result<(), Box<OneWrapperError>> {
     if let Err(e) = tufa_common::dev::three() {
-        return Err(Box::new(OneWrapperError {
-            source: OneWrapperErrorEnum::ThreeWrapper(*e),
+        return Err(Box::new(OneWrapperError::Something { 
+            source: OneWrapperErrorEnum::ThreeWrapper(*e), 
             code_occurence: tufa_common::common::code_occurence::CodeOccurenceOldWay::new(
                 once_cell::sync::Lazy::force(&crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES).clone(),
                     String::from(file!()),
