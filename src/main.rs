dbc7383b518e28580429c37789d9aadcc78eb54e
kuqmua <serezhashmakov@outlook.com>
2023-02-28 23:15:37 +0300

    next steps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
     // tufa_server::entry::entry();
-    tufa_server::dev::dev();
-    println!("-----------");
-    tufa_server::dev::dev_with_deserialize();
+    // tufa_server::dev::dev();
 }
