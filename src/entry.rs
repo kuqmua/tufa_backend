cfd253bf1ed04e88a7a43acebdbd3ebafa7d3ea8
kuqmua <serezhashmakov@outlook.com>
2023-03-01 21:35:34 +0300

    next steps

--- src/entry.rs
+++ src/entry.rs
@@ -1,88 +1,88 @@
-use crate::global_variables::hardcode::PROJECT_NAME;
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::preparation::prepare_server::prepare_server;
-use crate::prints::print_colorful_message::print_colorful_message;
-use crate::server_wrapper::server_wrapper;
-use crate::telemetry::get_subscriber::get_subscriber;
-use crate::telemetry::init_subscriber::init_subscriber;
-use tufa_common::config_mods::print_type::PrintType;
-use tufa_common::traits::fields::GetLogType;
-use tufa_common::traits::get_git_source_file_link::GetGitSourceFileLink;
-use valuable::Valuable;
+// use crate::global_variables::hardcode::PROJECT_NAME;
+// use crate::global_variables::runtime::config::CONFIG;
+// use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
+// use crate::preparation::prepare_server::prepare_server;
+// use crate::prints::print_colorful_message::print_colorful_message;
+// use crate::server_wrapper::server_wrapper;
+// use crate::telemetry::get_subscriber::get_subscriber;
+// use crate::telemetry::init_subscriber::init_subscriber;
+// use tufa_common::config_mods::print_type::PrintType;
+// use tufa_common::traits::fields::GetLogType;
+// use tufa_common::traits::get_git_source_file_link::GetGitSourceFileLink;
+// use valuable::Valuable;
 
-#[derive(Clone, Debug, Valuable)]
-struct User {
-    name: String,
-    age: u32,
-    something: Vec<bool>,
-    address: Address,
-}
+// #[derive(Clone, Debug, Valuable)]
+// struct User {
+//     name: String,
+//     age: u32,
+//     something: Vec<bool>,
+//     address: Address,
+// }
 
-#[derive(Clone, Debug, Valuable)]
-struct Address {
-    country: String,
-    city: String,
-    street: String,
-}
+// #[derive(Clone, Debug, Valuable)]
+// struct Address {
+//     country: String,
+//     city: String,
+//     street: String,
+// }
 
 pub fn entry() {
-    match tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(num_cpus::get())
-        .enable_all()
-        .build()
-    {
-        Err(e) => {
-            print_colorful_message(
-                None,
-                tufa_common::config_mods::print_type::PrintType::WarningHigh,
-                vec![format!("{}:{}:{}", file!(), line!(), column!())],
-                vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
-                format!("Cannot build tokio runtime {e:#?}"),
-            );
-        }
-        Ok(runtime) => {
-            if let tufa_common::config_mods::log_type::LogType::Tracing = CONFIG.log_type {
-                if let Err(e) = init_subscriber(get_subscriber(
-                    PROJECT_NAME.into(),
-                    CONFIG.tracing_type.to_lower_snake_case(),
-                    std::io::stdout,
-                )) {
-                    print_colorful_message(
-                        None,
-                        tufa_common::config_mods::print_type::PrintType::WarningHigh,
-                        vec![format!("{}:{}:{}", file!(), line!(), column!())],
-                        vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
-                        format!("tracing init_subscriber error: {:#?}", e),
-                    );
-                    return;
-                };
-            }
-            // let user = User {
-            //     name: "Arwen Undomiel".to_string(),
-            //     age: 3000,
-            //     something: vec![true, false],
-            //     address: Address {
-            //         country: "Middle Earth".to_string(),
-            //         city: "Rivendell".to_string(),
-            //         street: "leafy lane".to_string(),
-            //     },
-            // };
-            // tracing::error!(valuable = false, user = ?user);
-            if let true = CONFIG.is_preparation_enabled {
-                if runtime.block_on(prepare_server(true)).is_err() {
-                    return;
-                }
-            }
-            if let Err(e) = server_wrapper() {
-                print_colorful_message(
-                    None,
-                    tufa_common::config_mods::print_type::PrintType::WarningHigh,
-                    vec![format!("{}:{}:{}", file!(), line!(), column!())],
-                    vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
-                    format!("Cannot run actix-web HttpServer, error: {:#?}", e),
-                );
-            }
-        }
-    }
+    // match tokio::runtime::Builder::new_multi_thread()
+    //     .worker_threads(num_cpus::get())
+    //     .enable_all()
+    //     .build()
+    // {
+    //     Err(e) => {
+    //         print_colorful_message(
+    //             None,
+    //             tufa_common::config_mods::print_type::PrintType::WarningHigh,
+    //             vec![format!("{}:{}:{}", file!(), line!(), column!())],
+    //             vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
+    //             format!("Cannot build tokio runtime {e:#?}"),
+    //         );
+    //     }
+    //     Ok(runtime) => {
+    //         if let tufa_common::config_mods::log_type::LogType::Tracing = CONFIG.log_type {
+    //             if let Err(e) = init_subscriber(get_subscriber(
+    //                 PROJECT_NAME.into(),
+    //                 CONFIG.tracing_type.to_lower_snake_case(),
+    //                 std::io::stdout,
+    //             )) {
+    //                 print_colorful_message(
+    //                     None,
+    //                     tufa_common::config_mods::print_type::PrintType::WarningHigh,
+    //                     vec![format!("{}:{}:{}", file!(), line!(), column!())],
+    //                     vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
+    //                     format!("tracing init_subscriber error: {:#?}", e),
+    //                 );
+    //                 return;
+    //             };
+    //         }
+    //         // let user = User {
+    //         //     name: "Arwen Undomiel".to_string(),
+    //         //     age: 3000,
+    //         //     something: vec![true, false],
+    //         //     address: Address {
+    //         //         country: "Middle Earth".to_string(),
+    //         //         city: "Rivendell".to_string(),
+    //         //         street: "leafy lane".to_string(),
+    //         //     },
+    //         // };
+    //         // tracing::error!(valuable = false, user = ?user);
+    //         // if let true = CONFIG.is_preparation_enabled {
+    //         //     if runtime.block_on(prepare_server(true)).is_err() {
+    //         //         return;
+    //         //     }
+    //         // }
+    //         // if let Err(e) = server_wrapper() {
+    //         //     print_colorful_message(
+    //         //         None,
+    //         //         tufa_common::config_mods::print_type::PrintType::WarningHigh,
+    //         //         vec![format!("{}:{}:{}", file!(), line!(), column!())],
+    //         //         vec![GIT_INFO_WITHOUT_LIFETIMES.get_git_source_file_link(file!(), line!())],
+    //         //         format!("Cannot run actix-web HttpServer, error: {:#?}", e),
+    //         //     );
+    //         // }
+    //     }
+    // }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,34 @@
 // )]
 // #![allow(clippy::too_many_arguments)]
 
-// mod authentication;
+mod authentication;
 // mod check_new_providers_posts;
-// mod config_mods;
-// pub mod configuration;
-// pub mod domain;
-// pub mod email_client;
-// pub mod entry;
+mod config_mods;
+pub mod configuration;
+pub mod domain;
+pub mod email_client;
+pub mod entry;
 // mod fetch;
 pub mod global_variables;
-// mod idempotency;
+mod idempotency;
 // mod init_dbs_logic;
 // pub mod issue_delivery_worker;
 // mod logs_logic;
 // mod mongo_integration;
-// mod net_check;
+mod net_check;
 // mod postgres_integration;
 // mod preparation;
 // mod prints;
 // mod providers;
 // mod routes;
 // mod server_wrapper;
-// mod session_state;
+mod session_state;
 // pub mod startup;
-// pub mod telemetry;
-// #[cfg(test)]
-// mod tests;
+pub mod telemetry;
+#[cfg(test)]
+mod tests;
 // pub mod traits;
-// mod utils;
-// pub mod write_error_posts_wrapper;
+mod utils;
+pub mod write_error_posts_wrapper;
 
 pub mod dev;
