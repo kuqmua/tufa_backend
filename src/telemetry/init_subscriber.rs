f7a32f0c4abf35dca837a7c04793ca9edabde55e
kuqmua <serezhashmakov@outlook.com>
2023-03-02 22:12:51 +0300

    init_subcriber_error_enum in tufa_common

--- src/telemetry/init_subscriber.rs
+++ src/telemetry/init_subscriber.rs
@@ -1,12 +1,17 @@
-use crate::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum;
-use tracing::subscriber::set_global_default;
-use tracing::Subscriber;
-use tracing_log::LogTracer;
-
-pub fn init_subscriber(
-    subscriber: impl Subscriber + Send + Sync,
-) -> Result<(), InitSubcriberErrorEnum> {
-    LogTracer::init()?;
-    set_global_default(subscriber)?;
+pub fn init_subscriber<'a>(
+    subscriber: impl tracing::Subscriber + Send + Sync,
+) -> Result<(), tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum<'a>>{
+    if let Err(e) = tracing_log::LogTracer::init() {
+        return Err(tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum::SetLogger {
+            error: e.to_string(),
+            code_occurence: tufa_common::code_occurence!(),
+        });
+    }
+    if let Err(e) = tracing::subscriber::set_global_default(subscriber) {
+        return Err(tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum::SetGlobalDefault {
+            error: e.to_string(),
+            code_occurence: tufa_common::code_occurence!(),
+        });
+    }
     Ok(())
 }
