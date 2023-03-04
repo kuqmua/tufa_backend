5ca2446c7e8458f6ce38421b886d20bd84555f58
kuqmua <serezhashmakov@outlook.com>
2023-03-04 06:06:27 +0300

    error_occurence::ImplErrorOccurence works with types without display and Serialize

--- src/entry.rs
+++ src/entry.rs
@@ -27,12 +27,6 @@ struct Address {
 }
 
 pub fn entry() {
-    if let Err(e) =
-        tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::one()
-    {
-        let r = e.into_serialize_deserialize_version();
-        println!("{:#?}", r);
-    }
     match tokio::runtime::Builder::new_multi_thread()
         .worker_threads(num_cpus::get())
         .enable_all()
--- src/telemetry/init_subscriber.rs
+++ src/telemetry/init_subscriber.rs
@@ -3,13 +3,13 @@ pub fn init_subscriber<'a>(
 ) -> Result<(), tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum<'a>>{
     if let Err(e) = tracing_log::LogTracer::init() {
         return Err(tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum::SetLogger {
-            error: e.to_string(),
+            error: e,
             code_occurence: tufa_common::code_occurence!(),
         });
     }
     if let Err(e) = tracing::subscriber::set_global_default(subscriber) {
         return Err(tufa_common::repositories_types::tufa_server::telemetry::init_subcriber_error_enum::InitSubcriberErrorEnum::SetGlobalDefault {
-            error: e.to_string(),
+            error: e,
             code_occurence: tufa_common::code_occurence!(),
         });
     }
