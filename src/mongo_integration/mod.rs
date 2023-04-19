19376018c9c165a74c80e8739bf08ccac21e9b89
kuqmua <serezhashmakov@outlook.com>
2023-04-19 18:41:32 +0300

    next steps

--- src/mongo_integration/mod.rs
+++ src/mongo_integration/mod.rs
@@ -1,7 +1,7 @@
 pub mod mongo_check_collection_is_not_empty;
 pub mod mongo_client_options_parse;
 pub mod mongo_client_with_options;
-// pub mod mongo_drop_collection;
+pub mod mongo_drop_collection;
 // pub mod mongo_drop_db;
 // pub mod mongo_drop_empty_collection;
 // pub mod mongo_drop_empty_db;
