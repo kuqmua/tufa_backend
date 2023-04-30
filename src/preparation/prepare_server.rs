b74f0dbb93db05edc3a29353859f8ff1723fade9
kuqmua <serezhashmakov@outlook.com>
2023-05-01 01:32:53 +0300

    next steps

--- src/preparation/prepare_server.rs
+++ src/preparation/prepare_server.rs
@@ -1,30 +1,17 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
 // use crate::init_dbs_logic::init_dbs::init_dbs;
 // use crate::init_dbs_logic::init_dbs::InitDbsWrapperError;
-use crate::preparation::check_availability::check_availability;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_git_info::ImplGetGitInfoFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::get_log_with_additional_where_was::GetLogWithAdditionalWhereWas;
-use tufa_common::traits::get_source::GetSource;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-
 pub async fn prepare_server<'a>(should_trace: bool) -> Result<(), Box<tufa_common::repositories_types::tufa_server::preparation::prepare_server::PrepareServerError<'a>>> {
-    if let Err(e) = check_availability(false).await {
+    if let Err(e) = crate::preparation::check_availability::check_availability(false).await {
         return Err(Box::new(
             tufa_common::repositories_types::tufa_server::preparation::prepare_server::PrepareServerError::CheckAvailability { 
-                inner_error: *e, 
+                check_availability: *e, 
                 code_occurence: tufa_common::code_occurence!(),
             }
         ));
     }
     //todo: add params dependency function to config after new to check. like if is_mongo_initialization_enabled is true but is_dbs_initialization_enabled is false so is_mongo_initialization_enabled is also false
-    if !CONFIG.is_dbs_initialization_enabled
-        || (!CONFIG.is_mongo_initialization_enabled && !CONFIG.is_postgres_initialization_enabled)
+    if !crate::global_variables::runtime::config::CONFIG.is_dbs_initialization_enabled
+        || (!crate::global_variables::runtime::config::CONFIG.is_mongo_initialization_enabled && !crate::global_variables::runtime::config::CONFIG.is_postgres_initialization_enabled)
     {
         return Ok(());
     }
@@ -41,7 +28,7 @@ pub async fn prepare_server<'a>(should_trace: bool) -> Result<(), Box<tufa_commo
     //                 column: column!(),
     //                 git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
     //             },
-    //             &CONFIG.source_place_type,
+    //             &crate::global_variables::runtime::config::CONFIG.source_place_type,
     //             should_trace,
     //         ),
     //     ));
