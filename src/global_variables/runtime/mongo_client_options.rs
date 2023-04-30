87cbe63ef0c874e1e412d02d826e0b3a2b1eb2c2
kuqmua <serezhashmakov@outlook.com>
2023-05-01 01:23:11 +0300

    lil tweaks

--- src/global_variables/runtime/mongo_client_options.rs
+++ src/global_variables/runtime/mongo_client_options.rs
@@ -1,9 +1,14 @@
-use crate::global_variables::runtime::config::CONFIG;
-use mongodb::options::ClientOptions;
-use once_cell::sync::Lazy;
-use tufa_common::traits::get_mongo_url::GetMongoUrl;
-
-pub static MONGO_CLIENT_OPTIONS: Lazy<ClientOptions> = Lazy::new(|| {
-    futures::executor::block_on(ClientOptions::parse(CONFIG.get_mongo_url()))
+pub static MONGO_CLIENT_OPTIONS: once_cell::sync::Lazy<mongodb::options::ClientOptions> = once_cell::sync::Lazy::new(
+    || 
+    {
+        futures::executor::block_on(
+            mongodb::options::ClientOptions::parse(
+                {
+                    use tufa_common::traits::get_mongo_url::GetMongoUrl;
+                    crate::global_variables::runtime::config::CONFIG.get_mongo_url()
+                }
+            )
+        )
         .expect("cannot construct mongo client options")
-});
+    }
+);
--- src/preparation/check_availability.rs
+++ src/preparation/check_availability.rs
@@ -1,32 +1,21 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::global_variables::runtime::mongo_client_options::MONGO_CLIENT_OPTIONS;
-use futures::join;
-use impl_display_for_error::ImplDisplayForError;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_git_info::ImplGetGitInfoFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use std::ops::Deref;
-use tufa_common::server::mongo::mongo_check_availability::mongo_check_availability;
-use tufa_common::server::net::net_check_availability::net_check_availability;
-use tufa_common::server::postgres::postgres_check_availability::postgres_check_availability;
-use tufa_common::traits::get_postgres_url::GetPostgresUrl;
-
 pub async fn check_availability<'a>(
     should_trace: bool,
 ) -> Result<(), Box<tufa_common::repositories_types::tufa_server::preparation::check_availability::CheckAvailabilityError<'a>>>{
-    let net_url = CONFIG.starting_check_link.clone();
-    let postgres_url = CONFIG.get_postgres_url();
-    match join!(
-        net_check_availability(net_url),
-        postgres_check_availability(postgres_url, false),
-        mongo_check_availability(
-            MONGO_CLIENT_OPTIONS.deref().to_owned(), //std::time::Duration::from_millis(CONFIG.mongo_connection_timeout),
-            &CONFIG.mongo_providers_logs_db_name,
-            &CONFIG.source_place_type,
-            false,
+    match futures::join!(
+        tufa_common::server::net::net_check_availability::net_check_availability(&crate::global_variables::runtime::config::CONFIG.starting_check_link),
+        tufa_common::server::postgres::postgres_check_availability::postgres_check_availability(
+            {
+                use tufa_common::traits::get_postgres_url::GetPostgresUrl;
+                crate::global_variables::runtime::config::CONFIG.get_postgres_url()
+            }, 
+            crate::global_variables::runtime::config::CONFIG.postgres_connection_timeout
+        ),
+        tufa_common::server::mongo::mongo_check_availability::mongo_check_availability(
+            {
+                use std::ops::Deref;
+                crate::global_variables::runtime::mongo_client_options::MONGO_CLIENT_OPTIONS.deref().to_owned() //todo timeout std::time::Duration::from_millis(CONFIG.mongo_connection_timeout),
+            },
+            &crate::global_variables::runtime::config::CONFIG.mongo_providers_logs_db_name,
         ),
     ) {
         (Ok(_), Ok(_), Ok(_)) => Ok(()),
