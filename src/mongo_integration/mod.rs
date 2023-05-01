27d21ac119eeb8c903c2572fd05c436d9466951e
kuqmua <serezhashmakov@outlook.com>
2023-05-01 03:11:35 +0300

    next steps

--- src/mongo_integration/mod.rs
+++ src/mongo_integration/mod.rs
@@ -8,4 +8,4 @@ pub mod mongo_drop_empty_db;
 pub mod mongo_get_documents_as_string_vector;
 pub mod mongo_get_providers_link_parts;
 pub mod mongo_insert_data;
-// pub mod mongo_insert_many;
+pub mod mongo_insert_many;
--- src/mongo_integration/mongo_insert_many.rs
+++ src/mongo_integration/mongo_insert_many.rs
@@ -1,37 +1,30 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
-use crate::traits::provider_kind_methods::ProviderKindMethods;
-use futures::future::join_all;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use mongodb::bson::doc;
-use mongodb::bson::Document;
-use mongodb::error::Error;
-use mongodb::Database;
-use std::collections::HashMap;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use tufa_common::traits::where_was_methods::WhereWasMethods;
 
 pub async fn mongo_insert_many<'a>(
-    providers_json_local_data_hashmap: HashMap<ProviderKind, Vec<String>>,
-    db: Database,
+    providers_json_local_data_hashmap: std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+    db: mongodb::Database,
     should_trace: bool,
 ) -> Result<(), Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginError<'a>>> {
-    let error_vec_insert_many = join_all(
+    let error_vec_insert_many = futures::future::join_all(
         providers_json_local_data_hashmap.iter().map(
                 |(pk, data_vec)|
                 async {
-                    let docs: Vec<Document> = data_vec
-                    .iter()
-                    .map(|data|
-                        doc! { &CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: data }
+                    (
+                        pk.to_string(), 
+                        db
+                        .collection(&{
+                            use crate::traits::provider_kind_methods::ProviderKindMethods;
+                            pk.get_db_tag()
+                        })
+                        .insert_many(
+                            data_vec
+                            .iter()
+                            .map(|data|
+                                mongodb::bson::doc! { &crate::global_variables::runtime::config::CONFIG.mongo_providers_logs_db_collection_document_field_name_handle: data }
+                            )
+                            .collect::<Vec<mongodb::bson::Document>>(), 
+                            None
+                        ).await
                     )
-                    .collect();
-                    (pk.to_string(), db.collection(&pk.get_db_tag()).insert_many(docs, None).await)
                 }
             )
         ).await
@@ -40,8 +33,8 @@ pub async fn mongo_insert_many<'a>(
         if let Err(e) = result {
             return Some((
                 pk.to_string(), 
-                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorEnum::Mongo(
-                    tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorEnumError::Mongo { 
+                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorUnnamed::Mongo(
+                    tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorNamed::Mongo { 
                         error: e, 
                         code_occurence: tufa_common::code_occurence!()  
                     }
@@ -50,14 +43,14 @@ pub async fn mongo_insert_many<'a>(
         }
         None
     })
-    .collect::<HashMap<String, tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorEnum>>();
-    if !error_vec_insert_many.is_empty() {
-        return Err(Box::new(
+    .collect::<std::collections::HashMap<String, tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorUnnamed>>();
+    match error_vec_insert_many.is_empty() {
+        true => Ok(()),
+        false => Err(Box::new(
             tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginError::Mongo { 
                 inner_errors: error_vec_insert_many, 
                 code_occurence: tufa_common::code_occurence!() 
             },
-        ));
+        ))
     }
-    Ok(())
 }
