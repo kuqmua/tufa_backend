460a8f532b8b5fb34cd1c73c429bac067255101f
kuqmua <serezhashmakov@outlook.com>
2023-05-02 06:38:30 +0300

    mongo_drop_empty_db refactoring

--- src/mongo_integration/mongo_drop_empty_db.rs
+++ src/mongo_integration/mongo_drop_empty_db.rs
@@ -1,106 +1,46 @@
-use mongodb::options::ClientOptions;
-use mongodb::Client;
-use tufa_common::common::where_was::WhereWas;
-
-#[derive(Debug)]
-pub enum MongoDropEmptyDbErrorEnum {
-    ClientOptionsParse {
-        source: mongodb::error::Error,
-        where_was: WhereWas,
-    },
-    ClientWithOptions {
-        source: mongodb::error::Error,
-        where_was: WhereWas,
-    },
-    ListCollectionNames {
-        source: mongodb::error::Error,
-        where_was: WhereWas,
-    },
-    CollectionNamesListIsEmpty {
-        source: String,
-        where_was: WhereWas,
-    },
-    DatabaseDrop {
-        source: mongodb::error::Error,
-        where_was: WhereWas,
-    },
-}
-
-pub async fn mongo_drop_empty_db(
-    mongo_url: &str,
-    db_name: &str,
-) -> Result<(), Box<MongoDropEmptyDbErrorEnum>> {
-    match ClientOptions::parse(mongo_url).await {
-        Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ClientOptionsParse {
-            source: e,
-            where_was: WhereWas {
-                time: std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH)
-                    .expect("cannot convert time to unix_epoch"),
-                file: String::from(file!()),
-                line: line!(),
-                column: column!(),
-                git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-            },
-        })),
-        Ok(client_options) => match Client::with_options(client_options) {
-            Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ClientWithOptions {
-                source: e,
-                where_was: WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-            })),
+pub async fn mongo_drop_empty_db<'a>(
+    mongo_url: &'a str,
+    db_name: &'a str,
+) -> Result<(), Box<tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed<'a>>> {
+    match mongodb::options::ClientOptions::parse(mongo_url).await {
+        Err(e) => Err(Box::new(
+            tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed::MongoDB {
+                mongodb: e,
+                code_occurence: tufa_common::code_occurence!(),
+            }
+        )),
+        Ok(client_options) => match mongodb::Client::with_options(client_options) {
+            Err(e) => Err(Box::new(
+                tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed::MongoDB {
+                    mongodb: e,
+                    code_occurence: tufa_common::code_occurence!(),
+                }
+            )),
             Ok(client) => {
                 let db = client.database(db_name);
                 match db.list_collection_names(None).await {
-                    Err(e) => Err(Box::new(MongoDropEmptyDbErrorEnum::ListCollectionNames {
-                        source: e,
-                        where_was: WhereWas {
-                            time: std::time::SystemTime::now()
-                                .duration_since(std::time::UNIX_EPOCH)
-                                .expect("cannot convert time to unix_epoch"),
-                            file: String::from(file!()),
-                            line: line!(),
-                            column: column!(),
-                            git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                        },
-                    })),
+                    Err(e) => Err(Box::new(
+                        tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed::MongoDB {
+                            mongodb: e,
+                            code_occurence: tufa_common::code_occurence!(),
+                        }
+                    )),
                     Ok(collections_names_list) => {
                         if !collections_names_list.is_empty() {
                             return Err(Box::new(
-                                MongoDropEmptyDbErrorEnum::CollectionNamesListIsEmpty {
-                                    source: db_name.to_string(),
-                                    where_was:                 WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                                },
+                                tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed::CollectionNamesListIsEmpty {
+                                    database: db_name,
+                                    code_occurence: tufa_common::code_occurence!(),
+                                }
                             ));
                         }
                         if let Err(e) = db.drop(None).await {
-                            return Err(Box::new(MongoDropEmptyDbErrorEnum::DatabaseDrop {
-                                source: e,
-                                where_was:                 WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                            }));
+                            return Err(Box::new(
+                                tufa_common::server::mongo::mongo_drop_empty_db::MongoDropEmptyDbErrorNamed::MongoDB {
+                                    mongodb: e,
+                                    code_occurence: tufa_common::code_occurence!(),
+                                }
+                            ));
                         }
                         Ok(())
                     }
