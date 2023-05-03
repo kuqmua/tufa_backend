673af2fc097f0d1e0091876de23dcf48b9308348
kuqmua <serezhashmakov@outlook.com>
2023-05-03 14:49:14 +0300

    postgres_establish_connection refactoring

--- src/postgres_integration/postgres_establish_connection.rs
+++ src/postgres_integration/postgres_establish_connection.rs
@@ -1,57 +1,21 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use sqlx::postgres::PgPoolOptions;
-use sqlx::Error;
-use sqlx::Postgres;
-use std::collections::HashMap;
-use std::time::Duration;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::get_postgres_url::GetPostgresUrl;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use tufa_common::traits::where_was_methods::WhereWasMethods;
-
-#[derive(
-    Debug,
-    InitErrorFromTufaCommon,
-    ImplGetSourceFromTufaCommon,
-    ImplGetWhereWasOriginOrWrapperFromTufaCommon,
-    ImplErrorWithTracingFromTufaCommon,
-)]
-pub struct PostgresEstablishConnectionOriginError {
-    pub source: Error,
-    pub where_was: WhereWas,
-}
-
 pub async fn postgres_establish_connection(
-    providers_json_local_data_hashmap: &HashMap<ProviderKind, Vec<String>>,
+    providers_json_local_data_hashmap: &std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
     should_trace: bool,
-) -> Result<sqlx::Pool<Postgres>, Box<PostgresEstablishConnectionOriginError>> {
-    match PgPoolOptions::new()
+) -> Result<sqlx::Pool<sqlx::Postgres>, Box<tufa_common::server::postgres::postgres_establish_connection::PostgresEstablishConnectionErrorNamed>> {
+    match sqlx::postgres::PgPoolOptions::new()
         .max_connections(providers_json_local_data_hashmap.len() as u32)
-        .connect_timeout(Duration::from_millis(CONFIG.postgres_connection_timeout)) //todo add timeout constant or env var
-        .connect(&CONFIG.get_postgres_url())
+        .connect_timeout(std::time::Duration::from_millis(crate::global_variables::runtime::config::CONFIG.postgres_connection_timeout)) //todo add timeout constant or env var
+        .connect(&{
+            use tufa_common::traits::get_postgres_url::GetPostgresUrl;
+            crate::global_variables::runtime::config::CONFIG.get_postgres_url()
+        })
         .await
     {
         Err(e) => Err(Box::new(
-            PostgresEstablishConnectionOriginError::init_error_with_possible_trace(
-                e,
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                &CONFIG.source_place_type,
-                should_trace,
-            ),
+            tufa_common::server::postgres::postgres_establish_connection::PostgresEstablishConnectionErrorNamed::Connect { 
+                sqlx_error: e, 
+                code_occurence: tufa_common::code_occurence!(), 
+            }
         )),
         Ok(pool) => Ok(pool),
     }
