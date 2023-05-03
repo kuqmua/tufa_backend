f29586167a25aec60e58a64e7c483d0a5c6e0791
kuqmua <serezhashmakov@outlook.com>
2023-05-03 20:13:39 +0300

    init_mongo refactoring

--- src/init_dbs_logic/init_mongo.rs
+++ src/init_dbs_logic/init_mongo.rs
@@ -1,71 +1,41 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::mongo_integration::mongo_check_collection_is_not_empty::mongo_check_collections_is_not_empty;
-use crate::mongo_integration::mongo_client_options_parse::mongo_client_options_parse;
-use crate::mongo_integration::mongo_client_with_options::mongo_client_with_options;
-use crate::mongo_integration::mongo_insert_many::mongo_insert_many;
-use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
-use crate::traits::provider_kind_methods::ProviderKindMethods;
-use futures::future::join_all;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_git_info::ImplGetGitInfoFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use mongodb::bson::doc;
-use mongodb::bson::Document;
-use mongodb::error::Error;
-use mongodb::options::ClientOptions;
-use mongodb::Client;
-use std::collections::HashMap;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::get_log_with_additional_where_was::GetLogWithAdditionalWhereWas;
-use tufa_common::traits::get_source::GetSource;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use tufa_common::traits::where_was_methods::WhereWasMethods;
-
 pub async fn init_mongo<'a>(
-    providers_json_local_data_hashmap: HashMap<ProviderKind, Vec<String>>,
-    should_trace: bool,
-) -> Result<(), Box<tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperError<'a>>>{
-    match mongo_client_options_parse().await {
+    providers_json_local_data_hashmap: std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+) -> Result<(), Box<tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed<'a>>>{
+    match crate::mongo_integration::mongo_client_options_parse::mongo_client_options_parse().await {
         Err(e) => Err(Box::new(
-            tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperError::Mongo {
-                inner_error: tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperErrorEnum::ClientOptionsParseWrapper(
-                    *e
-                ),
+            tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::ClientOptionsParse {
+                client_options_parse: *e,
                 code_occurence: tufa_common::code_occurence!()
             }
         )),
-        Ok(client_options) => match mongo_client_with_options(client_options, false) {
+        Ok(client_options) => match crate::mongo_integration::mongo_client_with_options::mongo_client_with_options(client_options) {
             Err(e) => Err(Box::new(
-                tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperError::Mongo {
-                    inner_error: tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperErrorEnum::ClientWithOptionsWrapper(*e),
+                tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::ClientWithOptions {
+                    client_with_options: *e,
                     code_occurence: tufa_common::code_occurence!()
                 }
             )),
             Ok(client) => {
-                let db = client.database(&CONFIG.mongo_providers_link_parts_db_name);
-                if let Err(error_vec_count_documents) = mongo_check_collections_is_not_empty(
+                let db = client.database(&crate::global_variables::runtime::config::CONFIG.mongo_providers_link_parts_db_name);
+                if let Err(e) = crate::mongo_integration::mongo_check_collection_is_not_empty::mongo_check_collections_is_not_empty(
                     providers_json_local_data_hashmap.clone(),
                     &db,
-                    false,
                 )
                 .await
                 {
                     return Err(Box::new(
-                        tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperError::Mongo {
-                            inner_error: tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperErrorEnum::CollectionIsNotEmpty(*error_vec_count_documents),
+                        tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::CollectionIsNotEmpty {
+                            collection_is_not_empty: *e,
                             code_occurence: tufa_common::code_occurence!()
                         }
                     ));
                 }
-                if let Err(error_vec_insert_many) =
-                    mongo_insert_many(providers_json_local_data_hashmap, db, false).await
+                if let Err(e) =
+                    crate::mongo_integration::mongo_insert_many::mongo_insert_many(providers_json_local_data_hashmap, db, false).await
                 {
                     return Err(Box::new(
-                        tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperError::Mongo {
-                            inner_error: tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoWrapperErrorEnum::InsertManyErrorWrapper(*error_vec_insert_many),
+                        tufa_common::repositories_types::tufa_server::init_dbs_logic::init_mongo::InitMongoErrorNamed::InsertMany {
+                            insert_many: *e,
                             code_occurence: tufa_common::code_occurence!()
                         }
                     ));
--- src/mongo_integration/mongo_insert_many.rs
+++ src/mongo_integration/mongo_insert_many.rs
@@ -3,7 +3,7 @@ pub async fn mongo_insert_many<'a>(
     providers_json_local_data_hashmap: std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
     db: mongodb::Database,
     should_trace: bool,
-) -> Result<(), Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginError<'a>>> {
+) -> Result<(), Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyErrorNamed<'a>>> {
     let error_vec_insert_many = futures::future::join_all(
         providers_json_local_data_hashmap.iter().map(
                 |(pk, data_vec)|
@@ -33,9 +33,9 @@ pub async fn mongo_insert_many<'a>(
         if let Err(e) = result {
             return Some((
                 pk.to_string(), 
-                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorUnnamed::Mongo(
-                    tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorNamed::Mongo { 
-                        error: e, 
+                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyErrorUnnamed::InsertMany(
+                    tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyHandleErrorNamed::InsertMany { 
+                        insert_many: e, 
                         code_occurence: tufa_common::code_occurence!()  
                     }
                 )
@@ -43,12 +43,12 @@ pub async fn mongo_insert_many<'a>(
         }
         None
     })
-    .collect::<std::collections::HashMap<String, tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginErrorUnnamed>>();
+    .collect::<std::collections::HashMap<String, tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyErrorUnnamed>>();
     match error_vec_insert_many.is_empty() {
         true => Ok(()),
         false => Err(Box::new(
-            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyOriginError::Mongo { 
-                inner_errors: error_vec_insert_many, 
+            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_many::MongoInsertManyErrorNamed::InsertMany { 
+                insert_many: error_vec_insert_many, 
                 code_occurence: tufa_common::code_occurence!() 
             },
         ))
