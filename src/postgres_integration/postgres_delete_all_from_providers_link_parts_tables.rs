96808ee00ac8d09ac36c339d4be73cd85876dabb
kuqmua <serezhashmakov@outlook.com>
2023-05-03 16:13:48 +0300

    postgres_delete_all_from_providers_link_parts_tables refactoring

--- src/postgres_integration/postgres_delete_all_from_providers_link_parts_tables.rs
+++ src/postgres_integration/postgres_delete_all_from_providers_link_parts_tables.rs
@@ -1,67 +1,31 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
-use crate::traits::provider_kind_methods::ProviderKindMethods;
-use futures::future::join_all;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use sqlx::Error;
-use sqlx::Pool;
-use sqlx::Postgres;
-use std::collections::HashMap;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use tufa_common::traits::where_was_methods::WhereWasMethods;
-
-#[derive(
-    Debug,
-    InitErrorFromTufaCommon,
-    ImplGetWhereWasOriginOrWrapperFromTufaCommon,
-    ImplGetSourceFromTufaCommon,
-    ImplErrorWithTracingFromTufaCommon,
-)]
-pub struct PostgresDeleteAllFromProvidersTablesOriginError {
-    pub source: HashMap<ProviderKind, Error>,
-    pub where_was: WhereWas,
-}
-
-pub async fn postgres_delete_all_from_providers_link_parts_tables(
-    providers_json_local_data_hashmap: &HashMap<ProviderKind, Vec<String>>,
-    pool: &Pool<Postgres>,
+pub async fn postgres_delete_all_from_providers_link_parts_tables<'a>(
+    providers_json_local_data_hashmap: &std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+    pool: &sqlx::Pool<sqlx::Postgres>,
     should_trace: bool,
-) -> Result<(), Box<PostgresDeleteAllFromProvidersTablesOriginError>> {
+) -> Result<(), Box<tufa_common::server::postgres::postgres_delete_all_from_providers_link_parts_tables::PostgresDeleteAllFromProvidersTablesErrorNamed<'a>>> {
     let delete_from_tables_error_hashmap =
-        join_all(providers_json_local_data_hashmap.keys().map(|pk| async {
-            let query_string = format!("DELETE FROM {} ;", pk.get_postgres_table_name());
+        futures::future::join_all(providers_json_local_data_hashmap.keys().map(|pk| async {
+            let query_string = format!("DELETE FROM {} ;", {
+                use crate::traits::provider_kind_methods::ProviderKindMethods;
+                pk.get_postgres_table_name()
+            });
             (*pk, sqlx::query(&query_string).execute(pool).await)
         }))
         .await
         .into_iter()
         .filter_map(|(pk, result)| {
             if let Err(e) = result {
-                return Some((pk, e));
+                return Some((pk.to_string(), e));
             }
             None
         })
-        .collect::<HashMap<ProviderKind, sqlx::Error>>();
+        .collect::<std::collections::HashMap<std::string::String, sqlx::Error>>();
     if !delete_from_tables_error_hashmap.is_empty() {
         return Err(Box::new(
-            PostgresDeleteAllFromProvidersTablesOriginError::init_error_with_possible_trace(
-                delete_from_tables_error_hashmap,
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                &CONFIG.source_place_type,
-                should_trace,
-            ),
+            tufa_common::server::postgres::postgres_delete_all_from_providers_link_parts_tables::PostgresDeleteAllFromProvidersTablesErrorNamed::DeleteTables { 
+                error_hashmap: delete_from_tables_error_hashmap, 
+                code_occurence: tufa_common::code_occurence!()
+            }
         ));
     }
     Ok(())
