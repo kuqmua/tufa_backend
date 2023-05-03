a14b79701e9fdb5ba48386e93528224f88ed6af2
kuqmua <serezhashmakov@outlook.com>
2023-05-03 12:38:36 +0300

    postgres_create_providers_tables_if_not_exists refactoring

--- src/postgres_integration/postgres_create_providers_tables_if_not_exists.rs
+++ src/postgres_integration/postgres_create_providers_tables_if_not_exists.rs
@@ -1,38 +1,9 @@
-use crate::global_variables::runtime::config::CONFIG;
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use crate::providers::provider_kind::provider_kind_enum::ProviderKind;
-use crate::traits::provider_kind_methods::ProviderKindMethods;
-use futures::future::join_all;
-use impl_error_with_tracing::ImplErrorWithTracingFromTufaCommon;
-use impl_get_source::ImplGetSourceFromTufaCommon;
-use impl_get_where_was_origin_or_wrapper::ImplGetWhereWasOriginOrWrapperFromTufaCommon;
-use init_error::InitErrorFromTufaCommon;
-use sqlx::Error;
-use sqlx::Pool;
-use sqlx::Postgres;
-use std::collections::HashMap;
-use tufa_common::common::where_was::WhereWas;
-use tufa_common::traits::init_error_with_possible_trace::InitErrorWithPossibleTrace;
-use tufa_common::traits::where_was_methods::WhereWasMethods;
-
-#[derive(
-    Debug,
-    InitErrorFromTufaCommon,
-    ImplGetWhereWasOriginOrWrapperFromTufaCommon,
-    ImplGetSourceFromTufaCommon,
-    ImplErrorWithTracingFromTufaCommon,
-)]
-pub struct PostgresCreateProvidersDbsOriginError {
-    pub source: HashMap<ProviderKind, Error>,
-    pub where_was: WhereWas,
-}
-
 pub async fn postgres_create_providers_tables_if_not_exists(
-    providers_json_local_data_hashmap: &HashMap<ProviderKind, Vec<String>>,
-    db: &Pool<Postgres>,
+    providers_json_local_data_hashmap: &std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
+    db: &sqlx::Pool<sqlx::Postgres>,
     should_trace: bool,
-) -> Result<(), Box<PostgresCreateProvidersDbsOriginError>> {
-    let table_creation_error_hashmap = join_all(
+) -> Result<(), Box<tufa_common::server::postgres::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed>> {
+    let table_creation_error_hashmap = futures::future::join_all(
         providers_json_local_data_hashmap.keys().map(|pk| async {
             let query_string = format!(
                 "CREATE TABLE IF NOT EXISTS {} (id integer GENERATED ALWAYS AS IDENTITY NOT NULL, link_part text, PRIMARY KEY (id));",
@@ -43,28 +14,17 @@ pub async fn postgres_create_providers_tables_if_not_exists(
         .into_iter()
         .filter_map(|(pk, result)| {
             if let Err(e) = result {
-                return Some((pk, e));
+                return Some((pk.to_string(), e));
             }
             None
         })
-        .collect::<HashMap<ProviderKind, sqlx::Error>>();
+        .collect::<std::collections::HashMap<crate::providers::provider_kind::provider_kind_enum::ProviderKind, sqlx::Error>>();
     if !table_creation_error_hashmap.is_empty() {
-        //todo iter over hashmap to support init_error_with_possible_trace
         return Err(Box::new(
-            PostgresCreateProvidersDbsOriginError::init_error_with_possible_trace(
-                table_creation_error_hashmap,
-                WhereWas {
-                    time: std::time::SystemTime::now()
-                        .duration_since(std::time::UNIX_EPOCH)
-                        .expect("cannot convert time to unix_epoch"),
-                    file: String::from(file!()),
-                    line: line!(),
-                    column: column!(),
-                    git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-                },
-                &CONFIG.source_place_type,
-                should_trace,
-            ),
+            tufa_common::server::postgres::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed::Postgres {
+                sqlx_error_hashmap: table_creation_error_hashmap,
+                code_occurence: tufa_common::code_occurence!()
+            }
         ));
     }
     Ok(())
