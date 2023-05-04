e591d2300a9a0fe6ba50e954abe2fbcadcf13684
kuqmua <serezhashmakov@outlook.com>
2023-05-04 15:13:03 +0300

    move

--- src/mongo_integration/mongo_insert_data.rs
+++ src/mongo_integration/mongo_insert_data.rs
@@ -1,7 +1,7 @@
 pub async fn mongo_insert_data<'a>(
     db_name_handle: &'a str,
     vec_of_link_parts_hashmap: std::collections::HashMap<tufa_common::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,//todo impl Display instead of ProviderKind
-) -> Result<(), Box<tufa_common::server::mongo::mongo_insert_data::MongoInsertDataErrorNamed<'a>>> {
+) -> Result<(), Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_data::MongoInsertDataErrorNamed<'a>>> {
     let error_hashmap = futures::future::join_all(vec_of_link_parts_hashmap.into_iter().map(
         |(pk, vec_of_link_parts)| async move {
             (
@@ -31,15 +31,15 @@ pub async fn mongo_insert_data<'a>(
         if let Err(e) = result {
             return Some((
                 pk.to_string(), 
-                tufa_common::server::mongo::mongo_insert_data::MongoInsertDataErrorUnnamed::MongoInsertDocsInEmptyCollection(*e)
+                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_data::MongoInsertDataErrorUnnamed::MongoInsertDocsInEmptyCollection(*e)
             ));
         }
         None
     })
-    .collect::<std::collections::HashMap<std::string::String, tufa_common::server::mongo::mongo_insert_data::MongoInsertDataErrorUnnamed>>();
+    .collect::<std::collections::HashMap<std::string::String, tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_data::MongoInsertDataErrorUnnamed>>();
     if !error_hashmap.is_empty() {
         return Err(Box::new(
-            tufa_common::server::mongo::mongo_insert_data::MongoInsertDataErrorNamed::Errors {
+            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_insert_data::MongoInsertDataErrorNamed::Errors {
                 errors_hashmap: error_hashmap,
                 code_occurence: tufa_common::code_occurence!()
             }
