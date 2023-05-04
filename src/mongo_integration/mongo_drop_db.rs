9b6fdcc19d19a047775b71b0252ee4d77658d367
kuqmua <serezhashmakov@outlook.com>
2023-05-04 11:46:33 +0300

    move

--- src/mongo_integration/mongo_drop_db.rs
+++ src/mongo_integration/mongo_drop_db.rs
@@ -1,17 +1,17 @@
 pub async fn mongo_drop_db<'a>(
     mongo_url: &'a str,
     db_name: &'a str,
-) -> Result<(), Box<tufa_common::server::mongo::mongo_drop_db::MongoDropDbErrorNamed<'a>>> {
+) -> Result<(), Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed<'a>>> {
     match  mongodb::options::ClientOptions::parse(mongo_url).await {
         Err(e) => Err(Box::new(
-            tufa_common::server::mongo::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
                 mongodb: e,
                 code_occurence: tufa_common::code_occurence!(),
             }
         )),
         Ok(client_options) => match mongodb::Client::with_options(client_options) {
             Err(e) => Err(Box::new(
-                tufa_common::server::mongo::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+                tufa_common::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
                     mongodb: e,
                     code_occurence: tufa_common::code_occurence!(),
                 }
@@ -19,7 +19,7 @@ pub async fn mongo_drop_db<'a>(
             Ok(client) => {
                 if let Err(e) = client.database(db_name).drop(None).await {
                     return Err(Box::new(
-                        tufa_common::server::mongo::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
+                        tufa_common::repositories_types::tufa_server::mongo_integration::mongo_drop_db::MongoDropDbErrorNamed::MongoDB {
                             mongodb: e,
                             code_occurence: tufa_common::code_occurence!(),
                         }
