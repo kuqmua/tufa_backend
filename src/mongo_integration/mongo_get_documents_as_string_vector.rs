2afe2b0ef96261e9ee8f96314dd4a442bdeb0092
kuqmua <serezhashmakov@outlook.com>
2023-05-04 13:11:02 +0300

    move

--- src/mongo_integration/mongo_get_documents_as_string_vector.rs
+++ src/mongo_integration/mongo_get_documents_as_string_vector.rs
@@ -2,10 +2,10 @@ pub async fn mongo_get_documents_as_string_vector(
     collection: mongodb::Collection<mongodb::bson::Document>,
     db_collection_document_field_name_handle: &str,
     option_aggregation: Option<mongodb::bson::Document>,
-) -> Result<Vec<String>, Box<tufa_common::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed>> {
+) -> Result<Vec<String>, Box<tufa_common::repositories_types::tufa_server::mongo_integration::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed>> {
     match collection.aggregate(option_aggregation, None).await {
         Err(e) => Err(Box::new(
-            tufa_common::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::MongoDB {
+            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::MongoDB {
                 mongodb: e,
                 code_occurence: tufa_common::code_occurence!()
             }
@@ -19,7 +19,7 @@ pub async fn mongo_get_documents_as_string_vector(
                 }.await {
                     Err(e) => {
                         return Err(Box::new(
-                            tufa_common::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::MongoDB {
+                            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::MongoDB {
                                 mongodb: e,
                                 code_occurence: tufa_common::code_occurence!()
                             }
@@ -32,7 +32,7 @@ pub async fn mongo_get_documents_as_string_vector(
                         Some(document) => {
                             match document.get(db_collection_document_field_name_handle) {
                                 None => return Err(Box::new(
-                                    tufa_common::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::NoKeyInDocument {
+                                    tufa_common::repositories_types::tufa_server::mongo_integration::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::NoKeyInDocument {
                                         key: db_collection_document_field_name_handle,
                                         code_occurence: tufa_common::code_occurence!()
                                     }
@@ -43,7 +43,7 @@ pub async fn mongo_get_documents_as_string_vector(
                                     }
                                     other_bson_type => {
                                         return Err(Box::new(
-                                            tufa_common::server::mongo::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::WrongBsonType {
+                                            tufa_common::repositories_types::tufa_server::mongo_integration::mongo_get_documents_as_string_vector::MongoGetDocumentsAsStringVectorErrorNamed::WrongBsonType {
                                                 bson: other_bson_type.clone(),
                                                 code_occurence: tufa_common::code_occurence!()
                                             }
