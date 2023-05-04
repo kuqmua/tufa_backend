9444da4c30f96ef9fb252e1f2d79d0130725479a
kuqmua <serezhashmakov@outlook.com>
2023-05-04 16:53:16 +0300

    move

--- src/postgres_integration/postgres_create_providers_tables_if_not_exists.rs
+++ src/postgres_integration/postgres_create_providers_tables_if_not_exists.rs
@@ -1,7 +1,7 @@
 pub async fn postgres_create_providers_tables_if_not_exists<'a>(
     providers_json_local_data_hashmap: &std::collections::HashMap<tufa_common::repositories_types::tufa_server::providers::provider_kind::provider_kind_enum::ProviderKind, Vec<String>>,
     db: &sqlx::Pool<sqlx::Postgres>,
-) -> Result<(), Box<tufa_common::server::postgres::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed<'a>>> {
+) -> Result<(), Box<tufa_common::repositories_types::tufa_server::postgres_integration::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed<'a>>> {
     let table_creation_error_hashmap = futures::future::join_all(
         providers_json_local_data_hashmap.keys().map(|pk| async {
             let query_string = format!(
@@ -23,7 +23,7 @@ pub async fn postgres_create_providers_tables_if_not_exists<'a>(
         .collect::<std::collections::HashMap<std::string::String, sqlx::Error>>();
     if !table_creation_error_hashmap.is_empty() {
         return Err(Box::new(
-            tufa_common::server::postgres::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed::Postgres {
+            tufa_common::repositories_types::tufa_server::postgres_integration::postgres_create_providers_tables_if_not_exists::PostgresCreateProvidersDbsErrorNamed::Postgres {
                 sqlx_error_hashmap: table_creation_error_hashmap,
                 code_occurence: tufa_common::code_occurence!()
             }
