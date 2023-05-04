c605eea9935d17f9bd558ae51c182bf2598d7496
kuqmua <serezhashmakov@outlook.com>
2023-05-04 16:02:00 +0300

    lil tweaks

--- src/postgres_integration/mod.rs
+++ src/postgres_integration/mod.rs
@@ -3,6 +3,3 @@ pub mod postgres_create_providers_tables_if_not_exists;
 pub mod postgres_delete_all_from_providers_link_parts_tables;
 pub mod postgres_establish_connection;
 // pub mod postgres_get_providers_link_parts;
-
-
-// pub mod postgres_insert_link_parts_into_providers_tables;
