45ce1ba175f69f7014e56629b7b15b9926305564
kuqmua <serezhashmakov@outlook.com>
2023-05-04 17:24:53 +0300

    move

--- src/postgres_integration/postgres_establish_connection.rs
+++ src/postgres_integration/postgres_establish_connection.rs
@@ -1,6 +1,6 @@
 pub async fn postgres_establish_connection<'a>(
     max_connections: u32,
-) -> Result<sqlx::Pool<sqlx::Postgres>, Box<tufa_common::server::postgres::postgres_establish_connection::PostgresEstablishConnectionErrorNamed<'a>>> {
+) -> Result<sqlx::Pool<sqlx::Postgres>, Box<tufa_common::repositories_types::tufa_server::postgres_integration::postgres_establish_connection::PostgresEstablishConnectionErrorNamed<'a>>> {
     match sqlx::postgres::PgPoolOptions::new()
         .max_connections(max_connections)
         .connect_timeout(std::time::Duration::from_millis(crate::global_variables::runtime::config::CONFIG.postgres_connection_timeout)) //todo add timeout constant or env var
@@ -11,7 +11,7 @@ pub async fn postgres_establish_connection<'a>(
         .await
     {
         Err(e) => Err(Box::new(
-            tufa_common::server::postgres::postgres_establish_connection::PostgresEstablishConnectionErrorNamed::Connect { 
+            tufa_common::repositories_types::tufa_server::postgres_integration::postgres_establish_connection::PostgresEstablishConnectionErrorNamed::Connect { 
                 sqlx_error: e, 
                 code_occurence: tufa_common::code_occurence!(), 
             }
