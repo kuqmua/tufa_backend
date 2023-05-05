003899e7a4ace2c157c2a1fc6d23c5bf1fe33e2f
kuqmua <serezhashmakov@outlook.com>
2023-05-05 13:42:18 +0300

    remove old code

--- src/preparation/prepare_server.rs
+++ src/preparation/prepare_server.rs
@@ -1,5 +1,3 @@
-// use crate::init_dbs_logic::init_dbs::init_dbs;
-// use crate::init_dbs_logic::init_dbs::InitDbsWrapperError;
 pub async fn prepare_server<'a>(should_trace: bool) -> Result<(), Box<tufa_common::repositories_types::tufa_server::preparation::prepare_server::PrepareServerErrorNamed<'a>>> {
     if let Err(e) = crate::preparation::check_availability::check_availability(false).await {
         return Err(Box::new(
@@ -15,23 +13,5 @@ pub async fn prepare_server<'a>(should_trace: bool) -> Result<(), Box<tufa_commo
     {
         return Ok(());
     }
-    // if let Err(e) = init_dbs(false).await {
-    //     return Err(Box::new(
-    //         PreparationWrapperError::init_error_with_possible_trace(
-    //             InitDbsProvidersLinkPartsWrapperErrorEnum::InitDbsWrapper(*e),
-    //             WhereWas {
-    //                 time: std::time::SystemTime::now()
-    //                     .duration_since(std::time::UNIX_EPOCH)
-    //                     .expect("cannot convert time to unix_epoch"),
-    //                 file: String::from(file!()),
-    //                 line: line!(),
-    //                 column: column!(),
-    //                 git_info: crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES.clone(),
-    //             },
-    //             &crate::global_variables::runtime::config::CONFIG.source_place_type,
-    //             should_trace,
-    //         ),
-    //     ));
-    // }
     Ok(())
 }
