fc09d492113b286987e91746b77d36fae85bdfce
kuqmua <serezhashmakov@outlook.com>
2023-05-06 11:53:19 +0300

    refactoring

--- src/routes/git/git_info_json.rs
+++ src/routes/git/git_info_json.rs
@@ -1,15 +1,11 @@
-use crate::global_variables::runtime::git_info_without_lifetimes::GIT_INFO_WITHOUT_LIFETIMES;
-use actix_web::{web, Responder};
-use tufa_common::common::git::git_info::GitInformation;
-
-pub async fn git_info_json() -> impl Responder {
-    web::Json(GitInformation {
-        git_commit_id: &GIT_INFO_WITHOUT_LIFETIMES.git_commit_id,
-        git_repo_link: &GIT_INFO_WITHOUT_LIFETIMES.git_repo_link,
-        git_author: &GIT_INFO_WITHOUT_LIFETIMES.git_author,
-        git_author_email: &GIT_INFO_WITHOUT_LIFETIMES.git_author_email,
-        git_commit_unix_time: &GIT_INFO_WITHOUT_LIFETIMES.git_commit_unix_time,
-        git_timezone: &GIT_INFO_WITHOUT_LIFETIMES.git_timezone,
-        git_message: &GIT_INFO_WITHOUT_LIFETIMES.git_message,
+pub async fn git_info_json() -> impl actix_web::Responder {
+    actix_web::web::Json(tufa_common::common::git::git_info::GitInformation {
+        git_commit_id: &crate::global_variables::compile_time::git_info::GIT_INFO.git_commit_id,
+        git_repo_link: &crate::global_variables::compile_time::git_info::GIT_INFO.git_repo_link,
+        git_author: &crate::global_variables::compile_time::git_info::GIT_INFO.git_author,
+        git_author_email: &crate::global_variables::compile_time::git_info::GIT_INFO.git_author_email,
+        git_commit_unix_time: &crate::global_variables::compile_time::git_info::GIT_INFO.git_commit_unix_time,
+        git_timezone: &crate::global_variables::compile_time::git_info::GIT_INFO.git_timezone,
+        git_message: &crate::global_variables::compile_time::git_info::GIT_INFO.git_message,
     })
 }
