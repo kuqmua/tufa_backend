1e802c77b4397982f3d8fc9c506720a5e7196544
kuqmua <serezhashmakov@outlook.com>
2023-05-09 15:27:22 +0300

    lil tweaks

--- src/routes/admin/newsletter/get.rs
+++ src/routes/admin/newsletter/get.rs
@@ -1,16 +1,15 @@
-use actix_web::HttpResponse;
 use actix_web_flash_messages::IncomingFlashMessages;
 use std::fmt::Write;
 
 pub async fn publish_newsletter_form(
     flash_messages: IncomingFlashMessages,
-) -> Result<HttpResponse, actix_web::Error> {
+) -> Result<actix_web::HttpResponse, actix_web::Error> {
     let mut msg_html = String::new();
     for m in flash_messages.iter() {
         writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
     }
     let idempotency_key = uuid::Uuid::new_v4();
-    Ok(HttpResponse::Ok()
+    Ok(actix_web::HttpResponse::Ok()
         .content_type(actix_web::http::header::ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
