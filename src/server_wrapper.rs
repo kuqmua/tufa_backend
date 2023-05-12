bc5e2f30bf620c613cb0d6d7b16798681f0525ed
kuqmua <serezhashmakov@outlook.com>
2023-05-12 04:44:27 +0300

    remove useless clone

--- src/server_wrapper.rs
+++ src/server_wrapper.rs
@@ -26,7 +26,7 @@ pub async fn server_wrapper(
         },
         application: tufa_common::repositories_types::tufa_server::configuration::ApplicationSettings {
             port: *config.get_server_port(),
-            host: config.get_server_ip().clone(),
+            host: config.get_server_ip(),
             base_url: config.get_base_url().clone(),
             hmac_secret: secrecy::Secret::new(config.get_hmac_secret().clone()),
         },
