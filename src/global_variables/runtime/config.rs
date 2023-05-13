33e18b8b814242b16678bbfdbca593a2801fa6fd
kuqmua <serezhashmakov@outlook.com>
2023-05-13 03:58:40 +0300

    renaming path

--- src/global_variables/runtime/config.rs
+++ src/global_variables/runtime/config.rs
@@ -1,5 +1,5 @@
-pub static CONFIG:  once_cell::sync::Lazy<tufa_common::repositories_types::tufa_server::config_mods::config_struct::ConfigStruct> =  once_cell::sync::Lazy::new(|| {
-    tufa_common::repositories_types::tufa_server::config_mods::config_struct::ConfigStruct::new()
+pub static CONFIG:  once_cell::sync::Lazy<tufa_common::repositories_types::tufa_server::config::config_struct::ConfigStruct> =  once_cell::sync::Lazy::new(|| {
+    tufa_common::repositories_types::tufa_server::config::config_struct::ConfigStruct::new()
         .expect("cannot create config")
         .wrap_config_checks()
         .expect("wrap_config_checks error")
