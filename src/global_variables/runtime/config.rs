04ddc884c0e19fdcc6b61d520c42d49856d26312
kuqmua <serezhashmakov@outlook.com>
2023-05-17 11:30:10 +0300

    lil tweaks

--- src/global_variables/runtime/config.rs
+++ src/global_variables/runtime/config.rs
@@ -1,9 +1,6 @@
 pub static CONFIG:  once_cell::sync::Lazy<tufa_common::repositories_types::tufa_server::config::config_struct::Config> =  once_cell::sync::Lazy::new(|| {
     tufa_common::repositories_types::tufa_server::config::config_struct::Config::try_from(
         tufa_common::repositories_types::tufa_server::config::config_struct::ConfigBuilder::new()
-        // .expect("cannot create config")
-        // .wrap_config_checks()
-        // .expect("wrap_config_checks error")
         .unwrap_or_else(|e| panic!("failed to ConfigBuilder::new(), reason: {e:#?}"))
     ).unwrap_or_else(|e| panic!("failed to Config try_from ConfigBuilder, reason: {e}"))
 });
