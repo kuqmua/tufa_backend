e2841b68f1412da87d45795ef37e032e37f32991
kuqmua <serezhashmakov@outlook.com>
2023-05-19 06:30:35 +0300

    lil tweaks

--- src/server_wrapper.rs
+++ src/server_wrapper.rs
@@ -26,7 +26,11 @@ pub async fn server_wrapper<'a>(
         + std::marker::Sync
     )
 ) -> Result<(), Box<tufa_common::repositories_types::tufa_server::server_wrapper::ServerWrapperErrorNamed<'a>>> {
-    let actix_web_dev_server = match crate::try_build_actix_web_dev_server::try_build_actix_web_dev_server(config).await {
+    let postgres_connection_pool = config.get_postgres_connection_pool();
+    let actix_web_dev_server = match crate::try_build_actix_web_dev_server::try_build_actix_web_dev_server(
+        config,
+        postgres_connection_pool
+    ).await {
         Err(e) => return Err(Box::new(tufa_common::repositories_types::tufa_server::server_wrapper::ServerWrapperErrorNamed::ApplicationBuild {
             application_build: *e,
             code_occurence: tufa_common::code_occurence!(),
@@ -45,7 +49,10 @@ pub async fn server_wrapper<'a>(
             Ok(_) => Ok(()),
         }
     });
-    // let worker_task = tokio::spawn(crate::issue_delivery_worker::run_worker_until_stopped(config));
+    // let worker_task = tokio::spawn(crate::issue_delivery_worker::run_worker_until_stopped(
+    //    config,
+    //    postgres_connection_pool//todo - maybe need to share PgPool between application_task and worker task
+    // ));
     tokio::select! {
         task = application_task => match task {
             Ok(Ok(())) => (),
--- src/try_build_actix_web_dev_server.rs
+++ src/try_build_actix_web_dev_server.rs
@@ -6,15 +6,15 @@ pub async fn try_build_actix_web_dev_server<'a>(
         + tufa_common::traits::get_email_client::GetEmailClient
         + tufa_common::traits::get_postgres_connect_options_with_db::GetPostgresConnectOptionsWithDb
         + tufa_common::traits::get_redis_url::GetRedisUrl
-        + tufa_common::traits::get_postgres_connection_pool::GetPostgresConnectionPool
         + tufa_common::traits::get_server_address::GetServerAddress
         + tufa_common::traits::try_create_tcp_listener::TryCreateTcpListener<'a>
         + std::marker::Send 
         + std::marker::Sync
-    )
-) -> Result<actix_web::dev::Server, Box<tufa_common::repositories_types::tufa_server::try_build_actix_web_dev_server::TryBuildActixWebDevServer<'a>>> {//todo - rename this error
-    let listener = match config.try_create_tcp_listener() {
-        Ok(listener) => listener,
+    ),
+    postgres_connection_pool: sqlx::PgPool
+) -> Result<actix_web::dev::Server, Box<tufa_common::repositories_types::tufa_server::try_build_actix_web_dev_server::TryBuildActixWebDevServer<'a>>> {
+    let tcp_listener = match config.try_create_tcp_listener() {
+        Ok(tcp_listener) => tcp_listener,
         Err(e) => {
             return Err(Box::new(tufa_common::repositories_types::tufa_server::try_build_actix_web_dev_server::TryBuildActixWebDevServer::TcpListenerBind {
                 tcp_listener_bind: *e,
@@ -22,15 +22,7 @@ pub async fn try_build_actix_web_dev_server<'a>(
             }))
         },
     };
-    let data_db_pool = actix_web::web::Data::new(config.get_postgres_connection_pool());
-    let email_client = actix_web::web::Data::new(config.get_email_client());
-    let secret_key = actix_web::cookie::Key::from({
-        use secrecy::ExposeSecret;
-        config.get_hmac_secret().expose_secret()
-    }.as_bytes());
-    let message_store = actix_web_flash_messages::storage::CookieMessageStore::builder(secret_key.clone()).build();
-    let message_framework = actix_web_flash_messages::FlashMessagesFramework::builder(message_store).build();
-    let redis_store = match actix_session::storage::RedisSessionStore::new({
+    let redis_session_store = match actix_session::storage::RedisSessionStore::new({
         use secrecy::ExposeSecret;
         config.get_redis_url().expose_secret()
     }).await {
@@ -43,11 +35,19 @@ pub async fn try_build_actix_web_dev_server<'a>(
         }
     };
     let server = match actix_web::HttpServer::new(move || {
+        let secret_key = actix_web::cookie::Key::from({
+            use secrecy::ExposeSecret;
+            config.get_hmac_secret().expose_secret()
+        }.as_bytes());
         actix_web::App::new()
-            .wrap(message_framework.clone())
+            .wrap(actix_web_flash_messages::FlashMessagesFramework::builder(
+                actix_web_flash_messages::storage::CookieMessageStore::builder(secret_key.clone()).build()
+                )
+                .build()
+            )
             .wrap(actix_session::SessionMiddleware::new(
-                redis_store.clone(),
-                secret_key.clone(),
+                redis_session_store.clone(),
+                secret_key,
             ))
             .wrap(tracing_actix_web::TracingLogger::default())
             .wrap(
@@ -93,12 +93,12 @@ pub async fn try_build_actix_web_dev_server<'a>(
                 "/get_providers_posts",
                 actix_web::web::post().to(tufa_common::repositories_types::tufa_server::routes::get_providers_posts_route::get_providers_posts_route),
             )
-            .app_data(data_db_pool.clone())
-            .app_data(email_client.clone())
+            .app_data(actix_web::web::Data::new(postgres_connection_pool.clone()))
+            .app_data(actix_web::web::Data::new(config.get_email_client()))
             .app_data( actix_web::web::Data::new("localhost"))
-            .app_data(actix_web::web::Data::new(config.get_hmac_secret().clone()))
+            .app_data(actix_web::web::Data::new(config.get_hmac_secret()))
     })
-    .listen(listener)
+    .listen(tcp_listener)
     {
         Ok(server) => server,
         Err(e) => {
