3c24ae658fb9d6ce03c35c027b5fe7b7bdd39fab
kuqmua <serezhashmakov@outlook.com>
2023-05-21 09:47:26 +0300

    config error handling

--- src/global_variables/runtime/config.rs
+++ src/global_variables/runtime/config.rs
@@ -1,5 +1,5 @@
 pub static CONFIG:  once_cell::sync::Lazy<tufa_common::repositories_types::tufa_server::config::config_struct::Config> =  once_cell::sync::Lazy::new(|| {
-    tufa_common::repositories_types::tufa_server::config::config_struct::Config::try_from(
+    tufa_common::repositories_types::tufa_server::config::config_struct::Config::try_from_config_unchecked(
         tufa_common::repositories_types::tufa_server::config::config_struct::ConfigUnchecked::new()
         .unwrap_or_else(|e| panic!("failed to ConfigUnchecked::new(), reason: {e:#?}"))
     ).unwrap_or_else(|e| panic!("failed to Config try_from ConfigUnchecked, reason: {e}"))
