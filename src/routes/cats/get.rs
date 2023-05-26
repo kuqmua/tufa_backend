30b7d4bc33cb6903354b2e183affd6f599168e58
kuqmua <serezhashmakov@outlook.com>
2023-05-26 12:47:06 +0300

    next steps

--- src/routes/cats/get.rs
+++ src/routes/cats/get.rs
@@ -3,13 +3,13 @@ pub async fn get(
     config: actix_web::web::Data<&tufa_common::repositories_types::tufa_server::config::config_struct::Config>,
 ) -> actix_web::HttpResponse {//or impl actix_web::Responder
     println!("cats");
-    println!("{}", {
-        use tufa_common::common::config::config_fields::GetMongoUrl;
-        use secrecy::ExposeSecret;
-        config.get_mongo_url().expose_secret()
-    });
-    //step1
-    let vec_cats_result = match sqlx::query_as!(
+    // println!("{}", {
+    //     use tufa_common::common::config::config_fields::GetMongoUrl;
+    //     use secrecy::ExposeSecret;
+    //     config.get_mongo_url().expose_secret()
+    // });
+//     //step1
+    match sqlx::query_as!(
         tufa_common::repositories_types::tufa_server::routes::cats::Cat,
         "SELECT * FROM cats WHERE name = $1",
         "black"
@@ -17,46 +17,53 @@ pub async fn get(
    .fetch_all(&**pool)
    .await {
         Ok(vec_cats) => {
-            println!("{vec_cats:#?}");
-            tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Ok(vec_cats)
+            tracing::info!("selected casts:\n{vec_cats:#?}");
+            actix_web::HttpResponse::Ok()
+            .json(actix_web::web::Json(
+                tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Ok(vec_cats)
+            ))
         },
         Err(e) => {
-            eprintln!("Unable to query cats table, error: {e:#?}");
-            tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Err(std::string::String::from("Unable to query cats table, error: {e:#?}"))
-        },
-    };
-    //step2
-    match sqlx::query_as!(
-        tufa_common::repositories_types::tufa_server::routes::cats::Cat,
-        "INSERT INTO cats(name) VALUES ($1) RETURNING *",
-        "white"
-    )
-    .fetch_all(&**pool)
-    .await {
-        Ok(vec_cats) => {
-            println!("{vec_cats:#?}");
-        },
-        Err(e) => {
-            eprintln!("Unable to insert a cat, error: {e:#?}");
+            tracing::error!("Unable to query cats table, error: {e:?}");
+            actix_web::HttpResponse::InternalServerError()
+            .json(
+                actix_web::web::Json(
+                    tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Err(
+                        std::string::String::from("Unable to query cats table, error: {e:#?}")
+                    )
+                )
+            )
         },
     }
-    //step3
-    match sqlx::query_as!(
-        tufa_common::repositories_types::tufa_server::routes::cats::Cat,
-        "UPDATE cats SET name = $1 WHERE id = $2 returning *",
-        "black",
-        1i64
-    )
-    .fetch_all(&**pool)
-    .await {
-        Ok(vec_cats) => {
-                println!("{vec_cats:#?}");
-            },
-        Err(e) => {
-            eprintln!("Unable to update a cat, error: {e:#?}");
-        },
-    }
-    actix_web::HttpResponse::Ok()
-    .json(actix_web::web::Json(vec_cats_result))
-    // .finish()
+//     //step2
+//     match sqlx::query_as!(
+//         tufa_common::repositories_types::tufa_server::routes::cats::Cat,
+//         "INSERT INTO cats(name) VALUES ($1) RETURNING *",
+//         "white"
+//     )
+//     .fetch_all(&**pool)
+//     .await {
+//         Ok(vec_cats) => {
+//             println!("{vec_cats:#?}");
+//         },
+//         Err(e) => {
+//             eprintln!("Unable to insert a cat, error: {e:#?}");
+//         },
+//     }
+//     //step3
+//     match sqlx::query_as!(
+//         tufa_common::repositories_types::tufa_server::routes::cats::Cat,
+//         "UPDATE cats SET name = $1 WHERE id = $2 returning *",
+//         "black",
+//         1i64
+//     )
+//     .fetch_all(&**pool)
+//     .await {
+//         Ok(vec_cats) => {
+//                 println!("{vec_cats:#?}");
+//             },
+//         Err(e) => {
+//             eprintln!("Unable to update a cat, error: {e:#?}");
+//         },
+//     }
 }
