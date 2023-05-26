5aee2bda4d9f9621e9aff76394d12886f7020ee1
kuqmua <serezhashmakov@outlook.com>
2023-05-26 11:56:17 +0300

    lil tweaks

--- src/routes/cats/post.rs
+++ src/routes/cats/post.rs
@@ -1,5 +1,7 @@
-// curl -X POST http://127.0.0.1:8080/api/cats -H 'Content-Type: application/json' -d '{"id":1,"name":"black"}'
-pub async fn post(cat: actix_web::web::Json<tufa_common::repositories_types::tufa_server::routes::cats::get::Cats>) -> impl actix_web::Responder {
+// curl -X POST http://127.0.0.1:8080/api/cats -H 'Content-Type: application/json' -d '[{"id":1,"name":"black"}]'
+pub async fn post(
+    cat: actix_web::web::Json<Vec<tufa_common::repositories_types::tufa_server::routes::cats::get::Cats>>
+) -> impl actix_web::Responder {
     println!("posted cat\n{:#?}", cat);
     actix_web::HttpResponse::Ok().finish()
 }
