a9b108365d5179fc69b61f9b71d6e75fd9652b51
kuqmua <serezhashmakov@outlook.com>
2023-05-26 12:05:26 +0300

    lil tweaks

--- src/routes/cats/get.rs
+++ src/routes/cats/get.rs
@@ -10,7 +10,7 @@ pub async fn get(
     });
     //step1
     let vec_cats_result = match sqlx::query_as!(
-        tufa_common::repositories_types::tufa_server::routes::cats::get::Cats,
+        tufa_common::repositories_types::tufa_server::routes::cats::Cat,
         "SELECT * FROM cats WHERE name = $1",
         "black"
     )
@@ -18,16 +18,16 @@ pub async fn get(
    .await {
         Ok(vec_cats) => {
             println!("{vec_cats:#?}");
-            tufa_common::repositories_types::tufa_server::routes::cats::get::CatsResult::Ok(vec_cats)
+            tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Ok(vec_cats)
         },
         Err(e) => {
             eprintln!("Unable to query cats table, error: {e:#?}");
-            tufa_common::repositories_types::tufa_server::routes::cats::get::CatsResult::Err(std::string::String::from("Unable to query cats table, error: {e:#?}"))
+            tufa_common::repositories_types::tufa_server::routes::cats::get::GetResponse::Err(std::string::String::from("Unable to query cats table, error: {e:#?}"))
         },
     };
     //step2
     match sqlx::query_as!(
-        tufa_common::repositories_types::tufa_server::routes::cats::get::Cats,
+        tufa_common::repositories_types::tufa_server::routes::cats::Cat,
         "INSERT INTO cats(name) VALUES ($1) RETURNING *",
         "white"
     )
@@ -42,7 +42,7 @@ pub async fn get(
     }
     //step3
     match sqlx::query_as!(
-        tufa_common::repositories_types::tufa_server::routes::cats::get::Cats,
+        tufa_common::repositories_types::tufa_server::routes::cats::Cat,
         "UPDATE cats SET name = $1 WHERE id = $2 returning *",
         "black",
         1i64
