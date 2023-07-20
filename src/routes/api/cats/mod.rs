6b0e7b2d8e781f782dfe9837f30494a70fc8b5a4
kuqmua <serezhashmakov@outlook.com>
2023-07-20 18:49:34 +0300

    fix

--- src/routes/api/cats/mod.rs
+++ src/routes/api/cats/mod.rs
@@ -92,23 +92,22 @@ pub async fn get_by_id<'a>(
     >,
 ) -> actix_web::HttpResponse {
     println!("get_by_id path_parameters id {}", path_parameters.id);
-    let bigserial_id = tufa_common::server::postgres::bigserial::Bigserial::try_from_i64(path_parameters.id).unwrap();
-    // let bigserial_id = match tufa_common::server::postgres::bigserial::Bigserial::try_from_i64(
-    //     path_parameters.id,
-    // ) {
-    //     Ok(bigserial_id) => bigserial_id,
-    //     Err(e) => {
-    //         let error = tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::route::GetByIdErrorNamed::Bigserial { 
-    //             bigserial: e, 
-    //             code_occurence: tufa_common::code_occurence!()
-    //         };
-    //         tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
-    //             &error,
-    //             &app_info.config,
-    //         );
-    //         return tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::route::GetByIdHttpResponse::from(error).into();
-    //     }
-    // };
+    let bigserial_id = match tufa_common::server::postgres::bigserial::Bigserial::try_from_i64(
+        path_parameters.id,
+    ) {
+        Ok(bigserial_id) => bigserial_id,
+        Err(e) => {
+            let error = tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetById::Bigserial { 
+                bigserial: e, 
+                code_occurence: tufa_common::code_occurence!()
+            };
+            tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
+                &error, 
+                &app_info.config
+            );
+            return tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetByIdResponseVariants::from(error).into();
+        }
+    };
     match sqlx::query_as!(
         tufa_common::repositories_types::tufa_server::routes::api::cats::Cat,
         "SELECT * FROM cats WHERE id = $1",
