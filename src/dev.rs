fffca4b96c8944103b87e05b0e6f4f7ccf1b97a5
kuqmua <serezhashmakov@outlook.com>
2023-07-21 01:31:35 +0300

    axum project commit checker middleware

--- src/dev.rs
+++ src/dev.rs
@@ -35,7 +35,7 @@
 //     OneTwo {
 //         #[source]
 //         one: OneError,
-//         // #[source] //error: duplicate #[source] attribute - и что с этим делать?
+//         // #[source] //error: duplicate #[source] attribute - what to do with that?
 //         two: TwoError,
 //     }
 // }
@@ -57,7 +57,7 @@
 //             two,
 //         }),
 //         (Err(one), Err(two)) => Err(ThreeError::OneTwo {
-//             //что с этим делать?
+//             //what to do with that?
 //             one,
 //             two,
 //         }),
@@ -71,206 +71,26 @@
 // // }
 
 pub async fn dev() {
-    // // run it with hyper on localhost:3000
-    // axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-    //     .serve(app.into_make_service())
-    //     .await
-    //     .unwrap();
-
-    let app = Router::new()
-        .route("/", get(handler))
-        .route_layer(middleware::from_fn(auth))
-        .route(
-            "/kekw",
-            axum::routing::get(|| async {
-                println!("handler2");
-                "Hello, World!"
-            }),
-        );
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(
+            axum::Router::new()
+                .route("/", axum::routing::get(handler))
+                .route_layer(axum::middleware::from_fn(
+                    tufa_common::server::middleware::project_commit_checker::project_commit_checker,
+                ))
+                .route(
+                    "/kekw",
+                    axum::routing::get(|| async {
+                        println!("handler2");
+                        "Hello, World!"
+                    }),
+                )
+                .into_make_service(),
+        )
         .await
         .unwrap();
 }
 
-use axum::{
-    extract::Extension,
-    http::{Request, StatusCode},
-    middleware::{self, Next},
-    response::{IntoResponse, Response},
-    routing::get,
-    Router,
-};
-
-// #[derive(Clone)]
-// struct CurrentUser {/* ... */}
-
-async fn auth<B>(
-    mut req: Request<B>,
-    next: Next<B>,
-) -> Result<
-    Response,
-    // StatusCode
-    axum::response::Response,
-> {
-    println!("authmiddleware");
-    let auth_header = req
-        .headers()
-        .get(tufa_common::common::git::project_git_info::PROJECT_COMMIT)
-        .and_then(|header| header.to_str().ok());
-    println!("{auth_header:#?}");
-    let auth_header = if let Some(auth_header) = auth_header {
-        println!("1");
-
-        auth_header
-    } else {
-        println!("2");
-        let error_with_serialize_deserialize = tufa_common::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamed::ProjectCommitExtractorNotEqual {
-                    project_commit_not_equal: "different project commit provided, services must work only with equal project commits",
-                    project_commit_to_use: tufa_common::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(&tufa_common::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO),
-                    code_occurence: tufa_common::code_occurence!(),
-                }.into_serialize_deserialize_version();
-        // serde_json::to_string(&error_with_serialize_deserialize).unwrap_or_else(|_|{
-        //     crate::common::to_default_stringified_json::ToDefaultStringifiedJson::to_default_stringified_json(&error_with_serialize_deserialize)
-        // });
-        println!("2");
-        // Err(Request::from_parts(parts, Body::from(bytes)))
-        // Err(axum::http::StatusCode::BAD_REQUEST)
-        return Err((
-            axum::http::StatusCode::UNAUTHORIZED,
-            axum::Json(error_with_serialize_deserialize),
-        )
-            .into_response());
-        // return Err(StatusCode::UNAUTHORIZED);
-    };
-    Ok(next.run(req).await)
-}
-
-async fn handler(// extract the current user, set by the middleware
-    // Extension(current_user): Extension<CurrentUser>,
-) {
+async fn handler() {
     println!("handler1");
-    // ...
 }
-
-// async fn auth<B>(
-//     mut req: http::Request<B>,
-//     next: axum::middleware::Next<B>,
-// ) -> Result<
-//     axum::response::Response,
-//     axum::http::StatusCode,
-//     // axum::response::Response,
-//     // (
-//     //     axum::http::StatusCode,
-//     //     axum::Json<tufa_common::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamedWithSerializeDeserialize>,
-//     // ),
-// > {
-//     use axum::response::IntoResponse;
-//     println!("authmiddleware");
-//     let auth_header = req
-//         .headers()
-//         .get(tufa_common::common::git::project_git_info::PROJECT_COMMIT)
-//         .and_then(|header| header.to_str().ok());
-//     println!("{auth_header:#?}");
-//     if let Some(auth_header) = auth_header {
-//         match auth_header
-//             == tufa_common::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
-//                 .project_commit
-//         {
-//             true => Ok(next.run(req).await),
-//             false => {
-//                 let error_with_serialize_deserialize = tufa_common::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamed::ProjectCommitExtractorNotEqual {
-//                     project_commit_not_equal: "different project commit provided, services must work only with equal project commits",
-//                     project_commit_to_use: tufa_common::common::git::get_git_commit_link::GetGitCommitLink::get_git_commit_link(&tufa_common::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO),
-//                     code_occurence: tufa_common::code_occurence!(),
-//                 }.into_serialize_deserialize_version();
-//                 // serde_json::to_string(&error_with_serialize_deserialize).unwrap_or_else(|_|{
-//                 //     crate::common::to_default_stringified_json::ToDefaultStringifiedJson::to_default_stringified_json(&error_with_serialize_deserialize)
-//                 // });
-//                 println!("2");
-//                 // Err(Request::from_parts(parts, Body::from(bytes)))
-//                 Err(axum::http::StatusCode::BAD_REQUEST)
-//                 // Err((
-//                 //     axum::http::StatusCode::UNAUTHORIZED,
-//                 //     axum::Json(error_with_serialize_deserialize),
-//                 // )
-//                 //     .into_response())
-//             }
-//         }
-//     } else {
-//         println!("3");
-//         let error_with_serialize_deserialize = tufa_common::server::extractors::project_commit_extractor::ProjectCommitExtractorCheckErrorNamed::NoProjectCommitExtractorHeader {
-//             no_project_commit_header: "project_commit header is not provided",
-//             code_occurence: tufa_common::code_occurence!(),
-//         }.into_serialize_deserialize_version();
-//         Err(axum::http::StatusCode::BAD_REQUEST)
-//         // Err((
-//         //     axum::http::StatusCode::BAD_REQUEST,
-//         //     axum::Json(error_with_serialize_deserialize),
-//         // )
-//         //     .into_response())
-
-//         // (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-//         // Err(Request::from_parts(parts, Body::from(bytes)))
-//     }
-// }
-
-// impl actix_web::FromRequest for ProjectCommitExtractor {
-//     type Error = actix_web::Error;
-//     type Future = std::future::Ready<Result<Self, Self::Error>>;
-//     fn from_request(
-//         req: &actix_web::HttpRequest,
-//         _payload: &mut actix_web::dev::Payload,
-//     ) -> Self::Future {
-//         match req
-//             .headers()
-//             .get(crate::common::git::project_git_info::PROJECT_COMMIT)
-//         {
-//             Some(project_commit_header_value) => {
-//                 match project_commit_header_value.to_str() {
-//                     Ok(possible_project_commit) => {
-//                         match possible_project_commit
-//                         == crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO
-//                             .project_commit
-//                     {
-//                         true => std::future::ready(Ok(ProjectCommitExtractor {})),
-//                         false => {
-//                             std::future::ready(Err({
-//                                 let error_with_serialize_deserialize = ProjectCommitExtractorCheckErrorNamed::ProjectCommitExtractorNotEqual {
-//                                     project_commit_not_equal: "different project commit provided, services must work only with equal project commits",
-//                                     project_commit_to_use: {
-//                                         use crate::common::git::get_git_commit_link::GetGitCommitLink;
-//                                         crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO.get_git_commit_link()
-//                                     },
-//                                     code_occurence: crate::code_occurence_tufa_common!(),
-//                                 }.into_serialize_deserialize_version();
-//                                 actix_web::error::ErrorBadRequest(actix_web::web::Json(serde_json::to_string(&error_with_serialize_deserialize).unwrap_or_else(|_|{
-//                                     crate::common::to_default_stringified_json::ToDefaultStringifiedJson::to_default_stringified_json(&error_with_serialize_deserialize)
-//                                 })))
-//                             }))
-//                         }
-//                     }
-//                     }
-//                     Err(e) => std::future::ready(Err({
-//                         let error_with_serialize_deserialize = ProjectCommitExtractorCheckErrorNamed::ProjectCommitExtractorToStrConversion {
-//                             project_commit_to_str_conversion: e,
-//                             code_occurence: crate::code_occurence_tufa_common!()
-//                         }.into_serialize_deserialize_version();
-//                         actix_web::error::ErrorBadRequest(actix_web::web::Json(serde_json::to_string(&error_with_serialize_deserialize).unwrap_or_else(|_|{
-//                             crate::common::to_default_stringified_json::ToDefaultStringifiedJson::to_default_stringified_json(&error_with_serialize_deserialize)
-//                         })))
-//                     })),
-//                 }
-//             }
-//             None => std::future::ready(Err({
-//                 let error_with_serialize_deserialize = ProjectCommitExtractorCheckErrorNamed::NoProjectCommitExtractorHeader {
-//                     no_project_commit_header: "project_commit header is not provided",
-//                     code_occurence: crate::code_occurence_tufa_common!()
-//                 }.into_serialize_deserialize_version();
-//                 actix_web::error::ErrorBadRequest(actix_web::web::Json(serde_json::to_string(&error_with_serialize_deserialize).unwrap_or_else(|_|{
-//                     crate::common::to_default_stringified_json::ToDefaultStringifiedJson::to_default_stringified_json(&error_with_serialize_deserialize)
-//                 })))
-//             })),
-//         }
-//     }
-// }
