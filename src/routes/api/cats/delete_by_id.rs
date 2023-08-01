abb3026ed9200a4d63a54eb4550d1d6dddb50475
kuqmua <serezhashmakov@outlook.com>
2023-08-01 19:26:48 +0300

    path_extractor_result

--- src/routes/api/cats/delete_by_id.rs
+++ src/routes/api/cats/delete_by_id.rs
@@ -1,16 +1,29 @@
 pub(crate) async fn delete_by_id_axum<'a>(
-    //todo - rewrite as extract path result
-    axum::extract::Path(path_parameters): axum::extract::Path<tufa_common::repositories_types::tufa_server::routes::api::cats::DeleteByIdPathParameters>,
+    path_parameters_extraction_result: Result<
+        axum::extract::Path<tufa_common::repositories_types::tufa_server::routes::api::cats::DeleteByIdPathParameters>,
+        axum::extract::rejection::PathRejection,
+    >,
     axum::extract::State(app_info): axum::extract::State<tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync>,
 ) -> impl axum::response::IntoResponse {
+    let axum::extract::Path(path_parameters) = match path_parameters_extraction_result {
+        Ok(path_parameters) => path_parameters,
+        Err(err) => {
+            let error = tufa_common::server::routes::helpers::path_extractor_error::PathExtractorErrorNamed::from(err);
+            tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
+                &error,
+                &app_info.get_config(),
+            );
+            return tufa_common::repositories_types::tufa_server::routes::api::cats::delete_by_id::TryDeleteByIdResponseVariants::from(error);
+        }
+    };
     println!("delete_by_id {}", path_parameters.id);
     let bigserial_id = match tufa_common::server::postgres::bigserial::Bigserial::try_from_i64(
         path_parameters.id,
     ) {
         Ok(bigserial_id) => bigserial_id,
         Err(e) => {
-            let error = tufa_common::repositories_types::tufa_server::routes::api::cats::delete_by_id::TryDeleteById::Bigserial { 
-                bigserial: e, 
+            let error = tufa_common::repositories_types::tufa_server::routes::api::cats::delete_by_id::TryDeleteById::Bigserial {
+                bigserial: e,
                 code_occurence: tufa_common::code_occurence!()
             };
             tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
@@ -32,7 +45,7 @@ pub(crate) async fn delete_by_id_axum<'a>(
         Err(e) => {
             let error = tufa_common::repositories_types::tufa_server::routes::api::cats::delete_by_id::TryDeleteById::from(e);
             tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
-                &error, 
+                &error,
                 &app_info.get_config()
             );
             tufa_common::repositories_types::tufa_server::routes::api::cats::delete_by_id::TryDeleteByIdResponseVariants::from(error)
