ed8aa429ac00492a347d1c2a7b68e44710a16ba6
kuqmua <serezhashmakov@outlook.com>
2023-08-13 15:50:29 +0300

    next steps

--- src/routes/api/cats/get.rs
+++ src/routes/api/cats/get.rs
@@ -19,76 +19,16 @@ pub(crate) async fn get(
             return err;
         },
     };
-    println!(
-        "get query_parameters limit {:?}, id {:?} name {:?} color {:?} select {:?}",
-        query_parameters.limit,
-        query_parameters.id,
-        query_parameters.name,
-        query_parameters.color,
-        query_parameters.select
-    );
-    let mut additional_parameters = std::string::String::from("");
-    let mut increment: u64 = 0;
-    if let Some(value) = &query_parameters.id {
-        let prefix = match additional_parameters.is_empty() {
-            true => format!("{}", tufa_common::server::postgres::constants::WHERE_NAME),
-            false => format!(" {}", tufa_common::server::postgres::constants::AND_NAME),
-        };
-        additional_parameters.push_str(&format!(
-            "{prefix} id = {}({}[{}])", 
-            tufa_common::server::postgres::constants::ANY_NAME,
-            tufa_common::server::postgres::constants::ARRAY_NAME,
-            tufa_common::server::postgres::generate_bind_increments::GenerateBindIncrements::generate_bind_increments(value, &mut increment)
-        ));
-    }
-    if let Some(value) = &query_parameters.name {
-        let prefix = match additional_parameters.is_empty() {
-            true => format!("{}", tufa_common::server::postgres::constants::WHERE_NAME),
-            false => format!(" {}", tufa_common::server::postgres::constants::AND_NAME),
-        };
-        additional_parameters.push_str(&format!(
-            "{prefix} name = {}({}[{}])",
-            tufa_common::server::postgres::constants::ANY_NAME,
-            tufa_common::server::postgres::constants::ARRAY_NAME,
-            tufa_common::server::postgres::generate_bind_increments::GenerateBindIncrements::generate_bind_increments(value, &mut increment)
-        ));
-    }
-    if let Some(value) = &query_parameters.color {
-        let prefix = match additional_parameters.is_empty() {
-            true => format!("{}", tufa_common::server::postgres::constants::WHERE_NAME),
-            false => format!(" {}", tufa_common::server::postgres::constants::AND_NAME),
-        };
-        additional_parameters.push_str(&format!(
-            "{prefix} color = {}({}[{}])",
-            tufa_common::server::postgres::constants::ANY_NAME,
-            tufa_common::server::postgres::constants::ARRAY_NAME,
-            tufa_common::server::postgres::generate_bind_increments::GenerateBindIncrements::generate_bind_increments(value, &mut increment)
-        ));
-    }
-    {
-        increment += 1;
-        let limit_prefix = match additional_parameters.is_empty() {
-            true => format!("{}", tufa_common::server::postgres::constants::LIMIT_NAME),
-            false => format!(" {}", tufa_common::server::postgres::constants::LIMIT_NAME),
-        };
-        additional_parameters.push_str(&format!("{limit_prefix} ${increment}"));
-    }
-    let select = tufa_common::repositories_types::tufa_server::routes::api::cats::GetSelect::from(
+    tufa_common::repositories_types::tufa_server::routes::api::cats::GetSelect::from(
         query_parameters.select.clone(),
-    );
-    let query_string = format!(
-        "{} {select} {} {} {additional_parameters}",
-        tufa_common::server::postgres::constants::SELECT_NAME,
-        tufa_common::server::postgres::constants::FROM_NAME,
-        tufa_common::repositories_types::tufa_server::routes::api::cats::CATS
-    );
-    println!("{query_string}");
-    select
-        .execute_query(
-            query_string,
-            &*app_info_state.get_postgres_pool(),
-            query_parameters,
-            &app_info_state,
-        )
-        .await
+    )
+    .execute_query(
+        tufa_common::server::postgres::generate_get_query::GenerateGetQuery::generate_get_query(
+            &query_parameters,
+        ),
+        &*app_info_state.get_postgres_pool(),
+        query_parameters,
+        &app_info_state,
+    )
+    .await
 }
