a2805a13fe1a64a584fd8727027dd693debeabad
kuqmua <serezhashmakov@outlook.com>
2023-08-16 12:34:28 +0300

    patch_by_id logic

--- src/routes/api/cats/patch_by_id.rs
+++ src/routes/api/cats/patch_by_id.rs
@@ -1,10 +1,29 @@
 pub(crate) async fn patch_by_id<'a>(
+    path_parameters_extraction_result: Result<
+        axum::extract::Path<
+            tufa_common::repositories_types::tufa_server::routes::api::cats::PatchByIdPathParameters,
+        >,
+        axum::extract::rejection::PathRejection,
+    >,
     app_info_state: axum::extract::State<tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync>,
     payload_extraction_result: Result<
         axum::Json<tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch>,
         axum::extract::rejection::JsonRejection,
     >,
 ) -> impl axum::response::IntoResponse {
+    let path_parameters = match tufa_common::server::routes::helpers::path_extractor_error::PathValueResultExtractor::<
+        tufa_common::repositories_types::tufa_server::routes::api::cats::PatchByIdPathParameters,
+        tufa_common::repositories_types::tufa_server::routes::api::cats::patch_by_id::TryPatchByIdResponseVariants
+    >::try_extract_value(
+        path_parameters_extraction_result,
+        &app_info_state
+    ) {
+        Ok(path_parameters) => path_parameters,
+        Err(err) => {
+            return err;
+        },
+    };
+    println!("patch_by_id path_parameters {path_parameters:#?}");
     let payload = match tufa_common::server::routes::helpers::json_extractor_error::JsonValueResultExtractor::<
         tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch,
         tufa_common::repositories_types::tufa_server::routes::api::cats::patch_by_id::TryPatchByIdResponseVariants
@@ -17,35 +36,39 @@ pub(crate) async fn patch_by_id<'a>(
             return err;
         },
     };
-    println!("patch {payload:#?}");
-    let bigserial_id = *tufa_common::server::postgres::bigserial::GetPostgresBigserialId::get_postgres_bigserial_id(&payload).to_inner();
+    println!("patch payload {payload:#?}");
     let query_result = match payload {
-        tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch::IdName {
-            id: _id,
-            name,
-        } => {
+        tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch::Name { name } => {
             sqlx::query_as!(
                 tufa_common::repositories_types::tufa_server::routes::api::cats::Cat,
                 "UPDATE cats SET name = $1 WHERE id = $2",
                 name,
-                bigserial_id
+                path_parameters.id.to_inner()
             )
             .fetch_all(&*app_info_state.get_postgres_pool())
             .await
-        }
-        tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch::IdColor {
-            id: _id,
-            color,
-        } => {
+        },
+        tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch::Color { color } => {
             sqlx::query_as!(
                 tufa_common::repositories_types::tufa_server::routes::api::cats::Cat,
                 "UPDATE cats SET color = $1 WHERE id = $2",
                 color,
-                bigserial_id
+                path_parameters.id.to_inner()
             )
             .fetch_all(&*app_info_state.get_postgres_pool())
             .await
-        }
+        },
+        tufa_common::repositories_types::tufa_server::routes::api::cats::CatToPatch::NameColor { name, color } => {
+            sqlx::query_as!(
+                tufa_common::repositories_types::tufa_server::routes::api::cats::Cat,
+                "UPDATE cats SET name = $1, color = $2 WHERE id = $3",
+                name,
+                color,
+                path_parameters.id.to_inner()
+            )
+            .fetch_all(&*app_info_state.get_postgres_pool())
+            .await
+        },
     };
     match query_result {
         Ok(_) => tufa_common::repositories_types::tufa_server::routes::api::cats::patch_by_id::TryPatchByIdResponseVariants::Desirable(()),
