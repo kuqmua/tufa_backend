bae19e6d3e1693d1be4a737558000ff7ec2dd255
kuqmua <serezhashmakov@outlook.com>
2023-09-02 10:25:47 +0300

    renaming

--- src/routes/api/cats/mod.rs
+++ src/routes/api/cats/mod.rs
@@ -1,11 +1,11 @@
 mod delete;
 mod delete_by_id;
 mod get;
-mod get_by_id;
 mod patch_by_id;
 mod post;
 mod post_search;
 mod put_by_id;
+mod read_by_id;
 
 //todo how to handle sql injection ?
 //todo - maybe check max length for field here instead of put it in postgres and recieve error ? color VARCHAR (255) NOT NULL
@@ -33,10 +33,10 @@ fn crud(
             "/search",
             axum::routing::post(crate::routes::api::cats::post_search::post_search),
         )
-        .route(
-            "/delete_specific",
-            axum::routing::delete(crate::routes::api::cats::post_search::post_search),
-        )
+        // .route(
+        //     "/delete_specific",
+        //     axum::routing::delete(crate::routes::api::cats::post_search::post_search),
+        // )
         .route(
             "/",
             axum::routing::get(crate::routes::api::cats::get::get)
@@ -45,7 +45,7 @@ fn crud(
         )
         .route(
             "/id/:id",
-            axum::routing::get(crate::routes::api::cats::get_by_id::get_by_id)
+            axum::routing::get(crate::routes::api::cats::read_by_id::read_by_id)
                 .put(crate::routes::api::cats::put_by_id::put_by_id)
                 .patch(crate::routes::api::cats::patch_by_id::patch_by_id)
                 .delete(crate::routes::api::cats::delete_by_id::delete_by_id),
--- src/routes/api/cats/read_by_id.rs
+++ src/routes/api/cats/read_by_id.rs
@@ -1,21 +1,21 @@
-pub(crate) async fn get_by_id(
+pub(crate) async fn read_by_id(
     path_parameters_extraction_result: Result<
         axum::extract::Path<
-            tufa_common::repositories_types::tufa_server::routes::api::cats::GetByIdPathParameters,
+            tufa_common::repositories_types::tufa_server::routes::api::cats::ReadByIdPathParameters,
         >,
         axum::extract::rejection::PathRejection,
     >,
     query_parameters_extraction_result: Result<
         axum::extract::Query<
-            tufa_common::repositories_types::tufa_server::routes::api::cats::GetByIdQueryParameters,
+            tufa_common::repositories_types::tufa_server::routes::api::cats::ReadByIdQueryParameters,
         >,
         axum::extract::rejection::QueryRejection,
     >,
     app_info_state: axum::extract::State<tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync>,
 ) -> impl axum::response::IntoResponse {
     let path_parameters = match tufa_common::server::routes::helpers::path_extractor_error::PathValueResultExtractor::<
-        tufa_common::repositories_types::tufa_server::routes::api::cats::GetByIdPathParameters,
-        tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetByIdResponseVariants
+        tufa_common::repositories_types::tufa_server::routes::api::cats::ReadByIdPathParameters,
+        tufa_common::repositories_types::tufa_server::routes::api::cats::read_by_id::TryReadByIdResponseVariants
     >::try_extract_value(
         path_parameters_extraction_result,
         &app_info_state
@@ -25,10 +25,10 @@ pub(crate) async fn get_by_id(
             return err;
         },
     };
-    println!("get_by_id path_parameters {path_parameters:#?}");
+    println!("read_by_id path_parameters {path_parameters:#?}");
     let query_parameters = match tufa_common::server::routes::helpers::query_extractor_error::QueryValueResultExtractor::<
-        tufa_common::repositories_types::tufa_server::routes::api::cats::GetByIdQueryParameters,
-        tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetByIdResponseVariants
+        tufa_common::repositories_types::tufa_server::routes::api::cats::ReadByIdQueryParameters,
+        tufa_common::repositories_types::tufa_server::routes::api::cats::read_by_id::TryReadByIdResponseVariants
     >::try_extract_value(
         query_parameters_extraction_result,
         &app_info_state
@@ -38,7 +38,7 @@ pub(crate) async fn get_by_id(
             return err;
         },
     };
-    println!("get_by_id query_parameters {query_parameters:#?}");
+    println!("read_by_id query_parameters {query_parameters:#?}");
     match sqlx::query_as!(
         tufa_common::repositories_types::tufa_server::routes::api::cats::Cat,
         "SELECT * FROM cats WHERE id = $1",
@@ -47,14 +47,14 @@ pub(crate) async fn get_by_id(
     .fetch_one(&*app_info_state.get_postgres_pool())
     .await
     {
-        Ok(value) => tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetByIdResponseVariants::Desirable(value),
+        Ok(value) => tufa_common::repositories_types::tufa_server::routes::api::cats::read_by_id::TryReadByIdResponseVariants::Desirable(value),
         Err(e) => {
-            let error = tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetById::from(e);
+            let error = tufa_common::repositories_types::tufa_server::routes::api::cats::read_by_id::TryReadById::from(e);
             tufa_common::common::error_logs_logic::error_log::ErrorLog::error_log(
                 &error,
                 app_info_state.as_ref()
             );
-            tufa_common::repositories_types::tufa_server::routes::api::cats::get_by_id::TryGetByIdResponseVariants::from(error)
+            tufa_common::repositories_types::tufa_server::routes::api::cats::read_by_id::TryReadByIdResponseVariants::from(error)
         }
     }
 }
