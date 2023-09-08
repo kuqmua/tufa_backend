05a1d2abac3768cdd8268ac56bc71190fd8b5138
kuqmua <serezhashmakov@outlook.com>
2023-09-08 22:13:46 +0300

    next steps

--- src/routes/api/cats/create_or_update.rs
+++ src/routes/api/cats/create_or_update.rs
@@ -1,1 +1,44 @@
-
+// pub(crate) async fn create_or_update<'a>(
+//     path_parameters_extraction_result: Result<
+//         axum::extract::Path<
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::CreateOrUpdatePath,
+//         >,
+//         axum::extract::rejection::PathRejection,
+//     >,
+//     app_info_state: axum::extract::State<tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync>,
+//     payload_extraction_result: Result<
+//         axum::Json<
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::CreateOrUpdatePayload,
+//         >,
+//         axum::extract::rejection::JsonRejection,
+//     >,
+// ) -> impl axum::response::IntoResponse {
+//     let parameters = tufa_common::repositories_types::tufa_server::routes::api::cats::CreateOrUpdateParameters {
+//         path: match tufa_common::server::routes::helpers::path_extractor_error::PathValueResultExtractor::<
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::CreateOrUpdatePath,
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::create_or_update::TryCreateOrUpdateResponseVariants
+//         >::try_extract_value(
+//             path_parameters_extraction_result,
+//             &app_info_state
+//         ) {
+//             Ok(path_parameters) => path_parameters,
+//             Err(err) => {
+//                 return err;
+//             },
+//         },
+//         payload: match tufa_common::server::routes::helpers::json_extractor_error::JsonValueResultExtractor::<
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::CreateOrUpdatePayload,
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::create_or_update::TryCreateOrUpdateResponseVariants
+//         >::try_extract_value(
+//             payload_extraction_result,
+//             &app_info_state
+//         ) {
+//             Ok(payload) => payload,
+//             Err(err) => {
+//                 return err;
+//             },
+//         },
+//     };
+//     println!("{parameters:#?}");
+//     parameters.prepare_and_execute_query(&app_info_state).await
+// }
--- src/routes/api/cats/update.rs
+++ src/routes/api/cats/update.rs
@@ -1,1 +1,44 @@
-
+// pub(crate) async fn update<'a>(
+//     //todo how to check with type system what http request function params and route path query and payload params are same?
+//     path_parameters_extraction_result: Result<
+//         axum::extract::Path<
+//             tufa_common::repositories_types::tufa_server::routes::api::cats::UpdatePath,
+//         >,
+//         axum::extract::rejection::PathRejection,
+//     >,
+//     app_info_state: axum::extract::State<tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync>,
+//     payload_extraction_result: Result<
+//         axum::Json<tufa_common::repositories_types::tufa_server::routes::api::cats::UpdatePayload>,
+//         axum::extract::rejection::JsonRejection,
+//     >,
+// ) -> impl axum::response::IntoResponse {
+//     let parameters =
+//         tufa_common::repositories_types::tufa_server::routes::api::cats::UpdateParameters {
+//             path: match tufa_common::server::routes::helpers::path_extractor_error::PathValueResultExtractor::<
+//                 tufa_common::repositories_types::tufa_server::routes::api::cats::UpdatePath,
+//                 tufa_common::repositories_types::tufa_server::routes::api::cats::update::TryUpdateResponseVariants
+//             >::try_extract_value(
+//                 path_parameters_extraction_result,
+//                 &app_info_state
+//             ) {
+//                 Ok(path_parameters) => path_parameters,
+//                 Err(err) => {
+//                     return err;
+//                 },
+//             },
+//             payload: match tufa_common::server::routes::helpers::json_extractor_error::JsonValueResultExtractor::<
+//                 tufa_common::repositories_types::tufa_server::routes::api::cats::UpdatePayload,
+//                 tufa_common::repositories_types::tufa_server::routes::api::cats::update::TryUpdateResponseVariants
+//             >::try_extract_value(
+//                 payload_extraction_result,
+//                 &app_info_state
+//             ) {
+//                 Ok(payload) => payload,
+//                 Err(err) => {
+//                     return err;
+//                 },
+//             },
+//         };
+//     println!("{parameters:#?}");
+//     parameters.prepare_and_execute_query(&app_info_state).await
+// }
