3d2f8cd491f6faa0a4658c82aa68cd5adf1e5868
kuqmua <serezhashmakov@outlook.com>
2023-09-27 20:01:41 +0300

    try create move to proc_macro

--- src/routes/api/cats/create.rs
+++ src/routes/api/cats/create.rs
@@ -8,7 +8,7 @@ pub(crate) async fn create(
     let parameters = tufa_common::repositories_types::tufa_server::routes::api::cats::CreateParameters {
         payload: match tufa_common::server::routes::helpers::json_extractor_error::JsonValueResultExtractor::<
             tufa_common::repositories_types::tufa_server::routes::api::cats::CreatePayload,
-            tufa_common::repositories_types::tufa_server::routes::api::cats::create::TryCreateResponseVariants
+            tufa_common::repositories_types::tufa_server::routes::api::cats::TryCreateResponseVariants
         >::try_extract_value(
             payload_extraction_result,
             &app_info_state
