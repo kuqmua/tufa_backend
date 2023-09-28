f7f0875314be4467029bbf485eac2862b1f2efdd
kuqmua <serezhashmakov@outlook.com>
2023-09-28 14:57:18 +0300

    delete_token_stream http_request gen

--- src/routes/api/cats/delete.rs
+++ src/routes/api/cats/delete.rs
@@ -10,7 +10,7 @@ pub(crate) async fn delete<'a>(
     let parameters = tufa_common::repositories_types::tufa_server::routes::api::cats::DeleteParameters {
         query: match tufa_common::server::routes::helpers::query_extractor_error::QueryValueResultExtractor::<
             tufa_common::repositories_types::tufa_server::routes::api::cats::DeleteQuery,
-            tufa_common::repositories_types::tufa_server::routes::api::cats::delete::TryDeleteResponseVariants
+            tufa_common::repositories_types::tufa_server::routes::api::cats::TryDeleteResponseVariants
         >::try_extract_value(
             query_parameters_extraction_result,
             &app_info_state
