e5ca568f1bbf0729593f444f24583f3397e46ab9
kuqmua <serezhashmakov@outlook.com>
2023-10-31 18:21:45 +0300

    next steps

--- src/routes/api/cats/mod.rs
+++ src/routes/api/cats/mod.rs
@@ -57,8 +57,7 @@ fn crud(
                 tufa_common::repositories_types::tufa_server::routes::api::cats::read_one,
             ) 
             .patch(tufa_common::repositories_types::tufa_server::routes::api::cats::update_one)
-            ,
-               // .delete(tufa_common::repositories_types::tufa_server::routes::api::cats::delete_one),
+            .delete(tufa_common::repositories_types::tufa_server::routes::api::cats::delete_one),
         )
         .layer(tower_http::cors::CorsLayer::new().allow_methods(
             tufa_common::repositories_types::tufa_server::routes::api::cats::ALLOW_METHODS,
