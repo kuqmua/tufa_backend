d3c188743f23a3b2433399a3725b0a431165ed6d
kuqmua <serezhashmakov@outlook.com>
2023-12-31 15:38:57 +0300

    todo

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -3,8 +3,5 @@ mod cats;
 pub fn routes(
     app_info: tufa_common::repositories_types::tufa_server::routes::api::cats::DynArcGetConfigGetPostgresPoolSendSync,
 ) -> axum::Router {
-    axum::Router::new().nest(
-        "/api",
-        axum::Router::new().merge(crate::routes::api::cats::routes(app_info)),
-    )
+    axum::Router::new().merge(crate::routes::api::cats::routes(app_info))
 }
